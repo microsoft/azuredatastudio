b43c58de6689fe5ae9d9b767abee81039f51eace
Anthony Dresser <andresse@microsoft.com>
2018-01-30 10:55:00 -0800

    fixed more unrelated compile bugs

--- src/sql/workbench/api/node/extHostModalDialog.ts
+++ src/sql/workbench/api/node/extHostModalDialog.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-import { SqlMainContext, MainThreadModalDialogShape, ExtHostModalDialogsShape } from 'sql/workbench/api/node/sqlextHost.protocol';
+import { SqlMainContext, MainThreadModalDialogShape, ExtHostModalDialogsShape } from 'sql/workbench/api/node/sqlExtHost.protocol';
 import { IMainContext } from 'vs/workbench/api/node/extHost.protocol';
 import * as vscode from 'vscode';
 import * as data from 'data';
