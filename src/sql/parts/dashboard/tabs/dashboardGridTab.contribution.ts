ee96ff0e28151b3f384b1bd7dc6d101f8557c55f
Leila Lali <llali@microsoft.com>
2018-02-21 13:53:46 -0800

    addressed code review comments

--- src/sql/parts/dashboard/tabs/dashboardGridTab.contribution.ts
+++ src/sql/parts/dashboard/tabs/dashboardGridTab.contribution.ts
@@ -5,15 +5,15 @@
 import { IJSONSchema } from 'vs/base/common/jsonSchema';
 import * as nls from 'vs/nls';
 
-import { generateDashboardWidgetSchema } from 'sql/parts/dashboard/pages/dashboardPageContribution';
+import { generateDashboardGridLayoutSchema } from 'sql/parts/dashboard/pages/dashboardPageContribution';
 import { registerTabContent } from 'sql/platform/dashboard/common/dashboardRegistry';
 
 export const GRID_TABS = 'grid-tab';
 
 let gridContentsSchema: IJSONSchema = {
 	type: 'array',
-	description: nls.localize('dashboard.tab.content.widgets', "The list of widgets that will be displayed in this tab."),
-	items: generateDashboardWidgetSchema(undefined, true)
+	description: nls.localize('dashboard.gridtab.content.items', "The list of widgets or webviews that will be displayed in this tab."),
+	items: generateDashboardGridLayoutSchema(undefined, true)
 };
 
 registerTabContent(GRID_TABS, gridContentsSchema);
