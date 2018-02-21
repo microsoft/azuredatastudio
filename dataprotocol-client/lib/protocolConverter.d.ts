07948153b40e912e5dc4984c80829d152c46b081
AbbiePetcht <appetcht@microsoft.com>
2018-02-21 14:59:35 -0800

    removed extra files that added by accident

--- dataprotocol-client/lib/protocolConverter.d.ts
+++
@@ -1,7 +0,0 @@
-import * as data from 'data';
-import * as types from './types';
-export interface Ip2c {
-    asProviderMetadata(params: types.MetadataQueryResult): data.ProviderMetadata;
-    asServerCapabilities(result: types.CapabiltiesDiscoveryResult): data.DataProtocolServerCapabilities;
-}
-export declare const p2c: Ip2c;
