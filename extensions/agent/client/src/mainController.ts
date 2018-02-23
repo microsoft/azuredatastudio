ac976f2482889c3f736f15b90ab0eb8a99066f55
Karl Burtram <karlb@microsoft.com>
2018-02-23 15:29:16 -0800

    wip

--- extensions/agent/client/src/mainController.ts
+++ extensions/agent/client/src/mainController.ts
@@ -31,48 +31,8 @@ export class MainController  {
 
     public activate(): void {
 
-        this._apiWrapper.registerWebviewProvider('data-management-spark', webview => {
-            webview.html = '<div>1</div>';
-            // this.createTabForPort(webview, '30004');
-        });
-        this._apiWrapper.registerWebviewProvider('data-management-hadoop', webview => {
-            webview.html = '<div>2</div>';
-          //  this.createTabForPort(webview, '31000');
-        });
-        this._apiWrapper.registerWebviewProvider('data-management-hdfs', webview => {
-            webview.html = '<div>3</div>';
-
-          //  this.createTabForPort(webview, '30001');
+        this._apiWrapper.registerWebviewProvider('data-management-agent', webview => {
+            webview.html = '<div><h1>SQL Agent</h1></div>';
         });
     }
-
-
-    // private createTabForPort(webview: data.DashboardWebview, port: string): void {
-    //     let self = this;
-
-    //     // TODO need profile access to detect correct IP for connection
-    //     // for now, assume active connection is correct
-    //     // long term, should be able to query DMV for all endpoints and use those instead
-    //     data.connection.getCurrentConnection()
-    //         .then(connection => {
-    //             if (connection.providerName === 'MSSQL' && connection.options['server']) {
-    //                 // Strip TDS port number from the server URI
-    //                 let serverName: string = connection.options['server'].split(',')[0];
-
-    //                 // Put together the template variables and render the template
-    //                 let templateValues = {
-    //                     url: `http://${serverName}:${port}`,
-    //                     timeNow: new Date().getTime()
-    //                 };
-
-    //                 return Utils.renderTemplateHtml(self._context.extensionPath, 'clusterWebTab.html', templateValues);
-    //             } else {
-    //                 let templateValues = {
-    //                     message: LocalizedConstants.msgMissingSqlConnection
-    //                 };
-    //                 return Utils.renderTemplateHtml(self._context.extensionPath, 'emptyTab.html', templateValues);
-    //             }
-    //         })
-    //         .then(html => { webview.html = html; });
-    // }
 }
