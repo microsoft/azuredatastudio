1ab8fb9bd58ae4de8d4a69e739cce6aa48b85acd
Anthony Dresser <anthonydresserdev@gmail.com>
2018-03-05 18:13:02 -0800

    moved src out of client folder

--- extensions/mssql/src/typings/refs.d.ts
+++ extensions/mssql/src/typings/refs.d.ts
@@ -3,5 +3,5 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/// <reference path='../../../../../src/sql/sqlops.d.ts'/>
-/// <reference path='../../../../../src/vs/vscode.d.ts'/>
+/// <reference path='../../../../src/sql/sqlops.d.ts'/>
+/// <reference path='../../../../src/vs/vscode.d.ts'/>
