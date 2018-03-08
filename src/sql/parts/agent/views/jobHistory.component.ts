2569b69a5dc143ae824726a0e57d6a407dd213cb
Aditya Bist <adbist@microsoft.com>
2018-03-07 16:38:35 -0800

    fixed the dropdown arrow for the overview section

--- src/sql/parts/agent/views/jobHistory.component.ts
+++ src/sql/parts/agent/views/jobHistory.component.ts
@@ -5,7 +5,7 @@
 
 import 'vs/css!./jobHistory';
 
-import { OnInit, Component, Inject, forwardRef, ElementRef, ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
+import { OnInit, Component, Inject, forwardRef, ElementRef, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { PanelComponent } from 'sql/base/browser/ui/panel/panel.component';
 import { IBootstrapService, BOOTSTRAP_SERVICE_ID } from 'sql/services/bootstrap/bootstrapService';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
@@ -13,12 +13,11 @@ import { IAgentService } from '../common/interfaces';
 import { Tree } from 'vs/base/parts/tree/browser/treeImpl';
 import { ExplorerDataSource } from 'sql/parts/dashboard/widgets/explorer/explorerTree';
 import { TreeCreationUtils } from 'sql/parts/registeredServer/viewlet/treeCreationUtils';
-import { Builder, $ } from 'vs/base/browser/builder';
 import { ICancelableEvent } from 'vs/base/parts/tree/browser/treeDefaults';
 import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { attachListStyler } from 'vs/platform/theme/common/styler';
-import { SavedConnectionTreeController } from 'sql/parts/connection/connectionDialog/savedConnectionTreeController';
-import { render } from 'vs/base/browser/ui/octiconLabel/octiconLabel';
+// import { JobHistoryController, JobHistoryDataSource,
+// 	JobHistoryRenderer, JobHistoryFilter } from 'sql/parts/agent/views/jobHistoryTree';
 
 export const DASHBOARD_SELECTOR: string = 'jobHistory-component';
 
@@ -26,12 +25,15 @@ export const DASHBOARD_SELECTOR: string = 'jobHistory-component';
 	selector: DASHBOARD_SELECTOR,
 	templateUrl: decodeURI(require.toUrl('./jobHistory.component.html'))
 })
-export class JobHistoryComponent implements OnInit, OnDestroy {
+export class JobHistoryComponent implements OnInit {
 
 	private _agentService: IAgentService;
 	private _tree: Tree;
-	private _treeDataSource = new ExplorerDataSource();
-	private _prevRunsBuilder: Builder;
+
+	// private _treeController: JobHistoryController;
+	// private _treeDataSource: JobHistoryDataSource;
+	// private _treeRenderer: JobHistoryRenderer;
+	// private _treeFilter: JobHistoryFilter;
 
 	@ViewChild('table') private _tableContainer: ElementRef;
 
@@ -46,42 +48,22 @@ export class JobHistoryComponent implements OnInit, OnDestroy {
 	}
 
 	ngOnInit() {
-		//this.renderPreviousRuns();
+		// this._tree = new Tree(this._tableContainer.nativeElement, {
+		// 	controller: this._treeController,
+		// 	dataSource: this._treeDataSource,
+		// 	filter: this._treeFilter,
+		// 	renderer: this._treeRenderer
+		// });
 	}
 
-	ngOnDestroy() {
-	}
-
-	private renderPreviousRuns(): void {
-		let prevRunContainer = $('.prev-runs');
-		prevRunContainer.div({ class: 'prev-run-list'}, (divContainer: Builder) => {
-			this._prevRunsBuilder = new Builder(divContainer.getHTMLElement());
-			this._prevRunsBuilder.div({ class: 'explorer-servers' }, (treeContainer: Builder) => {
-				let leftClick = (element: any, eventish: ICancelableEvent, origin: string) => {
-					// element will be a server group if the tree is clicked rather than a item
-					// if (element instanceof ConnectionProfile) {
-					// 	this.onConnectionClick({ payload: { origin: origin, originalEvent: eventish } }, element);
-					// }
-				};
-				//let actionProvider = this._instantiationService.createInstance(RecentConnectionActionsProvider);
-
-				//let controller = new RecentConnectionTreeController(leftClick, actionProvider, this._connectionManagementService, this._contextMenuService);
-
-				let controller = new SavedConnectionTreeController(leftClick);
-
-				// actionProvider.onRecentConnectionRemoved(() => {
-				// 	this.open(this._connectionManagementService.getRecentConnections().length > 0);
-				// });
-				// controller.onRecentConnectionRemoved(() => {
-				// 	this.open(this._connectionManagementService.getRecentConnections().length > 0);
-				// });
-				this._tree = TreeCreationUtils.createConnectionTree(treeContainer.getHTMLElement(), this._instantiationService, controller);
-
-				// Theme styler
-				//this._register(styler.attachListStyler(this._tree, this._themeService));
-				this._prevRunsBuilder.append(this._tree.getHTMLElement());
-			});
-		});
+	private toggleCollapse(): void {
+		let arrow: HTMLElement = $('.resultsViewCollapsible').get(0);
+		let checkbox: any = document.getElementById('accordion');
+		if (arrow.className === 'resultsViewCollapsible' && checkbox.checked === false) {
+			arrow.className = 'resultsViewCollapsible collapsed';
+		} else if (arrow.className === 'resultsViewCollapsible collapsed' && checkbox.checked === true) {
+			arrow.className = 'resultsViewCollapsible';
+		}
 	}
 
 }
