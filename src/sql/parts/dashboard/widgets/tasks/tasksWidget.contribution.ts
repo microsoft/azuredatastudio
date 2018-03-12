bfbb2422cfb75bda19d77a19f9535b16d950064b
Anthony Dresser <andresse@microsoft.com>
2018-03-12 16:32:13 -0700

    inital task update

--- src/sql/parts/dashboard/widgets/tasks/tasksWidget.contribution.ts
+++ src/sql/parts/dashboard/widgets/tasks/tasksWidget.contribution.ts
@@ -2,30 +2,38 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { IJSONSchema } from 'vs/base/common/jsonSchema';
 import { registerDashboardWidget } from 'sql/platform/dashboard/common/widgetRegistry';
 import { TaskRegistry } from 'sql/platform/tasks/common/tasks';
+
+import { Registry } from 'vs/platform/registry/common/platform';
+import { IJSONSchema } from 'vs/base/common/jsonSchema';
+import { IJSONContributionRegistry, Extensions } from 'vs/platform/jsonschemas/common/jsonContributionRegistry';
+
+const schemaRegistry = <IJSONContributionRegistry>Registry.as(Extensions.JSONContribution);
+
+const singleTaskSchema: IJSONSchema = {
+	type: 'string',
+	enum: TaskRegistry.getTasks()
+};
 
 const tasksSchema: IJSONSchema = {
 	type: 'array',
 	items: {
-		anyOf: [{
-			type: 'string',
-			enum: TaskRegistry.getTasks()
-		},
-		{
-			type: 'object',
-			properties: {
-				name: {
-					type: 'string',
-					enum: TaskRegistry.getTasks()
-				},
-				when: {
-					type: 'string'
+		anyOf: [
+			singleTaskSchema,
+			{
+				type: 'object',
+				properties: {
+					name: singleTaskSchema,
+					when: {
+						type: 'string'
+					}
 				}
 			}
-		}]
+		]
 	}
 };
+
+TaskRegistry.onTaskRegistered(e => singleTaskSchema.enum.push(e));
 
 registerDashboardWidget('tasks-widget', '', tasksSchema);
