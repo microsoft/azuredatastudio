40550d0840e21025a313220105dc3a13ca2de8f4
Anthony Dresser <andresse@microsoft.com>
2018-03-16 19:52:33 -0700

    rename context key databasename to databaseName (#903)
    * rename context key databasename to databaseName
    
    * maintain correct context on database name

--- src/sql/services/bootstrap/bootstrapParams.ts
+++ src/sql/services/bootstrap/bootstrapParams.ts
@@ -5,7 +5,8 @@
 
 import { DataService } from 'sql/parts/grid/services/dataService';
 import { IConnectionProfile } from 'sql/parts/connection/common/interfaces';
-import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
+import { IContextKeyService, IContextKey } from 'vs/platform/contextkey/common/contextkey';
+import { ConnectionContextkey } from 'sql/parts/connection/common/connectionContextKey';
 
 export interface BootstrapParams {
 }
@@ -22,6 +23,7 @@ export interface DashboardComponentParams extends BootstrapParams {
 	connection: IConnectionProfile;
 	ownerUri: string;
 	scopedContextService: IContextKeyService;
+	connectionContextKey: ConnectionContextkey;
 }
 
 export interface TaskDialogComponentParams extends BootstrapParams {
