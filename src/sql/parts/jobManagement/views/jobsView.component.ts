161d95b98e4ecbbc84e7c65621e73c1729f922eb
Karl Burtram <karlb@microsoft.com>
2018-03-20 16:04:18 -0700

    Convert rowdetail slickgrid plug to TypeScript

--- src/sql/parts/jobManagement/views/jobsView.component.ts
+++ src/sql/parts/jobManagement/views/jobsView.component.ts
@@ -29,6 +29,7 @@ import { IGridDataSet } from 'sql/parts/grid/common/interfaces';
 import { FieldType, IObservableCollection, CollectionChange, SlickGrid } from 'angular2-slickgrid';
 import { Table } from 'sql/base/browser/ui/table/table';
 import { attachTableStyler } from 'sql/common/theme/styler';
+import { RowDetailView } from 'sql/base/browser/ui/table/plugins/rowdetailview';
 
 export const JOBSVIEW_SELECTOR: string = 'jobsview-component';
 
@@ -96,10 +97,27 @@ export class JobsViewComponent implements OnInit, OnDestroy {
 				this._jobManagementService.getJobHistory(ownerUri, job.jobId).then((result) => {
 					if (result.jobs) {
 						this.jobHistories[job.jobId] = result.jobs;
+						this.expandJobsWithFailures();
 					}
 				});
 			});
 		}
+	}
+
+	private expandJobsWithFailures(): void {
+		for (let i: number = 0; i < this.jobs.length; ++i) {
+			let job = this.jobs[i];
+			let jobHistory = this.jobHistories[job.jobId];
+			if (jobHistory && jobHistory.length > 0) {
+				let latestExecution = jobHistory[jobHistory.length - 1];
+				if (latestExecution.runStatus !== 0) {
+					this.expandJobRowDetails(i);
+				}
+			}
+		}
+	}
+
+	private expandJobRowDetails(rowIdx: number): void {
 	}
 
 	onFirstVisible() {
@@ -116,7 +134,7 @@ export class JobsViewComponent implements OnInit, OnDestroy {
 		};
 
 		this.dataView = new Slick.Data.DataView({ inlineFilters: false });
-		let rowDetail = new Slick.Plugins.RowDetailView({
+		let rowDetail = new RowDetailView({
 			cssClass: 'detailView-toggle',
 			preTemplate: this.loadingTemplate,
 			postTemplate: (itemDetail) => {
