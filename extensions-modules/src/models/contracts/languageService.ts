4b00025230b018e12c2023053f19d94edc76ec8a
Karl Burtram <karlb@microsoft.com>
2018-03-21 17:28:39 -0700

    Update Agent job registration

--- extensions-modules/src/models/contracts/languageService.ts
+++ extensions-modules/src/models/contracts/languageService.ts
@@ -62,21 +62,58 @@ export interface ILanguageClientHelper {
 	createServerOptions(servicePath: string, runtimeId?: Runtime): ServerOptions;
 }
 
+// Job Management types
 export interface AgentJobsParams {
 	ownerUri: string;
+	jobId: string;
 }
 
-export interface AgentJobsResponse {
-	jobInfo: sqlops.AgentJobInfo[];
+export interface AgentJobsResult {
+	succeeded: boolean;
+	errorMessage: string;
+	jobs: sqlops.AgentJobInfo[];
+}
+
+export interface AgentJobHistoryParams {
+	ownerUri: string;
+	jobId: string;
+}
+
+export interface AgentJobHistoryResult {
+	succeeded: boolean;
+	errorMessage: string;
+	jobs: sqlops.AgentJobHistoryInfo[];
+}
+
+export interface AgentJobActionParams {
+	ownerUri: string;
+	jobName: string;
+	action: string;
+}
+
+export interface AgentJobActionResult {
+	succeeded: boolean;
+	errorMessage: string;
 }
 
 export namespace AgentJobsRequest {
-	export const type = new RequestType<AgentJobsParams, AgentJobsResponse, void, void>('agent/jobs');
+	export const type = new RequestType<AgentJobsParams, AgentJobsResult, void, void>('agent/jobs');
+}
+
+export namespace AgentJobHistoryRequest {
+	export const type = new RequestType<AgentJobHistoryParams, AgentJobHistoryResult, void, void>('agent/jobhistory');
+}
+
+
+export namespace AgentJobActionRequest {
+	export const type = new RequestType<AgentJobActionParams, AgentJobActionResult, void, void>('agent/jobaction');
 }
 
 export class AgentServicesFeature extends SqlOpsFeature<undefined> {
 	private static readonly messagesTypes: RPCMessageType[] = [
-		AgentJobsRequest.type
+		AgentJobsRequest.type,
+		AgentJobHistoryRequest.type,
+		AgentJobActionRequest.type
 	];
 
 	constructor(client: SqlOpsDataClient) {
@@ -98,8 +135,8 @@ export class AgentServicesFeature extends SqlOpsFeature<undefined> {
 	protected registerProvider(options: undefined): Disposable {
 		const client = this._client;
 
-		let getJobs = (ownerUri: string): Thenable<sqlops.AgentJobInfo[]> => {
-			let params: AgentJobsParams = { ownerUri };
+		let getJobs = (ownerUri: string): Thenable<sqlops.AgentJobsResult> => {
+			let params: AgentJobsParams = { ownerUri: ownerUri, jobId: null };
 			return client.sendRequest(AgentJobsRequest.type, params).then(
 				r => r,
 				e => {
@@ -109,9 +146,34 @@ export class AgentServicesFeature extends SqlOpsFeature<undefined> {
 			);
 		};
 
+		let getJobHistory = (connectionUri: string, jobID: string): Thenable<sqlops.AgentJobHistoryResult> => {
+			let params: AgentJobHistoryParams = { ownerUri: connectionUri, jobId: jobID };
+
+			return client.sendRequest(AgentJobHistoryRequest.type, params).then(
+				r => r,
+				e => {
+					client.logFailedRequest(AgentJobHistoryRequest.type, e);
+					return Promise.resolve(undefined);
+				}
+			);
+		};
+
+		let jobAction = (connectionUri: string, jobName: string, action: string): Thenable<sqlops.AgentJobActionResult> => {
+			let params: AgentJobActionParams = { ownerUri: connectionUri, jobName: jobName, action: action };
+			return client.sendRequest(AgentJobActionRequest.type, params).then(
+				r => r,
+				e => {
+					client.logFailedRequest(AgentJobActionRequest.type, e);
+					return Promise.resolve(undefined);
+				}
+			);
+		};
+
 		return sqlops.dataprotocol.registerAgentServicesProvider({
 			providerId: client.providerId,
-			getJobs
+			getJobs,
+			getJobHistory,
+			jobAction
 		});
 	}
 }
