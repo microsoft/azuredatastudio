9b87bb04fbadcd459bd3e52e49a50bb4640fa071
Karl Burtram <karlb@microsoft.com>
2018-03-23 22:04:31 -0700

    Revert "Results grid options (#962)"
    This reverts commit f2779f2a50fac0f8383b9459fcf470fa3def7ae4.

--- src/sql/parts/query/editor/queryResultsEditor.ts
+++ src/sql/parts/query/editor/queryResultsEditor.ts
@@ -11,59 +11,15 @@ import { EditorOptions } from 'vs/workbench/common/editor';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { RawContextKey } from 'vs/platform/contextkey/common/contextkey';
 import { BaseEditor } from 'vs/workbench/browser/parts/editor/baseEditor';
-import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
-import { BareFontInfo } from 'vs/editor/common/config/fontInfo';
-import { getZoomLevel } from 'vs/base/browser/browser';
-import { Configuration } from 'vs/editor/browser/config/configuration';
-import { IThemeService } from 'vs/platform/theme/common/themeService';
-import * as dom from 'vs/base/browser/dom';
-import * as types from 'vs/base/common/types';
-
 import { QueryResultsInput } from 'sql/parts/query/common/queryResultsInput';
 import { IQueryModelService } from 'sql/parts/query/execution/queryModel';
 import { IBootstrapService } from 'sql/services/bootstrap/bootstrapService';
 import { QueryComponentParams } from 'sql/services/bootstrap/bootstrapParams';
+import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { QueryOutputModule } from 'sql/parts/query/views/queryOutput.module';
 import { QUERY_OUTPUT_SELECTOR } from 'sql/parts/query/views/queryOutput.component';
 
-export const RESULTS_GRID_DEFAULTS = {
-	cellPadding: [6, 10, 5],
-	rowHeight: 29
-};
-
 export const TextCompareEditorVisible = new RawContextKey<boolean>('textCompareEditorVisible', false);
-
-export class BareResultsGridInfo extends BareFontInfo {
-
-	public static createFromRawSettings(opts: {
-		fontFamily?: string;
-		fontWeight?: string;
-		fontSize?: number | string;
-		lineHeight?: number | string;
-		letterSpacing?: number | string;
-		cellPadding?: number | number[];
-	}, zoomLevel: number): BareResultsGridInfo {
-		let cellPadding = !types.isUndefinedOrNull(opts.cellPadding) ? opts.cellPadding : RESULTS_GRID_DEFAULTS.cellPadding;
-
-		return new BareResultsGridInfo(BareFontInfo.createFromRawSettings(opts, zoomLevel), { cellPadding });
-	}
-
-	readonly cellPadding: number | number[];
-
-	protected constructor(fontInfo: BareFontInfo, opts: {
-		cellPadding: number | number[];
-	}) {
-		super({
-			zoomLevel: fontInfo.zoomLevel,
-			fontFamily: fontInfo.fontFamily,
-			fontWeight: fontInfo.fontWeight,
-			fontSize: fontInfo.fontSize,
-			lineHeight: fontInfo.lineHeight,
-			letterSpacing: fontInfo.letterSpacing
-		});
-		this.cellPadding = opts.cellPadding;
-	}
-}
 
 /**
  * Editor associated with viewing and editing the data of a query results grid.
@@ -72,45 +28,14 @@ export class QueryResultsEditor extends BaseEditor {
 
 	public static ID: string = 'workbench.editor.queryResultsEditor';
 	public static AngularSelectorString: string = 'slickgrid-container.slickgridContainer';
-	protected _rawOptions: BareResultsGridInfo;
-	protected _input: QueryResultsInput;
 
 	constructor(
 		@ITelemetryService telemetryService: ITelemetryService,
 		@IThemeService themeService: IThemeService,
 		@IQueryModelService private _queryModelService: IQueryModelService,
-		@IBootstrapService private _bootstrapService: IBootstrapService,
-		@IConfigurationService private _configurationService: IConfigurationService
+		@IBootstrapService private _bootstrapService: IBootstrapService
 	) {
 		super(QueryResultsEditor.ID, telemetryService, themeService);
-		this._rawOptions = BareResultsGridInfo.createFromRawSettings(this._configurationService.getValue('resultsGrid'), getZoomLevel());
-		this._configurationService.onDidChangeConfiguration(e => {
-			if (e.affectsConfiguration('resultsGrid')) {
-				this._rawOptions = BareResultsGridInfo.createFromRawSettings(this._configurationService.getValue('resultsGrid'), getZoomLevel());
-				this.applySettings();
-			}
-		});
-	}
-
-	public get input(): QueryResultsInput {
-		return this._input;
-	}
-
-	private applySettings() {
-		if (this.input && this.input.container) {
-			Configuration.applyFontInfoSlow(this.getContainer().getHTMLElement(), this._rawOptions);
-			if (!this.input.css) {
-				this.input.css = dom.createStyleSheet(this.input.container);
-			}
-			let cssRuleText = '';
-			if (types.isNumber(this._rawOptions.cellPadding)) {
-				cssRuleText = this._rawOptions.cellPadding + 'px';
-			} else {
-				cssRuleText = this._rawOptions.cellPadding.join('px ') + 'px;';
-			}
-			let content = `.grid .slick-cell { padding: ${cssRuleText}; }`;
-			this.input.css.innerHTML = content;
-		}
 	}
 
 	createEditor(parent: Builder): void {
@@ -121,7 +46,6 @@ export class QueryResultsEditor extends BaseEditor {
 
 	setInput(input: QueryResultsInput, options: EditorOptions): TPromise<void> {
 		super.setInput(input, options);
-		this.applySettings();
 		if (!input.hasBootstrapped) {
 			this._bootstrapAngular();
 		}
