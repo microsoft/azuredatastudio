efe8e81b6ecf4dc42b0d51cf5b8a04e8a2f44d4a
Anthony Dresser <andresse@microsoft.com>
2018-04-12 11:29:16 -0700

    Connection inputs labels (#1129)
    * adds aria-label to inputs for connection
    
    * formatting

--- src/sql/base/browser/ui/editableDropdown/dropdown.ts
+++ src/sql/base/browser/ui/editableDropdown/dropdown.ts
@@ -49,6 +49,10 @@ export interface IDropdownOptions extends IDropdownStyles {
 	 * Error Message to show if input is not part of the supplied list, only used if strictSelection = false
 	 */
 	errorMessage?: string;
+	/**
+	 * Value to use as aria-label for the input box
+	 */
+	ariaLabel?: string;
 }
 
 export interface IDropdownStyles {
@@ -124,7 +128,8 @@ export class Dropdown extends Disposable {
 				validation: v => this._inputValidator(v)
 			},
 			placeholder: this._options.placeholder,
-			actions: [this._toggleAction]
+			actions: [this._toggleAction],
+			ariaLabel: this._options.ariaLabel
 		});
 
 		this._register(DOM.addDisposableListener(this._input.inputElement, DOM.EventType.FOCUS, () => {
