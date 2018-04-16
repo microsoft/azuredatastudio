aecf5c9901c8ee620061c7d76c1920a7e918d4a2
Aditya Bist <adbist@microsoft.com>
2018-04-16 16:13:52 -0700

    localized the strings

--- src/sql/parts/jobManagement/common/agentJobUtilities.ts
+++ src/sql/parts/jobManagement/common/agentJobUtilities.ts
@@ -5,38 +5,50 @@
 
 'use strict';
 
+import * as nls from 'vs/nls';
+
 export class AgentJobUtilities {
 
 	public static convertToStatusString(status: number): string {
 		switch(status) {
-			case(0): return 'Failed';
-			case(1): return 'Succeeded';
-			case(3): return 'Canceled';
-			case(5): return 'Status Unknown';
-			default: return 'Status Unknown';
+			case(0): return nls.localize('agentUtilities.failed','Failed');
+			case(1): return nls.localize('agentUtilities.succeeded', 'Succeeded');
+			case(3): return nls.localize('agentUtilities.canceled', 'Canceled');
+			case(5): return nls.localize('agentUtilities.statusUnknown', 'Status Unknown');
+			default: return nls.localize('agentUtilities.statusUnknown', 'Status Unknown');
 		}
 	}
 
 	public static convertToExecutionStatusString(status: number): string {
 		switch(status) {
-			case(1): return 'Executing';
-			case(2): return 'Waiting for Thread';
-			case(3): return 'Between Retries';
-			case(4): return 'Idle';
-			case(5): return 'Suspended';
-			case(6): return '[Obsolete]';
-			case(7): return 'PerformingCompletionActions';
-			default: return 'Status Unknown';
+			case(1): return nls.localize('agentUtilities.executing', 'Executing');
+			case(2): return nls.localize('agentUtilities.waitingForThread', 'Waiting for Thread');
+			case(3): return nls.localize('agentUtilities.betweenRetries', 'Between Retries');
+			case(4): return nls.localize('agentUtilities.idle', 'Idle');
+			case(5): return nls.localize('agentUtilities.suspended', 'Suspended');
+			case(6): return nls.localize('agentUtilities.obsolete', '[Obsolete]');
+			case(7): return nls.localize('agentUtilities.performingCompletionActions', 'PerformingCompletionActions');
+			default: return nls.localize('agentUtilities.statusUnknown', 'Status Unknown');
 		}
 	}
 
 	public static convertToResponse(bool: boolean) {
-		return bool ? 'Yes' : 'No';
+		return bool ? nls.localize('agentUtilities.yes', 'Yes') : nls.localize('agentUtilities.no', 'No');
 	}
 
 	public static convertToNextRun(date: string) {
 		if (date.includes('1/1/0001')) {
-			return 'Not Scheduled';
+			return nls.localize('agentUtilities.notScheduled', 'Not Scheduled');
+		} else {
+			return date;
+		}
+	}
+
+	public static convertToLastRun(date: string) {
+		if (date.includes('1/1/0001')) {
+			return nls.localize('agentUtilities.neverRun', 'Never Run');
+		} else {
+			return date;
 		}
 	}
 }
