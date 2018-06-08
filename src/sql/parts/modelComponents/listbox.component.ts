3afd3b0ff32afa90275e558fd31c3cf3c4403157
Leila Lali <llali@microsoft.com>
2018-06-08 15:47:29 -0700

    fixed the compile error (#1601)

--- src/sql/parts/modelComponents/listbox.component.ts
+++ src/sql/parts/modelComponents/listbox.component.ts
@@ -17,7 +17,7 @@ import { ListBox } from 'sql/base/browser/ui/listBox/listBox';
 import { attachListBoxStyler } from 'sql/common/theme/styler';
 import { IWorkbenchThemeService } from 'vs/workbench/services/themes/common/workbenchThemeService';
 import { IContextViewService } from 'vs/platform/contextview/browser/contextView';
-import Event, { Emitter } from 'vs/base/common/event';
+import { Emitter } from 'vs/base/common/event';
 import * as nls from 'vs/nls';
 import { IClipboardService } from 'vs/platform/clipboard/common/clipboardService';
 
