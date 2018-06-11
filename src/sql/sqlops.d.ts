a22624943edc36ab7a89a6aa2d4c88618e0b8bd8
Karl Burtram <karlb@microsoft.com>
2018-06-11 16:08:32 -0700

    Tabify sqlops.d.ts.

--- src/sql/sqlops.d.ts
+++ src/sql/sqlops.d.ts
@@ -1027,32 +1027,32 @@ declare module 'sqlops' {
 
 	// Agent Services types
 	export enum WeekDays {
-        sunday = 1,
-        monday = 2,
-        tuesday = 4,
-        wednesday = 8,
-        thursday = 16,
-        friday = 32,
-        weekDays = 62,
-        saturday = 64,
-        weekEnds = 65,
-        everyDay = 127
+		sunday = 1,
+		monday = 2,
+		tuesday = 4,
+		wednesday = 8,
+		thursday = 16,
+		friday = 32,
+		weekDays = 62,
+		saturday = 64,
+		weekEnds = 65,
+		everyDay = 127
 	}
 
 	export enum NotifyMethods {
-        none = 0,
-        notifyEmail = 1,
-        pager = 2,
-        netSend = 4,
-        notifyAll = 7
-    }
+		none = 0,
+		notifyEmail = 1,
+		pager = 2,
+		netSend = 4,
+		notifyAll = 7
+	}
 
-    export enum AlertType {
-        sqlServerEvent = 1,
-        sqlServerPerformanceCondition = 2,
-        nonSqlServerEvent = 3,
-        wmiEvent = 4
-    }
+	export enum AlertType {
+		sqlServerEvent = 1,
+		sqlServerPerformanceCondition = 2,
+		nonSqlServerEvent = 3,
+		wmiEvent = 4
+	}
 
 	export interface AgentJobInfo {
 		name: string;
@@ -1102,59 +1102,59 @@ declare module 'sqlops' {
 	}
 
 	export interface AgentProxyInfo {
-        id: number;
-        accountName: string;
-        description: string;
-        credentialName: string;
-        credentialIdentity: string;
-        credentialId: number;
-        isEnabled: boolean;
+		id: number;
+		accountName: string;
+		description: string;
+		credentialName: string;
+		credentialIdentity: string;
+		credentialId: number;
+		isEnabled: boolean;
 	}
 
-    export interface AgentAlertInfo {
-        id: number;
-        delayBetweenResponses: number;
-        eventDescriptionKeyword: string;
-        eventSource: string;
-        hasNotification: number;
-        includeEventDescription: NotifyMethods;
-        isEnabled: boolean;
-        jobId: string;
-        jobName: string;
-        lastOccurrenceDate: string;
-        lastResponseDate: string;
-        messageId: number;
-        notificationMessage: string;
-        occurrenceCount: number;
-        performanceCondition: string;
-        severity: number;
-        databaseName: string;
-        countResetDate: string;
-        categoryName: string;
-        alertType: AlertType;
-        wmiEventNamespace: string;
-        wmiEventQuery: string;
-    }
+	export interface AgentAlertInfo {
+		id: number;
+		delayBetweenResponses: number;
+		eventDescriptionKeyword: string;
+		eventSource: string;
+		hasNotification: number;
+		includeEventDescription: NotifyMethods;
+		isEnabled: boolean;
+		jobId: string;
+		jobName: string;
+		lastOccurrenceDate: string;
+		lastResponseDate: string;
+		messageId: number;
+		notificationMessage: string;
+		occurrenceCount: number;
+		performanceCondition: string;
+		severity: number;
+		databaseName: string;
+		countResetDate: string;
+		categoryName: string;
+		alertType: AlertType;
+		wmiEventNamespace: string;
+		wmiEventQuery: string;
+	}
 
 	export interface AgentOperatorInfo {
-    	name: string;
-        id: number;
-        emailAddress: string;
-        enabled: boolean;
-        lastEmailDate: string;
-        lastNetSendDate: string;
-        lastPagerDate: string;
-        pagerAddress: string;
-        categoryName: string;
-        pagerDays: WeekDays;
-        saturdayPagerEndTime: string;
-        saturdayPagerStartTime: string;
-        sundayPagerEndTime: string;
-        sundayPagerStartTime: string;
-        netSendAddress: string;
-        weekdayPagerStartTime: string;
-        weekdayPagerEndTime: string;
-    }
+		name: string;
+		id: number;
+		emailAddress: string;
+		enabled: boolean;
+		lastEmailDate: string;
+		lastNetSendDate: string;
+		lastPagerDate: string;
+		pagerAddress: string;
+		categoryName: string;
+		pagerDays: WeekDays;
+		saturdayPagerEndTime: string;
+		saturdayPagerStartTime: string;
+		sundayPagerEndTime: string;
+		sundayPagerStartTime: string;
+		netSendAddress: string;
+		weekdayPagerStartTime: string;
+		weekdayPagerEndTime: string;
+	}
 
 	export interface ResultStatus {
 		success: boolean;
@@ -1194,8 +1194,8 @@ declare module 'sqlops' {
 	}
 
 	export interface AgentAlertsResult extends ResultStatus {
-        alerts: AgentAlertInfo[];
-    }
+		alerts: AgentAlertInfo[];
+	}
 
 	export interface CreateAgentAlertResult extends ResultStatus {
 		alert: AgentJobStepInfo;
@@ -1206,8 +1206,8 @@ declare module 'sqlops' {
 	}
 
 	export interface AgentOperatorsResult extends ResultStatus {
-        operators: AgentOperatorInfo[];
-    }
+		operators: AgentOperatorInfo[];
+	}
 
 	export interface CreateAgentOperatorResult extends ResultStatus {
 		operator: AgentOperatorInfo;
@@ -1218,8 +1218,8 @@ declare module 'sqlops' {
 	}
 
 	export interface AgentProxiesResult extends ResultStatus {
-        operators: AgentOperatorInfo[];
-    }
+		operators: AgentOperatorInfo[];
+	}
 
 	export interface CreateAgentProxyResult extends ResultStatus {
 		operator: AgentOperatorInfo;
