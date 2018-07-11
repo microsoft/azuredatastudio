20d209bc94e13f19c315219bfd5da18cd7155531
Matt Irvine <mairvine@microsoft.com>
2018-07-11 16:00:10 -0700

    support themes including high contrast

--- src/sql/platform/dialog/wizardNavigation.component.ts
+++ src/sql/platform/dialog/wizardNavigation.component.ts
@@ -6,10 +6,13 @@
 'use strict';
 
 import 'vs/css!./media/wizardNavigation';
-import { Component, Inject, forwardRef, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, Inject, forwardRef, ElementRef, AfterViewInit, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { IBootstrapParams } from 'sql/services/bootstrap/bootstrapService';
 import { Event, Emitter } from 'vs/base/common/event';
 import { Wizard } from './dialogTypes';
+import { IWorkbenchThemeService } from 'vs/workbench/services/themes/common/workbenchThemeService';
+import { attachStyler } from 'vs/platform/theme/common/styler';
+import { SIDE_BAR_BACKGROUND } from 'vs/workbench/common/theme';
 
 export class WizardNavigationParams implements IBootstrapParams {
 	wizard: Wizard;
@@ -20,12 +23,12 @@ export class WizardNavigationParams implements IBootstrapParams {
 	selector: 'wizard-navigation',
 	providers: [],
 	template: `
-		<div class="wizardNavigation-container">
+		<div #container class="wizardNavigation-container">
 			<ng-container *ngFor="let item of _params.wizard.pages; let i = index">
 				<div class="wizardNavigation-pageNumber">
 					<div class="wizardNavigation-connector" [ngClass]="{'invisible': !hasTopConnector(i), 'active': isActive(i)}"></div>
 					<a [attr.href]="isActive(i) ? '' : null" [title]="item.title">
-						<span class="wizardNavigation-dot" [ngClass]="{'active': isActive(i)}" (click)="navigate(i)">{{i+1}}</span>
+						<span class="wizardNavigation-dot" [ngClass]="{'active': isActive(i), 'currentPage': isCurrentPage(i)}" (click)="navigate(i)">{{i+1}}</span>
 					</a>
 					<div class="wizardNavigation-connector" [ngClass]="{'invisible': !hasBottomConnector(i), 'active': isActive(i)}"></div>
 				</div>
@@ -37,14 +40,17 @@ export class WizardNavigation implements AfterViewInit {
 	private _onResize = new Emitter<void>();
 	public readonly onResize: Event<void> = this._onResize.event;
 
-	public modelViewId: string;
+	@ViewChild('container', { read: ElementRef }) private _container: ElementRef;
 	constructor(
 		@Inject(forwardRef(() => ElementRef)) private _el: ElementRef,
 		@Inject(forwardRef(() => ChangeDetectorRef)) private _changeRef: ChangeDetectorRef,
-		@Inject(IBootstrapParams) private _params: WizardNavigationParams) {
+		@Inject(IBootstrapParams) private _params: WizardNavigationParams,
+		@Inject(IWorkbenchThemeService) private _themeService: IWorkbenchThemeService) {
 	}
 
 	ngAfterViewInit() {
+		this._themeService.onThemeChange(() => this.style());
+		this.style();
 		this._params.wizard.onPageChanged(() => this._changeRef.detectChanges());
 	}
 
@@ -60,9 +66,24 @@ export class WizardNavigation implements AfterViewInit {
 		return index <= this._params.wizard.currentPage;
 	}
 
+	isCurrentPage(index: number): boolean {
+		return index === this._params.wizard.currentPage;
+	}
+
 	navigate(index: number): void {
 		if (this.isActive(index)) {
 			this._params.navigationHandler(index);
 		}
+	}
+
+	private style(): void {
+		let theme = this._themeService.getTheme();
+		let navigationBackgroundColor = theme.getColor(SIDE_BAR_BACKGROUND);
+		if (theme.type === 'light') {
+			navigationBackgroundColor = navigationBackgroundColor.lighten(0.03);
+		} else if (theme.type === 'dark') {
+			navigationBackgroundColor = navigationBackgroundColor.darken(0.03);
+		}
+		(this._container.nativeElement as HTMLElement).style.backgroundColor = navigationBackgroundColor.toString();
 	}
 }
