d1d4bb9c80a3fcd287bc637ae82dc5c7b7169402
Matt Irvine <mairvine@microsoft.com>
2018-07-13 14:07:18 -0700

    Fix another use of innerHtml

--- src/sql/parts/dashboard/containers/dashboardErrorContainer.component.ts
+++ src/sql/parts/dashboard/containers/dashboardErrorContainer.component.ts
@@ -40,7 +40,7 @@ export class DashboardErrorContainer extends DashboardTab implements AfterViewIn
 
 	ngAfterViewInit() {
 		let errorMessage = this._errorMessageContainer.nativeElement as HTMLElement;
-		errorMessage.innerHTML = nls.localize('dashboardNavSection_loadTabError', 'The "{0}" section has invalid content. Please contact extension owner.', this.tab.title);
+		errorMessage.innerText = nls.localize('dashboardNavSection_loadTabError', 'The "{0}" section has invalid content. Please contact extension owner.', this.tab.title);
 	}
 
 	public get id(): string {
