fd49c081c27538adc9b01c889e112c24015fc6ff
Matt Irvine <mji7wb@virginia.edu>
2018-07-13 15:24:24 -0700

    Fix uses of innerHtml when we could just set element text (#1919)

--- src/sql/workbench/errorMessageDialog/errorMessageDialog.ts
+++ src/sql/workbench/errorMessageDialog/errorMessageDialog.ts
@@ -101,7 +101,7 @@ export class ErrorMessageDialog extends Modal {
 	private updateDialogBody(): void {
 		let builder = new Builder(this._body).empty();
 		builder.div({ class: 'error-message' }, (errorContainer) => {
-			errorContainer.innerHtml(this._message);
+			errorContainer.getHTMLElement().innerText = this._message;
 		});
 	}
 
@@ -168,7 +168,7 @@ export class ErrorMessageDialog extends Modal {
 
 	private resetActions(): void {
 		this._actions = [];
-		for(let actionButton of this._actionButtons) {
+		for (let actionButton of this._actionButtons) {
 			actionButton.element.style.visibility = 'hidden';
 		}
 	}
