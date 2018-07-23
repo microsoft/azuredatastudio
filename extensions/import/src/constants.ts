85e13bf08d2105f6fd69ec7aaad3bdb011873799
Matt Irvine <mairvine@microsoft.com>
2018-07-23 14:50:11 -0700

    Extension code compiles

---
+++ extensions/import/src/constants.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+export const extensionConfigSectionName = 'flatFileImport';
+export const serviceName = 'Flat File Import Service';
+export const providerId = 'FlatFileImport';
+export const configLogDebugInfo = 'logDebugInfo';
+export const sqlConfigSectionName = 'sql';
+
+export const serviceCrashLink = 'TODO';
+
