9871bc1b46bdadec9a1d5094cd56ce370966ea2d
Pavel Kondratyev <t-pakond@microsoft.com>
2018-07-24 13:33:46 -0700

    Added provider

--- extensions/import/src/wizard/prosePreview.ts
+++ extensions/import/src/wizard/prosePreview.ts
@@ -8,13 +8,18 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as sqlops from 'sqlops';
 import { create } from 'domain';
+import { FlatFileProvider } from '../../out/services/contracts';
 
 var fileDelimiter = ',';
 
-export async function prosePreview(view: sqlops.ModelView) : Promise<void> {
+export async function prosePreview(view: sqlops.ModelView, provider : FlatFileProvider) : Promise<void> {
 	//from services sample placeholder code
 	//let formWrapper = view.modelBuilder.loadingComponent().component();
-
+	let data;
+	provider.sendDataPreviewRequest({ filePath: 'Hello World' }).then(response => {
+		data = new DataObject(response.dataPreview[0], response.dataPreview.slice(1,response.dataPreview.length));
+		//vscode.window.showInformationMessage('Response: ' + response.dataPreview);
+	});
 	let table = await createTable(view, data);
 	let formModel = view.modelBuilder.formContainer()
 		.withFormItems(
@@ -31,13 +36,10 @@ export async function prosePreview(view: sqlops.ModelView) : Promise<void> {
 }
 
 async function createTable(view: sqlops.ModelView, tableData: IDataObject) : Promise<sqlops.TableComponent> {
-
-	let validData = true;
-
 	let columns = tableData.columns;
-
 	let rows;
 	let rowsLength = tableData.rows.length;
+
 	if(rowsLength > 100){
 		rows = tableData.rows.slice(0,100);
 	}
@@ -70,11 +72,11 @@ class DataObject implements IDataObject{
 	}
 }
 
-var data = new DataObject(
+/*var data = new DataObject(
 	['created_utc','score','domain','id'],
 	[
 		['1370264768.0','674','twitter.com','1fktz4'],
 		['1370264798.0','675','twitter.com','2gatz4'],
 		['1370264768.0','676','twitter.com','1fkrzf']
 	]
-);
+);*/
