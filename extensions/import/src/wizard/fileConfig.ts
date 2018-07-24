474159cf1be4ad586a945bab02b9654e2cc828fd
Amir Omidi <amir@aaomidi.com>
2018-07-24 11:13:54 -0700

    Pushing latest changes.

--- extensions/import/src/wizard/fileConfig.ts
+++ extensions/import/src/wizard/fileConfig.ts
@@ -6,15 +6,18 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import * as path from 'path';
 import * as sqlops from 'sqlops';
 
-export async function fileConfig(view: sqlops.ModelView): Promise<void> {
-	//from services sample placeholder code
-	let server: sqlops.connection.Connection = null;
+let server: sqlops.connection.Connection = null;
 
-	let serverDropdown = await createServerDropdown(view);
-	let databaseDropdown = await createDatabaseDropdown(view, server);
+let serverDropdown: sqlops.DropDownComponent;
+let databaseDropdown: sqlops.DropDownComponent;
+let fileTextBox : sqlops.InputBoxComponent;
+let fileButton : sqlops.ButtonComponent;
+
+export async function fileConfig(view: sqlops.ModelView): Promise<void> {
+	let serverComponent = await createServerDropdown(view);
+	let databaseComponent = await createDatabaseDropdown(view, server);
 
 	serverDropdown.onValueChanged((params) => {
 		console.log('Params:' + params);
@@ -23,31 +26,57 @@ export async function fileConfig(view: sqlops.ModelView): Promise<void> {
 		populateDatabaseDropdown(server, databaseDropdown);
 	});
 
+	let fileBrowserModel = await createFileBrowser(view);
 
 	let formModel = view.modelBuilder.formContainer()
 		.withFormItems(
 			[
-				{
-					component: serverDropdown,
-					title: 'Server'
-				},
-				{
-					component: databaseDropdown,
-					title: 'Database'
-				}
+				serverComponent,
+				databaseComponent,
+				fileBrowserModel
 			]).component();
 	let formWrapper = view.modelBuilder.loadingComponent().withItem(formModel).component();
 	formWrapper.loading = false;
 	await view.initializeModel(formWrapper);
 }
 
-async function createServerDropdown(view: sqlops.ModelView): Promise<sqlops.DropDownComponent> {
+async function createFileBrowser(view: sqlops.ModelView): Promise<sqlops.FormComponent> {
+ 	fileTextBox = view.modelBuilder.inputBox().component();
+ 	fileButton = view.modelBuilder.button().withProperties({
+		label: 'Browse'
+	}).component();
+
+	fileButton.onDidClick(async (click) => {
+		let fileUris = await vscode.window.showOpenDialog(
+			{
+				canSelectFiles: true,
+				canSelectFolders: false,
+				canSelectMany: false,
+				openLabel: 'Open'
+			}
+		);
+		if (!fileUris || fileUris.length === 0) {
+			return;
+		}
+
+		let fileUri = fileUris[0];
+		fileTextBox.value = fileUri.fsPath;
+	});
+
+	return Promise.resolve({
+		component: fileTextBox,
+		title: 'Location of file to be imported',
+		actions: [fileButton]
+	});
+}
+
+async function createServerDropdown(view: sqlops.ModelView): Promise<sqlops.FormComponent> {
 	let cons = await sqlops.connection.getActiveConnections();
 	// This user has no active connections ABORT MISSION
 	if (!cons || cons.length === 0) {
 		return;
 	}
-	let serverDropdown = view.modelBuilder.dropDown().withProperties({
+	serverDropdown = view.modelBuilder.dropDown().withProperties({
 		values: cons.map(c => {
 			return {
 				connection: c,
@@ -56,17 +85,25 @@ async function createServerDropdown(view: sqlops.ModelView): Promise<sqlops.Drop
 			};
 		})
 	}).component();
-	return Promise.resolve(serverDropdown);
-}
-async function createDatabaseDropdown(view: sqlops.ModelView, server: sqlops.connection.Connection): Promise<sqlops.DropDownComponent>{
-	let dbDropdown=view.modelBuilder.dropDown().component();
-	populateDatabaseDropdown(server, dbDropdown);
 
-	return Promise.resolve(dbDropdown);
+	return Promise.resolve({
+		component: serverDropdown,
+		title:'Server the database is in',
+	});
 }
 
-async function populateDatabaseDropdown(server: sqlops.connection.Connection, dbDropdown: sqlops.DropDownComponent){
-	if(server === null){
+async function createDatabaseDropdown(view: sqlops.ModelView, server: sqlops.connection.Connection): Promise<sqlops.FormComponent> {
+	let databaseDropdown = view.modelBuilder.dropDown().component();
+	await populateDatabaseDropdown(server, databaseDropdown);
+
+	return Promise.resolve({
+		component: databaseDropdown,
+		title:'Database the table is created in',
+	});
+}
+
+async function populateDatabaseDropdown(server: sqlops.connection.Connection, dbDropdown: sqlops.DropDownComponent): Promise<boolean> {
+	if (server === null) {
 		return;
 	}
 
@@ -74,13 +111,15 @@ async function populateDatabaseDropdown(server: sqlops.connection.Connection, db
 	let databases = await connectionProvider.listDatabases(server.connectionId);
 
 	dbDropdown.updateProperties({
-		values: databases.databaseNames.map(db=>{
+		values: databases.databaseNames.map(db => {
 			return {
 				displayName: db,
 				name: db
 			};
 		})
-	})
+	});
+
+	Promise.resolve(true);
 }
 
 interface ConnectionDropdownValue extends sqlops.CategoryValue {
