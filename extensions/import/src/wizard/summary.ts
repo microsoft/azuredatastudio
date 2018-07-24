3b2b2310ca4280f5a1cfd7e0099a131f3c790895
Sean <sean@Seans-MacBook-Pro.local>
2018-07-23 19:51:51 -0700

    Added summary table with hardcoded values and status text

--- extensions/import/src/wizard/summary.ts
+++ extensions/import/src/wizard/summary.ts
@@ -9,8 +9,38 @@ import * as path from 'path';
 import * as sqlops from 'sqlops';
 
 export async function summary(view: sqlops.ModelView) : Promise<void> {
-	//from services sample placeholder code
-	let formWrapper = view.modelBuilder.loadingComponent().component();
+
+	let table = view.modelBuilder.table()
+		.withProperties({
+			data: [['Database name', 'test'],
+				['Table schema','dbo'],
+				['File to be imported', 'test file']],
+			columns: ['Object type', 'Name'],
+			height: 250,
+			width: 500
+		})
+		.component();
+
+	let statusText = view.modelBuilder.text()
+		.withProperties({
+			value: '✔ Awesome! You have successfully inserted the data into a table.'
+		})
+		.component();
+
+	let formModel = view.modelBuilder.formContainer()
+		.withFormItems(
+			[
+				{
+					component: table,
+					title: 'Import information'
+				},
+				{
+					component: statusText,
+					title: 'Status'
+				}
+			]
+		).component();
+	let formWrapper = view.modelBuilder.loadingComponent().withItem(formModel).component();
 	formWrapper.loading = false;
 	await view.initializeModel(formWrapper);
 }
