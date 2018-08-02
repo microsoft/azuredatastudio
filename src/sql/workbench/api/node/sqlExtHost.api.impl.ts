41a0238984dee5bd53722474ae2018cadf901e3b
Leila Lali <llali@microsoft.com>
2018-08-02 15:25:44 -0700

    merged from master

--- src/sql/workbench/api/node/sqlExtHost.api.impl.ts
+++ src/sql/workbench/api/node/sqlExtHost.api.impl.ts
@@ -104,11 +104,9 @@ export function createApiFactory(
 				getCredentials(connectionId: string): Thenable<{ [name: string]: string }> {
 					return extHostConnectionManagement.$getCredentials(connectionId);
 				},
-
 				openConnectionDialog(callback: (connection: sqlops.connection.Connection) => void) {
 					extHostConnectionManagement.$openConnectionDialog(callback);
-				}
-
+				},
 				listDatabases(connectionId: string): Thenable<string[]> {
 					return extHostConnectionManagement.$listDatabases(connectionId);
 				},
