f4fa18ec05e5ff3c872ff764ca1589a9280bfa0d
Karl Burtram <karlb@microsoft.com>
2018-08-01 20:15:14 -0400

    Add GetConnectionString command (without build break) (#2120)
    * Revert "Revert "Adds "Get Connection String" command (#2108)" (#2116)"
    
    This reverts commit c6d1fa2b7de21f99c87c0951c6651db3a63eae37.
    
    * Fix build breaks

--- src/sql/workbench/api/node/sqlExtHost.api.impl.ts
+++ src/sql/workbench/api/node/sqlExtHost.api.impl.ts
@@ -105,6 +105,9 @@ export function createApiFactory(
 				listDatabases(connectionId: string): Thenable<string[]> {
 					return extHostConnectionManagement.$listDatabases(connectionId);
 				},
+				getConnectionString(connectionId: string, includePassword: boolean): Thenable<string> {
+					return extHostConnectionManagement.$getConnectionString(connectionId, includePassword);
+				},
 				getUriForConnection(connectionId: string): Thenable<string> {
 					return extHostConnectionManagement.$getUriForConnection(connectionId);
 				}
