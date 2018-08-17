b282b639321917712047e1aa086c48ef429be06a
Alex <alexander.forssman@gmail.com>
2018-08-17 21:58:55 +0200

    Added feature for showing file location after saving new file.

---
+++ src/sql/workbench/common/workspaceActions.ts
@@ -0,0 +1,13 @@
+import { TPromise } from 'vs/base/common/winjs.base';
+import { Action } from 'vs/base/common/actions';
+import { IWindowsService } from 'vs/platform/windows/common/windows';
+
+export class ShowFileInFolderAction extends Action {
+
+	constructor(private path: string, label: string, private windowsService: IWindowsService) {
+		super('showItemInFolder.action.id', label);
+	}
+ 	run(): TPromise<void> {
+		return this.windowsService.showItemInFolder(this.path);
+	}
+}
