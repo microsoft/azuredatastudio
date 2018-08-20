6668ec4b5d5641df248675d476af37e7564faa7d
Anthony Dresser <andresse@microsoft.com>
2018-08-20 16:19:29 -0700

    Fix accessibility bugs in Chart Viewer and Advanced properties (#2240)
    * fix accessibility bugs in chart viewer
    
    * add advanced properties
    
    * add comments for changes in vs

--- src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
+++ src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
@@ -110,6 +110,11 @@ export class SelectBoxList implements ISelectBoxDelegate, IDelegate<ISelectOptio
 		this.selectElement = document.createElement('select');
 		this.selectElement.className = 'monaco-select-box';
 
+		// {{SQL CARBON EDIT}}
+		if (selectBoxOptions && selectBoxOptions.ariaLabel) {
+			this.selectElement.setAttribute('aria-label', selectBoxOptions.ariaLabel);
+		}
+
 		this._onDidSelect = new Emitter<ISelectData>();
 		this.styles = styles;
 
