0513a47da3660770ab45ca99bd10471bdf8bab74
Anthony Dresser <andresse@microsoft.com>
2018-08-31 17:23:48 -0700

    remove commented code

--- src/sql/parts/grid/services/sharedServices.ts
+++ src/sql/parts/grid/services/sharedServices.ts
@@ -3,7 +3,6 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { escape } from 'sql/base/common/strings';
 import { $ } from 'vs/base/browser/dom';
 
 export class DBCellValue {
@@ -14,47 +13,6 @@ export class DBCellValue {
 		return (object !== undefined && object.displayValue !== undefined && object.isNull !== undefined);
 	}
 }
-
-// /**
-//  * Format xml field into a hyperlink and performs HTML entity encoding
-//  */
-// export function hyperLinkFormatter(row: number, cell: any, value: any, columnDef: any, dataContext: any): string {
-// 	let cellClasses = 'grid-cell-value-container';
-// 	let valueToDisplay: string = '';
-
-// 	if (DBCellValue.isDBCellValue(value)) {
-// 		valueToDisplay = 'NULL';
-// 		if (!value.isNull) {
-// 			cellClasses += ' xmlLink';
-// 			valueToDisplay = escape(value.displayValue);
-// 			return `<a class="${cellClasses}" href="#" >${valueToDisplay}</a>`;
-// 		} else {
-// 			cellClasses += ' missing-value';
-// 		}
-// 	}
-// 	return `<span title="${valueToDisplay}" class="${cellClasses}">${valueToDisplay}</span>`;
-// }
-
-// /**
-//  * Format all text to replace all new lines with spaces and performs HTML entity encoding
-//  */
-// export function textFormatter(row: number, cell: any, value: any, columnDef: any, dataContext: any): string {
-// 	let cellClasses = 'grid-cell-value-container';
-// 	let valueToDisplay: string = '';
-
-// 	if (DBCellValue.isDBCellValue(value)) {
-// 		valueToDisplay = 'NULL';
-// 		if (!value.isNull) {
-// 			valueToDisplay = escape(value.displayValue.replace(/(\r\n|\n|\r)/g, ' '));
-// 		} else {
-// 			cellClasses += ' missing-value';
-// 		}
-// 	} else if (typeof value === 'string') {
-// 		valueToDisplay = escape(value);
-// 	}
-
-// 	return `<span title="${valueToDisplay}" class="${cellClasses}">${valueToDisplay}</span>`;
-// }
 
 /**
  * Format xml field into a hyperlink and performs HTML entity encoding
