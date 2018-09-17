1d37b9ae9cc1de55edf374d2572a487d9a479f5a
Aditya Bist <adbist@microsoft.com>
2018-09-17 13:02:38 -0700

    fixes scrolling in query plan (#2609)

--- src/sql/parts/queryPlan/queryPlan.ts
+++ src/sql/parts/queryPlan/queryPlan.ts
@@ -41,6 +41,8 @@ export class QueryPlanView implements IPanelView {
 			}
 		}
 		container.appendChild(this.container);
+		container.style.overflow = 'scroll';
+		container.style.background = '#FFFFFF';
 	}
 
 	public layout(dimension: Dimension): void {
