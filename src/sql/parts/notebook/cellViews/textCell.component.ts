533f2734f1047b2386b250da8049e3671789d873
Abbie Petchtes <appetcht@microsoft.com>
2018-10-26 16:20:06 -0700

    Add markdown cell to Notebook (#3014)
    * add markdown cell
    
    * add markdown preview for Notebook
    
    * formatting
    
    * address comment

---
+++ src/sql/parts/notebook/cellViews/textCell.component.ts
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the Source EULA. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+import 'vs/css!./textCell';
+
+import { OnInit, Component, Input, Inject, forwardRef, ElementRef, ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
+
+import { CommonServiceInterface } from 'sql/services/common/commonServiceInterface.service';
+import { CellView, ICellModel } from 'sql/parts/notebook/cellViews/interfaces';
+
+import { IColorTheme, IWorkbenchThemeService } from 'vs/workbench/services/themes/common/workbenchThemeService';
+import * as themeColors from 'vs/workbench/common/theme';
+import { ICommandService } from 'vs/platform/commands/common/commands';
+
+export const TEXT_SELECTOR: string = 'text-cell-component';
+
+@Component({
+	selector: TEXT_SELECTOR,
+	templateUrl: decodeURI(require.toUrl('./textCell.component.html'))
+})
+export class TextCellComponent extends CellView implements OnInit {
+	@ViewChild('preview', { read: ElementRef }) private output: ElementRef;
+	@Input() cellModel: ICellModel;
+	private _content: string;
+	constructor(
+		@Inject(forwardRef(() => CommonServiceInterface)) private _bootstrapService: CommonServiceInterface,
+		@Inject(forwardRef(() => ChangeDetectorRef)) private _changeRef: ChangeDetectorRef,
+		@Inject(IWorkbenchThemeService) private themeService: IWorkbenchThemeService,
+		@Inject(ICommandService) private _commandService: ICommandService
+	) {
+		super();
+	}
+
+	ngOnChanges() {
+		this.updatePreview();
+	}
+
+	private updatePreview() {
+		if (this._content !== this.cellModel.source) {
+			this._content = this.cellModel.source;
+			// todo: pass in the notebook filename instead of undefined value
+			this._commandService.executeCommand('notebook.showPreview', undefined, this._content).then((htmlcontent) => {
+				let outputElement = <HTMLElement>this.output.nativeElement;
+				outputElement.innerHTML = htmlcontent;
+			});
+		}
+	}
+
+	ngOnInit() {
+		this._register(this.themeService.onDidColorThemeChange(this.updateTheme, this));
+		this.updateTheme(this.themeService.getColorTheme());
+	}
+
+	// Todo: implement layout
+	public layout() {
+	}
+
+	private updateTheme(theme: IColorTheme): void {
+		let outputElement = <HTMLElement>this.output.nativeElement;
+		outputElement.style.borderTopColor = theme.getColor(themeColors.SIDE_BAR_BACKGROUND, true).toString();
+	}
+
+	public handleContentChanged(): void {
+		this.updatePreview();
+	}
+}
