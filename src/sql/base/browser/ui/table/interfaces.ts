f282fe84b03b4c8a9399543906358bd25b05f734
Anthony Dresser <andresse@microsoft.com>
2018-10-26 16:46:57 -0700

    add missing value

---
+++ src/sql/base/browser/ui/table/interfaces.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IListStyles } from 'vs/base/browser/ui/list/listWidget';
+import { Color } from 'vs/base/common/color';
+
+export interface IDisposableDataProvider<T> extends Slick.DataProvider<T> {
+	dispose(): void;
+}
+
+export interface ITableMouseEvent {
+	anchor: HTMLElement | { x: number, y: number };
+	cell?: { row: number, cell: number };
+}
+
+export interface ITableStyles extends IListStyles {
+	tableHeaderBackground?: Color;
+	tableHeaderForeground?: Color;
+}
+
+export interface ITableSorter<T> {
+	sort(args: Slick.OnSortEventArgs<T>);
+}
+
+export interface ITableConfiguration<T> {
+	dataProvider?: IDisposableDataProvider<T> | Array<T>;
+	columns?: Slick.Column<T>[];
+	sorter?: ITableSorter<T>;
+}
