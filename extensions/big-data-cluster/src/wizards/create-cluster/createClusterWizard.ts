0d1ebce1a1ba6cc8a1fe9feadcd0d90f4cbd2536
Ronald Quan <rquan07@gmail.com>
2019-02-28 14:26:50 -0800

    Ron/bdc script (#4221)
    * WIP adding scripting support.
    
    * Adding deploy command along with additional env vars needed.
    
    * Adding script generation that sets envars, kube context, and mssqlctl
    
    * Adding test email for docker email envar until we update UI.
    
    * Adding cluster platform detection and disabling generate script after first click.
    
    * Fix spacing and adding comment.

--- extensions/big-data-cluster/src/wizards/create-cluster/createClusterWizard.ts
+++ extensions/big-data-cluster/src/wizards/create-cluster/createClusterWizard.ts
@@ -14,13 +14,15 @@ import { WizardBase } from '../wizardBase';
 import * as nls from 'vscode-nls';
 import { Kubectl } from '../../kubectl/kubectl';
 import { SelectTargetClusterTypePage } from './pages/selectTargetClusterTypePage';
-
+import { ScriptGenerator } from '../../scripting/scripting';
 const localize = nls.loadMessageBundle();
 
 export class CreateClusterWizard extends WizardBase<CreateClusterModel, CreateClusterWizard> {
+	private scripter : ScriptGenerator;
 	constructor(context: ExtensionContext, kubectl: Kubectl) {
 		let model = new CreateClusterModel(kubectl);
 		super(model, context, localize('bdc-create.wizardTitle', 'Create a big data cluster'));
+		this.scripter = new ScriptGenerator(kubectl);
 	}
 
 	protected initialize(): void {
@@ -32,10 +34,16 @@ export class CreateClusterWizard extends WizardBase<CreateClusterModel, CreateCl
 		this.setPages([targetClusterTypePage, settingsPage, clusterProfilePage, selectTargetClusterPage, summaryPage]);
 
 		this.wizardObject.generateScriptButton.label = localize('bdc-create.generateScriptsButtonText', 'Generate Scripts');
-		this.wizardObject.generateScriptButton.hidden = true;
+		this.wizardObject.generateScriptButton.hidden = false;
 		this.wizardObject.doneButton.label = localize('bdc-create.createClusterButtonText', 'Create');
 
-		this.wizardObject.generateScriptButton.onClick(() => { });
+		this.wizardObject.generateScriptButton.onClick(async () => {
+															this.wizardObject.generateScriptButton.enabled = false;
+															this.scripter.generateDeploymentScript(this.model).then( () => {
+																this.wizardObject.generateScriptButton.enabled = true;
+																//TODO: Add error handling.
+															});
+														});
 		this.wizardObject.doneButton.onClick(() => { });
 	}
 }
