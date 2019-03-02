0236c8e7f81245e56f30301163e27bacde5e0356
Anthony Dresser <andresse@microsoft.com>
2019-03-01 17:47:28 -0800

    Data Explorer Disconnect and Error Handling (#4243)
    * adding context
    
    * apply extension changes
    
    * shimming disconnect
    
    * add data explorer context menu and add disconnect to it
    
    * clean up shim code; better handle errors
    
    * remove tpromise
    
    * simplify code

--- src/sql/workbench/common/views.ts
+++ src/sql/workbench/common/views.ts
@@ -3,7 +3,7 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ITreeViewDataProvider, ITreeItem as vsITreeItem } from 'vs/workbench/common/views';
+import { ITreeViewDataProvider, ITreeItem as vsITreeItem, IViewDescriptor, ITreeView as vsITreeView } from 'vs/workbench/common/views';
 import { IConnectionProfile } from 'azdata';
 
 export interface ITreeComponentItem extends vsITreeItem {
@@ -21,4 +21,22 @@ export interface ITreeItem extends vsITreeItem {
 	providerHandle?: string;
 	childProvider?: string;
 	payload?: IConnectionProfile; // its possible we will want this to be more generic
+}
+
+export interface ITreeView extends vsITreeView {
+
+	collapse(itemOrItems: ITreeItem | ITreeItem[]): Thenable<void>;
+
+}
+
+export type TreeViewItemHandleArg = {
+	$treeViewId: string,
+	$treeItemHandle: string,
+	$treeItem?: ITreeItem
+};
+
+export interface ICustomViewDescriptor extends IViewDescriptor {
+
+	readonly treeView: ITreeView;
+
 }
