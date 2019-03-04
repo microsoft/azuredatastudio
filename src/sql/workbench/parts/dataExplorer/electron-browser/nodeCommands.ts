01325c41d32ff2a582bf6fba489e8957c984ffc0
Anthony Dresser <andresse@microsoft.com>
2019-03-04 11:38:17 -0800

    add new Query action

--- src/sql/workbench/parts/dataExplorer/electron-browser/nodeCommands.ts
+++ src/sql/workbench/parts/dataExplorer/electron-browser/nodeCommands.ts
@@ -7,8 +7,14 @@ import { IOEShimService } from 'sql/parts/objectExplorer/common/objectExplorerVi
 import { ICustomViewDescriptor, TreeViewItemHandleArg } from 'sql/workbench/common/views';
 import { CommandsRegistry } from 'vs/platform/commands/common/commands';
 import { ViewsRegistry } from 'vs/workbench/common/views';
+import { IQueryEditorService } from 'sql/workbench/services/queryEditor/common/queryEditorService';
+import { IConnectionManagementService, IConnectableInput, IConnectionCompletionOptions, ConnectionType } from 'sql/platform/connection/common/connectionManagement';
+import { ConnectionProfile } from 'sql/platform/connection/common/connectionProfile';
+import { ICapabilitiesService } from 'sql/platform/capabilities/common/capabilitiesService';
 
-export const DISCONNECT_COMMAND_ID = 'disconnect';
+export const DISCONNECT_COMMAND_ID = 'dataExplorerDisconnect';
+export const MANAGE_COMMAND_ID = 'dataExplorerManage';
+export const NEW_QUERY_COMMAND_ID = 'dataExplorerNewQuery';
 
 CommandsRegistry.registerCommand({
 	id: DISCONNECT_COMMAND_ID,
@@ -19,6 +25,29 @@ CommandsRegistry.registerCommand({
 				const { treeView } = (<ICustomViewDescriptor>ViewsRegistry.getView(args.$treeViewId));
 				// we need to collapse it then refresh it so that the tree doesn't try and use it's cache next time the user expands the node
 				return treeView.collapse(args.$treeItem).then(() => treeView.refresh([args.$treeItem]).then(() => true));
+			});
+		}
+		return Promise.resolve(true);
+	}
+});
+
+CommandsRegistry.registerCommand({
+	id: NEW_QUERY_COMMAND_ID,
+	handler: (accessor, args: TreeViewItemHandleArg) => {
+		if (args.$treeItem) {
+			let queryEditorService = accessor.get(IQueryEditorService);
+			let connectionService = accessor.get(IConnectionManagementService);
+			let capabilitiesService = accessor.get(ICapabilitiesService);
+			return queryEditorService.newSqlEditor().then((owner: IConnectableInput) => {
+				// Connect our editor to the input connection
+				let options: IConnectionCompletionOptions = {
+					params: { connectionType: ConnectionType.editor, input: owner },
+					saveTheConnection: false,
+					showDashboard: false,
+					showConnectionDialogOnError: true,
+					showFirewallRuleOnError: true
+				};
+				return connectionService.connect(new ConnectionProfile(capabilitiesService, args.$treeItem.payload), owner.uri, options);
 			});
 		}
 		return Promise.resolve(true);
