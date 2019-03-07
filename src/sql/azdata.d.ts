0a1d6be712367227837a11e3f718eefc189d8d42
Karl Burtram <karlb@microsoft.com>
2019-03-07 10:56:37 -0800

    Initial connection API refactor

--- src/sql/azdata.d.ts
+++ src/sql/azdata.d.ts
@@ -27,12 +27,42 @@ declare module 'azdata' {
 			saveProfile: boolean;
 			azureTenantId?: string;
 
-			static createFrom(options: any[]): ConnectionProfile;
+			/**
+			 * Get connection string
+			 */
+			getConnectionString(includePassword: boolean): Thenable<string>;
+
+			/**
+			 * Get the credentials for an active connection
+			 * @param {string} connectionId The id of the connection
+			 * @returns {{ [name: string]: string}} A dictionary containing the credentials as they would be included in the connection's options dictionary
+			 */
+			getCredentials(): Thenable<{ [name: string]: string }>;
+
+			/**
+			 * create a connection profile from an options map
+			 */
+			static from(options: any[]): ConnectionProfile;
 		}
 
 		/**
 		 * Get the current connection based on the active editor or Object Explorer selection
 		*/
 		export function getCurrentConnection(): Thenable<ConnectionProfile>;
+
+		/**
+		 * Get all connections
+		*/
+		export function getConnections(): Thenable<ConnectionProfile[]>;
+
+		/**
+		 * Get all active connections
+		*/
+		export function getActiveConnections(): Thenable<ConnectionProfile[]>;
+
+		/**
+		 * Get connection profile from connectionId
+		*/
+		export function getConnectionProfile(connectionId: string): Thenable<ConnectionProfile>;
 	}
 }
