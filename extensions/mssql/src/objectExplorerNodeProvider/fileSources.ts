ee16282c50c7f020fcd76e8016ac8b508bc9288d
Maddy Koripalli <makoripa@microsoft.com>
2019-03-11 15:13:30 -0700

    Added warning message for the Users to inform the data truncation.

--- extensions/mssql/src/objectExplorerNodeProvider/fileSources.ts
+++ extensions/mssql/src/objectExplorerNodeProvider/fileSources.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 'use strict';
+import * as vscode from 'vscode';
 import * as fspath from 'path';
 import * as fs from 'fs';
 import * as meter from 'stream-meter';
@@ -181,24 +182,29 @@ export class HdfsFileSource implements IFileSource {
 	public readFile(path: string, maxBytes?: number): Promise<Buffer> {
 		return new Promise((resolve, reject) => {
 			let error: HdfsError = undefined;
-			let remoteFileStream = this.client.createReadStream(path, maxBytes ? {'length': maxBytes} : {});
+			let remoteFileStream = this.client.createReadStream(path);
 			remoteFileStream.on('error', (err) => {
 				error = <HdfsError>err;
 				reject(error);
 			});
 
+			let data: any[] = [];
 			if (maxBytes) {
 				remoteFileStream = remoteFileStream.pipe(meter(maxBytes));
 				remoteFileStream.on('error', (err) => {
 					error = <HdfsError>err;
 					if (error.message.includes('Stream exceeded specified max')) {
-						error.message = localize('maxSizeReached', 'File exceeds max size of ${0}', bytes(maxBytes));
+						// We have data > maxbytes, show we're truncating
+						let previewNote: string = '########################### NOTICE: This file has been truncated at '+ bytes(maxBytes) +' for preview ########################### \r\n';
+						data.splice(0, 0, Buffer.from(previewNote, 'utf-8'));
+						vscode.window.showWarningMessage(localize('maxSizeReached', "Preview only loads {0} data of the file and truncates the rest.", bytes(maxBytes)));
+						resolve(Buffer.concat(data));
+					} else {
+						reject(error);
 					}
-					reject(error);
 				});
 			}
 
-			let data: any[] = [];
 			remoteFileStream.on('data', (chunk) => {
 				data.push(chunk);
 			});
