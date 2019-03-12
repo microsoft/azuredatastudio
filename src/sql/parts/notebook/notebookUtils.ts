8cbdffcd6c38b9a4bb3e7a9b1a3707e5080cbb1b
REDMOND\yuronhe <yuronhe@microsoft.com>
2019-03-12 10:53:21 -0700

    Resolve PR comment

--- src/sql/parts/notebook/notebookUtils.ts
+++ src/sql/parts/notebook/notebookUtils.ts
@@ -118,7 +118,7 @@ export function tryMatchCellMagic(input: string): string {
 	return magicName;
 }
 
-export async function asyncForEach(array: any, callback: any): Promise<void> {
+export async function asyncForEach(array: any, callback: any): Promise<any> {
 	for (let index = 0; index < array.length; index++) {
 		await callback(array[index], index, array);
 	}
