6de984a4e5d8f92ea0fb13b7b6758269a898a38c
Charles Gagnon <chgagnon@microsoft.com>
2019-03-14 08:47:03 -0700

    Add .bat for running extension tests (currently only Notebook) and set up launch.json with 2 new launch configs for running & debugging extension tests.

---
+++ scripts/test-extensions.bat
@@ -0,0 +1,20 @@
+setlocal
+
+pushd %~dp0\..
+
+set VSCODEUSERDATADIR=%TMP%\adsuser-%RANDOM%-%TIME:~6,5%
+set VSCODEEXTENSIONSDIR=%TMP%\adsext-%RANDOM%-%TIME:~6,5%
+echo %VSCODEUSERDATADIR%
+echo %VSCODEEXTENSIONSDIR%
+@echo OFF
+
+call .\scripts\code.bat --extensionDevelopmentPath=%~dp0\..\extensions\notebook --extensionTestsPath=%~dp0\..\extensions\notebook\out\test --user-data-dir=%VSCODEUSERDATADIR% --extensions-dir=%VSCODEEXTENSIONSDIR% --remote-debugging-port=9222
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+rmdir /s /q %VSCODEUSERDATADIR%
+rmdir /s /q %VSCODEEXTENSIONSDIR%
+
+popd
+
+endlocal
