243710e298bbfa6a6435ed2361541f55263f4291
abist <adbist@microsoft.com>
2019-03-26 15:58:39 -0700

    added some details

--- src/sql/platform/telemetry/telemetry.contribution.ts
+++ src/sql/platform/telemetry/telemetry.contribution.ts
@@ -9,6 +9,7 @@ import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { Disposable } from 'vs/base/common/lifecycle';
 import { IStorageService, StorageScope } from 'vs/platform/storage/common/storage';
+import { isActive } from 'windows-mutex';
 
 export class SqlTelemetryContribution extends Disposable implements IWorkbenchContribution {
 
@@ -18,32 +19,89 @@ export class SqlTelemetryContribution extends Disposable implements IWorkbenchCo
 	) {
 		super();
 
-		const dailyLastUseDate = Date.parse(storageService.get('telemetry.dailyLastUseDate', StorageScope.GLOBAL, '0'));
-		const weeklyLastUseDate = Date.parse(storageService.get('telemetry.weeklyLastUseDate', StorageScope.GLOBAL, '0'));
-		const monthlyLastUseDate = Date.parse(storageService.get('telemetry.monthlyLastUseDate', StorageScope.GLOBAL, '0'));
+		const dailyLastUseDate: number = Date.parse(storageService.get('telemetry.dailyLastUseDate', StorageScope.GLOBAL, '0'));
+		const weeklyLastUseDate: number  = Date.parse(storageService.get('telemetry.weeklyLastUseDate', StorageScope.GLOBAL, '0'));
+		const monthlyLastUseDate: number  = Date.parse(storageService.get('telemetry.monthlyLastUseDate', StorageScope.GLOBAL, '0'));
+		const firstTimeUser: boolean = dailyLastUseDate === Date.parse('0');
 
-		let today = new Date().toUTCString();
+		let today: Date = new Date();
+		let todayString: string = new Date().toUTCString();
 
 		// daily user event
-		if (this.diffInDays(Date.parse(today), dailyLastUseDate) >= 1) {
+		if (this.diffInDays(Date.parse(todayString), dailyLastUseDate) >= 1) {
 			// daily first use
 			telemetryService.publicLog('telemetry.dailyFirstUse', { dailyFirstUse: true });
-			storageService.store('telemetry.dailyLastUseDate', today, StorageScope.GLOBAL);
+			storageService.store('telemetry.dailyLastUseDate', todayString, StorageScope.GLOBAL);
 		}
 
 		// weekly user event
-		if (this.diffInDays(Date.parse(today), weeklyLastUseDate) >= 7) {
+		if (this.diffInDays(Date.parse(todayString), weeklyLastUseDate) >= 7) {
 			// weekly first use
 			telemetryService.publicLog('telemetry.weeklyFirstUse', { weeklyFirstUse: true });
-			storageService.store('telemetry.weeklyLastUseDate', today, StorageScope.GLOBAL);
+			storageService.store('telemetry.weeklyLastUseDate', todayString, StorageScope.GLOBAL);
 		}
 
 		// monthly user events
-		if (this.diffInDays(Date.parse(today), monthlyLastUseDate) >= 30) {
+		if (this.diffInDays(Date.parse(todayString), monthlyLastUseDate) >= 30) {
 			telemetryService.publicLog('telemetry.monthlyUse', { monthlyFirstUse: true });
-			storageService.store('telemetry.monthlyLastUseDate', today, StorageScope.GLOBAL);
+			storageService.store('telemetry.monthlyLastUseDate', todayString, StorageScope.GLOBAL);
 		}
 
+		/* send monthly uses at the end of month */
+		const monthlyUseCount: number = storageService.getNumber('telemetry.monthlyUseCount', StorageScope.GLOBAL, 0);
+		let lastMonthDateString: string = storageService.get('telemetry.monthlyLastUseDate', StorageScope.GLOBAL, '0');
+		let lastMonthDate: Date = new Date(lastMonthDateString);
+		if (today.getFullYear() === lastMonthDate.getFullYear()) {
+			if (today.getMonth() === lastMonthDate.getMonth()) {
+				// if it's the same month
+				storageService.store('telemetry.monthlyUseCount', monthlyUseCount+1, StorageScope.GLOBAL);
+			} else {
+				// otherwise the month changed, so send the monthly count for last month and reset the count for this month
+				telemetryService.publicLog('telemetry.monthlyUseCount',
+					{ monthlyUseCount: monthlyUseCount, month: lastMonthDate.getMonth().toString(), year: lastMonthDate.getFullYear().toString() });
+
+				const wasActiveLastMonth: boolean = storageService.getBoolean('telemetry.wasActiveLastMonth', StorageScope.GLOBAL, false);
+				const isActiveThisMonth: boolean = storageService.getNumber('telemetry.monthlyUseCount', StorageScope.GLOBAL, 0) >= 2;
+				const isChurnedThisMonth: boolean = wasActiveLastMonth && !isActiveThisMonth;
+				const isContinuing: boolean = wasActiveLastMonth && isActiveThisMonth;
+				const wasChurnedLastMonth: boolean = storageService.getBoolean('telemetry.wasChurnedLastMonth', StorageScope.GLOBAL, false);
+				const isReturning: boolean = wasChurnedLastMonth && isActiveThisMonth;
+
+				if (firstTimeUser) {
+					// new user
+					telemetryService.publicLog('telemetry.userGrowthType', {
+						 userType: 'New', month: lastMonthDate.getMonth().toString(), year: lastMonthDate.getFullYear().toString()});
+				}
+
+				// if isChurnedThisMonth, set wasChurnedLastMonth
+				if (isChurnedThisMonth) {
+					// churned user
+					telemetryService.publicLog('telemetry.userGrowthType',
+						{ userType: 'Churned', month: lastMonthDate.getMonth().toString(), year: lastMonthDate.getFullYear().toString()});
+					storageService.store('telemetry.wasChurnedLastMonth', true, StorageScope.GLOBAL);
+				}
+
+				if (isContinuing) {
+					// continuing user
+					telemetryService.publicLog('telemetry.userGrowthType',
+						{ userType: 'Continuing', month: lastMonthDate.getMonth().toString(), year: lastMonthDate.getFullYear().toString()});
+				}
+
+				if (isReturning) {
+					// returning user
+					telemetryService.publicLog('telemetry.userGrowthType',
+						{ userType: 'Returning', month: lastMonthDate.getMonth().toString(), year: lastMonthDate.getFullYear().toString()});
+				}
+
+				// if isActiveThisMonth, set wasActiveUserLastMonth
+				if (isActiveThisMonth) {
+					storageService.store('telemetry.wasActiveLastMonth', true, StorageScope.GLOBAL);
+				}
+
+				// reset the monthly count for the new month
+				storageService.store('telemetry.monthlyUseCount', 0, StorageScope.GLOBAL);
+			}
+		}
 	}
 
 	private diffInDays(nowDate: number, lastUseDate: number): number {
