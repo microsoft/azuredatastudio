b3b24e035499e26857f4d5918ca11bbe3d377958
REDMOND\yuronhe <yuronhe@microsoft.com>
2019-03-27 19:49:31 -0700

    Resolve PR comments

--- extensions/integration-tests/src/notebook.util.ts
+++ extensions/integration-tests/src/notebook.util.ts
@@ -9,6 +9,8 @@ import 'mocha';
 import * as azdata from 'azdata';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as os from 'os';
+const path = require('path');
 
 export class CellTypes {
 	public static readonly Code = 'code';
@@ -72,9 +74,8 @@ export const pythonKernelMetadata = {
 };
 
 export function writeNotebookToFile(pythonNotebook: azdata.nb.INotebookContents): vscode.Uri {
-	var path = require('path');
 	let notebookContentString = JSON.stringify(pythonNotebook);
-	let localFile = path.join(process.cwd(), 'notebook' + Math.floor(Math.random() * 101) + '.ipynb');
+	let localFile = path.join(os.tmpdir(), 'notebook' + Math.floor(Math.random() * 101) + '.ipynb');
 	console.log(`Local file is created: '${localFile}'`);
 	fs.writeFileSync(localFile, notebookContentString);
 	let uri = vscode.Uri.file(localFile);
