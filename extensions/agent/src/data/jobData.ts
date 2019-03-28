19d7553fc0bf89ca8890d7201d2f51dde5987c0f
abist <adbist@microsoft.com>
2019-03-28 16:52:03 -0700

    added errors for failures

--- extensions/agent/src/data/jobData.ts
+++ extensions/agent/src/data/jobData.ts
@@ -129,8 +129,13 @@ export class JobData implements IAgentDialogData {
 			? await this._agentService.createJob(this.ownerUri,  jobInfo)
 			: await this._agentService.updateJob(this.ownerUri, this.originalName, jobInfo);
 		if (!result || !result.success) {
-			vscode.window.showErrorMessage(
-				localize('jobData.saveErrorMessage', "Job update failed '{0}'", result.errorMessage ? result.errorMessage : 'Unknown'));
+			if (this.dialogMode === AgentDialogMode.EDIT) {
+				vscode.window.showErrorMessage(
+					localize('jobData.saveErrorMessage', "Job update failed '{0}'", result.errorMessage ? result.errorMessage : 'Unknown'));
+			} else {
+				vscode.window.showErrorMessage(
+					localize('jobData.newJobErrorMessage', "Job creation failed '{0}'", result.errorMessage ? result.errorMessage : 'Unknown'));
+			}
 		} else {
 			if (this.dialogMode === AgentDialogMode.EDIT) {
 				vscode.window.showInformationMessage(
