4ebde0014a34f8f494caa9032caf8cad098afc0c
Anthony Dresser <andresse@microsoft.com>
2019-04-03 15:53:04 -0700

    make null undefined

--- src/sql/base/browser/ui/scrollableSplitview/heightMap.ts
+++ src/sql/base/browser/ui/scrollableSplitview/heightMap.ts
@@ -31,8 +31,8 @@ export class HeightMap {
 		return !last ? 0 : last.top + last.height;
 	}
 
-	public onInsertItems(iterator: INextIterator<IViewItem>, afterItemId: string | null = null): number | undefined {
-		let viewItem: IViewItem | null = null;
+	public onInsertItems(iterator: INextIterator<IViewItem>, afterItemId: string | undefined = undefined): number | undefined {
+		let viewItem: IViewItem | undefined = undefined;
 		let i: number, j: number;
 		let totalSize: number;
 		let sizeDiff = 0;
@@ -89,9 +89,9 @@ export class HeightMap {
 
 	// Contiguous items
 	public onRemoveItems(iterator: INextIterator<string>): void {
-		let itemId: string | null = null;
+		let itemId: string | undefined = undefined;
 		let viewItem: IViewItem;
-		let startIndex: number | null = null;
+		let startIndex: number | undefined = undefined;
 		let i = 0;
 		let sizeDiff = 0;
 
@@ -108,12 +108,12 @@ export class HeightMap {
 			delete this.indexes[itemId];
 			this.onRemoveItem(viewItem);
 
-			if (startIndex === null) {
+			if (startIndex === undefined) {
 				startIndex = i;
 			}
 		}
 
-		if (sizeDiff === 0 || startIndex === null) {
+		if (sizeDiff === 0 || startIndex === undefined) {
 			return;
 		}
 
