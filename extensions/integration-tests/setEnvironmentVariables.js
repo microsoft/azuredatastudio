9f4ae96e15d94d70683e68554e35002c84489719
Alan Ren <alanren@microsoft.com>
2019-04-08 14:59:54 -0700

    fix some errors

--- extensions/integration-tests/setEnvironmentVariables.js
+++ extensions/integration-tests/setEnvironmentVariables.js
@@ -9,20 +9,15 @@ const fs = require('fs');
  */
 const LAUNCH_TERMINAL_WINDOWS = 'start cmd.exe';
 const LAUNCH_TERMINAL_MAC = 'open -a Terminal -n';
-const LAUNCH_GIT_BASH_WINDOWS = '"C:\\Program Files\\Git\\git-bash.exe"';
+const LAUNCH_GIT_BASH_WINDOWS = 'C:\\Program Files\\Git\\git-bash.exe';
 const LAUNCH_VSCODE = 'code';
 
-let LAUNCH_OPTION = LAUNCH_VSCODE;
+let LAUNCH_OPTION;
 
-const VSCODE_WARNING_MESSAGE = 'Trying to lauch vscode, make sure you have it set properly in the PATH environment variable';
 // Parse the command-line argument
 if (process.argv.length === 3 && process.argv[2]) {
 	const argValue = process.argv[2];
 	switch (argValue.toUpperCase()) {
-		case 'CODE':
-			console.warn(VSCODE_WARNING_MESSAGE);
-			LAUNCH_OPTION = LAUNCH_VSCODE;
-			break;
 		case 'TERMINAL':
 			if (os.platform() === 'win32') {
 				LAUNCH_OPTION = LAUNCH_TERMINAL_WINDOWS;
@@ -30,19 +25,29 @@ if (process.argv.length === 3 && process.argv[2]) {
 				LAUNCH_OPTION = LAUNCH_TERMINAL_MAC;
 			} else {
 				console.warn(`Launch terminal option is not implemented for your os: ${os.platform()}, vscode will be launched`);
-				console.warn(VSCODE_WARNING_MESSAGE);
 			}
 			break;
 		case 'BASHWIN':
 			if (!fs.existsSync(LAUNCH_GIT_BASH_WINDOWS)) {
 				throw `Not able to find git-bash.exe in its default location: ${LAUNCH_GIT_BASH_WINDOWS}, please update the variable LAUNCH_GIT_BASH_WINDOWS accordingly.`;
 			}
-			LAUNCH_OPTION = LAUNCH_GIT_BASH_WINDOWS;
+			let bashPath = LAUNCH_GIT_BASH_WINDOWS;
+			// quote the path with double quote if it contains spaces
+			if (bashPath.indexOf(' ') != -1) {
+				bashPath = '"' + bashPath + '"';
+			}
+
+			LAUNCH_OPTION = bashPath;
 			break;
 		default:
 			break;
 	}
 }
+
+if (!LAUNCH_OPTION) {
+	LAUNCH_OPTION = LAUNCH_VSCODE;
+}
+
 
 /**
  * Below are the environment variable values that are not saved in AKV and might vary by machine
@@ -137,6 +142,9 @@ msrestAzure.interactiveLogin().then((credentials) => {
 		() => {
 			console.log('Done reading values from Azure KeyVault!');
 			console.log(`Launching new window: ${LAUNCH_OPTION}...`);
+			if (LAUNCH_OPTION === LAUNCH_VSCODE) {
+				console.warn('Trying to lauch vscode, make sure you have it set properly in the PATH environment variable');
+			}
 			child_process.execSync(LAUNCH_OPTION);
 			console.log('New window for running test has been opened.');
 		}
