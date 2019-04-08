e06f03ccfaf85e99d8176349ba3c5279eb016e11
abist <adbist@microsoft.com>
2019-04-08 09:44:29 -0700

    added couple tests

--- extensions/agent/src/test/agent.test.ts
+++ extensions/agent/src/test/agent.test.ts
@@ -2,20 +2,30 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+'use strict';
 
+import * as should from 'should';
+import * as TypeMoq from 'typemoq';
 import * as assert from 'assert';
 import 'mocha';
 import * as vscode from 'vscode';
 import * as azdata from 'azdata';
 import { JobData } from '../data/jobData';
-import { TestAgentService } from './testAgentService';
+import { AgentUtils } from '../agentUtils';
 
 const testOwnerUri = 'agent://testuri';
+let mockAgentService: TypeMoq.Mock<azdata.AgentServicesProvider>;
+let mockAgentUtils: TypeMoq.IMock.ofInstance<AgentUtils>;
 
-suite('Agent extension', () => {
-	test('Create Job Data', async () => {
-		let testAgentService = new TestAgentService();
-		let data = new JobData(testOwnerUri, undefined, testAgentService);
+describe('Agent extension', function (): void {
+	beforeEach(() => {
+		mockAgentService = TypeMoq.Mock.ofType<azdata.AgentServicesProvider>(TypeMoq.It.isAny());
+		mockAgentUtils = TypeMoq.Mock.ofType<AgentUtils>(TypeMoq.It.isAny());
+		mockAgentUtils.setup((o) => o.getAgentService().returns(() => mockAgentService));
+	});
+
+	it('Create Job Data', async () => {
+		let data = new JobData(testOwnerUri, undefined, null);
 		data.save();
 	});
 });
