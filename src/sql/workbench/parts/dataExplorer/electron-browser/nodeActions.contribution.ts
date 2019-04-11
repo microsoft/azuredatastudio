e39c5fb2257cdab3f639e847e844a73365d6240c
Charles Gagnon <chgagnon@microsoft.com>
2019-04-11 13:55:58 -0700

    Switch to regex instead of separate menu items

--- src/sql/workbench/parts/dataExplorer/electron-browser/nodeActions.contribution.ts
+++ src/sql/workbench/parts/dataExplorer/electron-browser/nodeActions.contribution.ts
@@ -6,7 +6,7 @@
 import { localize } from 'vs/nls';
 import { MenuId, MenuRegistry } from 'vs/platform/actions/common/actions';
 import { DISCONNECT_COMMAND_ID, MANAGE_COMMAND_ID, NEW_QUERY_COMMAND_ID, REFRESH_COMMAND_ID } from './nodeCommands';
-import { ContextKeyDefinedExpr, ContextKeyExpr, ContextKeyEqualsExpr } from 'vs/platform/contextkey/common/contextkey';
+import { ContextKeyDefinedExpr, ContextKeyExpr, ContextKeyEqualsExpr, ContextKeyRegexExpr } from 'vs/platform/contextkey/common/contextkey';
 
 MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
 	group: 'connection',
@@ -32,37 +32,11 @@ MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
 		new ContextKeyDefinedExpr('isConnectable'),
 		new ContextKeyEqualsExpr('isConnectable', true),
 		new ContextKeyDefinedExpr('viewItem'),
-		new ContextKeyEqualsExpr('viewItem', 'Database'))
+		new ContextKeyRegexExpr('viewItem', /Database|azure.resource.itemType.database|azure.resource.itemType.databaseServer/))
 });
 
-// For database nodes under the SQL Databases folder
-MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
-	group: 'connection',
-	order: 2,
-	command: {
-		id: NEW_QUERY_COMMAND_ID,
-		title: localize('newQuery', 'New Query')
-	},
-	when: ContextKeyExpr.and(
-		new ContextKeyDefinedExpr('isConnectable'),
-		new ContextKeyEqualsExpr('isConnectable', true),
-		new ContextKeyDefinedExpr('viewItem'),
-		new ContextKeyEqualsExpr('viewItem', 'azure.resource.itemType.database'))
-});
-
-MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
-	group: 'connection',
-	command: {
-		id: NEW_QUERY_COMMAND_ID,
-		title: localize('newQuery', 'New Query')
-	},
-	when: ContextKeyExpr.and(
-		new ContextKeyDefinedExpr('isConnectable'),
-		new ContextKeyEqualsExpr('isConnectable', true),
-		new ContextKeyDefinedExpr('viewItem'),
-		new ContextKeyEqualsExpr('viewItem', 'azure.resource.itemType.databaseServer'))
-});
-
+// Note that we don't show this for Databases under Server nodes (viewItem == Database) because
+// of an issue there where the connection always being master instead of the actual DB
 MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
 	group: 'connection',
 	order: 1,
@@ -74,24 +48,7 @@ MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
 		new ContextKeyDefinedExpr('isConnectable'),
 		new ContextKeyEqualsExpr('isConnectable', true),
 		new ContextKeyDefinedExpr('viewItem'),
-		new ContextKeyEqualsExpr('viewItem', 'azure.resource.itemType.databaseServer'))
-});
-
-// For database nodes under the SQL Databases folder
-// Note that we don't show this for Databases under Server nodes because of an issue there
-// where the connection always being master instead of the actual DB
-MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
-	group: 'connection',
-	order: 1,
-	command: {
-		id: MANAGE_COMMAND_ID,
-		title: localize('manage', 'Manage')
-	},
-	when: ContextKeyExpr.and(
-		new ContextKeyDefinedExpr('isConnectable'),
-		new ContextKeyEqualsExpr('isConnectable', true),
-		new ContextKeyDefinedExpr('viewItem'),
-		new ContextKeyEqualsExpr('viewItem', 'azure.resource.itemType.database'))
+		new ContextKeyRegexExpr('viewItem', /azure.resource.itemType.databaseServer|azure.resource.itemType.database/))
 });
 
 MenuRegistry.appendMenuItem(MenuId.DataExplorerContext, {
