f8e88d53a2a782c0319227b6beb96186924067b6
Udeesha Gautam <udgautam@microsoft.com>
2019-04-22 13:47:51 -0700

    Adding some CR comments

--- extensions/integration-tests/src/schemaCompare.test.ts
+++ extensions/integration-tests/src/schemaCompare.test.ts
@@ -8,6 +8,7 @@
 import 'mocha';
 import * as azdata from 'azdata';
 import * as utils from './utils';
+const path = require('path');
 import { context } from './testContext';
 import assert = require('assert');
 import { getStandaloneServer } from './testConfig';
@@ -22,16 +23,16 @@ if (context.RunTest) {
 		test('Schema compare dacpac comparision and script generation', async function () {
 			let service = await azdata.dataprotocol.getProvider<azdata.SchemaCompareServicesProvider>('MSSQL', azdata.DataProviderType.SchemaCompareServicesProvider);
 			assert(service, 'Schema Compare Service Provider is not available');
-			
+
 			let source: azdata.SchemaCompareEndpointInfo = {
 				endpointType: azdata.SchemaCompareEndpointType.dacpac,
-				packageFilePath: utils.combinePath(__dirname, 'testData/Database1.dacpac'),
+				packageFilePath: path.join(__dirname, 'testData/Database1.dacpac'),
 				databaseName: 'database1',
 				ownerUri: '',
 			};
 			let target: azdata.SchemaCompareEndpointInfo = {
 				endpointType: azdata.SchemaCompareEndpointType.dacpac,
-				packageFilePath: utils.combinePath(__dirname, 'testData/Database2.dacpac'),
+				packageFilePath: path.join(__dirname, 'testData/Database2.dacpac'),
 				databaseName: 'database2',
 				ownerUri: '',
 			};
@@ -42,7 +43,7 @@ if (context.RunTest) {
 			assert(schemaCompareResult.success === true, `Expected: success in schema compare, Actual: Failre`);
 			assert(schemaCompareResult.differences.length === 4, `Expected: 4 differences. Actual differences: "${schemaCompareResult.differences.length}"`);
 
-			let status = await service.schemaCompareGenerateScript(schemaCompareResult.operationId, 'testDb', utils.combinePath(__dirname, 'testScript_dacpac.sql'), azdata.TaskExecutionMode.execute);
+			let status = await service.schemaCompareGenerateScript(schemaCompareResult.operationId, 'testDb', path.join(__dirname, 'testScript_dacpac.sql'), azdata.TaskExecutionMode.execute);
 			assert(status.success === true, `Expected: success true Actual: "${status.success}" Error Message: "${status.errorMessage}`);
 		});
 		test('Schema compare database comparision and script generation', async function () {
@@ -52,9 +53,10 @@ if (context.RunTest) {
 
 			let dacfxService = await azdata.dataprotocol.getProvider<azdata.DacFxServicesProvider>('MSSQL', azdata.DataProviderType.DacFxServicesProvider);
 			assert(dacfxService, 'DacFx Service Provider is not available');
-			
-			let result1 = await dacfxService.deployDacpac(utils.combinePath(__dirname, 'testData/Database1.dacpac'),'database1', true, ownerUri, azdata.TaskExecutionMode.execute);
-			let result2 =await dacfxService.deployDacpac(utils.combinePath(__dirname, 'testData/Database2.dacpac'),'database2', true, ownerUri, azdata.TaskExecutionMode.execute);
+
+			let now = new Date();
+			let result1 = await dacfxService.deployDacpac(path.join(__dirname, 'testData/Database1.dacpac'), 'database1' + now.getTime().toString(), true, ownerUri, azdata.TaskExecutionMode.execute);
+			let result2 = await dacfxService.deployDacpac(path.join(__dirname, 'testData/Database2.dacpac'), 'database2' + now.getTime().toString(), true, ownerUri, azdata.TaskExecutionMode.execute);
 
 			assert(result1.success === true, 'Deploy database 1 should succeed');
 			assert(result2.success === true, 'Deploy database 2 should succeed');
@@ -81,7 +83,7 @@ if (context.RunTest) {
 			assert(schemaCompareResult.success === true, `Expected: success in schema compare, Actual: Failre`);
 			assert(schemaCompareResult.differences.length === 4, `Expected: 4 differences. Actual differences: "${schemaCompareResult.differences.length}"`);
 
-			let status = await service.schemaCompareGenerateScript(schemaCompareResult.operationId, 'testDb', utils.combinePath(__dirname, 'testScript_database.sql'), azdata.TaskExecutionMode.execute);
+			let status = await service.schemaCompareGenerateScript(schemaCompareResult.operationId, 'testDb', path.join(__dirname, 'testScript_database.sql'), azdata.TaskExecutionMode.execute);
 			assert(status.success === true, `Expected: success true Actual: "${status.success} Error Message: "${status.errorMessage}"`);
 		});
 	});
