fbdb4c1ba95b31be2a937153d3dddd25cb01c572
abist <adbist@microsoft.com>
2019-04-23 10:17:44 -0700

    added more tests

--- src/sqltest/stubs/jobsManagementViewStub.ts
+++ src/sqltest/stubs/jobsManagementViewStub.ts
@@ -3,15 +3,18 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/***
+/**
  * Class to test Job Management Views
  */
 export class TestJobManagementView {
-	refreshJobs() {
-		return undefined;
-	}
 
-	openCreateJobDialog() {
-		return undefined;
-	}
+	refreshJobs() { return undefined; }
+
+	openCreateJobDialog() { return undefined; }
+
+	openCreateAlertDialog() { return undefined; }
+
+	openCreateOperatorDialog() { return undefined; }
+
+	openCreateProxyDialog() { return undefined; }
 }
