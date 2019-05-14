be7c615b45e6fa3b14ab0c142ab5818f17401171
Alan Ren <alanren@microsoft.com>
2019-05-14 00:33:43 -0700

    resource types

--- extensions/resource-deployment/src/interfaces.ts
+++ extensions/resource-deployment/src/interfaces.ts
@@ -3,3 +3,27 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 'use strict';
+
+export interface ResourceType {
+	name: string;
+	displayName: string;
+	options: ResourceTypeOption[];
+	providers: DeploymentProvider[];
+}
+
+export interface ResourceTypeOption {
+	name: string;
+	displayName: string;
+	values: ResourceTypeOptionValue[];
+}
+
+export interface ResourceTypeOptionValue {
+	name: string;
+	displayName: string;
+}
+
+export interface DeploymentProvider {
+	notebook: string;
+	requiredTools: string[];
+	when: string;
+}
--- extensions/resource-deployment/src/main.ts
+++ extensions/resource-deployment/src/main.ts
@@ -6,8 +6,18 @@
 
 import vscode = require('vscode');
 import { ResourceDeploymentDialog } from './ui/resourceDeploymentDialog';
+import { ResourceTypeParser } from './resourceTypeParser';
+import { ResourceTypeValidator } from './resourceTypeValidator';
 
 export function activate(context: vscode.ExtensionContext) {
+	const resourceTypes = ResourceTypeParser.getResourceTypes();
+	const errorMessages = ResourceTypeValidator.validate(resourceTypes);
+	if (errorMessages.length !== 0) {
+		console.error('Error detected in the supported resource type configuration.');
+		errorMessages.forEach(message => console.error(message));
+		return;
+	}
+
 	vscode.commands.registerCommand('azdata.resource.sql-image.deploy', () => {
 		let dialog = new ResourceDeploymentDialog();
 		dialog.open();
