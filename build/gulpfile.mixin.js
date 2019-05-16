1001084c0e05094a0505ba15119c3d70ee853b86
Karl Burtram <karlb@microsoft.com>
2019-05-15 17:30:32 -0700

    Update mixin file

--- build/gulpfile.mixin.js
+++ build/gulpfile.mixin.js
@@ -9,6 +9,7 @@ const gulp = require('gulp');
 
 // {{SQL CARBON EDIT}}
 const jeditor = require('gulp-json-editor');
+const product = require('../product.json');
 
 gulp.task('mixin', function () {
 	// {{SQL CARBON EDIT}}
@@ -27,10 +28,27 @@ gulp.task('mixin', function () {
 
 	// {{SQL CARBON EDIT}}
 	let serviceUrl = 'https://sqlopsextensions.blob.core.windows.net/marketplace/v1/extensionsGallery.json';
+	let nameShort = product.nameShort;
+	let nameLong = product.nameLong;
+	let applicationName = product.applicationName;
+	let dataFolderName = product.dataFolderName;
+	let win32MutexName = product.win32MutexName;
 	if (quality === 'insider') {
+		let suffix =  '-' + quality;
 		serviceUrl = `https://sqlopsextensions.blob.core.windows.net/marketplace/v1/extensionsGallery-${quality}.json`;
+		nameShort += suffix;
+		nameLong += suffix;
+		applicationName += suffix;
+		dataFolderName += suffix;
+		win32MutexName += suffix;
 	}
+
 	let newValues = {
+		"nameShort": nameShort,
+		"nameLong": nameLong,
+		"applicationName": applicationName,
+		"dataFolderName": dataFolderName,
+		"win32MutexName": win32MutexName,
 		"updateUrl": updateUrl,
 		"quality": quality,
 		"extensionsGallery": {
