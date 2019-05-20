f134e9e0770140aa5939936b9f48f9d524a9a64d
Alan Ren <alanren@microsoft.com>
2019-05-20 12:59:26 -0700

    resource type tests

---
+++ extensions/resource-deployment/src/test/resourceTypeService.test.ts
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import 'mocha';
+import * as TypeMoq from 'typemoq';
+import assert = require('assert');
+import { EOL } from 'os';
+import { ResourceTypeService } from '../services/resourceTypeService';
+import { IPlatformService } from '../services/platformService';
+import { ToolsService } from '../services/toolsService';
+
+suite('Resource Type Service Tests', function (): void {
+
+	test('test resource types', () => {
+		const mockPlatformService = TypeMoq.Mock.ofType<IPlatformService>();
+		const toolsService = new ToolsService();
+		const resourceTypeService = new ResourceTypeService(mockPlatformService.object, toolsService);
+		// index 0: platform name, index 1: number of expected resource types
+		const platforms: any[][] = [['win32', 2], ['darwin', 2], ['linux', 2]];
+		const totalResourceTypeCount = 2;
+		platforms.forEach(platformInfo => {
+			mockPlatformService.reset();
+			mockPlatformService.setup(service => service.platform()).returns(() => platformInfo[0]);
+			mockPlatformService.setup(service => service.showErrorMessage(TypeMoq.It.isAnyString()));
+			const resourceTypes = resourceTypeService.getResourceTypes(true);
+			assert.equal(resourceTypes.length, platformInfo[1], `number of resource types for platform:${platformInfo[0]} does not meet expected value.`);
+		});
+
+		const allResourceTypes = resourceTypeService.getResourceTypes(false);
+		assert.equal(allResourceTypes.length, totalResourceTypeCount, `number of resource types does not meet expected value.`);
+
+		const validationErrors = resourceTypeService.validateResourceTypes(allResourceTypes);
+		assert(validationErrors.length === 0, `Validation errors detected in the package.json: ${validationErrors.join(EOL)}.`);
+	});
+});
