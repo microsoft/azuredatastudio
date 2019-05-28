da2fed059e80d7009069e1e92034596430f37633
YurongHe <yuronhe@microsoft.com>
2019-05-28 10:40:42 -0700

    Undo our change, vscode fixed the issue

--- src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
+++ src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
@@ -288,8 +288,6 @@ export class SelectBoxList implements ISelectBoxDelegate, IListVirtualDelegate<I
 		}
 
 		this.selectElement.selectedIndex = this.selected;
-		// {{SQL CARBON EDIT}}
-		this.selectElement.title = this.options[this.selected] && this.options[this.selected].text ? this.options[this.selected].text : '';
 	}
 
 	public setAriaLabel(label: string): void {
