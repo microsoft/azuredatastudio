9cae7a0a491f6b0dc29ec52548fcb70f215447f4
Arvind Ranasaria <arvran@microsoft.com>
2019-05-29 11:18:20 -0700

    First version of Stress (#5499)
    * First version of Stress - moving over from feat/Stress1 branch
    
    * a working version - still issues with stresssified notebook tests
    
    * update notebooks to use new message event (#5395)
    
    * Latest changes for notebook tests
    
    * Stressify objectExplorer tests
    
    * formatting changes
    
    * removing the tracing added previously and ability to set tsc verbose option in tsconfig.json
    
    * addressing review feedback
    
    * addressing review feedback
    
    * implementing runtime parameter for Stress
    
    * addresing review feedback and moved out stress modules to its own project outside of azuredata source tree
    
    * referencing adstest from the github location
    
    * incorporating review feedback
    
    * Review feedbak
    
    * removing uncommong entries added to .gitignore
    
    * removing unrelated change
    
    * replacing debug/trace statements with console.info or cosole.warn statments in integration-tests\main.tx

--- extensions/integration-tests/src/index.ts
+++ extensions/integration-tests/src/index.ts
@@ -4,24 +4,30 @@
  *--------------------------------------------------------------------------------------------*/
 import * as vscode from 'vscode';
 import { context } from './testContext';
+import { getSuiteType, SuiteType } from 'adstest';
 
 const path = require('path');
 const testRunner = require('vscode/lib/testrunner');
 
-const suite = 'Integration Tests';
+const suite = getSuiteType();
 
 const options: any = {
 	ui: 'tdd',
 	useColors: true,
-	timeout: 600000
+	timeout: 600000 	// 600 seconds
 };
 
+if (suite === SuiteType.Stress) {
+	options.timeout = 7200000;	// 2 hours
+}
+
 if (process.env.BUILD_ARTIFACTSTAGINGDIRECTORY) {
+	console.log(`environment variable BUILD_ARTIFACTSTAGINGDIRECTORY is set to ${process.env.BUILD_ARTIFACTSTAGINGDIRECTORY} so configuring multiple reporters for test results.\n For this to work the ${process.env.BUILD_ARTIFACTSTAGINGDIRECTORY} must be fully qualified directory and must exist`);
 	options.reporter = 'mocha-multi-reporters';
 	options.reporterOptions = {
 		reporterEnabled: 'spec, mocha-junit-reporter',
 		mochaJunitReporterReporterOptions: {
-			testsuitesTitle: `${suite} ${process.platform}`,
+			testsuitesTitle: `${suite} Tests ${process.platform}`,
 			mochaFile: path.join(process.env.BUILD_ARTIFACTSTAGINGDIRECTORY, `test-results/${process.platform}-${suite.toLowerCase().replace(/[^\w]/g, '-')}-results.xml`)
 		}
 	};
