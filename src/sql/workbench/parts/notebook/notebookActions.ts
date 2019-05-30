a026b682c4fe284c96bf5d5d7519c7692a189188
Yurong He <linda0306_hyr@hotmail.com>
2019-05-30 15:19:06 -0700

    Fixed #4485 add max-width for attachTo and tooltip (#5493)
    * Added tooltip of selectBox and set max width of attach to
    
    * Undo our change, vscode fixed the issue

--- src/sql/workbench/parts/notebook/notebookActions.ts
+++ src/sql/workbench/parts/notebook/notebookActions.ts
@@ -409,14 +409,13 @@ export class AttachToDropdown extends SelectBox {
 			else {
 				if (connections.length === 1 && connections[0] === msgAddNewConnection) {
 					connections.unshift(msgSelectConnection);
-					this.selectWithOptionName(msgSelectConnection);
 				}
 				else {
 					if (!connections.includes(msgAddNewConnection)) {
 						connections.push(msgAddNewConnection);
 					}
 				}
-				this.setOptions(connections);
+				this.setOptions(connections, 0);
 			}
 		}
 	}
@@ -426,11 +425,11 @@ export class AttachToDropdown extends SelectBox {
 			if (!connections.includes(msgSelectConnection)) {
 				connections.unshift(msgSelectConnection);
 			}
-			this.selectWithOptionName(msgSelectConnection);
+
 			if (!connections.includes(msgAddNewConnection)) {
 				connections.push(msgAddNewConnection);
 			}
-			this.setOptions(connections);
+			this.setOptions(connections, 0);
 		}
 		return connections;
 	}
