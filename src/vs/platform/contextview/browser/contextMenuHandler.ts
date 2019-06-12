90cae87393d962b3b373d3c8029cb5e887c3890a
Gene Lee <gelee@microsoft.com>
2019-06-11 19:55:33 -0700

    Resolved PR comments

--- src/vs/platform/contextview/browser/contextMenuHandler.ts
+++ src/vs/platform/contextview/browser/contextMenuHandler.ts
@@ -76,7 +76,7 @@ export class ContextMenuHandler {
 					actionViewItemProvider: delegate.getActionViewItem,
 					context: delegate.getActionsContext ? delegate.getActionsContext() : null,
 					actionRunner,
-					getKeyBinding: delegate.getKeyBinding
+					getKeyBinding: delegate.getKeyBinding ? delegate.getKeyBinding : action => this.keybindingService.lookupKeybinding(action.id)
 				});
 
 				menuDisposables.push(attachMenuStyler(menu, this.themeService));
