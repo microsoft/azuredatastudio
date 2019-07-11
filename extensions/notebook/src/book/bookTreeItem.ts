27ecd16fb4198731d63cd3216b51aa683a87d713
Lucy Zhang <lucyzhang@Lucys-MacBook-Pro.local>
2019-07-11 13:53:10 -0700

    added interface and enum

--- extensions/notebook/src/book/bookTreeItem.ts
+++ extensions/notebook/src/book/bookTreeItem.ts
@@ -9,87 +9,113 @@ import * as fs from 'fs';
 import * as nls from 'vscode-nls';
 const localize = nls.loadMessageBundle();
 
+export enum BookTreeItemType {
+	Book = 'Book',
+	Notebook = 'Notebook',
+	Markdown = 'Markdown',
+	ExternalLink = 'ExternalLink'
+}
+
+export interface BookTreeItemFormat {
+	title: string;
+	root: string;
+	tableOfContents: any[];
+	page: any;
+	type: BookTreeItemType;
+}
+
 export class BookTreeItem extends vscode.TreeItem {
-	public sections: any[];
-	public uri: string;
-	public previousUri: string;
-	public nextUri: string;
+	private _sections: any[];
+	private _uri: string;
+	private _previousUri: string;
+	private _nextUri: string;
 	public command: vscode.Command;
 
-	constructor(
-		public title: string,
-		public root: string,
-		public tableOfContents: any[],
-		public page: any,
-		public pageType: string,
-	) {
-		super(title, vscode.TreeItemCollapsibleState.Collapsed);
-		if (pageType === 'book') {
+	constructor(public book: BookTreeItemFormat) {
+		super(book.title, vscode.TreeItemCollapsibleState.Collapsed);
+
+		if (book.type === BookTreeItemType.Book) {
 			this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
-			this.sections = page;
+			this._sections = book.page;
 		} else {
 			this.setPageVariables();
 			this.setCommand();
 		}
 	}
 
-	contextValue = this.pageType;
-
 	private setPageVariables() {
-		this.collapsibleState = (this.page.sections || this.page.subsections) && this.page.expand_sections ?
+		this.collapsibleState = (this.book.page.sections || this.book.page.subsections) && this.book.page.expand_sections ?
 			vscode.TreeItemCollapsibleState.Expanded :
-			this.page.sections || this.page.subsections ?
+			this.book.page.sections || this.book.page.subsections ?
 				vscode.TreeItemCollapsibleState.Collapsed :
 				vscode.TreeItemCollapsibleState.None;
-		this.sections = this.page.sections || this.page.subsections;
-		this.uri = this.page.url;
+		this._sections = this.book.page.sections || this.book.page.subsections;
+		this._uri = this.book.page.url;
 
-		let index = (this.tableOfContents.indexOf(this.page));
+		let index = (this.book.tableOfContents.indexOf(this.book.page));
 		this.setPreviousUri(index);
 		this.setNextUri(index);
 	}
 
 	private setCommand() {
-		if (this.pageType === 'notebook') {
-			let pathToNotebook = path.join(this.root, 'content', this.uri.concat('.ipynb'));
+		if (this.book.type === BookTreeItemType.Notebook) {
+			let pathToNotebook = path.join(this.book.root, 'content', this._uri.concat('.ipynb'));
 			this.command = { command: 'bookTreeView.openNotebook', title: localize('openNotebookCommand', 'Open Notebook'), arguments: [pathToNotebook], };
-		} else if (this.pageType === 'markdown') {
-			let pathToMarkdown = path.join(this.root, 'content', this.uri.concat('.md'));
+		} else if (this.book.type === BookTreeItemType.Markdown) {
+			let pathToMarkdown = path.join(this.book.root, 'content', this._uri.concat('.md'));
 			this.command = { command: 'bookTreeView.openMarkdown', title: localize('openMarkdownCommand', 'Open Markdown'), arguments: [pathToMarkdown], };
-		} else if (this.pageType === 'externalLink') {
-			this.command = { command: 'bookTreeView.openExternalLink', title: localize('openExternalLinkCommand', 'Open External Link'), arguments: [this.uri], };
+		} else if (this.book.type === BookTreeItemType.ExternalLink) {
+			this.command = { command: 'bookTreeView.openExternalLink', title: localize('openExternalLinkCommand', 'Open External Link'), arguments: [this._uri], };
 		}
 	}
 
 	private setPreviousUri(index: number): void {
 		let i = --index;
 		while (i > -1) {
-			if (this.tableOfContents[i].url) {
+			if (this.book.tableOfContents[i].url) {
 				// TODO: Currently only navigating to notebooks. Need to add logic for markdown.
-				let pathToNotebook = path.join(this.root, 'content', this.tableOfContents[i].url.concat('.ipynb'));
+				let pathToNotebook = path.join(this.book.root, 'content', this.book.tableOfContents[i].url.concat('.ipynb'));
 				if (fs.existsSync(pathToNotebook)) {
-					this.previousUri = pathToNotebook;
+					this._previousUri = pathToNotebook;
 					return;
 				}
 			}
 			i--;
 		}
-		return null;
 	}
 
 	private setNextUri(index: number): void {
 		let i = ++index;
-		while (i < this.tableOfContents.length) {
-			if (this.tableOfContents[i].url) {
+		while (i < this.book.tableOfContents.length) {
+			if (this.book.tableOfContents[i].url) {
 				// TODO: Currently only navigating to notebooks. Need to add logic for markdown.
-				let pathToNotebook = path.join(this.root, 'content', this.tableOfContents[i].url.concat('.ipynb'));
+				let pathToNotebook = path.join(this.book.root, 'content', this.book.tableOfContents[i].url.concat('.ipynb'));
 				if (fs.existsSync(pathToNotebook)) {
-					this.nextUri = pathToNotebook;
+					this._nextUri = pathToNotebook;
 					return;
 				}
 			}
 			i++;
 		}
-		return null;
+	}
+
+	public get root(): string {
+		return this.book.root;
+	}
+
+	public get tableOfContents(): any[] {
+		return this.book.tableOfContents;
+	}
+
+	public get sections(): any[] {
+		return this._sections;
+	}
+
+	public get previousUri(): string {
+		return this._previousUri;
+	}
+
+	public get nextUri(): string {
+		return this._nextUri;
 	}
 }
