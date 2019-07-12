41545a3a6d1def0900a8f2e62b81c56bdd3f7412
Karl Burtram <karlb@microsoft.com>
2019-07-11 17:15:34 -0700

    A couple other WIP changes

--- extensions/liveshare/src/main.ts
+++ extensions/liveshare/src/main.ts
@@ -4,19 +4,46 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import { registerLiveShareIntegrationCommands } from './commands';
+import { setExtensionContext } from './extension-context';
 
 declare var require: any;
 let vsls = require('vsls');
 
-export async function activate(context: vscode.ExtensionContext): Promise<any> {
+export async function activate(context: vscode.ExtensionContext) {
 
-	vsls.getApi().then((vslsApi) => {
-		vscode.window.showErrorMessage('Loaded api');
+	setExtensionContext(context);
+
+	const vslsApi = await vsls.getApi();
+	await vscode.commands.executeCommand(
+		'setContext',
+		'peacock:liveshare',
+		!!vslsApi
+	);
+
+
+	if (!vslsApi) {
+		return;
+	}
+
+	vslsApi!.onDidChangeSession(async function onLiveShareSessionCHange(e: any) {
+		// If there isn't a session ID, then that
+		// means the session has been ended.
+		const isHost = e.session.role === vsls.Role.Host;
+		if (!e.session.id && isHost) {
+			//return await revertLiveShareWorkspaceColors();
+			return;
+		}
+
+		return;
+		//return await setLiveShareSessionWorkspaceColors(isHost);
 	});
 
-	vscode.window.showErrorMessage('Unsupported platform');
-	return {};
+	registerLiveShareIntegrationCommands();
 }
 
 export function deactivate(): void {
 }
+
+
+
