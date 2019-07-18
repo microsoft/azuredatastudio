eb357f7319640f3b76f2fd83d48b635749bcf1f9
Lucy Zhang <t-luz@microsoft.com>
2019-07-18 16:06:30 -0700

    changed context key for nav buttons

--- src/sql/workbench/parts/notebook/notebook.component.ts
+++ src/sql/workbench/parts/notebook/notebook.component.ts
@@ -441,7 +441,7 @@ export class NotebookComponent extends AngularDisposable implements OnInit, OnDe
 	protected initNavSection(): void {
 		this._navProvider = this.notebookService.getNavigationProvider(this._notebookParams.notebookUri);
 
-		if (this.contextKeyService.getContextKeyValue('insiders') &&
+		if (this.contextKeyService.getContextKeyValue('notebookQuality') &&
 			this.contextKeyService.getContextKeyValue('bookOpened') &&
 			this._navProvider) {
 			this._navProvider.getNavigation(this._notebookParams.notebookUri).then(result => {
