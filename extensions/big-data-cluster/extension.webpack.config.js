2d744468d4b4a9e00e5f062918b33c0365ffec3c
Kevin Cunnane <kcunnane@microsoft.com>
2019-07-24 09:58:57 -0700

    Fix ##6485 big-data-cluster extension fails to load - Entry point is `extension` not `main`

--- extensions/big-data-cluster/extension.webpack.config.js
+++ extensions/big-data-cluster/extension.webpack.config.js
@@ -12,7 +12,7 @@ const withDefaults = require('../shared.webpack.config');
 module.exports = withDefaults({
 	context: __dirname,
 	entry: {
-		main: './src/extension.ts'
+		extension: './src/extension.ts'
 	},
 	externals: {
 	}
