b9fe22bf76f16f87b63e4c836fff5bf8e044e08f
Aasim Khan <aasimkhan30@gmail.com>
2019-07-30 10:24:55 -0700

    Added new notebook Dialog

---
+++ extensions/agent/src/data/notebookData.ts
@@ -0,0 +1,203 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import * as nls from 'vscode-nls';
+import * as azdata from 'azdata';
+import * as vscode from 'vscode';
+import { AgentUtils } from '../agentUtils';
+import { IAgentDialogData, AgentDialogMode } from '../interfaces';
+
+const localize = nls.loadMessageBundle();
+
+export class NotebookData implements IAgentDialogData {
+
+	private readonly JobCompletionActionCondition_Always: string = localize('jobData.whenJobCompletes', 'When the job completes');
+	private readonly JobCompletionActionCondition_OnFailure: string = localize('jobData.whenJobFails', 'When the job fails');
+	private readonly JobCompletionActionCondition_OnSuccess: string = localize('jobData.whenJobSucceeds', 'When the job succeeds');
+
+	// Error Messages
+	private readonly CreateJobErrorMessage_NameIsEmpty = localize('jobData.jobNameRequired', 'Job name must be provided');
+
+	private _ownerUri: string;
+	private _jobCategories: string[];
+	private _operators: string[];
+	private _defaultOwner: string;
+	private _jobCompletionActionConditions: azdata.CategoryValue[];
+	private _jobCategoryIdsMap: azdata.AgentJobCategory[];
+
+	public dialogMode: AgentDialogMode = AgentDialogMode.CREATE;
+	public name: string;
+	public originalName: string;
+	public enabled: boolean = true;
+	public description: string;
+	public category: string;
+	public categoryId: number;
+	public owner: string;
+	public emailLevel: azdata.JobCompletionActionCondition = azdata.JobCompletionActionCondition.OnFailure;
+	public pageLevel: azdata.JobCompletionActionCondition = azdata.JobCompletionActionCondition.OnFailure;
+	public eventLogLevel: azdata.JobCompletionActionCondition = azdata.JobCompletionActionCondition.OnFailure;
+	public deleteLevel: azdata.JobCompletionActionCondition = azdata.JobCompletionActionCondition.OnSuccess;
+	public operatorToEmail: string;
+	public operatorToPage: string;
+	public jobSteps: azdata.AgentJobStepInfo[];
+	public jobSchedules: azdata.AgentJobScheduleInfo[];
+	public alerts: azdata.AgentAlertInfo[];
+	public jobId: string;
+	public startStepId: number;
+	public categoryType: number;
+	public targetDatabase: string;
+	public templateId: number;
+	public templatePath: string;
+
+	constructor(
+		ownerUri: string,
+		notebookInfo: azdata.AgentNotebookInfo = undefined,
+		private _agentService: azdata.AgentServicesProvider = undefined) {
+
+		this._ownerUri = ownerUri;
+		if (notebookInfo) {
+			this.dialogMode = AgentDialogMode.EDIT;
+			this.name = notebookInfo.name;
+			this.originalName = notebookInfo.name;
+			this.owner = notebookInfo.owner;
+			this.category = notebookInfo.category;
+			this.description = notebookInfo.description;
+			this.enabled = notebookInfo.enabled;
+			this.jobSteps = notebookInfo.jobSteps;
+			this.jobSchedules = notebookInfo.jobSchedules;
+			this.alerts = notebookInfo.alerts;
+			this.jobId = notebookInfo.jobId;
+			this.startStepId = notebookInfo.startStepId;
+			this.categoryId = notebookInfo.categoryId;
+			this.categoryType = notebookInfo.categoryType;
+		}
+	}
+
+	public get jobCategories(): string[] {
+		return this._jobCategories;
+	}
+
+	public get jobCategoryIdsMap(): azdata.AgentJobCategory[] {
+		return this._jobCategoryIdsMap;
+	}
+
+	public get operators(): string[] {
+		return this._operators;
+	}
+
+	public get ownerUri(): string {
+		return this._ownerUri;
+	}
+
+	public get defaultOwner(): string {
+		return this._defaultOwner;
+	}
+
+	public get JobCompletionActionConditions(): azdata.CategoryValue[] {
+		return this._jobCompletionActionConditions;
+	}
+
+	public async initialize() {
+		this._agentService = await AgentUtils.getAgentService();
+		let jobDefaults = await this._agentService.getJobDefaults(this.ownerUri);
+		if (jobDefaults && jobDefaults.success) {
+			this._jobCategories = jobDefaults.categories.map((cat) => {
+				return cat.name;
+			});
+			this._jobCategoryIdsMap = jobDefaults.categories;
+			this._defaultOwner = jobDefaults.owner;
+
+			this._operators = ['', this._defaultOwner];
+			this.owner = this.owner ? this.owner : this._defaultOwner;
+		}
+
+		this._jobCompletionActionConditions = [{
+			displayName: this.JobCompletionActionCondition_OnSuccess,
+			name: azdata.JobCompletionActionCondition.OnSuccess.toString()
+		}, {
+			displayName: this.JobCompletionActionCondition_OnFailure,
+			name: azdata.JobCompletionActionCondition.OnFailure.toString()
+		}, {
+			displayName: this.JobCompletionActionCondition_Always,
+			name: azdata.JobCompletionActionCondition.Always.toString()
+		}];
+	}
+
+	public async save() {
+		let jobInfo: azdata.AgentJobInfo = this.toAgentJobInfo();
+		let result = this.dialogMode === AgentDialogMode.CREATE
+			? await this._agentService.createJob(this.ownerUri, jobInfo)
+			: await this._agentService.updateJob(this.ownerUri, this.originalName, jobInfo);
+		if (!result || !result.success) {
+			if (this.dialogMode === AgentDialogMode.EDIT) {
+				vscode.window.showErrorMessage(
+					localize('jobData.saveErrorMessage', "Job update failed '{0}'", result.errorMessage ? result.errorMessage : 'Unknown'));
+			} else {
+				vscode.window.showErrorMessage(
+					localize('jobData.newJobErrorMessage', "Job creation failed '{0}'", result.errorMessage ? result.errorMessage : 'Unknown'));
+			}
+		} else {
+			if (this.dialogMode === AgentDialogMode.EDIT) {
+				vscode.window.showInformationMessage(
+					localize('jobData.saveSucessMessage', "Job '{0}' updated successfully", jobInfo.name));
+			} else {
+				vscode.window.showInformationMessage(
+					localize('jobData.newJobSuccessMessage', "Job '{0}' created successfully", jobInfo.name));
+			}
+
+		}
+	}
+
+	public validate(): { valid: boolean, errorMessages: string[] } {
+		let validationErrors: string[] = [];
+
+		if (!(this.name && this.name.trim())) {
+			validationErrors.push(this.CreateJobErrorMessage_NameIsEmpty);
+		}
+
+		return {
+			valid: validationErrors.length === 0,
+			errorMessages: validationErrors
+		};
+	}
+
+	public toAgentJobInfo(): azdata.AgentNotebookInfo {
+		return {
+			name: this.name,
+			owner: this.owner ? this.owner : this.defaultOwner,
+			description: this.description,
+			emailLevel: this.emailLevel,
+			pageLevel: this.pageLevel,
+			eventLogLevel: this.eventLogLevel,
+			deleteLevel: this.deleteLevel,
+			operatorToEmail: this.operatorToEmail,
+			operatorToPage: this.operatorToPage,
+			enabled: this.enabled,
+			category: this.category,
+			alerts: this.alerts,
+			jobSchedules: this.jobSchedules,
+			jobSteps: this.jobSteps,
+			targetDatabase: this.targetDatabase,
+			// The properties below are not collected from UI
+			// We could consider using a seperate class for create job request
+			//
+			templateId: this.templateId,
+			currentExecutionStatus: 0,
+			lastRunOutcome: 0,
+			currentExecutionStep: '',
+			hasTarget: true,
+			hasSchedule: false,
+			hasStep: false,
+			runnable: true,
+			categoryId: this.categoryId,
+			categoryType: this.categoryType,
+			lastRun: '',
+			nextRun: '',
+			jobId: this.jobId,
+			startStepId: this.startStepId
+		};
+	}
+}
---
+++ extensions/agent/src/dialogs/notebookDialog.ts
@@ -0,0 +1,294 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+import * as nls from 'vscode-nls';
+import * as azdata from 'azdata';
+import { JobData } from '../data/jobData';
+import { JobStepDialog } from './jobStepDialog';
+import { PickScheduleDialog } from './pickScheduleDialog';
+import { AlertDialog } from './alertDialog';
+import { AgentDialog } from './agentDialog';
+import { AgentUtils } from '../agentUtils';
+import { JobStepData } from '../data/jobStepData';
+import { NotebookData } from '../data/notebookData';
+
+const localize = nls.loadMessageBundle();
+
+export class NotebookDialog extends AgentDialog<NotebookData>  {
+
+	// TODO: localize
+	// Top level
+	private static readonly CreateDialogTitle: string = localize('jobDialog.newJob', 'New Notebook');
+	private static readonly EditDialogTitle: string = localize('jobDialog.editJob', 'Edit Notebook');
+	private readonly GeneralTabText: string = localize('jobDialog.general', 'General');
+	private readonly SchedulesTabText: string = localize('jobDialog.schedules', 'Schedules');
+	private readonly BlankJobNameErrorText: string = localize('jobDialog.blankJobNameError', 'The name of the job cannot be blank.');
+
+	// General tab strings
+	private readonly NameTextBoxLabel: string = localize('jobDialog.name', 'Name');
+	private readonly OwnerTextBoxLabel: string = localize('jobDialog.owner', 'Owner');
+	private readonly TargetDatabaseDropdownLabel: string = localize('jobDialog.targetDatabase', 'Target Database');
+	private readonly TemplateNotebookTextBoxLabel: string = localize('jobDialog.templateNotebook', 'Template Notebook');
+	private readonly DescriptionTextBoxLabel: string = localize('jobDialog.description', 'Description');
+	private readonly EnabledCheckboxLabel: string = localize('jobDialog.enabled', 'Enabled');
+
+	// Schedules tab strings
+	private readonly SchedulesTopLabelString: string = localize('jobDialog.schedulesaLabel', 'Schedules list');
+	private readonly PickScheduleButtonString: string = localize('jobDialog.pickSchedule', 'Pick Schedule');
+	private readonly ScheduleNameLabelString: string = localize('jobDialog.scheduleNameLabel', 'Schedule Name');
+
+	// Event Name strings
+	private readonly NewJobDialogEvent: string = 'NewJobDialogOpened';
+	private readonly EditJobDialogEvent: string = 'EditJobDialogOpened';
+
+	// UI Components
+	private generalTab: azdata.window.DialogTab;
+	private schedulesTab: azdata.window.DialogTab;
+
+	// General tab controls
+	private nameTextBox: azdata.InputBoxComponent;
+	private ownerTextBox: azdata.InputBoxComponent;
+	private targetDatabaseDropDown: azdata.DropDownComponent;
+	private TemplateFilePathBox: azdata.InputBoxComponent;
+	private openTemplateFileButton: azdata.ButtonComponent;
+	private descriptionTextBox: azdata.InputBoxComponent;
+	private enabledCheckBox: azdata.CheckBoxComponent;
+
+	// Schedule tab controls
+	private removeScheduleButton: azdata.ButtonComponent;
+
+	// Schedule tab controls
+	private schedulesTable: azdata.TableComponent;
+	private pickScheduleButton: azdata.ButtonComponent;
+
+	// Alert tab controls
+	private alertsTable: azdata.TableComponent;
+	private newAlertButton: azdata.ButtonComponent;
+	private isEdit: boolean = false;
+
+	// Job objects
+	private steps: azdata.AgentJobStepInfo[];
+	private schedules: azdata.AgentJobScheduleInfo[];
+	private alerts: azdata.AgentAlertInfo[] = [];
+	private startStepDropdownValues: azdata.CategoryValue[] = [];
+
+	constructor(ownerUri: string, notebookInfo: azdata.AgentNotebookInfo = undefined) {
+		super(
+			ownerUri,
+			new NotebookData(ownerUri, notebookInfo),
+			notebookInfo ? NotebookDialog.EditDialogTitle : NotebookDialog.CreateDialogTitle);
+		this.steps = this.model.jobSteps ? this.model.jobSteps : [];
+		this.schedules = this.model.jobSchedules ? this.model.jobSchedules : [];
+		this.alerts = this.model.alerts ? this.model.alerts : [];
+		this.isEdit = notebookInfo ? true : false;
+		this.dialogName = this.isEdit ? this.EditJobDialogEvent : this.NewJobDialogEvent;
+	}
+
+	protected async initializeDialog() {
+		this.generalTab = azdata.window.createTab(this.GeneralTabText);
+		this.schedulesTab = azdata.window.createTab(this.SchedulesTabText);
+		this.initializeGeneralTab();
+		this.initializeSchedulesTab();
+		this.dialog.content = [this.generalTab, this.schedulesTab];
+		this.dialog.registerCloseValidator(() => {
+			this.updateModel();
+			let validationResult = this.model.validate();
+			if (!validationResult.valid) {
+				// TODO: Show Error Messages
+				console.error(validationResult.errorMessages.join(','));
+			}
+
+			return validationResult.valid;
+		});
+	}
+
+	private initializeGeneralTab() {
+		this.generalTab.registerContent(async view => {
+			this.nameTextBox = view.modelBuilder.inputBox().component();
+			this.nameTextBox.required = true;
+			this.nameTextBox.onTextChanged(() => {
+				if (this.nameTextBox.value && this.nameTextBox.value.length > 0) {
+					this.dialog.message = null;
+					// Change the job name immediately since steps
+					// depends on the job name
+					this.model.name = this.nameTextBox.value;
+				}
+			});
+			this.ownerTextBox = view.modelBuilder.inputBox().component();
+			this.targetDatabaseDropDown = view.modelBuilder.dropDown().component();
+			this.TemplateFilePathBox = view.modelBuilder.inputBox().component();
+			this.openTemplateFileButton = view.modelBuilder.button()
+				.withProperties({
+					label: this.TemplateNotebookTextBoxLabel,
+					title: this.TemplateNotebookTextBoxLabel,
+					width: '80px',
+					isFile: true
+				}).component();
+			this.descriptionTextBox = view.modelBuilder.inputBox().withProperties({
+				multiline: true,
+				height: 200
+			}).component();
+			this.enabledCheckBox = view.modelBuilder.checkBox()
+				.withProperties({
+					label: this.EnabledCheckboxLabel
+				}).component();
+			let formModel = view.modelBuilder.formContainer()
+				.withFormItems([{
+					component: this.nameTextBox,
+					title: this.NameTextBoxLabel
+				}, {
+					component: this.ownerTextBox,
+					title: this.OwnerTextBoxLabel
+				}, {
+					component: this.targetDatabaseDropDown,
+					title: this.TargetDatabaseDropdownLabel
+				}, {
+					component: this.TemplateFilePathBox,
+					title: this.TemplateNotebookTextBoxLabel,
+					actions: [this.openTemplateFileButton]
+				}, {
+					component: this.descriptionTextBox,
+					title: this.DescriptionTextBoxLabel
+				}, {
+					component: this.enabledCheckBox,
+					title: ''
+				}]).withLayout({ width: '100%' }).component();
+
+			await view.initializeModel(formModel);
+
+			this.nameTextBox.value = this.model.name;
+			this.ownerTextBox.value = this.model.owner;
+			//this.categoryDropdown.values = this.model.jobCategories;
+
+			let idx: number = undefined;
+			if (this.model.category && this.model.category !== '') {
+				idx = this.model.jobCategories.indexOf(this.model.category);
+			}
+			//this.categoryDropdown.value = this.model.jobCategories[idx > 0 ? idx : 0];
+
+			this.enabledCheckBox.checked = this.model.enabled;
+			this.descriptionTextBox.value = this.model.description;
+			this.openTemplateFileButton.onDidClick(e => {
+				console.log(e);
+
+			});
+		});
+	}
+
+
+	private initializeSchedulesTab() {
+		this.schedulesTab.registerContent(async view => {
+			this.schedulesTable = view.modelBuilder.table()
+				.withProperties({
+					columns: [
+						PickScheduleDialog.SchedulesIDText,
+						PickScheduleDialog.ScheduleNameLabelText,
+						PickScheduleDialog.ScheduleDescription
+					],
+					data: [],
+					height: 750,
+					width: 420
+				}).component();
+
+			this.pickScheduleButton = view.modelBuilder.button().withProperties({
+				label: this.PickScheduleButtonString,
+				width: 80
+			}).component();
+			this.removeScheduleButton = view.modelBuilder.button().withProperties({
+				label: 'Remove schedule',
+				width: 100
+			}).component();
+			this.pickScheduleButton.onDidClick(() => {
+				let pickScheduleDialog = new PickScheduleDialog(this.model.ownerUri, this.model.name);
+				pickScheduleDialog.onSuccess((dialogModel) => {
+					let selectedSchedule = dialogModel.selectedSchedule;
+					if (selectedSchedule) {
+						let existingSchedule = this.schedules.find(item => item.name === selectedSchedule.name);
+						if (!existingSchedule) {
+							selectedSchedule.jobName = this.model.name ? this.model.name : this.nameTextBox.value;
+							this.schedules.push(selectedSchedule);
+						}
+						this.populateScheduleTable();
+					}
+				});
+				pickScheduleDialog.showDialog();
+			});
+			this.removeScheduleButton.onDidClick(() => {
+				if (this.schedulesTable.selectedRows.length === 1) {
+					let selectedRow = this.schedulesTable.selectedRows[0];
+					let selectedScheduleName = this.schedulesTable.data[selectedRow][1];
+					for (let i = 0; i < this.schedules.length; i++) {
+						if (this.schedules[i].name === selectedScheduleName) {
+							this.schedules.splice(i, 1);
+						}
+					}
+					this.populateScheduleTable();
+				}
+			});
+			let formModel = view.modelBuilder.formContainer()
+				.withFormItems([{
+					component: this.schedulesTable,
+					title: this.SchedulesTopLabelString,
+					actions: [this.pickScheduleButton, this.removeScheduleButton]
+				}]).withLayout({ width: '100%' }).component();
+
+			await view.initializeModel(formModel);
+
+			this.populateScheduleTable();
+		});
+	}
+
+	private populateScheduleTable() {
+		let data = this.convertSchedulesToData(this.schedules);
+		this.schedulesTable.data = data;
+		this.schedulesTable.height = 750;
+
+	}
+
+	private createRowContainer(view: azdata.ModelView): azdata.FlexBuilder {
+		return view.modelBuilder.flexContainer().withLayout({
+			flexFlow: 'row',
+			alignItems: 'left',
+			justifyContent: 'space-between'
+		});
+	}
+	private convertSchedulesToData(jobSchedules: azdata.AgentJobScheduleInfo[]): any[][] {
+		let result = [];
+		jobSchedules.forEach(schedule => {
+			let cols = [];
+			cols.push(schedule.id);
+			cols.push(schedule.name);
+			cols.push(schedule.description);
+			result.push(cols);
+		});
+		return result;
+	}
+
+	protected updateModel() {
+		this.model.name = this.nameTextBox.value;
+		this.model.owner = this.ownerTextBox.value;
+		this.model.enabled = this.enabledCheckBox.checked;
+		this.model.description = this.descriptionTextBox.value;
+		this.model.templatePath = this.TemplateFilePathBox.value;
+		if (!this.model.jobSteps) {
+			this.model.jobSteps = [];
+		}
+		this.model.jobSteps = this.steps;
+		// Change the last step's success action to quit because the
+		// default is "Go To Next Step"
+		if (this.model.jobSteps.length > 0) {
+			this.model.jobSteps[this.model.jobSteps.length - 1].successAction = azdata.StepCompletionAction.QuitWithSuccess;
+		}
+		if (!this.model.jobSchedules) {
+			this.model.jobSchedules = [];
+		}
+		this.model.jobSchedules = this.schedules;
+		if (!this.model.alerts) {
+			this.model.alerts = [];
+		}
+		this.model.alerts = this.alerts;
+		this.model.categoryId = +this.model.jobCategoryIdsMap.find(cat => cat.name === this.model.category).id;
+	}
+}
