f5fbb82bfedd907040612a308c6c8d3be01985f9
Charles Gagnon <chgagnon@microsoft.com>
2019-08-08 17:10:21 -0700

    Add back in telemetry properties and remove user usage count stats

--- src/vs/platform/telemetry/node/workbenchCommonProperties.ts
+++ src/vs/platform/telemetry/node/workbenchCommonProperties.ts
@@ -7,8 +7,7 @@ import { IStorageService, StorageScope } from 'vs/platform/storage/common/storag
 import { resolveCommonProperties } from 'vs/platform/telemetry/node/commonProperties';
 import { instanceStorageKey, firstSessionDateStorageKey, lastSessionDateStorageKey } from 'vs/platform/telemetry/common/telemetry';
 
-// {{ SQL CARBON EDIT }}
-import product from 'vs/platform/product/node/product';
+import product from 'vs/platform/product/node/product'; // {{ SQL CARBON EDIT }}
 
 export async function resolveWorkbenchCommonProperties(storageService: IStorageService, commit: string | undefined, version: string | undefined, machineId: string, installSourcePath: string, remoteAuthority?: string): Promise<{ [name: string]: string | undefined }> {
 	const result = await resolveCommonProperties(commit, version, machineId, installSourcePath);
@@ -17,45 +16,23 @@ export async function resolveWorkbenchCommonProperties(storageService: IStorageS
 	const lastSessionDate = storageService.get(lastSessionDateStorageKey, StorageScope.GLOBAL)!;
 
 	// __GDPR__COMMON__ "common.version.shell" : { "classification": "SystemMetaData", "purpose": "PerformanceAndHealth" }
-	// result['common.version.shell'] = process.versions && process.versions['electron'];
+	result['common.version.shell'] = process.versions && process.versions['electron'];
 	// __GDPR__COMMON__ "common.version.renderer" : { "classification": "SystemMetaData", "purpose": "PerformanceAndHealth" }
-	// result['common.version.renderer'] = process.versions && process.versions['chrome'];
+	result['common.version.renderer'] = process.versions && process.versions['chrome'];
 	// __GDPR__COMMON__ "common.firstSessionDate" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
-	// result['common.firstSessionDate'] = firstSessionDate;
+	result['common.firstSessionDate'] = firstSessionDate;
 	// __GDPR__COMMON__ "common.lastSessionDate" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
-	// result['common.lastSessionDate'] = lastSessionDate || '';
+	result['common.lastSessionDate'] = lastSessionDate || '';
 	// __GDPR__COMMON__ "common.isNewSession" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
-	// result['common.isNewSession'] = !lastSessionDate ? '1' : '0';
+	result['common.isNewSession'] = !lastSessionDate ? '1' : '0';
 	// __GDPR__COMMON__ "common.instanceId" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
-	// result['common.instanceId'] = instanceId;
+	result['common.instanceId'] = instanceId;
 	// __GDPR__COMMON__ "common.remoteAuthority" : { "classification": "SystemMetaData", "purpose": "PerformanceAndHealth" }
-	// result['common.remoteAuthority'] = cleanRemoteAuthority(remoteAuthority);
+	result['common.remoteAuthority'] = cleanRemoteAuthority(remoteAuthority);
 
-	// {{SQL CARBON EDIT}}
-	result['common.application.name'] = product.nameLong;
-	// {{SQL CARBON EDIT}}
-	result['common.userId'] = '';
-
-	// {{SQL CARBON EDIT}}
-	setUsageDates(storageService);
+	result['common.application.name'] = product.nameLong; // {{SQL CARBON EDIT}}
 
 	return result;
-}
-
-// {{SQL CARBON EDIT}}
-function setUsageDates(storageService: IStorageService): void {
-	// daily last usage date
-	const appStartDate = new Date('January 1, 2000');
-	const dailyLastUseDate = storageService.get('telemetry.dailyLastUseDate', StorageScope.GLOBAL, appStartDate.toUTCString());
-	storageService.store('telemetry.dailyLastUseDate', dailyLastUseDate, StorageScope.GLOBAL);
-
-	// weekly last usage date
-	const weeklyLastUseDate = storageService.get('telemetry.weeklyLastUseDate', StorageScope.GLOBAL, appStartDate.toUTCString());
-	storageService.store('telemetry.weeklyLastUseDate', weeklyLastUseDate, StorageScope.GLOBAL);
-
-	// monthly last usage date
-	const monthlyLastUseDate = storageService.get('telemetry.monthlyLastUseDate', StorageScope.GLOBAL, appStartDate.toUTCString());
-	storageService.store('telemetry.monthlyLastUseDate', monthlyLastUseDate, StorageScope.GLOBAL);
 }
 
 function cleanRemoteAuthority(remoteAuthority?: string): string {
