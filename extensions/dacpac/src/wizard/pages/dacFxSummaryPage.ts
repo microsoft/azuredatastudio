ba193e870b9a01bffdfceb5d265ae371df4932f6
Kim Santiago <kisantia@microsoft.com>
2019-08-14 10:48:31 -0700

    addressing comments

--- extensions/dacpac/src/wizard/pages/dacFxSummaryPage.ts
+++ extensions/dacpac/src/wizard/pages/dacFxSummaryPage.ts
@@ -33,7 +33,7 @@ export class DacFxSummaryPage extends BasePage {
 
 	async start(): Promise<boolean> {
 		this.table = this.view.modelBuilder.table().withProperties({
-			title: 'Summary of settings'
+			title: localize('dacfx.summaryTableTitle', 'Summary of settings')
 		}).component();
 		this.loader = this.view.modelBuilder.loadingComponent().withItem(this.table).component();
 		this.form = this.view.modelBuilder.formContainer().withFormItems(
