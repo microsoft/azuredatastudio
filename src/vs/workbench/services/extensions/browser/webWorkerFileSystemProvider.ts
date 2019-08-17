19b6bd1e24f2940397da66e99dfbb886bc34243d
ADS Merger <andresse@microsoft.com>
2019-08-17 01:17:30 +0000

    Update License Headers

--- src/vs/workbench/services/extensions/browser/webWorkerFileSystemProvider.ts
+++ src/vs/workbench/services/extensions/browser/webWorkerFileSystemProvider.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { IFileSystemProvider, FileSystemProviderCapabilities, IStat, FileType, FileDeleteOptions, FileOverwriteOptions, FileWriteOptions, FileSystemProviderError, FileSystemProviderErrorCode } from 'vs/platform/files/common/files';
