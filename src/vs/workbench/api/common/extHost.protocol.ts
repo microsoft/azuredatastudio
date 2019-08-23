658cf518877542669485299c9f05763c64d07530
Anthony Dresser <andresse@microsoft.com>
2019-08-22 22:07:01 -0700

    Merge from vscode 0f73473c08055054f317c1c94502f7f39fdbb164 (#6892)
    * Merge from vscode 0f73473c08055054f317c1c94502f7f39fdbb164
    
    * fix tslinting

--- src/vs/workbench/api/common/extHost.protocol.ts
+++ src/vs/workbench/api/common/extHost.protocol.ts
@@ -571,6 +571,7 @@ export interface ExtHostWebviewsShape {
 export interface MainThreadUrlsShape extends IDisposable {
 	$registerUriHandler(handle: number, extensionId: ExtensionIdentifier): Promise<void>;
 	$unregisterUriHandler(handle: number): Promise<void>;
+	$createAppUri(extensionId: ExtensionIdentifier, options?: { payload?: Partial<UriComponents> }): Promise<UriComponents>;
 }
 
 export interface ExtHostUrlsShape {
@@ -939,7 +940,7 @@ export interface ISuggestDataDto {
 	k/* commitCharacters */?: string[];
 	l/* additionalTextEdits */?: ISingleEditOperation[];
 	m/* command */?: modes.Command;
-	n/* deprecated */?: boolean;
+	n/* kindModifier */?: modes.CompletionItemKindModifier[];
 	// not-standard
 	x?: ChainedCacheId;
 }
