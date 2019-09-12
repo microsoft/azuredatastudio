075b3bd91eb0be83398dc2925c044fb625efc360
Charles Gagnon <chgagnon@microsoft.com>
2019-09-12 09:12:07 -0700

    Update font weight to semi-bold (600)

--- extensions/big-data-cluster/src/bigDataCluster/constants.ts
+++ extensions/big-data-cluster/src/bigDataCluster/constants.ts
@@ -59,7 +59,7 @@ export class IconPathHelper {
 }
 
 export namespace cssStyles {
-	export const title = { 'font-size': '14px', 'font-weight': 'bold' };
+	export const title = { 'font-size': '14px', 'font-weight': '600' };
 	export const tableHeader = { 'font-weight': 'bold', 'text-transform': 'uppercase', 'font-size': '10px', 'user-select': 'text' };
 	export const selectedResourceHeaderTab = { 'font-weight': 'bold', 'color': '' };
 	export const unselectedResourceHeaderTab = { 'font-weight': '', 'color': '#0078d4' };
