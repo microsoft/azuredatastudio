ca90bf1bc247f5ccdc287f08f9b7c5a2f3d4972a
Amir Omidi <amomidi@microsoft.com>
2019-09-13 11:39:42 -0700

    Send notification

--- src/sql/platform/clipboard/browser/clipboardService.ts
+++ src/sql/platform/clipboard/browser/clipboardService.ts
@@ -5,13 +5,16 @@
 
 import { IClipboardService } from 'sql/platform/clipboard/common/clipboardService';
 import { IClipboardService as vsIClipboardService } from 'vs/platform/clipboard/common/clipboardService';
+import { INotificationService } from 'vs/platform/notification/common/notification';
 import { URI } from 'vs/base/common/uri';
+import { localize } from 'vs/nls';
 
-export class ClipboardService implements IClipboardService {
+export class BrowserClipboardService implements IClipboardService {
 	_serviceBrand: any;
 
 	constructor(
-		@vsIClipboardService private _vsClipboardService: vsIClipboardService
+		@vsIClipboardService private _vsClipboardService: vsIClipboardService,
+		@INotificationService private _notificationService: INotificationService
 	) { }
 
 	/**
@@ -19,7 +22,7 @@ export class ClipboardService implements IClipboardService {
 	 */
 	async writeImageDataUrl(data: string): Promise<void> {
 		// not implemented until web standards catch up
-		throw new Error('Not implemented');
+		this._notificationService.info(localize('imageCopyingNotSupported', "Copying images is not supported"));
 	}
 
 	writeText(text: string): Promise<void> {
