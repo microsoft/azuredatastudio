f1493caa96a1b001ed1ab06d1a3a3fdb31b47142
Charles Gagnon <chgagnon@microsoft.com>
2019-09-27 10:27:43 -0700

    Add clear all query history action/command

--- src/sql/workbench/parts/queryHistory/electron-browser/queryHistory.contribution.ts
+++ src/sql/workbench/parts/queryHistory/electron-browser/queryHistory.contribution.ts
@@ -7,5 +7,15 @@ import { Registry } from 'vs/platform/registry/common/platform';
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions';
 import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
 import { QueryHistoryWorkbenchContribution } from 'sql/workbench/parts/queryHistory/electron-browser/queryHistory';
+import { CommandsRegistry, ICommandService } from 'vs/platform/commands/common/commands';
+import { IQueryHistoryService } from 'sql/platform/queryHistory/common/queryHistoryService';
 
 Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench).registerWorkbenchContribution(QueryHistoryWorkbenchContribution, LifecyclePhase.Restored);
+
+CommandsRegistry.registerCommand({
+	id: 'queryHistory.clear',
+	handler: (accessor) => {
+		const queryHistoryService = accessor.get(IQueryHistoryService);
+		queryHistoryService.clearQueryHistory();
+	}
+});
