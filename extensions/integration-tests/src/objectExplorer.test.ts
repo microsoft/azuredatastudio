9670a29407811fddeba4e3ab5a45f6a9a60505b0
Amir Omidi <amomidi@microsoft.com>
2019-10-02 16:42:16 -0700

    Add azuredb to this list

--- extensions/integration-tests/src/objectExplorer.test.ts
+++ extensions/integration-tests/src/objectExplorer.test.ts
@@ -21,7 +21,7 @@ if (context.RunTest) {
 		test('Standalone instance node label test', async function () {
 			await (new ObjectExplorerTester()).standaloneNodeLabelTest();
 		});
-		test('Azure SQL DB instance node label test', async function () {
+		test('Azure SQL DB instance node label test *UNSTABLE*', async function () {
 			await (new ObjectExplorerTester()).sqlDbNodeLabelTest();
 		});
 		test('BDC instance context menu test', async function () {
