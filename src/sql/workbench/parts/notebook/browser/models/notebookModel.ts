c3f2e4529e02a8b0ae4430460b0b6d2b0b0f6384
Cory Rivera <corivera@microsoft.com>
2019-10-03 14:26:36 -0700

    Remove unnecessary changeType modification.

--- src/sql/workbench/parts/notebook/browser/models/notebookModel.ts
+++ src/sql/workbench/parts/notebook/browser/models/notebookModel.ts
@@ -1013,7 +1013,6 @@ export class NotebookModel extends Disposable implements INotebookModel {
 			case NotebookChangeType.CellOutputUpdated:
 			case NotebookChangeType.CellSourceUpdated:
 			case NotebookChangeType.CellInputVisibilityChanged:
-				changeInfo.changeType = NotebookChangeType.DirtyStateChanged;
 				changeInfo.isDirty = true;
 				changeInfo.modelContentChangedEvent = cell.modelContentChangedEvent;
 				break;
