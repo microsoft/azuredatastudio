e7233c6d405dcc3abb6bb301c8467d4c365b37f0
Amir Omidi <amomidi@microsoft.com>
2019-10-03 17:40:57 -0700

    test

--- scripts/sql-test-integration.sh
+++ scripts/sql-test-integration.sh
@@ -18,6 +18,19 @@ if [[ "$ADS_TEST_GREP" == "" ]]; then
 	export ADS_TEST_INVERT_GREP=1
 fi
 
+# Figure out which Electron to use for running tests
+if [ -z "$INTEGRATION_TEST_ELECTRON_PATH" ]
+then
+	# Run out of sources: no need to compile as code.sh takes care of it
+	INTEGRATION_TEST_ELECTRON_PATH="./scripts/code.sh"
+
+	echo "Running integration tests out of sources."
+else
+	# Run from a built: need to compile all test extensions
+
+	echo "Running integration tests with '$INTEGRATION_TEST_ELECTRON_PATH' as build."
+fi
+
 cd $ROOT
 echo VSCODEUSERDATADIR=$VSCODEUSERDATADIR
 echo VSCODEEXTDIR=$VSCODEEXTDIR
@@ -27,10 +40,10 @@ if [[ "$SKIP_PYTHON_INSTALL_TEST" == "1" ]]; then
 else
 	export PYTHON_TEST_PATH=$VSCODEUSERDATADIR/TestPythonInstallation
 	echo $PYTHON_TEST_PATH
-	./scripts/code.sh --extensionDevelopmentPath=$ROOT/extensions/notebook --extensionTestsPath=$ROOT/extensions/notebook/out/integrationTest --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR --remote-debugging-port=9222
+	$INTEGRATION_TEST_ELECTRON_PATH --extensionDevelopmentPath=$ROOT/extensions/notebook --extensionTestsPath=$ROOT/extensions/notebook/out/integrationTest --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR --remote-debugging-port=9222 --disable-telemetry --disable-crash-reporter --disable-updates --disable-extensions --skip-getting-started --disable-inspect
 fi
 
-./scripts/code.sh --extensionDevelopmentPath=$ROOT/extensions/integration-tests --extensionTestsPath=$ROOT/extensions/integration-tests/out --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR --remote-debugging-port=9222
+$INTEGRATION_TEST_ELECTRON_PATH --extensionDevelopmentPath=$ROOT/extensions/integration-tests --extensionTestsPath=$ROOT/extensions/integration-tests/out --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR --remote-debugging-port=9222 --disable-telemetry --disable-crash-reporter --disable-updates --disable-extensions --skip-getting-started --disable-inspect
 
 rm -r -f $VSCODEUSERDATADIR
 rm -r $VSCODEEXTDIR
