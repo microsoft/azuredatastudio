7d5bc29d4f97bc1d6ff0712015860c548940ebf8
Amir Omidi <amomidi@microsoft.com>
2019-10-05 12:30:58 -0700

    Change setup.js

--- src/sql/setup.js
+++ src/sql/setup.js
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-define(["require", "exports", "vs/base/common/errors"], function (require, errors) {
+define(["require", "exports", "vs/base/common/errors"], function (require, exports, errors) {
 	const jquerylib = require.__$__nodeRequire('jquery');
 
 	window['jQuery'] = jquerylib;
