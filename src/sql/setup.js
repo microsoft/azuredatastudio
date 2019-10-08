954cebeb5cdff855c00aa185f1081f5c492ae732
Amir Omidi <amomidi@microsoft.com>
2019-10-08 14:17:58 -0700

    Try this out

--- src/sql/setup.js
+++ src/sql/setup.js
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-define(["require", "exports", "vs/base/common/errors"], function (require, exports, errors) {
+define(["require", "exports"], function (require) {
 	const jquerylib = require.__$__nodeRequire('jquery');
 
 	window['jQuery'] = jquerylib;
@@ -23,5 +23,4 @@ define(["require", "exports", "vs/base/common/errors"], function (require, expor
 	require.__$__nodeRequire('chart.js');
 
 	window["Zone"]["__zone_symbol__ignoreConsoleErrorUncaughtError"] = true;
-	window["Zone"]["__zone_symbol__unhandledPromiseRejectionHandler"] = errors.onUnexpectedError;
 });
