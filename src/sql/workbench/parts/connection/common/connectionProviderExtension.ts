df3fc717da2d16af4c0777ff5002729fbcdc1734
Alex Ma <alma1@microsoft.com>
2019-10-17 09:36:43 -0700

    removed extra comment

--- src/sql/workbench/parts/connection/common/connectionProviderExtension.ts
+++ src/sql/workbench/parts/connection/common/connectionProviderExtension.ts
@@ -211,5 +211,4 @@ function resolveIconPath(extension: IExtensionPointUser<any>): void {
 	} else {
 		toAbsolutePath(properties['iconPath']);
 	}
-	//	resolver.add(extension.description.identifier.value);
 }
