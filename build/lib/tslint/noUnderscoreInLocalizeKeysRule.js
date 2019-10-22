2de86dd6c7860db65c4c8806484c698e5d5fb7f2
chgagnon <chgagnon@microsoft.com>
2019-10-22 08:06:35 -0700

    Fix bad merge

--- build/lib/tslint/noUnderscoreInLocalizeKeysRule.js
+++ build/lib/tslint/noUnderscoreInLocalizeKeysRule.js
@@ -29,7 +29,7 @@ class NoLocalizeKeysWithUnderscore extends Lint.RuleWalker {
         super.visitCallExpression(node);
     }
     checkCallExpression(node) {
-        // If this isn't one of the localize functions then continue one
+        // If this isn't one of the localize functions then continue on
         const functionName = node.expression.getText();
         if (functionName && !signatures.some(s => s === functionName)) {
             return;
