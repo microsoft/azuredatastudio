93685d3a0958acde8ef448d625d7dd21fd975778
Alan Ren <alanren@microsoft.com>
2019-10-24 14:57:17 -0700

    make the azdata install url configurable (#7989)
    * make the azdata install url configurable
    
    * use settings without reloading
    
    * comments

---
+++ extensions/resource-deployment/src/constants.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export const DeploymentConfigurationKey: string = 'deployment';
+export const AzdataPipInstallUriKey: string = 'azdataPipInstallUri';
+export const azdataPipInstallArgsKey: string = 'azdataPipInstallArgs';
