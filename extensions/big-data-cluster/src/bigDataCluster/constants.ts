053be4756dcc162dd014d457f3788386f452c1bf
Charles Gagnon <chgagnon@microsoft.com>
2019-10-24 18:31:57 -0700

    Fix column text overflow on BDC status pages (#7928)
    * Fix column text overflow on status page
    
    * Fix typo
    
    * Fix another typo

--- extensions/big-data-cluster/src/bigDataCluster/constants.ts
+++ extensions/big-data-cluster/src/bigDataCluster/constants.ts
@@ -63,6 +63,7 @@ export namespace cssStyles {
 	export const tableHeader = { 'font-weight': 'bold', 'text-transform': 'uppercase', 'font-size': '10px', 'user-select': 'text' };
 	export const hyperlink = { 'user-select': 'text', 'color': '#0078d4', 'text-decoration': 'underline', 'cursor': 'pointer' };
 	export const text = { 'margin-block-start': '0px', 'margin-block-end': '0px' };
+	export const overflowEllipsisText = { ...text, 'overflow': 'hidden', 'text-overflow': 'ellipsis' };
 	export const nonSelectableText = { ...cssStyles.text, 'user-select': 'none' };
 	export const tabHeaderText = { 'margin-block-start': '2px', 'margin-block-end': '0px', 'user-select': 'none' };
 	export const selectedResourceHeaderTab = { 'font-weight': 'bold', 'color': '' };
