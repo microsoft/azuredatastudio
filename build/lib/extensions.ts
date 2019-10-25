83f7663f38505180090f39b195a8f1d318a74afd
Leila Lali <llali@microsoft.com>
2019-10-25 10:18:20 -0700

    fix merge issues

--- build/lib/extensions.ts
+++ build/lib/extensions.ts
@@ -216,7 +216,7 @@ export function fromMarketplace(extensionName: string, version: string, metadata
 
 const excludedExtensions = [
 	'vscode-api-tests',
-	'vscode-colorize-tests'
+	'vscode-colorize-tests',
 	'vscode-test-resolver',
 	'ms-vscode.node-debug',
 	'ms-vscode.node-debug2',
@@ -225,11 +225,9 @@ const excludedExtensions = [
 ];
 
 // {{SQL CARBON EDIT}}
-const sqlExtensions = [
-	// This is the list of SQL extensions which the source code is included in this repository, but
-	// they get packaged separately. Adding extension name here, will make the build to create
-	// a separate vsix package for the extension and the exception will be excluded from the main package.
-	// Any extension not included here, will be installed by default.
+const sqlBuiltInExtensions = [
+	// Add SQL built-in extensions here.
+	// the extension will be excluded from SQLOps package and will have separate vsix packages
 	'admin-tool-ext-win',
 	'agent',
 	'import',
@@ -262,7 +260,7 @@ export function packageLocalExtensionsStream(): NodeJS.ReadWriteStream {
 		})
 		.filter(({ name }) => excludedExtensions.indexOf(name) === -1)
 		.filter(({ name }) => builtInExtensions.every(b => b.name !== name))
-		.filter(({ name }) => sqlExtensions.indexOf(name) === -1); // {{SQL CARBON EDIT}} Remove SQL Extensions with separate package
+		.filter(({ name }) => sqlBuiltInExtensions.indexOf(name) === -1); // {{SQL CARBON EDIT}} add aditional filter
 
 	const nodeModules = gulp.src('extensions/node_modules/**', { base: '.' });
 	const localExtensions = localExtensionDescriptions.map(extension => {
@@ -288,10 +286,7 @@ export function packageMarketplaceExtensionsStream(): NodeJS.ReadWriteStream {
 import * as _ from 'underscore';
 import * as vfs from 'vinyl-fs';
 
-export function packageSQLExtensions() {
-
-	// Create package for local SQL extensions
-	//
+export function packageBuiltInExtensions() {
 	const sqlBuiltInLocalExtensionDescriptions = glob.sync('extensions/*/package.json')
 		.map(manifestPath => {
 			const extensionPath = path.dirname(path.join(root, manifestPath));
@@ -300,7 +295,7 @@ export function packageSQLExtensions() {
 		})
 		.filter(({ name }) => excludedExtensions.indexOf(name) === -1)
 		.filter(({ name }) => builtInExtensions.every(b => b.name !== name))
-		.filter(({ name }) => sqlExtensions.indexOf(name) >= 0);
+		.filter(({ name }) => sqlBuiltInExtensions.indexOf(name) >= 0);
 	const visxDirectory = path.join(path.dirname(root), 'vsix');
 	try {
 		if (!fs.existsSync(visxDirectory)) {
