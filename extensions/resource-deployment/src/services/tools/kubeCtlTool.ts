a7f597c9431c2ee82e6175eb744c328e8c680019
Arvind Ranasaria <ranasaria@outlook.com>
2019-10-27 17:13:12 -0700

    dependencies messages, no curl on win32, fixes to min Version (#8039)
    * checking temp work to move to another branch
    
    * removing freeTds
    
    * dependencies Messages and No curl on Win32
    
    * elipsis instead of ...
    
    * add min version check post install and pr fixes
    
    * removing unnecessary comment
    
    * removing old TODO comment
    
    * fix text messages
    
    * add github toke nto electron download (#8047)
    
    * remove hardcode of kubectl version for download

--- extensions/resource-deployment/src/services/tools/kubeCtlTool.ts
+++ extensions/resource-deployment/src/services/tools/kubeCtlTool.ts
@@ -7,7 +7,7 @@ import { Command, ToolType, OsType } from '../../interfaces';
 import * as nls from 'vscode-nls';
 import { SemVer } from 'semver';
 import { IPlatformService } from '../platformService';
-import { ToolBase } from './toolBase';
+import { ToolBase, dependencyType } from './toolBase';
 
 const localize = nls.loadMessageBundle();
 
@@ -75,86 +75,92 @@ export class KubeCtlTool extends ToolBase {
 		[OsType.darwin, macOsInstallationCommands],
 		[OsType.others, defaultInstallationCommands]
 	]);
+
+	protected dependenciesByOsType: Map<OsType, dependencyType[]> = new Map<OsType, dependencyType[]>([
+		[OsType.linux, []],
+		[OsType.win32, []],
+		[OsType.darwin, [dependencyType.Brew]],
+		[OsType.others, [dependencyType.Curl]]
+	]);
 }
 
 const macOsInstallationCommands = [
 	{
-		comment: localize('resourceDeployment.Kubectl.UpdatingBrewRepository', "updating your brew repository for kubectl installation ..."),
+		comment: localize('resourceDeployment.Kubectl.UpdatingBrewRepository', "updating your brew repository for kubectl installation …"),
 		command: 'brew update'
 	},
 	{
-		comment: localize('resourceDeployment.Kubectl.InstallingKubeCtl', "installing kubectl ..."),
+		comment: localize('resourceDeployment.Kubectl.InstallingKubeCtl', "installing kubectl …"),
 		command: 'brew install kubectl'
 	}
 ];
 const linuxInstallationCommands = [
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.AptGetUpdate', "updating repository information ..."),
+		comment: localize('resourceDeployment.Kubectl.AptGetUpdate', "updating repository information …"),
 		command: 'apt-get update'
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.AptGetPackages', "getting packages needed for kubectl installation ..."),
+		comment: localize('resourceDeployment.Kubectl.AptGetPackages', "getting packages needed for kubectl installation …"),
 		command: 'apt-get install -y apt-transport-https'
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.DownloadAndInstallingSigningKey', "downloading and installing the signing key for kubectl ..."),
+		comment: localize('resourceDeployment.Kubectl.DownloadAndInstallingSigningKey', "downloading and installing the signing key for kubectl …"),
 		command: 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -'
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.AddingKubectlRepositoryInformation', "adding the kubectl repository information ..."),
+		comment: localize('resourceDeployment.Kubectl.AddingKubectlRepositoryInformation', "adding the kubectl repository information …"),
 		command: 'echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list'
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.AptGetUpdate', "updating repository information ..."),
+		comment: localize('resourceDeployment.Kubectl.AptGetUpdate', "updating repository information …"),
 		command: 'apt-get update'
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.InstallingKubectl', "installing kubectl ..."),
+		comment: localize('resourceDeployment.Kubectl.InstallingKubectl', "installing kubectl …"),
 		command: 'apt-get install -y kubectl'
 	}
 ];
-// TODO: Remove dependency on curl on Win32 and use powershell Invoke-WebRequest instead
 const win32InstallationCommands = [
 	{
-		comment: localize('resourceDeployment.Kubectl.DeletePreviousDownloadedKubectl.exe', "deleting previously downloaded kubectl.exe if one exists ..."),
-		command: `IF EXIST .\kubectl.exe DEL /F .\kubectl.exe`,
+		comment: localize('resourceDeployment.Kubectl.DeletePreviousDownloadedKubectl.exe', "deleting previously downloaded kubectl.exe if one exists …"),
+		command: 'IF EXIST .\\kubectl.exe DEL /F .\\kubectl.exe',
 	},
 	{
-		comment: localize('resourceDeployment.Kubectl.DownloadingAndInstallingKubectl', "downloading and installing the latest kubectl.exe ..."),
-		command: `for /f %i in ('curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt') do curl -LO https://storage.googleapis.com/kubernetes-release/release/%i/bin/windows/amd64/kubectl.exe`
+		comment: localize('resourceDeployment.Kubectl.DownloadingAndInstallingKubectl', "downloading and installing the latest kubectl.exe …"),
+		command: `powershell -Command "& {$WebClient = New-Object System.Net.WebClient; $Version=$WebClient.DownloadString('https://storage.googleapis.com/kubernetes-release/release/stable.txt').Trim();Write-Output \\\"KubeCtl Version=$Version\\\";$Url=\\\"https://storage.googleapis.com/kubernetes-release/release/$Version/bin/windows/amd64/kubectl.exe\\\"; Write-Output \\\"Downloading file: $Url\\\"; $WebClient.DownloadFile($Url, 'kubectl.exe')}"`
 	}
 ];
 const defaultInstallationCommands = [
 	{
-		comment: localize('resourceDeployment.Kubectl.DeletePreviousDownloadedKubectl', "deleting previously downloaded kubectl if one exists ..."),
+		comment: localize('resourceDeployment.Kubectl.DeletePreviousDownloadedKubectl', "deleting previously downloaded kubectl if one exists …"),
 		command: `[ -e ./kubectl ] && rm -f ./kubectl`,
 	},
 	{
-		comment: localize('resourceDeployment.Kubectl.DownloadingKubectl', "downloading the latest kubectl release ..."),
+		comment: localize('resourceDeployment.Kubectl.DownloadingKubectl', "downloading the latest kubectl release …"),
 		command: 'curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl'
 	},
 	{
-		comment: localize('resourceDeployment.Kubectl.MakingExecutable', "making kubectl executable ..."),
+		comment: localize('resourceDeployment.Kubectl.MakingExecutable', "making kubectl executable …"),
 		command: 'chmod +x ./kubectl',
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.CleaningUpOldBackups', "cleaning up any previously backed up version in the install location if they exist ..."),
-		command: `[ -e /usr/local/bin/kubectl] && [ -e /usr/local/bin/kubectl_movedByADS ] && rm -f /usr/local/bin/kubectl_movedByADS`
+		comment: localize('resourceDeployment.Kubectl.CleaningUpOldBackups', "cleaning up any previously backed up version in the install location if they exist …"),
+		command: '[ -e /usr/local/bin/kubectl] && [ -e /usr/local/bin/kubectl_movedByADS ] && rm -f /usr/local/bin/kubectl_movedByADS'
 	},
 	{
 		sudo: true,
-		comment: localize('resourceDeployment.Kubectl.BackupCurrentBinary', "backing up any existing kubectl in the install location ..."),
-		command: `[ -e /usr/local/bin/kubectl ] && mv /usr/local/bin/kubectl /usr/local/bin/kubectl_movedByADS`
+		comment: localize('resourceDeployment.Kubectl.BackupCurrentBinary', "backing up any existing kubectl in the install location …"),
+		command: '[ -e /usr/local/bin/kubectl ] && mv /usr/local/bin/kubectl /usr/local/bin/kubectl_movedByADS'
 	},
 	{
-		comment: localize('resourceDeployment.Kubectl.MoveToSystemPath', "moving kubectl into the install location in the PATH ..."),
+		comment: localize('resourceDeployment.Kubectl.MoveToSystemPath', "moving kubectl into the install location in the PATH …"),
 		sudo: true,
 		command: 'mv ./kubectl /usr/local/bin/kubectl'
 	}
