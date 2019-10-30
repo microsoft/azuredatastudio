9745c961284074b5e99e268153e7f67955a65a38
Amir Omidi <amomidi@microsoft.com>
2019-10-30 15:32:49 -0700

    Add audit script

--- scripts/audit-packages.sh
+++ scripts/audit-packages.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -e
-
 sources=$(pwd)
 
 find . -name package.json | grep -v node_modules | while read path; do
 	dir=$(dirname "$path")
 	echo "Checking $path …"
 	cd $dir
-	yarn audit --level critical --groups dependencies
-	if [ $? -gt 16 ]
+	yarn audit --groups dependencies>&1
+	exitCode="$?"
+	echo "Exit code was $exitCode"
+	if [ $exitCode -gt 16 ]
 	then
+		echo "$path has a critical yarn audit issue."
 		exit 1
 	fi
 	cd $sources
