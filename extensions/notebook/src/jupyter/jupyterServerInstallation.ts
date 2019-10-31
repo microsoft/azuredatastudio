75c597714373f44009791afd998921e8312ee70e
maddydev <makoripa@microsoft.com>
2019-10-31 16:43:15 -0700

    added package info and refactored the err logging

--- extensions/notebook/src/jupyter/jupyterServerInstallation.ts
+++ extensions/notebook/src/jupyter/jupyterServerInstallation.ts
@@ -36,7 +36,7 @@ const msgSkipPythonInstall = localize('msgSkipPythonInstall', "Python already ex
 const msgWaitingForInstall = localize('msgWaitingForInstall', "Another Python installation is currently in progress. Waiting for it to complete.");
 function msgDependenciesInstallationFailed(errorMessage: string): string { return localize('msgDependenciesInstallationFailed', "Installing Notebook dependencies failed with error: {0}", errorMessage); }
 function msgDownloadPython(platform: string, pythonDownloadUrl: string): string { return localize('msgDownloadPython', "Downloading local python for platform: {0} to {1}", platform, pythonDownloadUrl); }
-function msgPackageRetrievalFailed(errorMessage: string): string { return localize('msgPackageRetrievalFailed', "Encountered an error when trying to retrieve list of installed packages: {0}", errorMessage); }
+function msgPackageRetrievalFailed(errorMessage: string, packageInfo: string): string { return localize('msgPackageRetrievalFailed', "Encountered an error when trying to retrieve list of installed packages: {0} /n Package Info: {1}", errorMessage, packageInfo); }
 
 export class JupyterServerInstallation {
 	public apiWrapper: ApiWrapper;
@@ -529,8 +529,7 @@ export class JupyterServerInstallation {
 				packagesResult = <PythonPkgDetails[]>JSON.parse(packagesInfo);
 			}
 			catch (err) {
-				let errorMsg = `Failed to parse package info, encountered following error: ${(utils.getErrorMessage(err))} \n PackageInfo: ${packagesInfo}`;
-				this.outputChannel.appendLine(msgPackageRetrievalFailed(errorMsg));
+				this.outputChannel.appendLine(msgPackageRetrievalFailed(utils.getErrorMessage(err), packagesInfo));
 			}
 		}
 		return packagesResult;
@@ -566,8 +565,7 @@ export class JupyterServerInstallation {
 				packagesResult = JSON.parse(packagesInfo);
 			}
 			catch (err) {
-				let errorMsg = `Failed to parse package info, encountered following error: ${(utils.getErrorMessage(err))} \n PackageInfo: ${packagesInfo}`;
-				this.outputChannel.appendLine(msgPackageRetrievalFailed(errorMsg));
+				this.outputChannel.appendLine(msgPackageRetrievalFailed(utils.getErrorMessage(err), packagesInfo));
 			}
 
 			if (Array.isArray(packagesResult)) {
