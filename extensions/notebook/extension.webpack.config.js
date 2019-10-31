1615585a872e353a67ebb79e7b49ab6736d04db5
Anthony Dresser <andresse@microsoft.com>
2019-10-31 15:52:01 -0700

    fix parsing for yarnrc

--- extensions/notebook/extension.webpack.config.js
+++ extensions/notebook/extension.webpack.config.js
@@ -11,16 +11,15 @@ const withDefaults = require('../shared.webpack.config');
 const fs = require('fs');
 const path = require('path');
 
-const yarnrcPath = path.join(__dirname, '.yarnrc');
-
 const externals = {
 	'node-fetch': 'commonjs node-fetch'
 };
 
 // conditionally add ws if we are going to be running in a node environment
+const yarnrcPath = path.join(__dirname, '.yarnrc');
 if (fs.existsSync(yarnrcPath)) {
 	const yarnrc = fs.readFileSync(yarnrcPath).toString();
-	const properties = yarnrc.split('\\n').map(r => r.split(' '));
+	const properties = yarnrc.split(/\r?\n/).map(r => r.split(' '));
 	if (properties.find(r => r[0] === 'runtime')[1] === '"node"') {
 		externals['ws'] = 'commonjs ws';
 	}
