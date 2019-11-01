da39f385f3fb9ab17d5e40b4c36c67290916958a
Charles Gagnon <chgagnon@microsoft.com>
2019-10-31 17:25:18 -0700

    PR comments

--- extensions/big-data-cluster/src/bigDataCluster/utils.ts
+++ extensions/big-data-cluster/src/bigDataCluster/utils.ts
@@ -279,26 +279,13 @@ export const ignoreSslConfigName = 'ignoreSslVerification';
  * Retrieves the current setting for whether to ignore SSL verification errors
  */
 export function getIgnoreSslVerificationConfigSetting(): boolean {
-	return _ignoreSslVerification;
-}
-
-let _ignoreSslVerification = false;
-
-vscode.workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
-	if (e.affectsConfiguration(`${bdcConfigSectionName}.${ignoreSslConfigName}`)) {
-		updateIgnoreSslConfigSetting();
-	}
-});
-
-function updateIgnoreSslConfigSetting(): void {
 	try {
 		const config = vscode.workspace.getConfiguration(bdcConfigSectionName);
-		_ignoreSslVerification = config.get<boolean>(ignoreSslConfigName) || false;
+		return config.get<boolean>(ignoreSslConfigName) || false;
 	} catch (error) {
 		console.error(`Unexpected error retrieving ${bdcConfigSectionName}.${ignoreSslConfigName} setting : ${error}`);
 	}
+	return false;
 }
-
-updateIgnoreSslConfigSetting();
 
 
