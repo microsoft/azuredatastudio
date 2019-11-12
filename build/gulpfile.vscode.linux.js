90299b162c46cd4e1bf72066c3374b8cc2e29525
Charles Gagnon <chgagnon@microsoft.com>
2019-11-12 08:57:35 -0800

    Revert "Undo all"
    This reverts commit efac800b63483fbbcacdac45d885275c53350941.

--- build/gulpfile.vscode.linux.js
+++ build/gulpfile.vscode.linux.js
@@ -53,6 +53,8 @@ function prepareDebPackage(arch) {
 			.pipe(replace('@@LICENSE@@', product.licenseName))
 			.pipe(rename('usr/share/appdata/' + product.applicationName + '.appdata.xml'));
 
+		console.log(`prepare Deb package quality = ${product.quality}`);
+		// const icon = gulp.src(product.quality !== 'stable' ? 'resources/linux/code-insiders.png' : 'resources/linux/code.png', { base: '.' }) // {{SQL CARBON EDIT}} Use separate icons for non-stable
 		const icon = gulp.src('resources/linux/code.png', { base: '.' })
 			.pipe(rename('usr/share/pixmaps/' + product.linuxIconName + '.png'));
 
@@ -147,7 +149,8 @@ function prepareRpmPackage(arch) {
 			.pipe(replace('@@LICENSE@@', product.licenseName))
 			.pipe(rename('usr/share/appdata/' + product.applicationName + '.appdata.xml'));
 
-		const icon = gulp.src('resources/linux/code.png', { base: '.' })
+		console.log(`prepare RPM quality = ${product.quality}`);
+		const icon = gulp.src(product.quality !== 'stable' ? 'resources/linux/code-insiders.png' : 'resources/linux/code.png', { base: '.' }) // {{SQL CARBON EDIT}} Use separate icons for non-stable
 			.pipe(rename('BUILD/usr/share/pixmaps/' + product.linuxIconName + '.png'));
 
 		const bash_completion = gulp.src('resources/completions/bash/code')
@@ -224,8 +227,9 @@ function prepareSnapPackage(arch) {
 			.pipe(replace('@@ICON@@', `\${SNAP}/meta/gui/${product.linuxIconName}.png`))
 			.pipe(replace('@@URLPROTOCOL@@', product.urlProtocol));
 
+		console.log(`prepareSnap quality = ${product.quality}`);
 		// An icon that is placed in snap/gui will be placed into meta/gui verbatim.
-		const icon = gulp.src('resources/linux/code.png', { base: '.' })
+		const icon = gulp.src(product.quality !== 'stable' ? 'resources/linux/code-insiders.png' : 'resources/linux/code.png', { base: '.' }) // {{SQL CARBON EDIT}} Use separate icons for non-stable
 			.pipe(rename(`snap/gui/${product.linuxIconName}.png`));
 
 		const code = gulp.src(binaryDir + '/**/*', { base: binaryDir })
