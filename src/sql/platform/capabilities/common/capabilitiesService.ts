a154d699a72b423f7c09325032d3257be9f3c8aa
Richard Smith <rismi@microsoft.com>
2019-11-14 15:56:43 -0800

    Finish merge with master

--- src/sql/platform/capabilities/common/capabilitiesService.ts
+++ src/sql/platform/capabilities/common/capabilitiesService.ts
@@ -20,6 +20,7 @@ export const clientCapabilities = {
 export interface ConnectionProviderProperties {
 	providerId: string;
 	displayName: string;
+	azureResource?: azdata.AzureResource;
 	connectionOptions: azdata.ConnectionOption[];
 }
 
