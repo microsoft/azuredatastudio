bed7befb181ea2712244c79e6e4d464a09b413c6
Cory Rivera <corivera@microsoft.com>
2019-11-22 17:36:31 -0800

    Add getProvidersForFileName tests.

--- src/sql/workbench/contrib/notebook/test/electron-browser/notebookUtils.test.ts
+++ src/sql/workbench/contrib/notebook/test/electron-browser/notebookUtils.test.ts
@@ -4,11 +4,29 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as assert from 'assert';
-import { nb } from 'azdata';
+import * as TypeMoq from 'typemoq';
 
-import { tryMatchCellMagic, getHostAndPortFromEndpoint, isStream } from 'sql/workbench/contrib/notebook/browser/models/notebookUtils';
+import { nb } from 'azdata';
+import { tryMatchCellMagic, getHostAndPortFromEndpoint, isStream, getProvidersForFileName } from 'sql/workbench/contrib/notebook/browser/models/notebookUtils';
+import { INotebookService, DEFAULT_NOTEBOOK_FILETYPE, DEFAULT_NOTEBOOK_PROVIDER } from 'sql/workbench/services/notebook/browser/notebookService';
+import { NotebookServiceStub } from 'sql/workbench/contrib/notebook/test/electron-browser/common';
 
 suite('notebookUtils', function (): void {
+	const mockNotebookService = TypeMoq.Mock.ofType<INotebookService>(NotebookServiceStub);
+	const defaultTestProvider = 'testDefaultProvider';
+	const testProvider = 'testProvider';
+
+	function setupMockNotebookService() {
+		mockNotebookService.setup(n => n.getProvidersForFileType(TypeMoq.It.isAnyString()))
+			.returns((fileName, service) => {
+				if (fileName === DEFAULT_NOTEBOOK_FILETYPE) {
+					return [defaultTestProvider];
+				} else {
+					return [testProvider];
+				}
+			});
+	}
+
 	test('isStream Test', async function (): Promise<void> {
 		let result = isStream(<nb.ICellOutput>{
 			output_type: 'stream'
@@ -27,52 +45,66 @@ suite('notebookUtils', function (): void {
 	});
 
 	test('getProvidersForFileName Test', async function (): Promise<void> {
+		setupMockNotebookService();
 
+		let result = getProvidersForFileName('', mockNotebookService.object);
+		assert.deepStrictEqual(result, [defaultTestProvider]);
+
+		result = getProvidersForFileName('fileWithoutExtension', mockNotebookService.object);
+		assert.deepStrictEqual(result, [defaultTestProvider]);
+
+		result = getProvidersForFileName('test.sql', mockNotebookService.object);
+		assert.deepStrictEqual(result, [testProvider]);
+
+		mockNotebookService.setup(n => n.getProvidersForFileType(TypeMoq.It.isAnyString()))
+			.returns(() => undefined);
+		result = getProvidersForFileName('test.sql', mockNotebookService.object);
+		assert.deepStrictEqual(result, [DEFAULT_NOTEBOOK_PROVIDER]);
 	});
 
 	test('getStandardKernelsForProvider Test', async function (): Promise<void> {
-
+		setupMockNotebookService();
 	});
 
 	test('tryMatchCellMagic Test', async function (): Promise<void> {
 		let result = tryMatchCellMagic(undefined);
-		assert.equal(result, undefined);
+		assert.strictEqual(result, undefined);
 
 		result = tryMatchCellMagic('    ');
-		assert.equal(result, undefined);
+		assert.strictEqual(result, undefined);
 
 		result = tryMatchCellMagic('text');
-		assert.equal(result, undefined);
+		assert.strictEqual(result, undefined);
 
 		result = tryMatchCellMagic('%%sql');
-		assert.equal(result, 'sql');
+		assert.strictEqual(result, 'sql');
 
 		result = tryMatchCellMagic('%%');
-		assert.equal(result, undefined);
+		assert.strictEqual(result, undefined);
 
 		result = tryMatchCellMagic('%% sql');
-		assert.equal(result, undefined);
+		assert.strictEqual(result, undefined);
 	});
 
 	test('getHostAndPortFromEndpoint Test', async function (): Promise<void> {
 		let result = getHostAndPortFromEndpoint('https://localhost:1433');
-		assert.equal(result.host, 'localhost');
-		assert.equal(result.port, '1433');
+		assert.strictEqual(result.host, 'localhost');
+		assert.strictEqual(result.port, '1433');
 
 		result = getHostAndPortFromEndpoint('tcp://localhost,1433');
-		assert.equal(result.host, 'localhost');
-		assert.equal(result.port, '1433');
+		assert.strictEqual(result.host, 'localhost');
+		assert.strictEqual(result.port, '1433');
 
 		result = getHostAndPortFromEndpoint('tcp://localhost');
-		assert.equal(result.host, 'localhost');
-		assert.equal(result.port, undefined);
+		assert.strictEqual(result.host, 'localhost');
+		assert.strictEqual(result.port, undefined);
 
 		result = getHostAndPortFromEndpoint('localhost');
-		assert.equal(result.host, '');
-		assert.equal(result.port, undefined);
+		assert.strictEqual(result.host, '');
+		assert.strictEqual(result.port, undefined);
 
 		result = getHostAndPortFromEndpoint('localhost:1433');
-		assert.equal(result.host, '');
-		assert.equal(result.port, undefined);
+		assert.strictEqual(result.host, '');
+		assert.strictEqual(result.port, undefined);
 	});
 });
