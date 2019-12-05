21b24352166cccaf7d99d0cad3bb2a0df1f95847
Anthony Dresser <anthonydresserdev@gmail.com>
2019-12-05 06:22:05 +0000

    fix copy artifacts

--- build/azure-pipelines/common/copyArtifacts.ts
+++ build/azure-pipelines/common/copyArtifacts.ts
@@ -7,9 +7,19 @@
 
 import * as vfs from 'vinyl-fs';
 
+const files = [
+	'.build/external/extensions/**/*.vsix', // external extensions
+	'.build/win32-x64/**/*.{exe,zip}', // windows binaries
+	'.build/linux/sha256hashes.txt', // linux hashes
+	'.build/linux/deb/amd64/deb/*.deb', // linux debs
+	'.build/linux/rpm/x86_64/*.rpm', // linux rpms
+	'.build/linux/archive/*.zip', // linux zip
+	'.build/darwin/*.zip' // darwin zip
+];
+
 async function main() {
 	return new Promise((resolve, reject) => {
-		const stream = vfs.src('.build/**/*.{deb,rpm,zip,txt,exe}', { base: '.' })
+		const stream = vfs.src(files, { base: '.build' })
 			.pipe(vfs.dest(process.env.BUILD_ARTIFACTSTAGINGDIRECTORY!));
 
 		stream.on('end', () => resolve());
