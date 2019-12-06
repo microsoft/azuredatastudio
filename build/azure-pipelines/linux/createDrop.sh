33ad688f8e5d4c1ee76555b0be5e403d1193df62
Anthony Dresser <anthonydresserdev@gmail.com>
2019-12-06 07:46:00 +0000

    fix linux drop

--- build/azure-pipelines/linux/createDrop.sh
+++ build/azure-pipelines/linux/createDrop.sh
@@ -25,7 +25,7 @@ rm -rf $ROOT/azuredatastudio-server-*.tar.*
 
 # create docker
 mkdir -p $REPO/.build/docker
-docker build -t azuredatastudio-server -f $REPO/build/azure-pipelines/docker/Dockerfile $SERVER_BUILD_NAME
+docker build -t azuredatastudio-server -f $REPO/build/azure-pipelines/docker/Dockerfile $ROOT/$SERVER_BUILD_NAME
 docker save azuredatastudio-server | gzip > $REPO/.build/docker/azuredatastudio-server-docker.tar.gz
 
 node build/azure-pipelines/common/copyArtifacts.js
