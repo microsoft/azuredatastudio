5713c2d77f4d8b7e6851d180d0718f214310d40d
Anthony Dresser <anthonydresserdev@gmail.com>
2019-12-07 01:31:20 +0000

    just use mkdir

--- build/azure-pipelines/win32/createDrop.ps1
+++ build/azure-pipelines/win32/createDrop.ps1
@@ -12,8 +12,8 @@ $ServerZipLocation = "$Repo\.build\win32-$Arch\server"
 $ServerZip = "$ServerZipLocation\azuredatastudio-server-win32-$Arch.zip"
 
 # Create server archive
-exec { New-Item -Path $ServerZipLocation -Type Directory }
-exec { Rename-Item -Path $LegacyServer -NewName $ServerName }
-exec { .\node_modules\7zip\7zip-lite\7z.exe a -tzip $ServerZip $Server -r }
+exec { mkdir -p $ServerZipLocation } "Make Directory"
+exec { Rename-Item -Path $LegacyServer -NewName $ServerName } "Rename Item"
+exec { .\node_modules\7zip\7zip-lite\7z.exe a -tzip $ServerZip $Server -r } "Zip Server"
 
-exec { node build/azure-pipelines/common/copyArtifacts.js }
+exec { node build/azure-pipelines/common/copyArtifacts.js } "Copy Artifacts"
