e585d4d21ef30f96bac2d79cd1fa3a86018b0827
Anthony Dresser <anthonydresserdev@gmail.com>
2019-12-07 00:38:59 +0000

    ensure windows drop location exists

--- build/azure-pipelines/win32/createDrop.ps1
+++ build/azure-pipelines/win32/createDrop.ps1
@@ -8,9 +8,11 @@ $Root = "$Repo\.."
 $LegacyServer = "$Root\azuredatastudio-reh-win32-$Arch"
 $ServerName = "azuredatastudio-server-win32-$Arch"
 $Server = "$Root\$ServerName"
-$ServerZip = "$Repo\.build\win32-$Arch\server\azuredatastudio-server-win32-$Arch.zip"
+$ServerZipLocation = "$Repo\.build\win32-$Arch\server"
+$ServerZip = "$ServerZipLocation\azuredatastudio-server-win32-$Arch.zip"
 
 # Create server archive
+exec { New-Item -Path $ServerZipLocation -Type Directory }
 exec { Rename-Item -Path $LegacyServer -NewName $ServerName }
 exec { .\node_modules\7zip\7zip-lite\7z.exe a -tzip $ServerZip $Server -r }
 
