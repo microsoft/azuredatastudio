12c04c993654a7b087263fb46eade73b89757b2e
Cory Rivera <corivera@microsoft.com>
2019-12-09 15:55:51 -0800

    Add removeNode case.

--- src/sql/workbench/contrib/notebook/test/browser/common.ts
+++ src/sql/workbench/contrib/notebook/test/browser/common.ts
@@ -58,3 +58,93 @@ export class NotebookComponentStub implements INotebookEditor {
 		throw new Error('Method not implemented.');
 	}
 }
+
+export class NodeStub implements Node {
+	baseURI: string;
+	childNodes: NodeListOf<ChildNode>;
+	firstChild: ChildNode;
+	isConnected: boolean;
+	lastChild: ChildNode;
+	namespaceURI: string;
+	nextSibling: ChildNode;
+	nodeName: string;
+	nodeType: number;
+	nodeValue: string;
+	ownerDocument: Document;
+	parentElement: HTMLElement;
+	parentNode: Node & ParentNode;
+	previousSibling: Node;
+	textContent: string;
+	appendChild<T extends Node>(newChild: T): T {
+		throw new Error('Method not implemented.');
+	}
+	cloneNode(deep?: boolean): Node {
+		throw new Error('Method not implemented.');
+	}
+	compareDocumentPosition(other: Node): number {
+		throw new Error('Method not implemented.');
+	}
+	contains(other: Node): boolean {
+		throw new Error('Method not implemented.');
+	}
+	getRootNode(options?: GetRootNodeOptions): Node {
+		throw new Error('Method not implemented.');
+	}
+	hasChildNodes(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	insertBefore<T extends Node>(newChild: T, refChild: Node): T {
+		throw new Error('Method not implemented.');
+	}
+	isDefaultNamespace(namespace: string): boolean {
+		throw new Error('Method not implemented.');
+	}
+	isEqualNode(otherNode: Node): boolean {
+		throw new Error('Method not implemented.');
+	}
+	isSameNode(otherNode: Node): boolean {
+		throw new Error('Method not implemented.');
+	}
+	lookupNamespaceURI(prefix: string): string {
+		throw new Error('Method not implemented.');
+	}
+	lookupPrefix(namespace: string): string {
+		throw new Error('Method not implemented.');
+	}
+	normalize(): void {
+		throw new Error('Method not implemented.');
+	}
+	removeChild<T extends Node>(oldChild: T): T {
+		throw new Error('Method not implemented.');
+	}
+	replaceChild<T extends Node>(newChild: Node, oldChild: T): T {
+		throw new Error('Method not implemented.');
+	}
+	ATTRIBUTE_NODE: number;
+	CDATA_SECTION_NODE: number;
+	COMMENT_NODE: number;
+	DOCUMENT_FRAGMENT_NODE: number;
+	DOCUMENT_NODE: number;
+	DOCUMENT_POSITION_CONTAINED_BY: number;
+	DOCUMENT_POSITION_CONTAINS: number;
+	DOCUMENT_POSITION_DISCONNECTED: number;
+	DOCUMENT_POSITION_FOLLOWING: number;
+	DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC: number;
+	DOCUMENT_POSITION_PRECEDING: number;
+	DOCUMENT_TYPE_NODE: number;
+	ELEMENT_NODE: number;
+	ENTITY_NODE: number;
+	ENTITY_REFERENCE_NODE: number;
+	NOTATION_NODE: number;
+	PROCESSING_INSTRUCTION_NODE: number;
+	TEXT_NODE: number;
+	addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void {
+		throw new Error('Method not implemented.');
+	}
+	dispatchEvent(event: Event): boolean {
+		throw new Error('Method not implemented.');
+	}
+	removeEventListener(type: string, callback: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void {
+		throw new Error('Method not implemented.');
+	}
+}
--- src/sql/workbench/contrib/notebook/test/browser/notebookInput.test.ts
+++ src/sql/workbench/contrib/notebook/test/browser/notebookInput.test.ts
@@ -13,6 +13,7 @@ import { UntitledNotebookInput } from 'sql/workbench/contrib/notebook/common/mod
 import { FileNotebookInput } from 'sql/workbench/contrib/notebook/common/models/fileNotebookInput';
 import { IConnectionProfile } from 'sql/platform/connection/common/interfaces';
 import { UntitledTextEditorModel } from 'vs/workbench/common/editor/untitledTextEditorModel';
+import { NodeStub } from 'sql/workbench/contrib/notebook/test/browser/common';
 
 suite('Notebook Input', function (): void {
 	const instantiationService = workbenchInstantiationService();
@@ -73,101 +74,22 @@ suite('Notebook Input', function (): void {
 		input.container = testContainer;
 		assert.strictEqual(input.container, testContainer);
 
+		// Dispose old container when setting new one
+		let removedContainer: HTMLElement;
 		let mockParentNode = TypeMoq.Mock.ofType<Node>(NodeStub);
-		mockParentNode.setup(e => e.removeChild(TypeMoq.It.isAny()));
+		mockParentNode.setup(e => e.removeChild(TypeMoq.It.isAny())).returns(oldChild => {
+			removedContainer = oldChild;
+			return oldChild;
+		});
 		testContainer = <HTMLElement>{ parentNode: mockParentNode.object };
 		input.container = testContainer;
 		assert.strictEqual(input.container, testContainer);
+
+		let oldContainer = testContainer;
+		testContainer = <HTMLElement>{};
+		input.container = testContainer;
+		assert.strictEqual(input.container, testContainer);
 		mockParentNode.verify(e => e.removeChild(TypeMoq.It.isAny()), TypeMoq.Times.once());
+		assert.strictEqual(removedContainer, oldContainer);
 	});
 });
-
-class NodeStub implements Node {
-	baseURI: string;
-	childNodes: NodeListOf<ChildNode>;
-	firstChild: ChildNode;
-	isConnected: boolean;
-	lastChild: ChildNode;
-	namespaceURI: string;
-	nextSibling: ChildNode;
-	nodeName: string;
-	nodeType: number;
-	nodeValue: string;
-	ownerDocument: Document;
-	parentElement: HTMLElement;
-	parentNode: Node & ParentNode;
-	previousSibling: Node;
-	textContent: string;
-	appendChild<T extends Node>(newChild: T): T {
-		throw new Error('Method not implemented.');
-	}
-	cloneNode(deep?: boolean): Node {
-		throw new Error('Method not implemented.');
-	}
-	compareDocumentPosition(other: Node): number {
-		throw new Error('Method not implemented.');
-	}
-	contains(other: Node): boolean {
-		throw new Error('Method not implemented.');
-	}
-	getRootNode(options?: GetRootNodeOptions): Node {
-		throw new Error('Method not implemented.');
-	}
-	hasChildNodes(): boolean {
-		throw new Error('Method not implemented.');
-	}
-	insertBefore<T extends Node>(newChild: T, refChild: Node): T {
-		throw new Error('Method not implemented.');
-	}
-	isDefaultNamespace(namespace: string): boolean {
-		throw new Error('Method not implemented.');
-	}
-	isEqualNode(otherNode: Node): boolean {
-		throw new Error('Method not implemented.');
-	}
-	isSameNode(otherNode: Node): boolean {
-		throw new Error('Method not implemented.');
-	}
-	lookupNamespaceURI(prefix: string): string {
-		throw new Error('Method not implemented.');
-	}
-	lookupPrefix(namespace: string): string {
-		throw new Error('Method not implemented.');
-	}
-	normalize(): void {
-		throw new Error('Method not implemented.');
-	}
-	removeChild<T extends Node>(oldChild: T): T {
-		throw new Error('Method not implemented.');
-	}
-	replaceChild<T extends Node>(newChild: Node, oldChild: T): T {
-		throw new Error('Method not implemented.');
-	}
-	ATTRIBUTE_NODE: number;
-	CDATA_SECTION_NODE: number;
-	COMMENT_NODE: number;
-	DOCUMENT_FRAGMENT_NODE: number;
-	DOCUMENT_NODE: number;
-	DOCUMENT_POSITION_CONTAINED_BY: number;
-	DOCUMENT_POSITION_CONTAINS: number;
-	DOCUMENT_POSITION_DISCONNECTED: number;
-	DOCUMENT_POSITION_FOLLOWING: number;
-	DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC: number;
-	DOCUMENT_POSITION_PRECEDING: number;
-	DOCUMENT_TYPE_NODE: number;
-	ELEMENT_NODE: number;
-	ENTITY_NODE: number;
-	ENTITY_REFERENCE_NODE: number;
-	NOTATION_NODE: number;
-	PROCESSING_INSTRUCTION_NODE: number;
-	TEXT_NODE: number;
-	addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void {
-		throw new Error('Method not implemented.');
-	}
-	dispatchEvent(event: Event): boolean {
-		throw new Error('Method not implemented.');
-	}
-	removeEventListener(type: string, callback: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void {
-		throw new Error('Method not implemented.');
-	}
-}
