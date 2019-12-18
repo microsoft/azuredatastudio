b4415f52e842a9337df34b81f7090d5548626977
Anthony Dresser <andresse@microsoft.com>
2019-12-17 18:34:29 -0800

    wip

--- extensions/workspace-fun/src/extension.ts
+++ extensions/workspace-fun/src/extension.ts
@@ -15,4 +15,44 @@ export function activate() {
 			}
 		}
 	});
+
+	vscode.workspace.registerFileSystemProvider('json', new JSONFileSystemProvider());
+}
+
+
+class JSONFileSystemProvider implements vscode.FileSystemProvider {
+	private readonly _onDidChangeFile = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
+	public readonly onDidChangeFile = this._onDidChangeFile.event;
+
+	watch(uri: vscode.Uri, options: { recursive: boolean; excludes: string[]; }): vscode.Disposable {
+		throw new Error('Method not implemented.');
+	}
+
+	stat(uri: vscode.Uri): vscode.FileStat | Thenable<vscode.FileStat> {
+		throw new Error('Method not implemented.');
+	}
+
+	readDirectory(uri: vscode.Uri): [string, vscode.FileType][] | Thenable<[string, vscode.FileType][]> {
+		throw new Error('Method not implemented.');
+	}
+
+	createDirectory(uri: vscode.Uri): void | Thenable<void> {
+		throw new Error('Method not implemented.');
+	}
+
+	readFile(uri: vscode.Uri): Uint8Array | Thenable<Uint8Array> {
+		throw new Error('Method not implemented.');
+	}
+
+	writeFile(uri: vscode.Uri, content: Uint8Array, options: { create: boolean; overwrite: boolean; }): void | Thenable<void> {
+		throw new Error('Method not implemented.');
+	}
+
+	delete(uri: vscode.Uri, options: { recursive: boolean; }): void | Thenable<void> {
+		throw new Error('Method not implemented.');
+	}
+
+	rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean; }): void | Thenable<void> {
+		throw new Error('Method not implemented.');
+	}
 }
