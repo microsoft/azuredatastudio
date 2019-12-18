952fd9a4cb13bad377e5007c3eb54aef1cd91e98
Anthony Dresser <anthonydresserdev@gmail.com>
2019-12-18 00:20:48 -0800

    update some of the readdirectory and stat

--- extensions/workspace-fun/src/extension.ts
+++ extensions/workspace-fun/src/extension.ts
@@ -33,26 +33,73 @@ class JSONFileSystemProvider implements vscode.FileSystemProvider {
 	private readonly _onDidChangeFile = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
 	public readonly onDidChangeFile = this._onDidChangeFile.event;
 
+	private childrenMap = new Map<vscode.Uri, [string, vscode.FileType][]>();
+
 	watch(uri: vscode.Uri, options: { recursive: boolean; excludes: string[]; }): vscode.Disposable {
 		return vscode.workspace.createFileSystemWatcher(uri.fsPath);
 	}
 
 	async stat(uri: vscode.Uri): Promise<vscode.FileStat> {
-		if (uri.path.endsWith('.json')) {
+		const extLocation = uri.path.lastIndexOf('.json');
+		const file = uri.path.slice(0, extLocation + 5);
+		let json = JSON.parse((await vscode.workspace.fs.readFile(uri.with({ scheme: 'file', path: file }))).toString());
+
+		if (extLocation + 5 !== uri.path.length) {
+			const indexPath = uri.path.slice(extLocation + 6);
+			for (const index of indexPath.split('/')) {
+				json = json[index];
+			}
+
+			const type = typeof json as 'string' | 'number' | 'boolean' | 'object';
+			if (['string', 'number', 'boolean'].includes(type)) {
+				return { size: 0, ctime: 0, mtime: 0, type: vscode.FileType.Unknown };
+			} else {
+				return { size: 0, ctime: 0, mtime: 0, type: vscode.FileType.Directory };
+			}
+		} else {
 			const stats = await vscode.workspace.fs.stat(uri.with({ scheme: 'file' }));
 			return { size: stats.size, ctime: stats.ctime, mtime: stats.mtime, type: vscode.FileType.Directory };
-		} else {
-			return { size: 0, ctime: 0, mtime: 0, type: vscode.FileType.Unknown };
 		}
 	}
 
 	async readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
-		let json = JSON.parse((await vscode.workspace.fs.readFile(uri.with({ scheme: 'file' }))).toString());
-		if (Array.isArray(json)) {
-			json = json[0];
+		if (!this.childrenMap.has(uri)) {
+			const extLocation = uri.path.lastIndexOf('.json');
+			const file = uri.path.slice(0, extLocation + 5);
+			let json = JSON.parse((await vscode.workspace.fs.readFile(uri.with({ scheme: 'file', path: file }))).toString());
+			let children: [string, vscode.FileType][];
+
+			if (extLocation + 5 !== uri.path.length) {
+				const indexPath = uri.path.slice(extLocation + 6);
+				for (const index of indexPath.split('/')) {
+					json = json[index];
+				}
+			}
+
+			if (Array.isArray(json)) {
+				children = json.map((v, i) => {
+					const type = typeof v as 'string' | 'number' | 'boolean' | 'object';
+					if (['string', 'number', 'boolean'].includes(type)) {
+						return [i.toString(), vscode.FileType.Unknown];
+					} else {
+						return [i.toString(), vscode.FileType.Directory];
+					}
+				});
+			} else {
+				children = Object.keys(json).map(k => {
+					const type = typeof json[k] as 'string' | 'number' | 'boolean' | 'object';
+					if (type === 'object') {
+						return [k, vscode.FileType.Directory];
+					} else {
+						return [k, vscode.FileType.Unknown];
+					}
+				});
+			}
+
+			this.childrenMap.set(uri, children);
 		}
 
-		return Object.keys(json).map(k => [k, vscode.FileType.Unknown]);
+		return this.childrenMap.get(uri)!;
 	}
 
 	createDirectory(uri: vscode.Uri): void | Thenable<void> {
