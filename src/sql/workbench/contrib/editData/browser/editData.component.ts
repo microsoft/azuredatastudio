c6d38b08bdf9920197a1ee59a9e11ec08c420cd2
Alex Ma <alma1@microsoft.com>
2020-01-02 14:04:49 -0800

    minor change

--- src/sql/workbench/contrib/editData/browser/editData.component.ts
+++ src/sql/workbench/contrib/editData/browser/editData.component.ts
@@ -172,9 +172,7 @@ export class EditDataComponent extends GridParentComponent implements OnInit, On
 			let returnVal = '';
 			// replace the line breaks with space since the edit text control cannot
 			// render line breaks and strips them, updating the value.
-			let isDBCellValue = Services.DBCellValue.isDBCellValue(value);
-			let isNullValue = (isDBCellValue && value.isNull === true);
-			if (isDBCellValue && !isNullValue) {
+			if (Services.DBCellValue.isDBCellValue(value) && !value.isNull) {
 				returnVal = this.spacefyLinebreaks(value.displayValue);
 			} else if (typeof value === 'string') {
 				returnVal = this.spacefyLinebreaks(value);
