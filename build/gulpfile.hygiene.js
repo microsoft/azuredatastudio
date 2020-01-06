51cd7a7ad6439579c1b5b84fbe07c0a3d2170335
Kim Santiago <kisantia@microsoft.com>
2020-01-06 15:28:26 -0800

    move change up a couple lines

--- build/gulpfile.hygiene.js
+++ build/gulpfile.hygiene.js
@@ -103,9 +103,9 @@ const indentationFilter = [
 	'!extensions/admin-tool-ext-win/ssmsmin/**',
 	'!extensions/resource-deployment/notebooks/**',
 	'!extensions/mssql/notebooks/**',
+	'!extensions/integration-tests/testData/**',
 	'!extensions/big-data-cluster/src/bigDataCluster/controller/apiGenerated.ts',
-	'!extensions/big-data-cluster/src/bigDataCluster/controller/clusterApiGenerated2.ts',
-	'!extensions/integration-tests/testData/**'
+	'!extensions/big-data-cluster/src/bigDataCluster/controller/clusterApiGenerated2.ts'
 ];
 
 const copyrightFilter = [
