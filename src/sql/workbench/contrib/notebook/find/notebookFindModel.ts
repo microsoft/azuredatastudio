a81c50e4e75a10bcde7c54a1a17ef3302b0dccca
Madhuri Koripalli <makoripa@microsoft.com>
2020-01-22 16:17:12 -0800

    test

--- src/sql/workbench/contrib/notebook/find/notebookFindModel.ts
+++ src/sql/workbench/contrib/notebook/find/notebookFindModel.ts
@@ -566,7 +566,7 @@ export class NotebookFindModel extends Disposable implements INotebookFindModel 
 		let searchText: string = input.substr(index);
 		while (findResults.length < maxMatches && searchText.indexOf(exp) > -1) {
 			if (wholeWord) {
-				// word in itself with no special characters around \\bword\\b, word that begins or ends with special character \\sword\\s
+				// word with no special characters around \\bword\\b, word that begins or ends with special character \\sword\\s
 				let wholeWordRegex = new RegExp(`(?:\\b|\\s)${this.escapeRegExp(exp)}(?:\\b|\\s)`);
 				start = searchText.search(wholeWordRegex) + 1;
 				if (start < 1) {
