eb60b7394b4a83432df9d2eec5ef557776297e52
Amir Omidi <amomidi@microsoft.com>
2020-01-29 13:24:05 -0800

    Show message to user on failure

--- src/sql/workbench/contrib/accounts/browser/accountDialog.ts
+++ src/sql/workbench/contrib/accounts/browser/accountDialog.ts
@@ -218,7 +218,12 @@ export class AccountDialog extends Modal {
 
 			const v = vals.filter(v => v.view.title === pickedValue)?.[0];
 
-			v?.addAccountAction?.run();
+			if (!v) {
+				this._notificationService.error(localize('accountDialog.noCloudsRegistered', "You have no clouds enabled. Go to Settings -> Search Azure Account Configuration -> Enable at least one cloud"));
+				return;
+			}
+
+			v.addAccountAction.run();
 		}));
 
 		DOM.append(container, this._noaccountViewContainer);
