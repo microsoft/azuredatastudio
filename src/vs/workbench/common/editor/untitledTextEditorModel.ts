df26c7175bcda3819c074c6a9f07e8cd67bfe519
Alex Ma <alma1@microsoft.com>
2020-02-05 16:10:02 -0800

    disable updateNameFromFirstLine

--- src/vs/workbench/common/editor/untitledTextEditorModel.ts
+++ src/vs/workbench/common/editor/untitledTextEditorModel.ts
@@ -242,7 +242,8 @@ export class UntitledTextEditorModel extends BaseTextEditorModel implements IUnt
 	}
 
 	private updateNameFromFirstLine(): void {
-		if (this.hasAssociatedFilePath) {
+		// {{SQL CARBON EDIT}} - For Query Editor, must not change name even without file path.
+		if (!this.hasAssociatedFilePath && this.hasAssociatedFilePath) {
 			return; // not in case of an associated file path
 		}
 
