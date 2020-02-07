b8d5c6459a900ef0e3dc94dd76d8ac428766bdd2
Alex Ma <alma1@microsoft.com>
2020-02-07 11:40:12 -0800

    added working revert

--- src/sql/workbench/contrib/editData/browser/editDataGridPanel.ts
+++ src/sql/workbench/contrib/editData/browser/editDataGridPanel.ts
@@ -33,6 +33,7 @@ import { ILogService } from 'vs/platform/log/common/log';
 import { deepClone, assign } from 'vs/base/common/objects';
 import { Emitter, Event } from 'vs/base/common/event';
 import { equals } from 'vs/base/common/arrays';
+import * as DOM from 'vs/base/browser/dom';
 
 export class EditDataGridPanel extends GridParentComponent {
 	// The time(in milliseconds) we wait before refreshing the grid.
@@ -110,6 +111,7 @@ export class EditDataGridPanel extends GridParentComponent {
 	onInit(): void {
 		const self = this;
 		this.baseInit();
+		this._register(DOM.addDisposableListener(this.nativeElement, DOM.EventType.KEY_DOWN, e => this.tryHandleKeyEvent(new StandardKeyboardEvent(e))));
 
 		// Add the subscription to the list of things to be disposed on destroy, or else on a new component init
 		// may get the "destroyed" object still getting called back.
