f10a504f45997f69cb3bf839f67af4e9051b37ac
Chris LaFreniere <40371649+chlafreniere@users.noreply.github.com>
2020-02-13 11:14:51 -0800

    Notebooks: Listen for Enter on Add Package Tab (#9124)
    * Pressing enter searches for package
    
    * cleanup

--- extensions/notebook/src/dialog/managePackages/addNewPackageTab.ts
+++ extensions/notebook/src/dialog/managePackages/addNewPackageTab.ts
@@ -33,6 +33,10 @@ export class AddNewPackageTab {
 
 		this.addNewPkgTab.registerContent(async view => {
 			this.newPackagesSearchBar = view.modelBuilder.inputBox().withProperties({ width: '400px' }).component();
+			// Search package by name when pressing enter
+			this.newPackagesSearchBar.onEnterKeyPressed(async () => {
+				await this.loadNewPackageInfo();
+			});
 
 			this.packagesSearchButton = view.modelBuilder.button()
 				.withProperties<azdata.ButtonProperties>({
