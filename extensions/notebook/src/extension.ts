9aae1f501826cc785ce526ee34b1d051e62403d2
Madhuri Koripalli <makoripa@microsoft.com>
2020-02-14 15:09:56 -0800

    throw error if filenames have unsupported chars

--- extensions/notebook/src/extension.ts
+++ extensions/notebook/src/extension.ts
@@ -264,50 +264,60 @@ async function updateBookMetadata(bookPath: string): Promise<void> {
 			let firstLevelUrls = result.match(/^(?:\s+$[\r\n]+)+(- url: [a-zA-Z0-9\\.\s-]+$[\r\n]+)/gm);
 			let title: string;
 			let replacedString: string;
-			if (firstLevelUrls) {
-				firstLevelUrls.map(url => {
-					title = url.substring(url.lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
-					// if there are sub folders inside content, just add this without sections
-					if (!headers) {
-						replacedString = `\n- title: ${title}\n  url: /${title}\n  not_numbered: true\n  expand_sections: true\n  sections:\n`;
-					} else {
-						replacedString = `\n- title: ${title}\n  url: /${title}\n  not_numbered: true\n`;
-					}
-					result = result.replace(url, replacedString);
-				});
-			}
-			if (headers) {
-				headers.map(header => {
-					title = header.substring(10).replace(/(\r\n|\n|\r)/gm, '');
-					// for the url you need to have home page on clicking on the header section
-					// check urls that are inside this folder and pick the first one as home
-					contentFolders.push(title.toLocaleLowerCase());
-					let index: number = urls.findIndex(x => x.indexOf(`${path.sep}${title.toLocaleLowerCase()}${path.sep}`) > -1);
-					let urlValue: string = urls[index].substring(urls[index].lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
-					replacedString = `- title: ${title}\n  url: /${title}/${urlValue}\n  not_numbered: true\n  expand_sections: true\n  sections:\n`;
-					result = result.replace(header, replacedString);
-					result = result.replace(urls[index], '');
-					urls.splice(index, 1);
-				});
-			}
-			if (urls) {
-				urls.map(url => {
-					title = url.substring(url.lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
-					let urlValue: string = title;
-					// if there are folders inside content, the url will be that foldrname/filename
-					if (contentFolders.length > -1) {
-						// get each folder from the path bread crumb and check if the last folder is inside content
-						let folders: string[] = url.substring(8).split(path.sep);
-						let parentFolder: number = contentFolders.findIndex(x => x === folders[folders.length - 2]);
-						if (parentFolder > -1) {
-							urlValue = `${contentFolders[parentFolder]}/${title}`;
+			if (firstLevelUrls || headers || urls) {
+				// there must only be one first level url
+				if (firstLevelUrls && firstLevelUrls.length === 1) {
+					firstLevelUrls.map(url => {
+						title = url.substring(url.lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
+						// if there are sub folders inside content, just add this without sections
+						if (!headers) {
+							let markdownUrl = urls[urls.length - 1];
+							title = markdownUrl.substring(markdownUrl.lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
+							replacedString = `\n- title: ${title}\n  url: /${title}\n  not_numbered: true\n  expand_sections: true\n  sections:\n` + url;
+							result = result.replace(markdownUrl, '');
+						} else {
+							replacedString = `\n- title: ${title}\n  url: /${title}\n  not_numbered: true\n`;
 						}
-					}
-					replacedString = `  - title: ${title}\n    url: /${urlValue}\n`;
-					result = result.replace(url, replacedString);
-				});
+						result = result.replace(url, replacedString);
+					});
+				}
+				if (headers) {
+					headers.map(header => {
+						title = header.substring(10).replace(/(\r\n|\n|\r)/gm, '');
+						// for the url you need to have home page on clicking on the header section
+						// check urls that are inside this folder and pick the first one as home
+						contentFolders.push(title.toLocaleLowerCase());
+						let filtered: string[] = urls.filter(x => x.indexOf(`${path.sep}${title.toLocaleLowerCase()}${path.sep}`) > -1);
+						// toc has markdown files listed at the bottom.
+						let index: number = urls.findIndex(x => x === filtered[filtered.length - 1]);
+						let urlValue: string = urls[index].substring(urls[index].lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
+						replacedString = `- title: ${title}\n  url: /${title}/${urlValue}\n  not_numbered: true\n  expand_sections: true\n  sections:\n`;
+						result = result.replace(header, replacedString);
+						result = result.replace(urls[index], '');
+						urls.splice(index, 1);
+					});
+				}
+				if (urls) {
+					urls.map(url => {
+						title = url.substring(url.lastIndexOf(path.sep) + 1).replace(/(\r\n|\n|\r)/gm, '');
+						let urlValue: string = title;
+						// if there are folders inside content, the url will be that foldrname/filename
+						if (contentFolders.length > 0) {
+							// get each folder from the path bread crumb and check if the last folder is inside content
+							let folders: string[] = url.substring(8).split(path.sep);
+							let parentFolder: number = contentFolders.findIndex(x => x === folders[folders.length - 2]);
+							if (parentFolder > -1) {
+								urlValue = `${contentFolders[parentFolder]}/${title}`;
+							}
+						}
+						replacedString = `  - title: ${title}\n    url: /${urlValue}\n`;
+						result = result.replace(url, replacedString);
+					});
+				}
+				fsPromises.writeFile(tocFilePath, result);
+			} else {
+				new ApiWrapper().showErrorMessage('File Name contains unsupported-characters (ex: underscores or spaces) by Jupyter Book');
 			}
-			fsPromises.writeFile(tocFilePath, result);
 		});
 		// update the book title
 		let configFilePath: string = path.join(bookPath, '_config.yml');
