10b681b3c845a53ceec663b5241cfb329f2ce49f
Charles Gagnon <chgagnon@microsoft.com>
2020-02-24 08:15:27 -0800

    Add Arc Resources to Azure view (#9271)
    * Initial work
    
    * Add setting change handler
    
    * Fix tests
    
    * Update loc names and add preview tag
    
    * Remove TODOs

---
+++ extensions/azurecore/src/localizedConstants.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as nls from 'vscode-nls';
+const localize = nls.loadMessageBundle();
+
+export const extensionName = localize('azurecore.extensionName', "Azure Accounts");
+
+export const requiresReload = localize('azurecore.requiresReload', "Modifying this setting requires reloading the window for all changes to take effect.");
+export const reload = localize('azurecore.reload', "Reload");
