01db78f7438dd65ab8da5531fa28e3ca62242bfa
Charles Gagnon <chgagnon@microsoft.com>
2020-03-02 07:54:13 -0800

    Add required property to checkbox and set it for deployment dialog (#9392)

--- src/sql/azdata.proposed.d.ts
+++ src/sql/azdata.proposed.d.ts
@@ -202,4 +202,8 @@ declare module 'azdata' {
 	export interface InputBoxProperties extends ComponentProperties {
 		validationErrorMessage?: string;
 	}
+
+	export interface CheckBoxProperties {
+		required?: boolean;
+	}
 }
