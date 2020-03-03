ca5436bebc9bbfc64ab114b51a7aa9d894b4ee3f
Kim Santiago <kisantia@microsoft.com>
2020-03-03 14:48:58 -0800

    move objects widget back to database home tab and reorder toolbar

--- src/sql/workbench/contrib/dashboard/browser/pages/databaseDashboardPage.contribution.ts
+++ src/sql/workbench/contrib/dashboard/browser/pages/databaseDashboardPage.contribution.ts
@@ -96,11 +96,21 @@ export const databaseDashboardSettingSchema: IJSONSchema = {
 			},
 			widget: {
 				'tasks-widget': [
-					'newQuery',
-					'mssqlCluster.task.newNotebook',
 					{ name: 'backup', when: '!mssql:iscloud && mssql:engineedition != 11' },
-					{ name: 'restore', when: '!mssql:iscloud && mssql:engineedition != 11' }
+					{ name: 'restore', when: '!mssql:iscloud && mssql:engineedition != 11' },
+					'newQuery',
+					'mssqlCluster.task.newNotebook'
 				]
+			}
+		},
+		{
+			name: 'Search',
+			gridItemConfig: {
+				sizex: 1,
+				sizey: 2
+			},
+			widget: {
+				'explorer-widget': {}
 			}
 		}
 	]
