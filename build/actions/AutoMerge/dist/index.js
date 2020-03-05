f07be3ed0a40ae544fdee51416ed6daa1c63b338
Amir Omidi <amomidi@microsoft.com>
2020-03-05 14:27:42 -0800

    Dist

--- build/actions/AutoMerge/dist/index.js
+++ build/actions/AutoMerge/dist/index.js
@@ -7847,6 +7847,10 @@ module.exports = isStream;
 
 "use strict";
 
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
