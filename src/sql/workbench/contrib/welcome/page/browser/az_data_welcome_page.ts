93f066133c611d9f22223737feaf1ab7d9ceedc1
Brian Brady (Insight Global <v-bbrady@microsoft.com>
2020-03-06 12:17:26 -0800

    updates markup for welcome page

--- src/sql/workbench/contrib/welcome/page/browser/az_data_welcome_page.ts
+++ src/sql/workbench/contrib/welcome/page/browser/az_data_welcome_page.ts
@@ -64,8 +64,8 @@ export default () => `
 						<div class="col">
 							<a class="header__bottom_nav__tile__link" href="command:registeredServers.addConnection">
 								<div class="header__bottom_nav__tile tile tile--connection content">
-									<h3>Create a Connection</h3>
-									<p>Install extensions to enhance the tool’s capabilities.</p>
+									<h3>${escape(localize("welcomePage.createConnection", "Create a Connection"))}</h3>
+									<p>${escape(localize("welcomePage.createConnectionBody", "Install extensions to enhance the tool’s capabilities."))}</p>
 									<div class="icon connection"></div>
 								</div>
 							</a>
@@ -74,8 +74,8 @@ export default () => `
 							<a class="header__bottom_nav__tile__link"
 								href="command:workbench.action.files.newUntitledFile">
 								<div class="header__bottom_nav__tile tile tile--query content">
-									<h3>Run a query</h3>
-									<p>Access your data in a query editor.</p>
+									<h3>${escape(localize("welcomePage.runQuery", "Run a query"))}</h3>
+									<p>${escape(localize("welcomePage.runQueryBody", "Access your data in a query editor."))}</p>
 									<div class="icon query"></div>
 								</div>
 							</a>
@@ -83,8 +83,8 @@ export default () => `
 						<div class="col">
 							<a class="header__bottom_nav__tile__link" href="command:notebook.command.new">
 								<div class="header__bottom_nav__tile tile tile--notebook content">
-									<h3>Create a notebook</h3>
-									<p>Start a local computational notebook.</p>
+									<h3>${escape(localize("welcomePage.createNotebook", "Create a notebook"))}</h3>
+									<p>${escape(localize("welcomePage.createNotebookBody", "Start a local computational notebook."))}</p>
 									<div class="icon notebook"></div>
 								</div>
 							</a>
@@ -92,29 +92,26 @@ export default () => `
 						<div class="col">
 							<a class="header__bottom_nav__tile__link" href="command:azdata.resource.deploy">
 								<div class="header__bottom_nav__tile tile tile--server content">
-									<h3>Deploy a server</h3>
-									<p>Install an image of SQL Server to a remote location.</p>
+									<h3>${escape(localize("welcomePage.deployServer", "Deploy a server"))}</h3>
+									<p>${escape(localize("welcomePage.deployServerBody", "Install an image of SQL Server to a remote location."))}</p>
 									<div class="icon server"></div>
 								</div>
 							</a>
 						</div>
 					</div>
-					<p class="showOnStartup"><input type="checkbox" id="showOnStartup" class="checkbox">
-						<label class="caption" for="showOnStartup">${escape(localize("welcomePage.showOnStartup", "Show welcome page on startup"))}</label>
-					</p>
 				</div>
 			</div>
 			<div class="ads_homepage__section content row ads_grid lg--cols-6 sm--cols-1">
 				<div class="col--lg--span-4 col--lg--start_1__span_4 col--sm--start_1__span_1 resources">
-					<h2>Resources</h2>
+					<h2>${escape(localize("welcomePage.resources", "Resources"))}</h2>
 					<div class="tabs">
 						<input class="input" name="tabs" type="radio" id="tab-1" checked="checked" />
-						<label class="label" for="tab-1">History</label>
+						<label class="label" for="tab-1">${escape(localize("welcomePage.history", "History"))}</label>
 						<div class="panel">
 							<div class="recent history">
 								<div class="flex flex--j_between list__header__container">
-									<h4 class="icon--document list__header">Name</h4>
-									<h4>Last Opened</h4>
+									<h4 class="icon--document list__header">${escape(localize("welcomePage.name", "Name"))}</h4>
+									<h4>${escape(localize("welcomePage.lastOpened", "Last Opened"))}</h4>
 								</div>
 								<ul class="list">
 									<!-- Filled programmatically -->
@@ -127,25 +124,29 @@ export default () => `
 							</div>
 						</div>
 					</div>
+					<p class="showOnStartup"><input type="checkbox" id="showOnStartup" class="checkbox">
+						<label class="caption" for="showOnStartup">${escape(localize("welcomePage.showOnStartup", "Show welcome page on startup"))}</label>
+					</p>
 				</div>
 				<div class="col--lg--start_5__span_2 col--sm--start_1__span_1">
 					<div class="links">
-						<h2>Useful Links</h2>
+						<h2>${escape(localize("welcomePage.usefuLinks", "Useful Links"))}</h2>
 						<h4>
 							<a class="link"
 								href="https://aka.ms/azuredatastudio">${escape(localize("welcomePage.gettingStarted",
 						"Getting Started"))}<span class="icon--link"</a>
 						</h4>
 						<p>
-						${escape(localize("welcomePage.gettingStartedDesciption",
+						${escape(localize("welcomePage.gettingStartedBody",
 							"Discover the capabilities offered by Aure Data Studio and learn how to make the most of them."))}
 						</p>
 						<h4>
 							<a class="link"
-								href="https://aka.ms/azuredatastudio">${escape(localize("welcomePage.productDocumentation",
+								href="https://aka.ms/azuredatastudio">${escape(localize("welcomePage.documentation",
 								"Documentation"))}<span class="icon--link"</a></a>
 						</h4>
-						<p>Discover the capabilities offered by Aure Data Studio and learn how to make the most of them.
+						<p>${escape(localize("welcomePage.documentationBody",
+									"Visit the documentation center for quickstarts, how-to guides, and references for PowerShell, APIs, etc."))}
 						</p>
 						<div class="videos_container row">
 							<div class="videos_container__video">
@@ -153,14 +154,16 @@ export default () => `
 									<source src="vid.mp4" type="video/mp4" />
 									Your browser does not support HTML5 video.
 								</video>
-								<h4>Overview of Azure Data Studio</h4>
+								<h4>${escape(localize("welcomePage.videoDescriptionOverview",
+										"Overview of Azure Data Studio"))}</h4>
 							</div>
 							<div class="videos_container__video">
 								<video width="100%" controls="">
 									<source src="vid.mp4" type="video/mp4" />
 									Your browser does not support HTML5 video.
 								</video>
-								<h4>Introduction to Azure Data Studio Notebooks | Data Exposed</h4>
+								<h4>${escape(localize("welcomePage.videoDescriptionIntroduction",
+											"Introduction to Azure Data Studio Notebooks | Data Exposed"))}</h4>
 							</div>
 						</div>
 					</div>
@@ -169,11 +172,12 @@ export default () => `
 			<div class="ads_homepage__section content extensions">
 				<div class="flex flex--j_between">
 					<h2>Extend your data studio</h2>
-					<a class="link--show_all" href="#">Show All <span class="entity">&rarr;</span></a>
+					<a class="link--show_all" href="#">${escape(localize("welcomePage.showAll",
+												"Show All"))} <span class="entity">&rarr;</span></a>
 				</div>
-				<div class="row ads_grid lg--cols-2 xxl--cols-30">
+				<div class="row ads_grid grip_gap--50 lg--cols-2 xxl--cols-8">
 					<div
-						class="ads_grid col--sm--start_1__span_14 row--xxl--start_1__span_4 col--lg--start_1__span_14 lg--cols-2 tile no_hover extension_pack">
+						class="ads_grid col--sm--start_1__span_2 row--xxl--start_1__span_4 col--xxl--start_1__span_4 lg--cols-2 tile no_hover extension_pack">
 						<div class="extension_pack__description">
 							<h2 class="extension_pack__header"></h2>
 							<p class="extension_pack__body"></p>
@@ -182,11 +186,12 @@ export default () => `
 							<div class="extension_pack__extension_list flex flex--d_column flex--j_evenly flex--a_start"></div>
 							<div class="flex flex--j_end extension_pack__btn_container flex flex--j_between flex--a_center"">
 							<div class=" extensionPack" href="#"></div>
-							<a class="a_self--end link--learn_more" href="#">Learn more <span class="entity">&rarr;</span></a>
+							<a class="a_self--end link--learn_more" href="#">${escape(localize("welcomePage.learnMore",
+													"Learn more "))} <span class="entity">&rarr;</span></a>
 						</div>
 					</div>
 				</div>
-				<div class="extension_list col col--sm--start_1__span_14 col--xxl--start_16__span_15 row--xxl--start_1__span_4 flex flex--d_column">
+				<div class="extension_list col col--sm--start_1__span_2 col--xxl--start_5__span_4 row--xxl--start_1__span_4 flex flex--d_column">
 					<!-- Dynamically populated -->
 				</div>
 				<br /><br /><br />
