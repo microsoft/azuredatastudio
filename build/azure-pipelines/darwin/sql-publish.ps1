8169595f093e96df46dc18420a691c7c659f44f6
Anthony Dresser <anthonydresserdev@gmail.com>
2020-03-18 23:02:25 +0000

    start publishing server bits

--- build/azure-pipelines/darwin/sql-publish.ps1
+++ build/azure-pipelines/darwin/sql-publish.ps1
@@ -15,5 +15,13 @@ $CommitId = $VersionJson.commit
 
 $ZipName = "azuredatastudio-darwin.zip"
 $Zip = "$artifactsDir\darwin\archive\$ZipName"
+$ServerZipName = "azuredatastudio-server-darwin.zip"
+$ServerZip = "$artifactsDir\darwin\server\$ServerZipName"
+$ServerZipNameWeb = "azuredatastudio-server-darwin-web.zip"
+$ServerZipWeb = "$artifactsDir\darwin\server\$ServerZipNameWeb"
 
 node $sourcesDir\build\azure-pipelines\common\publish.js $Quality darwin archive $ZipName $Version true $Zip $CommitId
+
+node $sourcesDir\build\azure-pipelines\common\publish.js $Quality "server-darwin" archive $ServerZipName true $ServerZip $CommitId
+
+node $sourcesDir\build\azure-pipelines\common\publish.js $Quality "server-darwin-web" archive $ServerZipNameWeb true $ServerZipWeb $CommitId
