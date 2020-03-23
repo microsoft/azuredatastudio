de0070df671db520d308cb0ea871ed206eb4de4d
Alan.Ren <alanren@microsoft.com>
2020-03-22 22:12:42 -0700

    fix properties widget

--- src/sql/workbench/browser/modelComponents/loadingSpinner.component.ts
+++ src/sql/workbench/browser/modelComponents/loadingSpinner.component.ts
@@ -4,20 +4,44 @@
  *--------------------------------------------------------------------------------------------*/
 
 import 'vs/css!./media/loadingComponent';
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import * as nls from 'vs/nls';
+import { status } from 'vs/base/browser/ui/aria/aria';
 
 @Component({
 	selector: 'loading-spinner',
 	template: `
 		<div class="modelview-loadingComponent-container" *ngIf="loading">
-			<div class="modelview-loadingComponent-spinner" *ngIf="loading" [title]=_loadingTitle #spinnerElement></div>
+			<div class="modelview-loadingComponent-spinner" *ngIf="loading" [title]="_loadingMessage" #spinnerElement></div>
 		</div>
 	`
 })
-export default class LoadingSpinner {
-	public readonly _loadingTitle = nls.localize('loadingMessage', "Loading");
+export default class LoadingSpinner implements OnChanges {
 
-	@Input() loading: boolean;
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes.loading !== undefined) {
+			const message = this.loading ? this._loadingMessage : this._loadingCompletedMessage;
+			status(message);
+		}
+	}
+
+	public readonly _defaultLoadingMessage = nls.localize('loadingMessage', "Loading");
+	public readonly _defaultLoadingCompletedMessage = nls.localize('loadingCompletedMessage', "Loading completed");
+
+	get _loadingMessage(): string {
+		return this.loadingMessage ? this.loadingMessage : this._defaultLoadingMessage;
+	}
+
+	get _loadingCompletedMessage(): string {
+		return this.loadingCompletedMessage ? this.loadingCompletedMessage : this._defaultLoadingCompletedMessage;
+	}
+
+	@Input()
+	loading: boolean;
+
+	@Input()
+	loadingMessage: string;
+
+	@Input()
+	loadingCompletedMessage: string;
 }
