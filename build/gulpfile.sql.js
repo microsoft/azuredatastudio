2b69fcfe3c2a03d0b48181029e17c382bc44541d
Anthony Dresser <andresse@microsoft.com>
2020-03-24 23:00:18 -0700

    fix script

--- build/gulpfile.sql.js
+++ build/gulpfile.sql.js
@@ -94,10 +94,10 @@ const formatStagedFiles = () => {
 	});
 };
 
-async function installService(configPath, platform) {
+async function installService(configPath, runtimId) {
 	const absoluteConfigPath = require.resolve(configPath);
 	const config = require(absoluteConfigPath);
-	const runtime = platform || await platform.getCurrent().runtimeId;
+	const runtime = runtimId || await platform.getCurrent().runtimeId;
 	// fix path since it won't be correct
 	config.installDirectory = path.join(path.dirname(absoluteConfigPath), config.installDirectory);
 	console.log('install diectory', config.installDirectory);
