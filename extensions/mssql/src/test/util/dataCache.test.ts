c115b1a14eeacf571b044181450804cd750567ea
Jeff Trimmer <jetrimme@microsoft.com>
2020-03-27 15:56:41 -0700

    remove blank line

--- extensions/mssql/src/test/util/dataCache.test.ts
+++ extensions/mssql/src/test/util/dataCache.test.ts
@@ -8,7 +8,6 @@ import 'mocha';
 import { should } from 'chai'; should();
 import * as TypeMoq from "typemoq";
 
-
 describe('DataItemCache', function (): void {
 
 	const testCacheItem = 'Test Cache Item';
