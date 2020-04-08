1868a7d3705e4dc32b438ab24303052ed2cc9f09
ADS Merger <andresse@microsoft.com>
2020-04-08 06:33:38 +0000

    Merge from vscode 10492ba146318412cbee8b76a8c630f226914734

--- src/sql/workbench/contrib/dashboard/browser/widgets/explorer/explorerWidget.component.ts
+++ src/sql/workbench/contrib/dashboard/browser/widgets/explorer/explorerWidget.component.ts
@@ -3,7 +3,6 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import 'vs/css!sql/media/icons/common-icons';
 import 'vs/css!./media/explorerWidget';
 
 import { Component, Inject, forwardRef, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
