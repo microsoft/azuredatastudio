c3e0541777eb423031fca9c4638970846f1d7328
Amir Omidi <amomidi@microsoft.com>
2020-04-08 13:35:21 -0700

    Update the internal URI

--- extensions/azurecore/src/azureResource/services/terminalService.ts
+++ extensions/azurecore/src/azureResource/services/terminalService.ts
@@ -37,7 +37,7 @@ const handleNeverUsed = async (): Promise<void> => {
 	const option = await vscode.window.showInformationMessage<TerminalMessageItem>(neverUsedString, openAzureShellButton, okButton);
 
 	if (option.action === TerminalOption.OPEN_SITE) {
-		vscode.env.openExternal(vscode.Uri.parse('https://shell.azure.com'));
+		vscode.env.openExternal(vscode.Uri.parse('https://aka.ms/AA83f8f'));
 	}
 };
 
