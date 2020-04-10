693b6bb00299456203c40de37ae5c6c517c955a6
Anthony Dresser <andresse@microsoft.com>
2020-04-10 14:59:06 -0700

    add docker build

---
+++ build/azure-pipelines/docker/createDrop.sh
@@ -0,0 +1,10 @@
+set -e
+REPO="$(pwd)"
+ROOT="$REPO/.."
+
+SERVER_BUILD_NAME="azuredatastudio-server-$PLATFORM_LINUX"
+
+# create docker
+mkdir -p $REPO/.build/docker
+docker build -t azuredatastudio-server -f $REPO/build/azure-pipelines/docker/Dockerfile $ROOT/$SERVER_BUILD_NAME
+docker save azuredatastudio-server | gzip > $REPO/.build/docker/azuredatastudio-server-docker.tar.gz
