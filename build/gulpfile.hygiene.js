dd6b95889809fe44b08c77b54116709ab7a5e65c
Anthony Dresser <andresse@microsoft.com>
2020-04-17 23:53:33 -0700

    Removing some unnecessary differences (#9495)
    * work on removing some unncessary differences
    
    * fix compile
    
    * skip another test

--- build/gulpfile.hygiene.js
+++ build/gulpfile.hygiene.js
@@ -67,7 +67,6 @@ const indentationFilter = [
 
 	// except multiple specific files
 	'!**/package.json',
-	'!**/package-lock.json', // {{SQL CARBON EDIT}}
 	'!**/yarn.lock',
 	'!**/yarn-error.log',
 
@@ -406,7 +405,7 @@ function createGitIndexVinyls(paths) {
 				return e(err);
 			}
 
-			cp.exec(`git show ":${relativePath}"`, { maxBuffer: 2000 * 1024, encoding: 'buffer' }, (err, out) => {
+			cp.exec(`git show :${relativePath}`, { maxBuffer: 2000 * 1024, encoding: 'buffer' }, (err, out) => {
 				if (err) {
 					return e(err);
 				}
