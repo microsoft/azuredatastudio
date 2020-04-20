0c67da2ca5b1bb8ac97c1648a0841d73384df4be
maddydev <makoripa@microsoft.com>
2020-04-20 11:12:40 -0700

    clean up unsavedBooks to providedBooks

--- extensions/notebook/src/book/bookTreeItem.ts
+++ extensions/notebook/src/book/bookTreeItem.ts
@@ -41,7 +41,7 @@ export class BookTreeItem extends vscode.TreeItem {
 			this.collapsibleState = book.treeItemCollapsibleState;
 			this._sections = book.page;
 			if (book.isUntitled) {
-				this.contextValue = 'unsavedBook';
+				this.contextValue = 'providedBook';
 			} else {
 				this.contextValue = 'savedBook';
 			}
