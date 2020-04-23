644246bd1f8df956236059cf5ec4c48bfa2a7099
chgagnon <chgagnon@microsoft.com>
2020-04-22 18:04:23 -0700

    Undo css addition

--- src/sql/base/browser/ui/propertiesContainer/propertiesContainer.component.ts
+++ src/sql/base/browser/ui/propertiesContainer/propertiesContainer.component.ts
@@ -2,8 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import 'vs/css!./media/propertiesContainer';
-
 import { Component, Inject, forwardRef, ChangeDetectorRef, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { EventType, addDisposableListener } from 'vs/base/browser/dom';
 import * as nls from 'vs/nls';
