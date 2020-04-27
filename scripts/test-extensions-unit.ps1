985f0f07944a883ab133d686dd6303a249d34b57
Benjin Dubishar <benjin.dubishar@gmail.com>
2020-04-27 13:50:28 -0700

    Added new powershell unit test driver

---
+++ scripts/test-extensions-unit.ps1
@@ -0,0 +1,64 @@
+# Runs Extension tests
+
+Push-Location "$PSScriptRoot\\.."
+
+$tempDirSuffix = "$(Get-Random)-$(Get-Date -Format "ss.ff")"
+$VSCODEUSERDATADIR = "$env:TEMP\\adsuser-$tempDirSuffix"
+$VSCODEEXTENSIONSDIR = "$env:TEMP\\adsext-$tempDirSuffix"
+
+Write-Host $VSCODEUSERDATADIR
+Write-Host $VSCODEEXTENSIONSDIR
+
+$extensions = @(
+	"admin-tool-ext-win",
+	"agent",
+	"azurecore",
+	"cms",
+	"dacpac",
+	"schema-compare",
+	"mssql",
+	"notebook",
+	"resource-deployment",
+	"machine-learning-services",
+	"sql-database-projects"
+)
+
+# Figure out which Electron to use for running tests
+If ([string]::IsNullOrEmpty($env:INTEGRATION_TEST_ELECTION_PATH)) {
+	# Run out of sources: no need to compile as code.bat takes care of it
+	$env:INTEGRATION_TEST_ELECTION_PATH=".\\scripts\\code.bat"
+	Write-Host "Running unit tests out of sources.";
+}
+Else {
+	# Run from a build: need to compile all test extensions
+	ForEach ($ext in $extensions) {
+		&"yarn" gulp compile-extension:$ext
+	}
+
+	Write-Host "Running unit tests with '$env:INTEGRATION_TEST_ELECTION_PATH' as build."
+}
+
+# Default to only running stable tests if test grep isn't set
+If ([string]::IsNullOrEmpty($env:ADS_TEST_GREP)) {
+	Write-Host "Running stable tests only"
+
+	$env:ADS_TEST_GREP = "@UNSTABLE@"
+	$env:ADS_TEST_INVERT_GREP = 1
+}
+
+ForEach ($ext in $extensions) {
+	Write-Host $("*" * ($ext.Length + 8))
+	Write-Host "*** $ext ***"
+	Write-Host $("*" * ($ext.Length + 8))
+
+	&"$env:INTEGRATION_TEST_ELECTION_PATH" --extensionDevelopmentPath=$PSScriptRoot\..\extensions\$ext --extensionTestsPath=$PSScriptRoot\..\extensions\$ext\out\test --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTENSIONSDIR --remote-debugging-port=9222 --disable-telemetry --disable-crash-reporter --disable-updates --nogpu
+}
+
+If ($LASTEXITCODE -ne 0) {
+	Return $LASTEXITCODE
+}
+
+Remove-Item $VSCODEUSERDATADIR -Recurse -Force
+Remove-Item $VSCODEEXTENSIONSDIR -Recurse -Force
+
+Pop-Location
