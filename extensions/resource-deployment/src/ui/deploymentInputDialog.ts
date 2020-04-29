353115668cd4d584a0f25e8b3ae5226ab6f1c57e
Arvind Ranasaria <ranasaria@outlook.com>
2020-04-29 12:11:48 -0700

    Features to resource deployment to add arc control create in ARC extension (#10088)
    * save not yet tested work
    
    * Merge from master.
    
    * Screeens Shared for Feeedback
    
    * Code complete
    
    * remove unneeded changes
    
    * remove unnecessary comma
    
    * remov wss
    
    * remove dead code
    
    * PR feedback
    
    * checkpoint fixes
    
    * PR & minor fixes
    
    * minor fix for feature of  resourceType options being optional.
    
    * reverting experimental change
    
    * separating out changes for future featurework.
    
    * revert unneeded change
    
    * review feedback fixes
    
    * review feedback
    
    * rename InputFieldComponent to InputComponent

--- extensions/resource-deployment/src/ui/deploymentInputDialog.ts
+++ extensions/resource-deployment/src/ui/deploymentInputDialog.ts
@@ -9,10 +9,9 @@ import * as nls from 'vscode-nls';
 import { DialogBase } from './dialogBase';
 import { INotebookService } from '../services/notebookService';
 import { DialogInfo, instanceOfNotebookBasedDialogInfo, NotebookBasedDialogInfo } from '../interfaces';
-import { Validator, initializeDialog, InputComponents, setModelValues, InputValueTransformer } from './modelViewUtils';
+import { Validator, initializeDialog, InputComponents, setModelValues, InputValueTransformer, InputComponent } from './modelViewUtils';
 import { Model } from './model';
 import { EOL } from 'os';
-import { getDateTimeString, getErrorMessage } from '../utils';
 import { IPlatformService } from '../services/platformService';
 
 const localize = nls.loadMessageBundle();
@@ -46,7 +45,7 @@ export class DeploymentInputDialog extends DialogBase {
 			onNewDisposableCreated: (disposable: vscode.Disposable): void => {
 				this._toDispose.push(disposable);
 			},
-			onNewInputComponentCreated: (name: string, component: azdata.DropDownComponent | azdata.InputBoxComponent | azdata.CheckBoxComponent, inputValueTransformer?: InputValueTransformer): void => {
+			onNewInputComponentCreated: (name: string, component: InputComponent, inputValueTransformer?: InputValueTransformer): void => {
 				this.inputComponents[name] = { component: component, inputValueTransformer: inputValueTransformer };
 			},
 			onNewValidatorCreated: (validator: Validator): void => {
@@ -88,39 +87,6 @@ export class DeploymentInputDialog extends DialogBase {
 	}
 
 	private executeNotebook(notebookDialogInfo: NotebookBasedDialogInfo): void {
-		azdata.tasks.startBackgroundOperation({
-			displayName: notebookDialogInfo.taskName!,
-			description: notebookDialogInfo.taskName!,
-			isCancelable: false,
-			operation: async op => {
-				op.updateStatus(azdata.TaskStatus.InProgress);
-				const notebook = await this.notebookService.getNotebook(notebookDialogInfo.notebook);
-				const result = await this.notebookService.executeNotebook(notebook);
-				if (result.succeeded) {
-					op.updateStatus(azdata.TaskStatus.Succeeded);
-				} else {
-					op.updateStatus(azdata.TaskStatus.Failed, result.errorMessage);
-					if (result.outputNotebook) {
-						const viewErrorDetail = localize('resourceDeployment.ViewErrorDetail', "View error detail");
-						const taskFailedMessage = localize('resourceDeployment.DeployFailed', "The task \"{0}\" has failed.", notebookDialogInfo.taskName);
-						const selectedOption = await vscode.window.showErrorMessage(taskFailedMessage, viewErrorDetail);
-						this.platformService.logToOutputChannel(taskFailedMessage);
-						if (selectedOption === viewErrorDetail) {
-							try {
-								this.notebookService.launchNotebookWithContent(`deploy-${getDateTimeString()}`, result.outputNotebook);
-							} catch (error) {
-								const launchNotebookError = localize('resourceDeployment.FailedToOpenNotebook', "An error occurred launching the output notebook. {1}{2}.", EOL, getErrorMessage(error));
-								this.platformService.logToOutputChannel(launchNotebookError);
-								vscode.window.showErrorMessage(launchNotebookError);
-							}
-						}
-					} else {
-						const errorMessage = localize('resourceDeployment.TaskFailedWithNoOutputNotebook', "The task \"{0}\" failed and no output Notebook was generated.", notebookDialogInfo.taskName);
-						this.platformService.logToOutputChannel(errorMessage);
-						vscode.window.showErrorMessage(errorMessage);
-					}
-				}
-			}
-		});
+		this.notebookService.backgroundExecuteNotebook(notebookDialogInfo.taskName, notebookDialogInfo.notebook, 'deploy', this.platformService);
 	}
 }
