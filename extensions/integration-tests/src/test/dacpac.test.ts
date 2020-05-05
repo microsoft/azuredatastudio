3ee9fc857188fe14bee6d5b20421c9de5b380eaa
chgagnon <chgagnon@microsoft.com>
2020-05-04 17:17:53 -0700

    Update to latest vscodetestcover and add tests back in

---
+++ extensions/integration-tests/src/test/dacpac.test.ts
@@ -0,0 +1,115 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import 'mocha';
+import * as azdata from 'azdata';
+import * as utils from './utils';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as mssql from '../../../mssql';
+import * as vscode from 'vscode';
+import { getStandaloneServer } from './testConfig';
+import * as assert from 'assert';
+import { promisify } from 'util';
+
+const retryCount = 24; // 2 minutes
+const dacpac1: string = path.join(__dirname, '../../testData/Database1.dacpac');
+suite('Dacpac integration test suite', () => {
+	suiteSetup(async function () {
+		await utils.sleep(5000); // To ensure the providers are registered.
+		console.log(`Start dacpac tests`);
+	});
+
+	test('Deploy and extract dacpac', async function () {
+		const server = await getStandaloneServer();
+		await utils.connectToServer(server);
+
+		const nodes = <azdata.objectexplorer.ObjectExplorerNode[]>await azdata.objectexplorer.getActiveConnectionNodes();
+		const index = nodes.findIndex(node => node.nodePath.includes(server.serverName));
+		const ownerUri = await azdata.connection.getUriForConnection(nodes[index].connectionId);
+		const now = new Date();
+		const databaseName = 'ADS_deployDacpac_' + now.getTime().toString();
+
+		try {
+			const dacfxService = ((await vscode.extensions.getExtension(mssql.extension.name).activate() as mssql.IExtension)).dacFx;
+			assert(dacfxService, 'DacFx Service Provider is not available');
+
+			// Deploy dacpac
+			const deployResult = await dacfxService.deployDacpac(dacpac1, databaseName, false, ownerUri, azdata.TaskExecutionMode.execute);
+			await utils.assertDatabaseCreationResult(databaseName, ownerUri, retryCount);
+			const dbConnectionId = await utils.connectToServer({
+				serverName: server.serverName,
+				database: databaseName,
+				userName: server.userName,
+				password: server.password,
+				authenticationTypeName: server.authenticationTypeName,
+				providerName: server.providerName
+			});
+			const dbConnectionOwnerUri = await azdata.connection.getUriForConnection(dbConnectionId);
+			await utils.assertTableCreationResult('dbo', 'Table1', dbConnectionOwnerUri, retryCount);
+			await utils.assertTableCreationResult('dbo', 'Table2', dbConnectionOwnerUri, retryCount);
+			assert(deployResult.success === true && deployResult.errorMessage === '', `Deploy dacpac should succeed Expected: there should be no error. Actual Error message: "${deployResult.errorMessage}"`);
+
+			// Extract dacpac
+			const folderPath = path.join(os.tmpdir(), 'DacFxTest');
+			if (!(await promisify(fs.exists)(folderPath))) {
+				await fs.promises.mkdir(folderPath);
+			}
+			const packageFilePath = path.join(folderPath, `${databaseName}.dacpac`);
+			const extractResult = await dacfxService.extractDacpac(databaseName, packageFilePath, databaseName, '1.0.0.0', ownerUri, azdata.TaskExecutionMode.execute);
+			await utils.assertFileGenerationResult(packageFilePath, retryCount);
+
+			assert(extractResult.success === true && extractResult.errorMessage === '', `Extract dacpac should succeed. Expected: there should be no error. Actual Error message: "${extractResult.errorMessage}"`);
+		} finally {
+			await utils.deleteDB(server, databaseName, ownerUri);
+		}
+	});
+
+	const bacpac1: string = path.join(__dirname, '..', '..', 'testData', 'Database1.bacpac');
+	test('Import and export bacpac', async function () {
+		const server = await getStandaloneServer();
+		await utils.connectToServer(server);
+
+		const nodes = <azdata.objectexplorer.ObjectExplorerNode[]>await azdata.objectexplorer.getActiveConnectionNodes();
+		const index = nodes.findIndex(node => node.nodePath.includes(server.serverName));
+		const ownerUri = await azdata.connection.getUriForConnection(nodes[index].connectionId);
+		const now = new Date();
+		const databaseName = 'ADS_importBacpac_' + now.getTime().toString();
+
+		try {
+			let dacfxService = ((await vscode.extensions.getExtension(mssql.extension.name).activate() as mssql.IExtension)).dacFx;
+			assert(dacfxService, 'DacFx Service Provider is not available');
+
+			// Import bacpac
+			const importResult = await dacfxService.importBacpac(bacpac1, databaseName, ownerUri, azdata.TaskExecutionMode.execute);
+			await utils.assertDatabaseCreationResult(databaseName, ownerUri, retryCount);
+			const dbConnectionId = await utils.connectToServer({
+				serverName: server.serverName,
+				database: databaseName,
+				userName: server.userName,
+				password: server.password,
+				authenticationTypeName: server.authenticationTypeName,
+				providerName: server.providerName
+			});
+			const dbConnectionOwnerUri = await azdata.connection.getUriForConnection(dbConnectionId);
+			await utils.assertTableCreationResult('dbo', 'Table1', dbConnectionOwnerUri, retryCount, true);
+			await utils.assertTableCreationResult('dbo', 'Table2', dbConnectionOwnerUri, retryCount, true);
+			assert(importResult.success === true && importResult.errorMessage === '', `Expected: Import bacpac should succeed and there should be no error. Actual Error message: "${importResult.errorMessage}"`);
+
+			// Export bacpac
+			const folderPath = path.join(os.tmpdir(), 'DacFxTest');
+			if (!(await promisify(fs.exists)(folderPath))) {
+				await fs.promises.mkdir(folderPath);
+			}
+			const packageFilePath = path.join(folderPath, `${databaseName}.bacpac`);
+			const exportResult = await dacfxService.exportBacpac(databaseName, packageFilePath, ownerUri, azdata.TaskExecutionMode.execute);
+			await utils.assertFileGenerationResult(packageFilePath, retryCount);
+			assert(exportResult.success === true && exportResult.errorMessage === '', `Expected: Export bacpac should succeed and there should be no error. Actual Error message: "${exportResult.errorMessage}"`);
+		} finally {
+			await utils.deleteDB(server, databaseName, ownerUri);
+		}
+	});
+});
---
+++ extensions/integration-tests/src/test/notebook.util.ts
@@ -0,0 +1,179 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import 'mocha';
+import * as azdata from 'azdata';
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+
+export class CellTypes {
+	public static readonly Code = 'code';
+	public static readonly Markdown = 'markdown';
+	public static readonly Raw = 'raw';
+}
+
+export const pySparkNotebookContent: azdata.nb.INotebookContents = {
+	cells: [{
+		cell_type: CellTypes.Code,
+		source: '1+1',
+		metadata: { language: 'python' },
+		execution_count: 1
+	}],
+	metadata: {
+		'kernelspec': {
+			'name': 'pysparkkernel',
+			'display_name': 'PySpark'
+		}
+	},
+	nbformat: 4,
+	nbformat_minor: 2
+};
+
+export const notebookContentForCellLanguageTest: azdata.nb.INotebookContents = {
+	cells: [{
+		cell_type: CellTypes.Code,
+		source: '1+1',
+		metadata: {},
+		execution_count: 1
+	}],
+	metadata: {
+		'kernelspec': {
+			'name': ''
+		},
+	},
+	nbformat: 4,
+	nbformat_minor: 2
+};
+
+export const pythonNotebookMultipleCellsContent: azdata.nb.INotebookContents = {
+	cells: [{
+		cell_type: CellTypes.Code,
+		source: '1+1',
+		metadata: { language: 'python' },
+		execution_count: 1
+	}, {
+		cell_type: CellTypes.Code,
+		source: '1+2',
+		metadata: { language: 'python' },
+		execution_count: 1
+	}, {
+		cell_type: CellTypes.Code,
+		source: '1+3',
+		metadata: { language: 'python' },
+		execution_count: 1
+	}, {
+		cell_type: CellTypes.Code,
+		source: '1+4',
+		metadata: { language: 'python' },
+		execution_count: 1
+	}],
+	metadata: {
+		'kernelspec': {
+			'name': 'python3',
+			'display_name': 'Python 3'
+		}
+	},
+	nbformat: 4,
+	nbformat_minor: 2
+};
+
+export const sqlNotebookContent: azdata.nb.INotebookContents = {
+	cells: [{
+		cell_type: CellTypes.Code,
+		source: 'select 1',
+		metadata: { language: 'sql' },
+		execution_count: 1
+	}],
+	metadata: {
+		'kernelspec': {
+			'name': 'SQL',
+			'display_name': 'SQL'
+		}
+	},
+	nbformat: 4,
+	nbformat_minor: 2
+};
+
+export const sqlNotebookMultipleCellsContent: azdata.nb.INotebookContents = {
+	cells: [{
+		cell_type: CellTypes.Code,
+		source: 'select 0',
+		metadata: { language: 'sql' },
+		execution_count: 1
+	}, {
+		cell_type: CellTypes.Code,
+		source: `WAITFOR DELAY '00:00:02'\nselect 1`,
+		metadata: { language: 'sql' },
+		execution_count: 1
+	}, {
+		cell_type: CellTypes.Code,
+		source: 'select 2',
+		metadata: { language: 'sql' },
+		execution_count: 1
+	}],
+	metadata: {
+		'kernelspec': {
+			'name': 'SQL',
+			'display_name': 'SQL'
+		}
+	},
+	nbformat: 4,
+	nbformat_minor: 2
+};
+
+export const pySparkKernelMetadata = {
+	'kernelspec': {
+		'name': 'pysparkkernel',
+		'display_name': 'PySpark'
+	}
+};
+
+export const pySparkKernelSpec = {
+	name: 'pyspark',
+	display_name: 'PySpark'
+};
+
+export const sqlKernelMetadata = {
+	'kernelspec': {
+		'name': 'SQL',
+		'display_name': 'SQL'
+	}
+};
+
+export const sqlKernelSpec: azdata.nb.IKernelSpec = {
+	name: 'SQL',
+	display_name: 'SQL'
+};
+
+export const pythonKernelMetadata = {
+	'kernelspec': {
+		'name': 'python3',
+		'display_name': 'Python 3'
+	}
+};
+
+export const pythonKernelSpec: azdata.nb.IKernelSpec = {
+	name: 'python3',
+	display_name: 'Python 3'
+};
+
+export function writeNotebookToFile(pythonNotebook: azdata.nb.INotebookContents, testName: string): vscode.Uri {
+	let fileName = getFileName(testName);
+	let notebookContentString = JSON.stringify(pythonNotebook);
+	// eslint-disable-next-line no-sync
+	fs.writeFileSync(fileName, notebookContentString);
+	console.log(`Local file is created: '${fileName}'`);
+	let uri = vscode.Uri.file(fileName);
+	return uri;
+}
+
+export function getFileName(testName: string): string {
+	if (testName) {
+		return path.join(os.tmpdir(), testName + '.ipynb');
+	}
+	return undefined;
+}
