b3296949c92c2f39ea9b09b1af7970b3df7168aa
Hale Rankin <harankin@microsoft.com>
2020-05-26 16:44:33 -0700

    Added EditCellAction which toggles between two icons.

--- src/sql/workbench/contrib/notebook/browser/cellToolbarActions.ts
+++ src/sql/workbench/contrib/notebook/browser/cellToolbarActions.ts
@@ -4,9 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Action } from 'vs/base/common/actions';
-import { INotebookService } from 'sql/workbench/services/notebook/browser/notebookService';
+import { localize } from 'vs/nls';
+import { INotebookService, INotebookEditor } from 'sql/workbench/services/notebook/browser/notebookService';
+import { NotebookComponent } from 'sql/workbench/contrib/notebook/browser/notebook.component';
 import { ICellModel } from 'sql/workbench/services/notebook/browser/models/modelInterfaces';
+import { CellType } from 'sql/workbench/services/notebook/common/contracts';
 import { CellToggleMoreActions } from 'sql/workbench/contrib/notebook/browser/cellToggleMoreActions';
+import { ToggleableAction } from 'sql/workbench/contrib/notebook/browser/notebookActions';
 
 export const HIDDEN_CLASS = 'actionhidden';
 
@@ -26,6 +30,34 @@ export class CellToolbarAction extends Action {
 	// Todo: Add actions references here?
 }
 
+export class AddCellAction extends Action {
+	public cellType: CellType;
+
+	constructor(
+		id: string, label: string, cssClass: string
+	) {
+		super(id, label, cssClass);
+	}
+	public async run(context: INotebookEditor): Promise<any> {
+		//Add Cell after current selected cell.
+		let index = 0;
+		if (context && context.cells) {
+			let notebookcomponent = context as NotebookComponent;
+			let id = notebookcomponent.activeCellId;
+			if (id) {
+				index = context.cells.findIndex(cell => cell.id === id);
+				index = index + 1;
+			}
+		}
+		context.addCell(this.cellType, index);
+	}
+}
+
+/**
+ * Not yet implemeneted
+ * Goal: show more actions available depending on what cell the toolbar is for.
+ * (ie. runAlActions when on code cell.)
+ */
 export class MoreActions extends Action {
 	private _cellToggleMoreActions: CellToggleMoreActions;
 
@@ -33,5 +65,50 @@ export class MoreActions extends Action {
 		id: string, label: string, cssClass: string
 	) {
 		super(id, label, cssClass);
+	}
+}
+
+export class EditCellAction extends ToggleableAction {
+	// Constants
+	private static readonly editLabel = localize('editLabel', "Edit");
+	private static readonly closeLabel = localize('closeLabel', "Close");
+	private static readonly baseClass = 'codicon';
+	private static readonly editCssClass = 'edit';
+	private static readonly closeCssClass = 'close';
+	private static readonly maskedIconClass = 'masked-icon';
+
+	constructor(
+		id: string, toggleTooltip: boolean
+	) {
+		super(id, {
+			baseClass: EditCellAction.baseClass,
+			toggleOnLabel: EditCellAction.closeLabel,
+			toggleOnClass: EditCellAction.closeCssClass,
+			toggleOffLabel: EditCellAction.editLabel,
+			toggleOffClass: EditCellAction.editCssClass,
+			maskedIconClass: EditCellAction.maskedIconClass,
+			shouldToggleTooltip: toggleTooltip,
+			isOn: false
+		});
+	}
+
+	public get editMode(): boolean {
+		return this.state.isOn;
+	}
+	public set editMode(value: boolean) {
+		this.toggle(value);
+	}
+
+	public run(context: INotebookEditor): Promise<boolean> {
+		let self = this;
+		return new Promise<boolean>((resolve, reject) => {
+			try {
+				self.editMode = !self.editMode;
+				context.model.activeCell.active = self.editMode;
+				resolve(true);
+			} catch (e) {
+				reject(e);
+			}
+		});
 	}
 }
--- src/sql/workbench/contrib/notebook/browser/cellViews/cellToolbar.component.ts
+++ src/sql/workbench/contrib/notebook/browser/cellViews/cellToolbar.component.ts
@@ -3,14 +3,17 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import 'vs/css!./cellToolbar';
-
+import * as DOM from 'vs/base/browser/dom';
 import { Component, Inject, Input, ViewChild, ElementRef } from '@angular/core';
 import { localize } from 'vs/nls';
 import { ICellModel } from 'sql/workbench/services/notebook/browser/models/modelInterfaces';
 import { NotebookModel } from 'sql/workbench/services/notebook/browser/models/notebookModel';
 import { Taskbar } from 'sql/base/browser/ui/taskbar/taskbar';
+import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { CellToolbarAction, MoreActions } from 'sql/workbench/contrib/notebook/browser/cellToolbarActions';
+import { CellToolbarAction, MoreActions, AddCellAction, EditCellAction } from 'sql/workbench/contrib/notebook/browser/cellToolbarActions';
+import { CellTypes } from 'sql/workbench/services/notebook/common/contracts';
+import { DropdownMenuActionViewItem } from 'sql/base/browser/ui/buttonMenu/buttonMenu';
 
 export const CELL_TOOLBAR_SELECTOR: string = 'cell-toolbar-component';
 
@@ -33,10 +36,12 @@ export class CellToolbarComponent {
 	}
 	private _actionBar: Taskbar;
 	private _model: NotebookModel;
+	private _editCellAction: EditCellAction;
 
 
 	constructor(
-		@Inject(IInstantiationService) private _instantiationService: IInstantiationService
+		@Inject(IInstantiationService) private _instantiationService: IInstantiationService,
+		@Inject(IContextMenuService) private contextMenuService: IContextMenuService
 	) {
 	}
 
@@ -49,26 +54,47 @@ export class CellToolbarComponent {
 	}
 
 	private initActionBar(): void {
-		// Todo: Wire up toolbarToggleEditMode
-		let editButton = this._instantiationService.createInstance(CellToolbarAction, 'notebook.editCell', '', 'codicon masked-icon edit', this.buttonEdit, this.cellModel);
-
-		// Todo: Wireup toolbarUnselectActiveCell
-		let closeButton = this._instantiationService.createInstance(CellToolbarAction, 'notebook.closeEditor', '', 'codicon masked-icon close', this.buttonClose, this.cellModel);
 
 		// Todo: set this up like the Add new cells is done in main toolbar
-		let addButton = this._instantiationService.createInstance(CellToolbarAction, 'notebook.addCell', '', 'codicon masked-icon new', this.buttonAdd, this.cellModel);
+		//let addButton = this._instantiationService.createInstance(CellToolbarAction, 'notebook.addCell', '', 'codicon masked-icon new', this.buttonAdd, this.cellModel);
+		let addCodeCellButton = new AddCellAction('notebook.AddCodeCell', localize('codePreview', "Code cell"), 'notebook-button masked-pseudo code');
+		addCodeCellButton.cellType = CellTypes.Code;
+
+		let addTextCellButton = new AddCellAction('notebook.AddTextCell', localize('textPreview', "Markdown cell"), 'notebook-button masked-pseudo markdown');
+		addTextCellButton.cellType = CellTypes.Markdown;
 
 		let deleteButton = this._instantiationService.createInstance(CellToolbarAction, 'notebook.deleteCell', '', 'codicon masked-icon delete', this.buttonDelete, this.cellModel);
 
 		let moreActionsButton = new MoreActions('notebook.moreActions', this.buttonMoreActions, 'codicon masked-icon more');
 
+		// Todo: Wire up toolbarToggleEditMode
+		// Todo: Wireup toolbarUnselectActiveCell
+		this._editCellAction = this._instantiationService.createInstance(EditCellAction, 'notebook.editCell', true);
+		this._editCellAction.enabled = true;
+
 		let taskbar = <HTMLElement>this.celltoolbar.nativeElement;
 		this._actionBar = new Taskbar(taskbar);
 		this._actionBar.context = this;
+
+		let buttonDropdownContainer = DOM.$('li.action-item');
+		buttonDropdownContainer.setAttribute('role', 'presentation');
+		let dropdownMenuActionViewItem = new DropdownMenuActionViewItem(
+			addCodeCellButton,
+			[addCodeCellButton, addTextCellButton],
+			this.contextMenuService,
+			undefined,
+			this._actionBar.actionRunner,
+			undefined,
+			'codicon masked-icon masked-pseudo-after new dropdown-arrow',
+			localize('addCell', "Cell"),
+			undefined
+		);
+		dropdownMenuActionViewItem.render(buttonDropdownContainer);
+		dropdownMenuActionViewItem.setActionContext(this);
+
 		this._actionBar.setContent([
-			{ action: editButton },
-			{ action: closeButton },
-			{ action: addButton },
+			{ action: this._editCellAction },
+			{ element: buttonDropdownContainer },
 			{ action: deleteButton },
 			// Todo: Get this to show the list of actions.
 			{ action: moreActionsButton }
