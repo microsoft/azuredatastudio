4f1e48b926fa09d27c1bfbf58fb87b5612055951
Madhuri Koripalli <makoripa@microsoft.com>
2020-05-28 16:53:21 -0700

    merge latest from master

--- src/sql/workbench/contrib/notebooksExplorer/browser/notebookExplorer.contribution.ts
+++ src/sql/workbench/contrib/notebooksExplorer/browser/notebookExplorer.contribution.ts
@@ -5,16 +5,14 @@
 
 import 'vs/css!./media/notebookExplorer.contribution';
 import { localize } from 'vs/nls';
-import { ViewletRegistry, Extensions as ViewletExtensions } from 'vs/workbench/browser/viewlet';
 import { Registry } from 'vs/platform/registry/common/platform';
-import { NotebookExplorerViewletViewsContribution, OpenNotebookExplorerViewletAction, VIEWLET_ID } from 'sql/workbench/contrib/notebooksExplorer/browser/notebookExplorerViewlet';
+import { NotebookExplorerViewletViewsContribution, OpenNotebookExplorerViewletAction } from 'sql/workbench/contrib/notebooksExplorer/browser/notebookExplorerViewlet';
 import { IWorkbenchActionRegistry, Extensions as ActionExtensions } from 'vs/workbench/common/actions';
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions';
 import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
 import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
 import { KeyMod, KeyCode } from 'vs/base/common/keyCodes';
 
-Registry.as<ViewletRegistry>(ViewletExtensions.Viewlets).setDefaultViewletId(VIEWLET_ID);
 const workbenchRegistry = Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench);
 workbenchRegistry.registerWorkbenchContribution(NotebookExplorerViewletViewsContribution, LifecyclePhase.Starting);
 const registry = Registry.as<IWorkbenchActionRegistry>(ActionExtensions.WorkbenchActions);
