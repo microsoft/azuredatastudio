2f8734606ced70d0daba0176dea85981e0782dd2
ADS Merger <andresse@microsoft.com>
2020-05-31 19:47:41 +0000

    Update License Headers

--- src/vs/workbench/contrib/notebook/browser/contrib/status/editorStatus.ts
+++ src/vs/workbench/contrib/notebook/browser/contrib/status/editorStatus.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { INotebookEditor, NOTEBOOK_EDITOR_FOCUSED, NOTEBOOK_IS_ACTIVE_EDITOR, NOTEBOOK_HAS_MULTIPLE_KERNELS } from 'vs/workbench/contrib/notebook/browser/notebookBrowser';
