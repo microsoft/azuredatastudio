d1d73c13ff12e87bd303323b6e3237ecd771d504
Hale Rankin <harankin@microsoft.com>
2020-06-03 16:04:48 -0700

    Switched order of the two dropdowns in main toolbar in preview.

--- src/sql/workbench/contrib/notebook/browser/notebook.component.ts
+++ src/sql/workbench/contrib/notebook/browser/notebook.component.ts
@@ -438,7 +438,7 @@ export class NotebookComponent extends AngularDisposable implements OnInit, OnDe
 			let taskbar = <HTMLElement>this.toolbar.nativeElement;
 			this._actionBar = new Taskbar(taskbar, { actionViewItemProvider: action => this.actionItemProvider(action as Action) });
 			this._actionBar.context = this;
-			taskbar.classList.add('in-preview');
+			taskbar.parentNode.parentElement.classList.add('in-preview');
 
 			let buttonDropdownContainer = DOM.$('li.action-item');
 			buttonDropdownContainer.setAttribute('role', 'presentation');
@@ -460,8 +460,8 @@ export class NotebookComponent extends AngularDisposable implements OnInit, OnDe
 				{ element: buttonDropdownContainer },
 				{ action: this._runAllCellsAction },
 				{ element: Taskbar.createTaskbarSeparator() },
-				{ element: attachToContainer },
 				{ element: kernelContainer },
+				{ element: attachToContainer },
 				{ element: spacerElement },
 				{ action: collapseCellsAction },
 				{ action: clearResultsButton },
