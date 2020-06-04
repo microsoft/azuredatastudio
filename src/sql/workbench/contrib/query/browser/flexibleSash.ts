92a1edd6b7fe15447af65038a02d8e42788f4f27
Alex Ma <alma1@microsoft.com>
2020-06-04 15:41:20 -0700

    more simplification

--- src/sql/workbench/contrib/query/browser/flexibleSash.ts
+++ src/sql/workbench/contrib/query/browser/flexibleSash.ts
@@ -95,7 +95,7 @@ export class HorizontalFlexibleSash extends Disposable implements IHorizontalSas
 	}
 
 	public getHorizontalSashWidth?(): number {
-		return this.dimension ? this.dimension.width : 0;
+		return this.dimension?.width ?? 0;
 	}
 
 	public setDimension(dimension: Dimension) {
