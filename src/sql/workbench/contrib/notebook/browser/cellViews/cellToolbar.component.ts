540e1992cb0b838a61b1670b9db0117c298b8f04
Alan.Ren <alanren@microsoft.com>
2020-06-05 00:40:09 -0700

    Revert "Only show edit cell action for markdown cells (#10719)"
    This reverts commit cc3554e987c489ca67bdaad96c83bb98e6f0d0bb.

--- src/sql/workbench/contrib/notebook/browser/cellViews/cellToolbar.component.ts
+++ src/sql/workbench/contrib/notebook/browser/cellViews/cellToolbar.component.ts
@@ -7,7 +7,7 @@ import 'vs/css!./cellToolbar';
 import * as DOM from 'vs/base/browser/dom';
 import { Component, Inject, ViewChild, ElementRef, Input } from '@angular/core';
 import { localize } from 'vs/nls';
-import { Taskbar, ITaskbarContent } from 'sql/base/browser/ui/taskbar/taskbar';
+import { Taskbar } from 'sql/base/browser/ui/taskbar/taskbar';
 import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { DeleteCellAction, EditCellAction, CellToggleMoreActions } from 'sql/workbench/contrib/notebook/browser/cellToolbarActions';
@@ -87,14 +87,11 @@ export class CellToolbarComponent {
 		dropdownMenuActionViewItem.render(buttonDropdownContainer);
 		dropdownMenuActionViewItem.setActionContext(context);
 
-		let taskbarContent: ITaskbarContent[] = [];
-		if (this.cellModel?.cellType === CellTypes.Markdown) {
-			taskbarContent.push({ action: this._editCellAction });
-		}
-		taskbarContent.push({ element: buttonDropdownContainer },
+		this._actionBar.setContent([
+			{ action: this._editCellAction },
+			{ element: buttonDropdownContainer },
 			{ action: deleteButton },
-			{ element: moreActionsContainer });
-
-		this._actionBar.setContent(taskbarContent);
+			{ element: moreActionsContainer }
+		]);
 	}
 }
