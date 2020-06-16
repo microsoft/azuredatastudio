f725ee96b9bf80768f69b53fb4087f129b306a5e
Aasim Khan <aasimkhan30@gmail.com>
2020-06-16 13:24:48 -0700

    Code refactoring for extension testing (#10529)
    * Setting up tests on import extension
    
    * -Added API wrappers for all the azdata and vscode APIs to make them easily mockable
    -Added some unit tests for the import extension
    -Some code logic separations
    
    * -added code report for the import extension in ci
    
    * Did some more code refractoring
    
    * -Added json report generation
    
    * updated vscodetestcoverage to latest version in import extension.
    
    * -remove duplicate codecoverageConfig.json

--- scripts/test-extensions-unit.bat
+++ scripts/test-extensions-unit.bat
@@ -22,6 +22,7 @@ if "%INTEGRATION_TEST_ELECTRON_PATH%"=="" (
 	call yarn gulp compile-extension:azurecore
 	call yarn gulp compile-extension:cms
 	call yarn gulp compile-extension:dacpac
+	call yarn gulp compile-extension:import
 	call yarn gulp compile-extension:schema-compare
 	call yarn gulp compile-extension:mssql
 	call yarn gulp compile-extension:notebook
@@ -65,6 +66,11 @@ echo *****************************
 echo *** starting dacpac tests ***
 echo *****************************
 call "%INTEGRATION_TEST_ELECTRON_PATH%" --extensionDevelopmentPath=%~dp0\..\extensions\dacpac --extensionTestsPath=%~dp0\..\extensions\dacpac\out\test --user-data-dir=%VSCODEUSERDATADIR% --extensions-dir=%VSCODEEXTENSIONSDIR% --remote-debugging-port=9222 --disable-telemetry --disable-crash-reporter --disable-updates --nogpu
+
+echo *****************************
+echo *** starting import tests ***
+echo *****************************
+call "%INTEGRATION_TEST_ELECTRON_PATH%" --extensionDevelopmentPath=%~dp0\..\extensions\import --extensionTestsPath=%~dp0\..\extensions\import\out\test --user-data-dir=%VSCODEUSERDATADIR% --extensions-dir=%VSCODEEXTENSIONSDIR% --remote-debugging-port=9222 --disable-telemetry --disable-crash-reporter --disable-updates --nogpu
 
 echo *************************************
 echo *** starting schema compare tests ***
