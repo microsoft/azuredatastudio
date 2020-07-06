ba82a162d0091133e6675fec11a13c66a7c3df37
v-bbrady <60623315+v-bbrady@users.noreply.github.com>
2020-07-06 14:01:08 -0700

    fixes show all extensions button #10445  #10625 (#11104)
    * fixes button alignment and missing icons issues with ads welcome page
    
    * removes dead code, registers new Button
    
    * creates one button component and adds proper attributes depending on platform
    
    * Add CodeQL Analysis workflow (#10195)
    
    * Add CodeQL Analysis workflow
    
    * Fix path
    
    * adds return types to functions
    
    * fixes show all extensions command
    
    * formats css
    
    * fixes layering issue
    
    * updates querySelectors to be more specific
    
    Co-authored-by: Justin Hutchings <jhutchings1@users.noreply.github.com>

--- src/sql/workbench/contrib/welcome/page/browser/az_data_welcome_page.ts
+++ src/sql/workbench/contrib/welcome/page/browser/az_data_welcome_page.ts
@@ -11,7 +11,7 @@ export default () => `
 	<div class="welcomePage">
 		<div class="ads-homepage splash">
 			<div class="gradient">
-				<div class="preview-text tool-tip">
+				<div class="ads-homepage-section tool-tip">
 					<div class="tool-tip-container" id="tool-tip-container-wide">
 						<a class="ads-welcome-page-link" aria-describedby="tooltip-text-wide" id="preview-link-wide" class="preview-link" tabindex="0" name="preview"><p>Preview</p><i class="icon-info themed-icon"></i></a>
 						<span role="tooltip" id="tooltip-text-wide" class="tool-tip-text" aria-hidden="true">
@@ -40,27 +40,10 @@ export default () => `
 										<span class="icon xs"></span><h1 class="caption"></h1>
 									</div>
 									<div class="flex btn-container">
-										<div>
-											<button id="dropdown-btn" class="btn btn-primary dropdown" role="navigation" aria-haspopup="true" aria-controls="dropdown">
-												<div class="dropdown-text" style="pointer-events: none;">
-													<span>${escape(localize('welcomePage.new', "New"))}</span><i class="icon-arrow-down"></i>
-												</div>
-											</button>
-											<nav role="navigation" class="dropdown-nav">
-												<ul id="dropdown" class="dropdown-content" aria-hidden="true" aria-label="submenu" role="menu" aria-labelledby="dropdown-btn">
-													<li role="none"><a class="ads-welcome-page-link" role="menuitem" tabIndex="-1" class="move" href="command:registeredServers.addConnection">${escape(localize('welcomePage.newConnection', "New connection"))}</a></li>
-													<li role="none"><a class="ads-welcome-page-link" role="menuitem" tabIndex="-1" class="move" href="command:workbench.action.files.newUntitledFile">${escape(localize('welcomePage.newQuery', "New query"))}</a></li>
-													<li role="none"><a class="ads-welcome-page-link" role="menuitem" tabIndex="-1" class="move" href="command:notebook.command.new">${escape(localize('welcomePage.newNotebook', "New notebook"))}</a></li>
-													<li role="none" id="dropdown-mac-only"><a class="ads-welcome-page-link" role="menuitem" tabIndex="-1" class="move mac-only" href="command:workbench.action.files.openLocalFileFolder">${escape(localize('welcomePage.openFileMac', "Open file"))}</a></li>
-													<li role="none" id="dropdown-windows-linux-only"><a class="ads-welcome-page-link" role="menuitem" tabIndex="-1" class="move windows-only linux-only" href="command:workbench.action.files.openFile">${escape(localize('welcomePage.openFileLinuxPC', "Open file"))}</a></li>
-												</ul>
-											</nav>
+										<div id="dropdown-btn-container" class="btn btn-primary dropdown">
 										</div>
-										<a class="windows-only linux-only btn btn-secondary"
-											href="command:workbench.action.files.openFile">
-											${escape(localize('welcomePage.openFileLinuxPC', "Open file"))}
-										</a>
-										<a class="mac-only btn btn-secondary" href="command:workbench.action.files.openLocalFileFolder">${escape(localize('welcomePage.openFileMac', "Open file"))}</a>
+										<div id="open-file-btn-container" class="btn btn-secondary">
+										</div>
 									</div>
 								</div>
 							</div>
@@ -158,8 +141,6 @@ export default () => `
 						<p>${escape(localize('welcomePage.documentationBody',
 				"Visit the documentation center for quickstarts, how-to guides, and references for PowerShell, APIs, etc."))}
 						</p>
-
-
 						<div class="videos-container row">
 							<h2>Videos</h2>
 							<div class="flex flex-container-video">
@@ -169,7 +150,6 @@ export default () => `
 										<h4>${escape(localize('welcomePage.videoDescriptionOverview',
 					"Overview of Azure Data Studio"))}</h4>
 									</a>
-
 								</div>
 								<div class="videos-container-video">
 									<a href="https://www.youtube.com/watch?v=Nt4kIHQ0IOc" class="video overview ads-welcome-page-link">
@@ -186,7 +166,7 @@ export default () => `
 			<div class="ads-homepage-section content extensions">
 				<div class="flex flex-j-between">
 					<h2>Extend your data studio</h2>
-					<a class="link-show-all flex ads-welcome-page-link" href="command:extensions.listView.focus">${escape(localize('welcomePage.showAll', "Show All"))} <span class="icon-arrow-right"></span></a>
+					<a class="link-show-all flex" href="command:workbench.view.extensions">${escape(localize('welcomePage.showAll', "Show All"))} <span class="icon-arrow-right"></span></a>
 				</div>
 				<div class="row ads-grid grip-gap-50">
 					<div
