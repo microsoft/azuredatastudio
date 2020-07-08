678678af32ce1d6897fc8b2c0f79c30ed6c190ab
AzureDataStudio <andresse@microsoft.com>
2020-07-08 13:25:53 -0700

    fix permissions

--- test/automation/src/playwrightDriver.ts
+++ test/automation/src/playwrightDriver.ts
@@ -143,7 +143,7 @@ function waitForEndpoint(): Promise<string> {
 export function connect(browserType: 'chromium' | 'webkit' | 'firefox' = 'chromium'): Promise<{ client: IDisposable, driver: IDriver }> {
 	return new Promise(async (c) => {
 		const browser = await playwright[browserType].launch({ headless: false });
-		const context = await browser.newContext();
+		const context = await browser.newContext({ permissions: ['clipboard-read'] }); // {{SQL CARBON EDIT}} avoid permissison request
 		const page = await context.newPage();
 		await page.setViewportSize({ width, height });
 		const payloadParam = `[["enableProposedApi",""]]`;
