881b9447b46f3e1b1758cc98e21403f16ca022ae
chgagnon <chgagnon@microsoft.com>
2020-07-07 19:37:19 -0700

    Remove copyrights

--- extensions/notebook/src/prompts/adapter.ts
+++ extensions/notebook/src/prompts/adapter.ts
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 // This code is originally from https://github.com/DonJayamanne/bowerVSCode
 // License: https://github.com/DonJayamanne/bowerVSCode/blob/master/LICENSE
 
