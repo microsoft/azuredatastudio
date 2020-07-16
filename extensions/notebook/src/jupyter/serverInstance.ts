e020f6a65154573c1f6bf78b28fa31b69eb4c55b
Cory Rivera <corivera@microsoft.com>
2020-07-16 12:32:13 -0700

    Only copy powershell kernel spec on SAWs.

--- extensions/notebook/src/jupyter/serverInstance.ts
+++ extensions/notebook/src/jupyter/serverInstance.ts
@@ -61,6 +61,9 @@ export class ServerInstanceUtils {
 	public copy(src: string, dest: string): Promise<void> {
 		return fs.copy(src, dest);
 	}
+	public copyFIles(src: string, dest: string): Promise<void> {
+		return fs.copyFile(src, dest);
+	}
 	public async exists(path: string): Promise<boolean> {
 		try {
 			await fs.access(path);
@@ -118,7 +121,6 @@ export class PerFolderServerInstance implements IServerInstance {
 	 */
 	private instanceDataRoot: string;
 
-	private _systemJupyterDir: string;
 	private _port: string;
 	private _uri: vscode.Uri;
 	private _isStarted: boolean = false;
@@ -213,11 +215,15 @@ export class PerFolderServerInstance implements IServerInstance {
 
 	private async copyKernelsToSystemJupyterDirs(): Promise<void> {
 		let kernelsExtensionSource = path.join(this.options.install.extensionPath, 'kernels');
-		this._systemJupyterDir = path.join(this.getSystemJupyterHomeDir(), 'kernels');
-		if (!(await this.utils.exists(this._systemJupyterDir))) {
-			await this.utils.mkDir(this._systemJupyterDir, this.options.install.outputChannel);
+		let copyDir = path.join(this.getSystemJupyterHomeDir(), 'kernels');
+		if (this.options.install.runningOnSaw) {
+			kernelsExtensionSource = path.join(kernelsExtensionSource, 'powershell');
+			copyDir = path.join(copyDir, 'powershell');
 		}
-		await this.utils.copy(kernelsExtensionSource, this._systemJupyterDir);
+		if (!(await this.utils.exists(copyDir))) {
+			await this.utils.mkDir(copyDir, this.options.install.outputChannel);
+		}
+		await this.utils.copy(kernelsExtensionSource, copyDir);
 	}
 
 	private getSystemJupyterHomeDir(): string {
