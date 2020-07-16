cf0aa20353c8fcb6e7993b732ccf881c314d81ca
Anthony Dresser <anthonydresserdev@gmail.com>
2020-07-16 20:56:21 +0000

    add ability to specific commit for patching from

--- build/lib/patch.js
+++ build/lib/patch.js
@@ -7,12 +7,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path = require("path");
 const cp = require("child_process");
+const minimist = require("minimist");
+const args = minimist(process.argv, {
+    string: [
+        'commit',
+    ]
+});
 async function main() {
+    var _a;
     const patchPath = path.join(__dirname, '..', 'patches');
     const files = await fs_1.promises.readdir(patchPath);
+    const target = (_a = args.commit) !== null && _a !== void 0 ? _a : 'vsdistro/distro';
     for (const name of files) {
         const namePath = path.join(...name.split('.').slice(0, -1).join('.').split('_'));
-        cp.spawnSync('git', ['checkout', 'vsdistro/distro', '--', namePath]);
+        cp.spawnSync('git', ['fetch', 'vsdistro']);
+        cp.spawnSync('git', ['checkout', target, '--', namePath]);
         cp.spawnSync('git', ['apply', '--ignore-space-change', '--ignore-whitespace', path.join(patchPath, name)]);
     }
 }
--- build/lib/patch.ts
+++ build/lib/patch.ts
@@ -8,15 +8,25 @@
 import { promises as fs } from 'fs';
 import * as path from 'path';
 import * as cp from 'child_process';
+import * as minimist from 'minimist';
+
+const args = minimist(process.argv, {
+	string: [
+		'commit',
+	]
+});
 
 async function main(): Promise<void> {
 	const patchPath = path.join(__dirname, '..', 'patches');
 
 	const files = await fs.readdir(patchPath);
 
+	const target = args.commit ?? 'vsdistro/distro';
+
 	for (const name of files) {
 		const namePath = path.join(...name.split('.').slice(0, -1).join('.').split('_'));
-		cp.spawnSync('git', ['checkout', 'vsdistro/distro', '--', namePath]);
+		cp.spawnSync('git', ['fetch', 'vsdistro']);
+		cp.spawnSync('git', ['checkout', target, '--', namePath]);
 		cp.spawnSync('git', ['apply', '--ignore-space-change', '--ignore-whitespace', path.join(patchPath, name)]);
 	}
 }
