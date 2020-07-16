c4710f857d2896673ba2f46a066ab898afc9aa8f
Brian Brady <v-bbrady@microsoft.com>
2020-07-16 13:30:48 -0700

    formats html

--- src/sql/workbench/contrib/welcome/page/browser/az_data_welcome_page.ts
+++ src/sql/workbench/contrib/welcome/page/browser/az_data_welcome_page.ts
@@ -94,25 +94,25 @@ export default () => `
 				<div class="resources-container">
 					<h2>${escape(localize('welcomePage.resources', "Resources"))}</h2>
 					<div class="tabs">
-					<!-- Checkbox is not accessible to user yet, this feature is still in development -->
-					<input tabindex="-1" class="input" name="tabs" type="radio" id="tab-1" checked="checked" />
-					<label id="historyLabel" class="label" for="tab-1" tabIndex="0">${escape(localize('welcomePage.history', "History"))}</label>
-						<div class="panel">
-							<div class="recent history">
-								<div class="flex list-header-container">
-									<i class="icon-document themed-icon"></i>
-									<h4 class="list-header">${escape(localize('welcomePage.name', "Name"))}</h4>
-									<h4 class="list-header-last-opened">${escape(localize('welcomePage.lastOpened', "Last Opened"))}</h4>
+						<!-- Checkbox is not accessible to user yet, this feature is still in development -->
+						<input tabindex="-1" class="input" name="tabs" type="radio" id="tab-1" checked="checked" />
+						<label id="historyLabel" class="label" for="tab-1" tabIndex="0">${escape(localize('welcomePage.history', "History"))}</label>
+							<div class="panel">
+								<div class="recent history">
+									<div class="flex list-header-container">
+										<i class="icon-document themed-icon"></i>
+										<h4 class="list-header">${escape(localize('welcomePage.name', "Name"))}</h4>
+										<h4 class="list-header-last-opened">${escape(localize('welcomePage.lastOpened', "Last Opened"))}</h4>
+									</div>
+									<ul class="list">
+										<!-- Filled programmatically -->
+									</ul>
+									<p class="none detail">No recent folders</p>
+									<div class="moreRecent">
+										<a class="ads-welcome-page-link" href="command:workbench.action.openRecent">${escape(localize('welcomePage.moreRecent', "Show more"))}
+										<i class="icon-arrow-down-dark"></i>
+									</a>
 								</div>
-								<ul class="list">
-									<!-- Filled programmatically -->
-								</ul>
-								<p class="none detail">No recent folders</p>
-								<div class="moreRecent">
-									<a class="ads-welcome-page-link" href="command:workbench.action.openRecent">${escape(localize('welcomePage.moreRecent', "Show more"))}
-									<i class="icon-arrow-down-dark"></i>
-								</a>
-							</div>
 							</div>
 						</div>
 					</div>
