66ad46c16d3ddca6378403b3636a83438ad8c0e2
Arvind Ranasaria <ranasaria@outlook.com>
2020-07-17 09:04:22 -0700

    pr feedback

--- src/sql/workbench/contrib/notebook/test/browser/outputProcessor.test.ts
+++ src/sql/workbench/contrib/notebook/test/browser/outputProcessor.test.ts
@@ -3,7 +3,6 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// import * as TypeMoq from 'typemoq';
 import * as assert from 'assert';
 import { nb } from 'azdata';
 import * as op from 'sql/workbench/contrib/notebook/browser/models/outputProcessor';
@@ -14,72 +13,48 @@ import { getRandomString } from 'vs/editor/test/common/model/linesTextBuffer/tex
 
 suite('OutputProcessor functions', function (): void {
 	suite('getData', async function (): Promise<void> {
-		for (const outputType of ['execute_result', 'display_data', 'update_display_data', 'stream', 'error'] as nbformat.OutputType[]) {
-			const text = getRandomString(1, 10);
-			const output: nb.ICellOutput = {
-				output_type: outputType
-			};
-			if (nbformat.isError(output)) {
-				for (const traceback of [undefined, [], [getRandomString(1, 10), getRandomString(1, 10)]]) {
-					for (const evalue of [undefined, getRandomString(1, 10)]) {
-						for (const ename of [text, '']) {
-							test(`test for outputType:${outputType}, ename:'${ename}', evalue:${evalue}, and traceback:${JSON.stringify(traceback)}`, async () => {
-								output.ename = ename;
-								output.evalue = evalue;
-								output.traceback = traceback;
-								const result = op.getData(output);
-								const tracedata = (traceback === undefined || traceback === []) ? undefined : traceback.join('\n');
-								let expectedData: JSONObject = {
-									'application/vnd.jupyter.stderr': undefined
-								};
-								if (tracedata) {
-									expectedData = {
-										'application/vnd.jupyter.stderr': tracedata
-									};
-								} else if (evalue) {
-									if (ename !== '') {
-										expectedData = {
-											'application/vnd.jupyter.stderr': `${output.ename}: ${output.evalue}`
-										};
-									} else {
-										expectedData = {
-											'application/vnd.jupyter.stderr': `${output.evalue}`
-										};
-									}
+		const text = getRandomString(1, 10);
 
-								}
-								assert.deepEqual(result, <JSONObject>expectedData, `getData should return the expectedData:'${JSON.stringify(expectedData)}' object`);
-							});
-						}
-					}
+		// data tests
+		for (const outputType of ['execute_result', 'display_data', 'update_display_data'] as nbformat.OutputType[]) {
+			const output = <nbformat.IExecuteResult>{
+				output_type: outputType,
+				data: <op.IMimeBundle>{
+					result: text  // free form fields of the IMimeBundle. This is just an arbitrarily cooked-up hardcoded example for the test.
 				}
-			} else if (nbformat.isStream(output)) {
-				for (const name of ['stderr', 'stdout'] as nbformat.StreamType[]) {
-					test(`test for outputType:${outputType} and name:${name}`, async () => {
-						output.text = text;
-						output.name = name;
-						const expectedData =
-							name === 'stderr'
-								? {
-									'application/vnd.jupyter.stderr': output.text,
-								}
-								: {
-									'application/vnd.jupyter.stdout': output.text,
-								};
-						const result = op.getData(output);
-						assert.deepEqual(result, expectedData, `getData should return the expectedData:${expectedData} object`);
+			};
+			test(`test for outputType:${output.output_type}`, async () => {
+				verifyGetDataForDataOutput(output);
+			});
+		}
+
+		// error tests
+		for (const traceback of [undefined, [], [''], [getRandomString(0, 10), getRandomString(0, 10)]]) {
+			for (const evalue of [undefined, getRandomString(1, 10)]) {
+				for (const ename of [text, '']) {
+					const output = <nbformat.IError>{
+						output_type: 'error',
+						ename: ename,
+						evalue: evalue,
+						traceback: traceback
+					};
+					test(`test for outputType:'${output.output_type}', ename:'${ename}', evalue:${evalue}, and traceback:${JSON.stringify(traceback)}`, async () => {
+						verifyGetDataForErrorOutput(output);
 					});
 				}
-			} else {
-				test(`test for outputType:${outputType}`, async () => {
-					const expectedData = {
-						data: text
-					};
-					(output as nbformat.IExecuteResult).data = expectedData;
-					const result = op.getData(output);
-					assert.deepEqual(result, expectedData, `getData should return the expectedData:${expectedData} object`);
-				});
 			}
+		}
+
+		// stream tests
+		for (const name of ['stderr', 'stdout'] as nbformat.StreamType[]) {
+			const output = <nbformat.IStream>{
+				output_type: 'stream',
+				text: text,
+				name: name
+			};
+			test(`test for outputType:'${output.output_type} and name:${name}`, async () => {
+				verifyGetDataForStreamOutput(output);
+			});
 		}
 	});
 
@@ -94,9 +69,9 @@ suite('OutputProcessor functions', function (): void {
 				output.metadata = metadata;
 				const result = op.getMetadata(output);
 				if (nbformat.isExecuteResult(output) || nbformat.isDisplayData(output)) {
-					assert.deepEqual(result, metadata, `getData should return the expectedData:'${JSON.stringify(metadata)}' object`);
+					assert.deepEqual(result, metadata, `getData should return the metadata object passed in the output object to the getMetadata() call`);
 				} else {
-					assert.deepEqual(result, {}, `getMetadata should return the expectedData:'${JSON.stringify(metadata)}' object`);
+					assert.deepEqual(result, {}, `getMetadata should return an empty object when output_type is ont IDisplayData or IExecuteResult`);
 				}
 			});
 		}
@@ -125,11 +100,60 @@ suite('OutputProcessor functions', function (): void {
 					metadata: op.getMetadata(output),
 					trusted: trusted
 				};
-				assert.deepEqual(result, expected, `getBundleOptions did not return the expected object:'${JSON.stringify(expected)}'`);
+				assert.deepEqual(result, expected, `getBundleOptions should return an object that has data and metadata fields as returned by getData and getMetadata calls and a trusted field as the value of the 'trusted' field in options passed to it`);
 			});
 		}
 	});
 });
+
+function verifyGetDataForDataOutput(output: nbformat.IExecuteResult | nbformat.IDisplayData | nbformat.IDisplayUpdate) {
+	const result = op.getData(output);
+	// getData just returns the data property object for ExecutionResults/DisplayData/DisplayUpdate Output object sent to it.
+	assert.deepEqual(result, output.data, `getData should return the expectedData:${output.data} object`);
+}
+
+function verifyGetDataForStreamOutput(output: nbformat.IStream): void {
+	//expected return value is an object with a single property of 'application/vnd.jupyter.stderr' or 'application/vnd.jupyter.stdout' corresponding to the stream name
+	const expectedData = output.name === 'stderr'
+		? {
+			'application/vnd.jupyter.stderr': output.text,
+		}
+		: {
+			'application/vnd.jupyter.stdout': output.text,
+		};
+	const result = op.getData(output);
+	assert.deepEqual(result, expectedData, `getData should return the expectedData:${expectedData} object`);
+}
+
+function verifyGetDataForErrorOutput(output: nbformat.IError): void {
+	const result = op.getData(output);
+	const tracedata = (output.traceback === undefined || output.traceback === []) ? undefined : output.traceback.join('\n');
+	// getData returns an object with single property: 'application/vnd.jupyter.stderr'
+	// this property is assigned to a '\n' delimited traceback data when it is present.
+	// when absent ths property gets ename and evalue information information ': ' delimited unless
+	// ename is empty in which case it is just evalue information.
+	let expectedData: JSONObject = {
+		'application/vnd.jupyter.stderr': undefined
+	};
+	if (tracedata) {
+		expectedData = {
+			'application/vnd.jupyter.stderr': tracedata
+		};
+	}
+	else if (output.evalue) {
+		if (output.ename !== '') {
+			expectedData = {
+				'application/vnd.jupyter.stderr': `${output.ename}: ${output.evalue}`
+			};
+		}
+		else {
+			expectedData = {
+				'application/vnd.jupyter.stderr': `${output.evalue}`
+			};
+		}
+	}
+	assert.deepEqual(result, <JSONObject>expectedData, `getData should return the expectedData:'${JSON.stringify(expectedData)}' object`);
+}
 
 function getRandom<T>(...list: T[]): T {
 	return list[Math.floor((Math.random() * list.length))];
