026cc60387cf93f50dae76a655a49ec99aca956d
ADS Merger <andresse@microsoft.com>
2020-07-22 03:06:57 +0000

    Merge from vscode 0a7364f00514c46c9caceece15e1f82f82e3712f

--- src/vs/workbench/browser/parts/views/viewPaneContainer.ts
+++ src/vs/workbench/browser/parts/views/viewPaneContainer.ts
@@ -981,6 +981,8 @@ export class ViewPaneContainer extends Component implements IViewPaneContainer {
 				this.updateViewHeaders();
 			}
 		});
+
+		this._register(this.viewContainerModel.onDidChangeActiveViewDescriptors(() => this._onTitleAreaUpdate.fire()));
 	}
 
 	getTitle(): string {
