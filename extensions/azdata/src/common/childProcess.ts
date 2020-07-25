6cb639d7434e5d8f61901fad7c2a30444beb5254
chgagnon <chgagnon@microsoft.com>
2020-07-25 10:29:58 -0700

    Fix childprocess stuff

--- extensions/azdata/src/common/childProcess.ts
+++ extensions/azdata/src/common/childProcess.ts
@@ -23,9 +23,9 @@ export class ExitCodeError extends Error {
  * @param args Optional args to pass, every arg and arg value must be a separate item in the array
  * @param outputChannel Channel used to display diagnostic information
  */
-export async function executeCommand(command: string, args?: string[], outputChannel?: vscode.OutputChannel): Promise<{ stdout: string, stderr: string }> {
+export async function executeCommand(command: string, args: string[], outputChannel: vscode.OutputChannel): Promise<{ stdout: string, stderr: string }> {
 	return new Promise((resolve, reject) => {
-		outputChannel?.appendLine(loc.executingCommand(command, args ?? []));
+		outputChannel.appendLine(loc.executingCommand(command, args));
 		const stdoutBuffers: Buffer[] = [];
 		const stderrBuffers: Buffer[] = [];
 		const child = cp.spawn(command, args, { shell: true });
@@ -36,14 +36,14 @@ export async function executeCommand(command: string, args?: string[], outputCha
 			const stdout = Buffer.concat(stdoutBuffers).toString('utf8').trim();
 			const stderr = Buffer.concat(stderrBuffers).toString('utf8').trim();
 			if (stdout) {
-				outputChannel?.appendLine(loc.stdoutOutput(stdout));
+				outputChannel.appendLine(loc.stdoutOutput(stdout));
 			}
 			if (stderr) {
-				outputChannel?.appendLine(loc.stdoutOutput(stderr));
+				outputChannel.appendLine(loc.stdoutOutput(stderr));
 			}
 			if (code) {
 				const err = new ExitCodeError(code);
-				outputChannel?.appendLine(err.message);
+				outputChannel.appendLine(err.message);
 				reject(err);
 			} else {
 				resolve({ stdout: stdout, stderr: stderr });
@@ -59,20 +59,20 @@ export async function executeCommand(command: string, args?: string[], outputCha
  * @param args The additional args
  * @param outputChannel Channel used to display diagnostic information
  */
-export async function executeSudoCommand(command: string, outputChannel?: vscode.OutputChannel): Promise<{ stdout: string, stderr: string }> {
+export async function executeSudoCommand(command: string, outputChannel: vscode.OutputChannel): Promise<{ stdout: string, stderr: string }> {
 	return new Promise((resolve, reject) => {
-		outputChannel?.appendLine(loc.executingCommand(`sudo ${command}`, []));
+		outputChannel.appendLine(loc.executingCommand(`sudo ${command}`, []));
 		sudo.exec(command, { name: vscode.env.appName }, (error, stdout, stderr) => {
 			stdout = stdout?.toString() ?? '';
 			stderr = stderr?.toString() ?? '';
 			if (stdout) {
-				outputChannel?.appendLine(loc.stdoutOutput(stdout));
+				outputChannel.appendLine(loc.stdoutOutput(stdout));
 			}
 			if (stderr) {
-				outputChannel?.appendLine(loc.stdoutOutput(stderr));
+				outputChannel.appendLine(loc.stdoutOutput(stderr));
 			}
 			if (error) {
-				outputChannel?.appendLine(loc.unexpectedCommandError(error.message));
+				outputChannel.appendLine(loc.unexpectedCommandError(error.message));
 				reject(error);
 			} else {
 				resolve({ stdout: stdout, stderr: stderr });
