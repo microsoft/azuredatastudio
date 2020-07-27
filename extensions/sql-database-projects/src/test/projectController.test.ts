b3e29e362b01227c8140d3cef5aba64067fe6a79
Udeesha Gautam <udgautam@microsoft.com>
2020-07-27 11:24:26 -0700

    removing some parts of recent tests that might cause issues

--- extensions/sql-database-projects/src/test/projectController.test.ts
+++ extensions/sql-database-projects/src/test/projectController.test.ts
@@ -252,7 +252,6 @@ describe('ProjectsController', function (): void {
 
 				const publishHoller = 'hello from callback for publish()';
 				const generateHoller = 'hello from callback for generateScript()';
-				const profileHoller = 'hello from callback for readPublishProfile()';
 
 				let holler = 'nothing';
 
@@ -266,15 +265,6 @@ describe('ProjectsController', function (): void {
 				projController.setup(x => x.executionCallback(TypeMoq.It.isAny(), TypeMoq.It.is((_): _ is IPublishSettings => true))).returns(() => {
 					holler = publishHoller;
 					return Promise.resolve(undefined);
-				});
-				projController.setup(x => x.readPublishProfileCallback(TypeMoq.It.isAny())).returns(() => {
-					holler = profileHoller;
-					return Promise.resolve({
-						databaseName: '',
-						connectionId: '',
-						connectionString: '',
-						sqlCmdVariables: {}
-					});
 				});
 
 				projController.setup(x => x.executionCallback(TypeMoq.It.isAny(), TypeMoq.It.is((_): _ is IGenerateScriptSettings => true))).returns(() => {
@@ -291,13 +281,7 @@ describe('ProjectsController', function (): void {
 				await dialog.generateScriptClick();
 
 				should(holler).equal(generateHoller, 'executionCallback() is supposed to have been setup and called for GenerateScript scenario');
-
-				dialog = await projController.object.publishProject(proj);
-				await projController.object.readPublishProfileCallback(vscode.Uri.parse('test'));
-
-				should(holler).equal(profileHoller, 'executionCallback() is supposed to have been setup and called for ReadPublishProfile scenario');
 			});
-
 
 			it('Should copy dacpac to temp folder before publishing', async function (): Promise<void> {
 				const fakeDacpacContents = 'SwiftFlewHiawathasArrow';
