7c85397aa54429c429335be186c0841b3091f554
chlafreniere <hichise@gmail.com>
2020-07-30 12:54:31 -0700

    Add convert cell tests

--- src/sql/workbench/contrib/notebook/test/browser/cellToolbarActions.test.ts
+++ src/sql/workbench/contrib/notebook/test/browser/cellToolbarActions.test.ts
@@ -5,7 +5,7 @@
 
 import * as TypeMoq from 'typemoq';
 import * as assert from 'assert';
-import { CellToggleMoreActions, RunCellsAction, removeDuplicatedAndStartingSeparators, AddCellFromContextAction, CollapseCellAction } from 'sql/workbench/contrib/notebook/browser/cellToolbarActions';
+import { CellToggleMoreActions, RunCellsAction, removeDuplicatedAndStartingSeparators, AddCellFromContextAction, CollapseCellAction, ConvertCellAction } from 'sql/workbench/contrib/notebook/browser/cellToolbarActions';
 import { NotebookService } from 'sql/workbench/services/notebook/browser/notebookServiceImpl';
 import { INotificationService } from 'vs/platform/notification/common/notification';
 import { TestNotificationService } from 'vs/platform/notification/test/common/testNotificationService';
@@ -20,6 +20,16 @@ import { ContextMenuService } from 'vs/platform/contextview/browser/contextMenuS
 import { CellModel } from 'sql/workbench/services/notebook/browser/models/cell';
 import { IProductService } from 'vs/platform/product/common/productService';
 import { Separator } from 'vs/base/common/actions';
+import { INotebookModelOptions } from 'sql/workbench/services/notebook/browser/models/modelInterfaces';
+import { NotebookModel } from 'sql/workbench/services/notebook/browser/models/notebookModel';
+import { NotebookEditorContentManager } from 'sql/workbench/contrib/notebook/browser/models/notebookInput';
+import { URI } from 'vs/base/common/uri';
+import { ModelFactory } from 'sql/workbench/services/notebook/browser/models/modelFactory';
+import { CellTypes } from 'sql/workbench/services/notebook/common/contracts';
+import { nb } from 'azdata';
+import { InstantiationService } from 'vs/platform/instantiation/common/instantiationService';
+import { ServiceCollection } from 'vs/platform/instantiation/common/serviceCollection';
+import { NotebookManagerStub } from 'sql/workbench/contrib/notebook/test/stubs';
 
 suite('CellToolbarActions', function (): void {
 	suite('removeDuplicatedAndStartingSeparators', function (): void {
@@ -127,4 +137,80 @@ suite('CellToolbarActions', function (): void {
 			assert.equal(action['_moreActions']['viewItems'][0]['_action']['_actions'].length, 7, 'Unexpected number of valid elements');
 		});
 	});
+
+	suite('ConvertCellAction', function (): void {
+		let convertCellAction: ConvertCellAction;
+		let notebookModel: NotebookModel;
+
+		suiteSetup(async function (): Promise<void> {
+			convertCellAction = new ConvertCellAction('id', 'label', undefined);
+			notebookModel = await createandLoadNotebookModel();
+		});
+
+		test('No notebook model passed in', async function (): Promise<void> {
+			let cellModel = new CellModel({ cell_type: 'code', source: '' }, { isTrusted: true, notebook: undefined });
+			await convertCellAction.doRun({ cell: cellModel, model: undefined });
+			assert.equal(cellModel.cellType, 'code', 'Cell type should not be affected');
+		});
+
+		test('Convert to code cell', async function (): Promise<void> {
+			await notebookModel.loadContents();
+			await convertCellAction.doRun({ model: notebookModel, cell: notebookModel.cells[0] });
+			assert.equal(notebookModel.cells[0].cellType, 'markdown', 'Cell was not converted correctly');
+		});
+
+		test('Convert to markdown cell', async function (): Promise<void> {
+			await notebookModel.loadContents();
+			notebookModel.cells[0].cellType = 'markdown';
+			await convertCellAction.doRun({ model: notebookModel, cell: notebookModel.cells[0] });
+			assert.equal(notebookModel.cells[0].cellType, 'code', 'Cell was not converted correctly');
+		});
+
+		test('Convert to code cell and back', async function (): Promise<void> {
+			await notebookModel.loadContents();
+			notebookModel.cells[0].cellType = 'markdown';
+			await convertCellAction.doRun({ model: notebookModel, cell: notebookModel.cells[0] });
+			assert.equal(notebookModel.cells[0].cellType, 'code', 'Cell was not converted correctly');
+			await convertCellAction.doRun({ model: notebookModel, cell: notebookModel.cells[0] });
+			assert.equal(notebookModel.cells[0].cellType, 'markdown', 'Cell was not converted correctly second time');
+		});
+	});
 });
+
+async function createandLoadNotebookModel(codeContent?: nb.INotebookContents): Promise<NotebookModel> {
+	let defaultCodeContent: nb.INotebookContents = {
+		cells: [{
+			cell_type: CellTypes.Code,
+			source: [''],
+			metadata: { language: 'python' },
+			execution_count: 1
+		}],
+		metadata: {
+			kernelspec: {
+				name: 'python',
+				language: 'python'
+			}
+		},
+		nbformat: 4,
+		nbformat_minor: 5
+	};
+
+	let serviceCollection = new ServiceCollection();
+	let instantiationService = new InstantiationService(serviceCollection, true);
+	let mockContentManager = TypeMoq.Mock.ofType(NotebookEditorContentManager);
+	mockContentManager.setup(c => c.loadContent()).returns(() => Promise.resolve(codeContent ? codeContent : defaultCodeContent));
+	let defaultModelOptions: INotebookModelOptions = {
+		notebookUri: URI.file('/some/path.ipynb'),
+		factory: new ModelFactory(instantiationService),
+		notebookManagers: [new NotebookManagerStub()],
+		contentManager: mockContentManager.object,
+		notificationService: undefined,
+		connectionService: undefined,
+		providerId: 'SQL',
+		cellMagicMapper: undefined,
+		defaultKernel: undefined,
+		layoutChanged: undefined,
+		capabilitiesService: undefined
+	};
+	return new NotebookModel(defaultModelOptions, undefined, undefined, undefined, undefined);
+}
