26a225454b4e9d3d050772a4dd45e91e12145a02
cssuh <chsuh@microsoft.com>
2020-07-31 23:01:44 -0400

    remove telemetry check

--- src/vs/workbench/contrib/welcome/telemetryOptOut/browser/telemetryOptOut.ts
+++ src/vs/workbench/contrib/welcome/telemetryOptOut/browser/telemetryOptOut.ts
@@ -36,7 +36,7 @@ export abstract class AbstractTelemetryOptOut implements IWorkbenchContribution 
 	) { }
 
 	protected async handleTelemetryOptOut(): Promise<void> {
-		if (!this.environmentService.disableTelemetry && this.productService.telemetryOptOutUrl && !this.storageService.get(AbstractTelemetryOptOut.TELEMETRY_OPT_OUT_SHOWN, StorageScope.GLOBAL)) { // {{SQL CARBON EDIT}} add disableTelemetry check
+		if (this.productService.telemetryOptOutUrl && !this.storageService.get(AbstractTelemetryOptOut.TELEMETRY_OPT_OUT_SHOWN, StorageScope.GLOBAL)) {
 			const experimentId = 'telemetryOptOut';
 
 			const [count, experimentState] = await Promise.all([this.getWindowCount(), this.experimentService.getExperimentById(experimentId)]);
