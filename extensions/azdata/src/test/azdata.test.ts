ad05bfc6d42917f098499b601190a34e52aba367
Arvind Ranasaria <ranasaria@outlook.com>
2020-08-07 18:45:42 -0700

    findAzdata test fix

--- extensions/azdata/src/test/azdata.test.ts
+++ extensions/azdata/src/test/azdata.test.ts
@@ -27,12 +27,13 @@ describe('azdata', function () {
 
 	describe('findAzdata', function () {
 		// Mock call to --version to simulate azdata being installed
-		sinon.stub(childProcess, 'executeCommand').returns(Promise.resolve({ stdout: 'v1.0.0', stderr: '' }));
 		it('successful', async function (): Promise<void> {
+			sinon.stub(childProcess, 'executeCommand').returns(Promise.resolve({ stdout: 'v1.0.0', stderr: '' }));
 			sinon.stub(utils, 'searchForCmd').returns(Promise.resolve('C:\\path\\to\\azdata.cmd'));
 			await should(azdata.findAzdata(outputChannelMock.object)).not.be.rejected();
 		});
 		it('unsuccessful', async function (): Promise<void> {
+			sinon.stub(childProcess, 'executeCommand').returns(Promise.reject({ stdout: '', stderr: 'command not found: azdata' }));
 			sinon.stub(utils, 'searchForCmd').returns(Promise.reject(new Error('Could not find azdata')));
 			await should(azdata.findAzdata(outputChannelMock.object)).be.rejected();
 		});
