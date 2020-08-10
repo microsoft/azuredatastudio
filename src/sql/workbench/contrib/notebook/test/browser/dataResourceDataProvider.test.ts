8720f3eeaeb5f173b2ad5ab36a86d6a9666d7baf
Lucy Zhang <luczhan@microsoft.com>
2020-08-10 14:52:55 -0700

    use suiteSetup

--- src/sql/workbench/contrib/notebook/test/browser/dataResourceDataProvider.test.ts
+++ src/sql/workbench/contrib/notebook/test/browser/dataResourceDataProvider.test.ts
@@ -35,55 +35,57 @@ suite('Data Resource Data Provider', function () {
 	let selection: Slick.Range[];
 	let params: SerializeDataParams;
 
-	source = {
-		data: [{ 0: '1' }],
-		schema: { fields: [{ name: 'col1' }] }
-	};
-	resultSet = {
-		batchId: 0,
-		columnInfo: [{ columnName: 'col1' }],
-		complete: true,
-		id: 0,
-		rowCount: 1
-	};
-	documentUri = 'untitled:Notebook-0';
-	_notificationService = undefined;
-	_clipboardService = undefined;
-	_configurationService = undefined;
-	_textResourcePropertiesService = undefined;
-	_serializationService = undefined;
-	_instantiationService = workbenchInstantiationService();
-	dataResourceDataProvider = new DataResourceDataProvider(
-		source,
-		resultSet,
-		documentUri,
-		_notificationService,
-		_clipboardService,
-		_configurationService,
-		_textResourcePropertiesService,
-		_serializationService,
-		_instantiationService
-	);
-	serializer = _instantiationService.createInstance(ResultSerializer);
-	format = SaveFormat.CSV;
-	selection = undefined;
-	sinon.stub(ResultSerializer.prototype, 'getBasicSaveParameters').withArgs(format).returns({ resultFormat: SaveFormat.CSV });
-	params = dataResourceDataProvider.getSerializeRequestParams(serializer, URI.file(documentUri), format, selection);
+	suiteSetup(() => {
+		source = {
+			data: [{ 0: '1' }],
+			schema: { fields: [{ name: 'col1' }] }
+		};
+		resultSet = {
+			batchId: 0,
+			columnInfo: [{ columnName: 'col1' }],
+			complete: true,
+			id: 0,
+			rowCount: 1
+		};
+		documentUri = 'untitled:Notebook-0';
+		_notificationService = undefined;
+		_clipboardService = undefined;
+		_configurationService = undefined;
+		_textResourcePropertiesService = undefined;
+		_serializationService = undefined;
+		_instantiationService = workbenchInstantiationService();
+		dataResourceDataProvider = new DataResourceDataProvider(
+			source,
+			resultSet,
+			documentUri,
+			_notificationService,
+			_clipboardService,
+			_configurationService,
+			_textResourcePropertiesService,
+			_serializationService,
+			_instantiationService
+		);
+		serializer = _instantiationService.createInstance(ResultSerializer);
+		format = SaveFormat.CSV;
+		selection = undefined;
+		sinon.stub(ResultSerializer.prototype, 'getBasicSaveParameters').withArgs(format).returns({ resultFormat: SaveFormat.CSV });
+		params = dataResourceDataProvider.getSerializeRequestParams(serializer, URI.file(documentUri), format, selection);
+	});
 
 	test('getRowRange should return with headers when includeHeaders is true', async function (): Promise<void> {
 		let rowStart = 0;
 		let includeHeaders = true;
 		let numberOfRows = 1;
 		let results: DbCellValue[][] = params.getRowRange(rowStart, includeHeaders, numberOfRows);
-		assert.equal(results.length, 2, 'result should have 2 rows');
+		assert.equal(results.length, 2, 'Results should have 2 rows');
 	});
 
-	test('getRows should return without headers when includeHeaders is false', async function (): Promise<void> {
+	test('getRowRange should return without headers when includeHeaders is false', async function (): Promise<void> {
 		let rowStart = 0;
 		let includeHeaders = false;
 		let numberOfRows = 1;
 		let results: DbCellValue[][] = params.getRowRange(rowStart, includeHeaders, numberOfRows);
-		assert.equal(results.length, 1, 'results should have 1 row');
+		assert.equal(results.length, 1, 'Results should have 1 row');
 	});
 });
 
