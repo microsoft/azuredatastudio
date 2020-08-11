7a99a1b7b4bb211c1ac4537e216b10c2ee4bf1e2
Cory Rivera <corivera@microsoft.com>
2020-08-11 08:58:47 -0700

    Add test for getting pip packages.

--- extensions/notebook/src/test/python/jupyterInstallation.test.ts
+++ extensions/notebook/src/test/python/jupyterInstallation.test.ts
@@ -7,16 +7,18 @@ import * as vscode from 'vscode';
 import * as should from 'should';
 import * as sinon from 'sinon';
 import * as TypeMoq from 'typemoq';
-import { JupyterServerInstallation } from '../../jupyter/jupyterServerInstallation';
+import { JupyterServerInstallation, PythonPkgDetails } from '../../jupyter/jupyterServerInstallation';
+import uuid = require('uuid');
 
 describe('Jupyter Server Installation', function () {
-	const outputChannelStub = TypeMoq.Mock.ofType<vscode.OutputChannel>();
-	outputChannelStub.setup(c => c.show(TypeMoq.It.isAny()));
-	outputChannelStub.setup(c => c.appendLine(TypeMoq.It.isAnyString()));
-
+	let outputChannelStub: TypeMoq.IMock<vscode.OutputChannel>;
 	let installation: JupyterServerInstallation;
 
 	beforeEach(function (): void {
+		outputChannelStub = TypeMoq.Mock.ofType<vscode.OutputChannel>();
+		outputChannelStub.setup(c => c.show(TypeMoq.It.isAny()));
+		outputChannelStub.setup(c => c.appendLine(TypeMoq.It.isAnyString()));
+
 		installation = new JupyterServerInstallation('', outputChannelStub.object);
 	});
 
@@ -25,7 +27,37 @@ describe('Jupyter Server Installation', function () {
 	});
 
 	it('Get pip packages', async function() {
+		// Should return nothing if passed an invalid python path
+		let fakePath = uuid.v4().replace('-', '');
+		let pkgResult = await installation.getInstalledPipPackages(fakePath);
+		should(pkgResult).not.be.undefined();
+		should(pkgResult.length).be.equal(0);
 
+		// Should return nothing if python is not installed
+		sinon.stub(JupyterServerInstallation, 'isPythonInstalled').returns(false);
+		pkgResult = await installation.getInstalledPipPackages();
+		should(pkgResult).not.be.undefined();
+		should(pkgResult.length).be.equal(0);
+
+		// Should return nothing on error
+		sinon.stub(JupyterServerInstallation, 'isPythonInstalled').returns(true);
+		sinon.stub(installation, 'executeBufferedCommand').rejects(new Error('Expected test failure.'));
+		pkgResult = await installation.getInstalledPipPackages();
+		should(pkgResult).not.be.undefined();
+		should(pkgResult.length).be.equal(0);
+		outputChannelStub.verify(c => c.appendLine(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+
+		// Normal use case
+		let testPackages: PythonPkgDetails[] = [{
+			name: 'TestPkg1',
+			version: '1.2.3'
+		}, {
+			name: 'TestPkg2',
+			version: '4.5.6'
+		}];
+		sinon.stub(installation, 'executeBufferedCommand').returns(Promise.resolve(JSON.stringify(testPackages)));
+		pkgResult = await installation.getInstalledPipPackages();
+		should(pkgResult).be.deepEqual(testPackages);
 	});
 
 	it('Install pip package', async function() {
