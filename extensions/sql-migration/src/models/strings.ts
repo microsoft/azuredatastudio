70b32eaf9cd5756aff89654fc13c932f6b94fa4b
Amir Omidi <amomidi@microsoft.com>
2020-08-11 13:09:16 -0700

    Migration wizard skeleton setup

---
+++ extensions/sql-migration/src/models/strings.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as nls from 'vscode-nls';
+const localize = nls.loadMessageBundle();
+
+
+export const WIZARD_TITLE = localize('sql-migration.wizard.title', "SQL Migration Wizard");
+export const SOURCE_CONFIGURATION_PAGE_TITLE = localize('sql.migration.wizard.source_configuration.title', "SQL Source Configuration");
