8f6a7dc13e460f3408638d98083e21353118ce32
Amir Omidi <amomidi@microsoft.com>
2020-08-11 14:54:40 -0700

    Migration wizard skeleton setup (#11758)

--- extensions/sql-migration/src/wizard/wizardController.ts
+++ extensions/sql-migration/src/wizard/wizardController.ts
@@ -2,6 +2,10 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import * as azdata from 'azdata';
+import { WIZARD_TITLE } from '../models/strings';
+import { SourceConfigurationPage } from './sourceConfigurationPage';
+import { MigrationStateModel } from '../models/stateMachine';
 
 export class WizardController {
 	constructor() {
@@ -9,6 +13,12 @@ export class WizardController {
 	}
 
 	public async openWizard(): Promise<void> {
+	}
+
+	public async createWizard(): Promise<void> {
+		azdata.window.createWizard(WIZARD_TITLE, 'wide');
+		const stateModel = new MigrationStateModel({} as azdata.IConnectionProfile);
+		new SourceConfigurationPage(stateModel);
 
 	}
 }
