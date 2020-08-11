9933c8d6f928527f2d2a3d812e7ec4b475e2345a
Cory Rivera <corivera@microsoft.com>
2020-08-11 11:28:24 -0700

    Add conda package test.

--- extensions/notebook/src/test/python/jupyterInstallation.test.ts
+++ extensions/notebook/src/test/python/jupyterInstallation.test.ts
@@ -68,7 +68,41 @@ describe('Jupyter Server Installation', function () {
 	});
 
 	it('Get conda packages', async function() {
+		// Should return nothing if conda is not installed
+		sinon.stub(installation, 'isCondaInstalled').returns(false);
+		let pkgResult = await installation.getInstalledCondaPackages();
+		should(pkgResult).not.be.undefined();
+		should(pkgResult.length).be.equal(0);
 
+		// Should return nothing on error
+		sinon.restore();
+		sinon.stub(installation, 'isCondaInstalled').returns(true);
+		sinon.stub(installation, 'executeBufferedCommand').rejects(new Error('Expected test failure.'));
+		pkgResult = await installation.getInstalledCondaPackages();
+		should(pkgResult).not.be.undefined();
+		should(pkgResult.length).be.equal(0);
+		outputChannelStub.verify(c => c.appendLine(TypeMoq.It.isAnyString()), TypeMoq.Times.once());
+
+		// Normal use case
+		sinon.restore();
+		let testPackages: PythonPkgDetails[] = [{
+			name: 'TestPkg1',
+			version: '1.2.3',
+			channel: 'conda'
+		}, {
+			name: 'TestPkg2',
+			version: '4.5.6',
+			channel: 'pypi'
+		}, {
+			name: 'TestPkg3',
+			version: '7.8.9',
+			channel: 'conda'
+		}];
+		sinon.stub(installation, 'isCondaInstalled').returns(true);
+		sinon.stub(installation, 'executeBufferedCommand').returns(Promise.resolve(JSON.stringify(testPackages)));
+		pkgResult = await installation.getInstalledCondaPackages();
+		let filteredPackages = testPackages.filter(pkg => pkg.channel !== 'pypi');
+		should(pkgResult).be.deepEqual(filteredPackages);
 	});
 
 	it('Install conda package', async function() {
