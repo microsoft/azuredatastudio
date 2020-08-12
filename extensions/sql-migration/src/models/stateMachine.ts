c0bd2098293b41952f91d2f3c670f692a5609d05
Amir Omidi <amomidi@microsoft.com>
2020-08-12 13:33:18 -0700

    Introduces event queue processor to create a consistent UI

--- extensions/sql-migration/src/models/stateMachine.ts
+++ extensions/sql-migration/src/models/stateMachine.ts
@@ -27,6 +27,7 @@ export enum State {
 export interface Model {
 	readonly sourceConnection: azdata.connection.Connection;
 	readonly currentState: State;
+	gatheringInformationError: string | undefined;
 }
 
 export interface StateChangeEvent {
@@ -37,6 +38,7 @@ export interface StateChangeEvent {
 export class MigrationStateModel implements Model, vscode.Disposable {
 	private _stateChangeEventEmitter = new vscode.EventEmitter<StateChangeEvent>();
 	private _currentState: State;
+	private _gatheringInformationError: string | undefined;
 
 	constructor(private readonly _sourceConnection: azdata.connection.Connection) {
 		this._currentState = State.INIT;
@@ -56,6 +58,14 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 		this._currentState = newState;
 
 		this._stateChangeEventEmitter.fire({ oldState, newState: this.currentState });
+	}
+
+	public get gatheringInformationError(): string | undefined {
+		return this._gatheringInformationError;
+	}
+
+	public set gatheringInformationError(error: string | undefined) {
+		this._gatheringInformationError = error;
 	}
 
 	public get stateChangeEvent(): vscode.Event<StateChangeEvent> {
