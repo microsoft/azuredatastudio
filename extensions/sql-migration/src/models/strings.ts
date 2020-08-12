207a9a6a2540ce455df64de69735235ceb477131
Amir Omidi <amomidi@microsoft.com>
2020-08-11 17:12:05 -0700

    Sets up the first page with some filler information (#11762)
    * Sets up the first page with some filler information
    
    * Add some valid information
    
    * move to its own method

--- extensions/sql-migration/src/models/strings.ts
+++ extensions/sql-migration/src/models/strings.ts
@@ -7,5 +7,10 @@ import * as nls from 'vscode-nls';
 const localize = nls.loadMessageBundle();
 
 
+// #region wizard
 export const WIZARD_TITLE = localize('sql-migration.wizard.title', "SQL Migration Wizard");
 export const SOURCE_CONFIGURATION_PAGE_TITLE = localize('sql.migration.wizard.source_configuration.title', "SQL Source Configuration");
+// //#endregion
+
+export const COLLECTING_SOURCE_CONFIGURATIONS = localize('sql.migration.collecting_source_configurations', "Collecting source configurations");
+export const COLLECTING_SOURCE_CONFIGURATIONS_INFO = localize('sql.migration.collecting_source_configurations.info', "We need to collect some information about how your data is configured currently.\nThis may take some time.");
