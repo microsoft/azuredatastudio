f3cc9d532252a6bc46e709b8f8a2e4b391912ad8
Alan Ren <alanren@microsoft.com>
2020-08-18 11:06:41 -0700

    initial commit

---
+++ extensions/data-workspace/src/main.ts
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	vscode.commands.registerCommand('dataworkspace.addExistingProject', () => {
+	});
+}
+
+export function deactivate(): void {
+}
