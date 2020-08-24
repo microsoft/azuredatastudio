a8487ba803b7fed9f99ba157fc806118c28ba2bd
Arvind Ranasaria <ranasaria@outlook.com>
2020-08-24 12:34:42 -0700

    cleanup

--- extensions/azdata/src/test/common/utils.test.ts
+++ extensions/azdata/src/test/common/utils.test.ts
@@ -3,13 +3,9 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import * as should from 'should';
-import * as sinon from 'sinon';
 import { searchForCmd as searchForExe } from '../../common/utils';
 
 describe('utils', function () {
-	afterEach(function (): void {
-		sinon.restore();
-	});
 	describe('searchForExe', function (): void {
 		it('finds exe successfully', async function (): Promise<void> {
 			await searchForExe('node');
