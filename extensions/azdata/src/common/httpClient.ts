f3b7f3bba88f7c31a2b63d51d151903c6d8dd365
Charles Gagnon <chgagnon@microsoft.com>
2020-08-24 12:32:45 -0700

    Add static logger class for azdata extension (#11939)
    * Add static logger class for arc extension
    
    * Fix compile errors
    
    * Fix test

--- extensions/azdata/src/common/httpClient.ts
+++ extensions/azdata/src/common/httpClient.ts
@@ -3,11 +3,11 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as request from 'request';
 import * as path from 'path';
 import * as loc from '../localizedConstants';
+import Logger from './logger';
 
 const DownloadTimeout = 20000;
 
@@ -17,34 +17,33 @@ export namespace HttpClient {
 	 * Downloads a file from the given URL, resolving to the full path of the downloaded file when complete
 	 * @param downloadUrl The URL to download the file from
 	 * @param targetFolder The folder to download the file to
-	 * @param outputChannel Channel used to display diagnostic information
 	 * @returns Full path to the downloaded file
 	 */
-	export function download(downloadUrl: string, targetFolder: string, outputChannel: vscode.OutputChannel): Promise<string> {
+	export function download(downloadUrl: string, targetFolder: string): Promise<string> {
 		return new Promise((resolve, reject) => {
 			let totalMegaBytes: number | undefined = undefined;
 			let receivedBytes = 0;
 			let printThreshold = 0.1;
 			let downloadRequest = request.get(downloadUrl, { timeout: DownloadTimeout })
 				.on('error', downloadError => {
-					outputChannel.appendLine(loc.downloadError);
-					outputChannel.appendLine(downloadError?.message ?? downloadError);
+					Logger.log(loc.downloadError);
+					Logger.log(downloadError?.message ?? downloadError);
 					reject(downloadError);
 				})
 				.on('response', (response) => {
 					if (response.statusCode !== 200) {
-						outputChannel.appendLine(loc.downloadError);
-						outputChannel.appendLine(response.statusMessage);
+						Logger.log(loc.downloadError);
+						Logger.log(response.statusMessage);
 						return reject(response.statusMessage);
 					}
 					const filename = path.basename(response.request.path);
 					const targetPath = path.join(targetFolder, filename);
-					outputChannel.appendLine(loc.downloadingTo(filename, targetPath));
+					Logger.log(loc.downloadingTo(filename, targetPath));
 					// Wait to create the WriteStream until here so we can use the actual
 					// filename based off of the URI.
 					downloadRequest.pipe(fs.createWriteStream(targetPath))
 						.on('close', async () => {
-							outputChannel.appendLine(loc.downloadFinished);
+							Logger.log(loc.downloadFinished);
 							resolve(targetPath);
 						})
 						.on('error', (downloadError) => {
@@ -54,7 +53,7 @@ export namespace HttpClient {
 					let contentLength = response.headers['content-length'];
 					let totalBytes = parseInt(contentLength || '0');
 					totalMegaBytes = totalBytes / (1024 * 1024);
-					outputChannel.appendLine(loc.downloadingProgressMb('0', totalMegaBytes.toFixed(2)));
+					Logger.log(loc.downloadingProgressMb('0', totalMegaBytes.toFixed(2)));
 				})
 				.on('data', (data) => {
 					receivedBytes += data.length;
@@ -62,7 +61,7 @@ export namespace HttpClient {
 						let receivedMegaBytes = receivedBytes / (1024 * 1024);
 						let percentage = receivedMegaBytes / totalMegaBytes;
 						if (percentage >= printThreshold) {
-							outputChannel.appendLine(loc.downloadingProgressMb(receivedMegaBytes.toFixed(2), totalMegaBytes.toFixed(2)));
+							Logger.log(loc.downloadingProgressMb(receivedMegaBytes.toFixed(2), totalMegaBytes.toFixed(2)));
 							printThreshold += 0.1;
 						}
 					}
@@ -74,8 +73,8 @@ export namespace HttpClient {
 	 * Gets the filename for the specified URL - following redirects as needed
 	 * @param url The URL to get the filename of
 	 */
-	export async function getFilename(url: string, outputChannel: vscode.OutputChannel): Promise<string> {
-		outputChannel.appendLine(loc.gettingFilenameOfUrl(url));
+	export async function getFilename(url: string): Promise<string> {
+		Logger.log(loc.gettingFilenameOfUrl(url));
 		return new Promise((resolve, reject) => {
 			let httpRequest = request.get(url, { timeout: DownloadTimeout })
 				.on('error', downloadError => {
@@ -88,7 +87,7 @@ export namespace HttpClient {
 					// We don't want to actually download the file so abort the request now
 					httpRequest.abort();
 					const filename = path.basename(response.request.path);
-					outputChannel.appendLine(loc.gotFilenameOfUrl(response.request.path, filename));
+					Logger.log(loc.gotFilenameOfUrl(response.request.path, filename));
 					resolve(filename);
 				});
 		});
