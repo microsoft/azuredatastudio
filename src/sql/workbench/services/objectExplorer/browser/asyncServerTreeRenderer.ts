3f90d501e7e62d60e46139f72180101299c8d124
Justin M <63619224+JustinMDotNet@users.noreply.github.com>
2020-08-25 15:05:05 -0700

    3486 Added logic to render server icon when provider is Kusto in asyncServerTreeRenderer > set

--- src/sql/workbench/services/objectExplorer/browser/asyncServerTreeRenderer.ts
+++ src/sql/workbench/services/objectExplorer/browser/asyncServerTreeRenderer.ts
@@ -115,6 +115,11 @@ class ConnectionProfileTemplate extends Disposable {
 			label = localize('loading', "Loading...");
 		}
 
+		if (element.providerName === 'KUSTO') {
+			let iconPath = getIconPath(element, this._connectionManagementService);
+			renderServerIcon(this._icon, iconPath);
+		}
+
 		this._label.textContent = label;
 		this._root.title = element.serverInfo;
 	}
