cc132671643aa537b0e764550e7474dcf4810e9c
Anjali Agarwal <t-anjaga@microsoft.com>
2020-08-26 10:56:13 -0700

    fixing tests

--- extensions/sql-database-projects/src/test/newProjectTool.test.ts
+++ extensions/sql-database-projects/src/test/newProjectTool.test.ts
@@ -9,63 +9,45 @@ import * as newProjectTool from '../tools/newProjectTool';
 import * as constants from '../common/constants';
 import { generateTestFolderPath, createTestFile } from './testUtils';
 
-describe.skip('NewProjectTool: New project tool tests', function (): void {
+let previousSetting : string;
+let testFolderPath : string;
+
+describe('NewProjectTool: New project tool tests', function (): void {
+
+	beforeEach(async function () {
+		previousSetting = await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey)[constants.projectSaveLocationKey];
+		testFolderPath = await generateTestFolderPath();
+	});
+
+	afterEach(async function () {
+		await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, previousSetting, true);
+	});
 
 	it('Should generate correct default project names', async function (): Promise<void> {
-		// save original setting
-		const previousSetting = await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey)[constants.projectSaveLocationKey];
-		const testFolderPath = await generateTestFolderPath();
-
-		try {
-			await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, testFolderPath, true);
-			should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject1');
-			should(newProjectTool.defaultProjectNameFromDb('master')).equal('DatabaseProjectmaster');
-		}
-		finally {
-			// reset configuration
-			await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, previousSetting, true);
-		}
+		await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, testFolderPath, true);
+		should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject1');
+		should(newProjectTool.defaultProjectNameFromDb('master')).equal('DatabaseProjectmaster');
 	});
 
 	it('Should auto-increment default project names for new projects', async function (): Promise<void> {
-		// save original setting
-		const previousSetting = await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey)[constants.projectSaveLocationKey];
-		const testFolderPath = await generateTestFolderPath();
+		await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, testFolderPath, true);
+		should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject1');
 
-		try {
-			await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, testFolderPath, true);
-			should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject1');
+		await createTestFile('', 'DatabaseProject1', testFolderPath);
+		should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject2');
 
-			await createTestFile('', 'DatabaseProject1', testFolderPath);
-			should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject2');
-
-			await createTestFile('', 'DatabaseProject2', testFolderPath);
-			should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject3');
-		}
-		finally {
-			// reset configuration
-			await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, previousSetting, true);
-		}
+		await createTestFile('', 'DatabaseProject2', testFolderPath);
+		should(newProjectTool.defaultProjectNameNewProj()).equal('DatabaseProject3');
 	});
 
 	it('Should auto-increment default project names for import projects', async function (): Promise<void> {
-		// save original setting
-		const previousSetting = await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey)[constants.projectSaveLocationKey];
-		const testFolderPath = await generateTestFolderPath();
+		await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, testFolderPath, true);
+		should(newProjectTool.defaultProjectNameFromDb("master")).equal('DatabaseProjectmaster');
 
-		try {
-			await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, testFolderPath, true);
-			should(newProjectTool.defaultProjectNameFromDb("master")).equal('DatabaseProjectmaster');
+		await createTestFile('', 'DatabaseProjectmaster', testFolderPath);
+		should(newProjectTool.defaultProjectNameFromDb("master")).equal('DatabaseProjectmaster2');
 
-			await createTestFile('', 'DatabaseProjectmaster', testFolderPath);
-			should(newProjectTool.defaultProjectNameFromDb("master")).equal('DatabaseProjectmaster2');
-
-			await createTestFile('', 'DatabaseProjectmaster2', testFolderPath);
-			should(newProjectTool.defaultProjectNameFromDb("master")).equal('DatabaseProjectmaster3');
-		}
-		finally {
-			// reset configuration
-			await vscode.workspace.getConfiguration(constants.dbProjectConfigurationKey).update(constants.projectSaveLocationKey, previousSetting, true);
-		}
+		await createTestFile('', 'DatabaseProjectmaster2', testFolderPath);
+		should(newProjectTool.defaultProjectNameFromDb("master")).equal('DatabaseProjectmaster3');
 	});
 });
