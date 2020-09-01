3aa7f68edbe87a3312e206df7b162cdd57419dd4
Alan Ren <alanren@microsoft.com>
2020-09-01 11:12:11 -0700

    comments

--- extensions/data-workspace/src/main.ts
+++ extensions/data-workspace/src/main.ts
@@ -6,9 +6,31 @@
 import * as vscode from 'vscode';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	vscode.window.registerTreeDataProvider('dataworkspace.views.main', new ProjectsTree());
 	vscode.commands.registerCommand('projects.addProject', () => {
 	});
 }
 
 export function deactivate(): void {
+}
+
+export class ProjectsTree implements vscode.TreeDataProvider<TreeItem>{
+	private _onDidChangeTreeData: vscode.EventEmitter<void | TreeItem | null | undefined> | undefined = new vscode.EventEmitter<TreeItem | undefined | void>();
+	readonly onDidChangeTreeData?: vscode.Event<void | TreeItem | null | undefined> | undefined = this._onDidChangeTreeData?.event;
+	getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+	getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
+		return [
+			new TreeItem('abc', vscode.TreeItemCollapsibleState.Collapsed)
+		];
+	}
+}
+
+export class TreeItem extends vscode.TreeItem {
+	constructor(public readonly label: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command) {
+		super(label, collapsibleState);
+	}
 }
