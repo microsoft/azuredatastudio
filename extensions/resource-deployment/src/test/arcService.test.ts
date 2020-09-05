e7b8e9bfacbef0426cecffb088731b86a6c42df4
Arvind Ranasaria <ranasaria@outlook.com>
2020-09-04 22:16:31 -0700

    activate arc extension in test

--- extensions/resource-deployment/src/test/arcService.test.ts
+++ extensions/resource-deployment/src/test/arcService.test.ts
@@ -4,10 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 
 import 'mocha';
+import * as arc from 'arc';
+import * as vscode from 'vscode';
 import * as should from 'should';
 import { ArcService } from '../services/arcService';
 
 suite('arc service Tests', function (): void {
+	before(async () => {
+		const arcExtension = vscode.extensions.getExtension<arc.IExtension>(arc.extension.name);
+		if (arcExtension && !arcExtension.isActive) {
+			arcExtension.activate();
+		} else {
+			console.log(`arc extension was already activated when the tests started`);
+		}
+	});
+
 	const arcService = new ArcService();
 	test('arc service fetches registered arc data controllers() successfully', () => {
 		should(arcService.getRegisteredDataControllers()).not.be.undefined();
