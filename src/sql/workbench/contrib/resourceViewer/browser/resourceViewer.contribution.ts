6b264b1bdb9647b27b43f5f4259202ce44261342
Anthony Dresser <andresse@microsoft.com>
2020-09-08 12:21:40 -0700

    add inital resource view

--- src/sql/workbench/contrib/resourceViewer/browser/resourceViewer.contribution.ts
+++ src/sql/workbench/contrib/resourceViewer/browser/resourceViewer.contribution.ts
@@ -12,6 +12,15 @@ import { CommandsRegistry } from 'vs/platform/commands/common/commands';
 import { ServicesAccessor, IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { IEditorService, ACTIVE_GROUP } from 'vs/workbench/services/editor/common/editorService';
 import { isString } from 'vs/base/common/types';
+import { IViewContainersRegistry, ViewContainerLocation, Extensions as ViewExtensions, IViewsRegistry } from 'vs/workbench/common/views';
+import { RESOURCE_VIEWER_VIEW_CONTAINER_ID, RESOURCE_VIEWER_VIEW_ID } from 'sql/workbench/contrib/resourceViewer/common/resourceViewer';
+import { localize } from 'vs/nls';
+import { Codicon } from 'vs/base/common/codicons';
+import { ResourceViewerViewlet } from 'sql/workbench/contrib/resourceViewer/browser/resourceViewerViewlet';
+import { ResourceViewerView } from 'sql/workbench/contrib/resourceViewer/browser/resourceViewerView';
+import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions';
+import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
+import { ResourceViewResourcesExtensionHandler } from 'sql/workbench/contrib/resourceViewer/common/resourceViewerViewExtensionPoint';
 
 CommandsRegistry.registerCommand({
 	id: 'resourceViewer.openResourceViewer',
@@ -36,3 +45,21 @@ const resourceViewerDescriptor = EditorDescriptor.create(
 Registry.as<IEditorRegistry>(EditorExtensions.Editors)
 	.registerEditor(resourceViewerDescriptor, [new SyncDescriptor(ResourceViewerInput)]);
 
+Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench).registerWorkbenchContribution(ResourceViewResourcesExtensionHandler, LifecyclePhase.Ready);
+
+registerResourceViewerContainer();
+
+function registerResourceViewerContainer() {
+	const viewContainer = Registry.as<IViewContainersRegistry>(ViewExtensions.ViewContainersRegistry).registerViewContainer({
+		id: RESOURCE_VIEWER_VIEW_CONTAINER_ID,
+		name: localize('resourceViewer', "Resource Viewer"),
+		ctorDescriptor: new SyncDescriptor(ResourceViewerViewlet),
+		icon: Codicon.database.classNames,
+		alwaysUseContainerInfo: true
+	}, ViewContainerLocation.Sidebar);
+	// registry.registerWorkbenchAction(SyncActionDescriptor.from(OpenDebugViewletAction, { primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_D }), 'View: Show Run and Debug', nls.localize('view', "View"));
+
+	// Register default debug views
+	const viewsRegistry = Registry.as<IViewsRegistry>(ViewExtensions.ViewsRegistry);
+	viewsRegistry.registerViews([{ id: RESOURCE_VIEWER_VIEW_ID, name: localize('resourceViewer', "Resource Viewer"), containerIcon: Codicon.database.classNames, ctorDescriptor: new SyncDescriptor(ResourceViewerView), canToggleVisibility: false, canMoveView: false }], viewContainer);
+}
