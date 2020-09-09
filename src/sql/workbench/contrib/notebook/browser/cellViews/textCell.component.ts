ce10ba5f483671c4bb50af5f0dbb3613ec8599dd
chlafreniere <hichise@gmail.com>
2020-09-09 09:07:31 -0700

    welp dependency

--- src/sql/workbench/contrib/notebook/browser/cellViews/textCell.component.ts
+++ src/sql/workbench/contrib/notebook/browser/cellViews/textCell.component.ts
@@ -27,8 +27,8 @@ import { CodeComponent } from 'sql/workbench/contrib/notebook/browser/cellViews/
 import { NotebookRange, ICellEditorProvider } from 'sql/workbench/services/notebook/browser/notebookService';
 import { IColorTheme } from 'vs/platform/theme/common/themeService';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
-import TurndownService = require('turndown/lib/turndown.umd');
-import turndownPluginGfm = require('turndown-plugin-gfm/lib/turndown-plugin-gfm.cjs');
+import TurndownService = require('turndown');
+import turndownPluginGfm = require('turndown-plugin-gfm');
 
 
 export const TEXT_SELECTOR: string = 'text-cell-component';
