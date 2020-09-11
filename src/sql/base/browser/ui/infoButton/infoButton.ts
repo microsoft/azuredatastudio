cb7665be96004b6164079fd9ee2d8d833090867f
Hale Rankin <harankin@microsoft.com>
2020-09-11 13:36:57 -0700

    Removing ? and undefined from button properties as all are being passed from dashboard Widget

--- src/sql/base/browser/ui/infoButton/infoButton.ts
+++ src/sql/base/browser/ui/infoButton/infoButton.ts
@@ -11,14 +11,14 @@ import { URI } from 'vs/base/common/uri';
 type IUserFriendlyIcon = string | URI | { light: string | URI; dark: string | URI };
 
 export interface IInfoButtonOptions extends IButtonOptions {
-	description?: string,
-	buttonMaxHeight?: number,
-	iconClass?: string,
-	iconHeight?: number,
-	iconWidth?: number,
-	iconPath?: IUserFriendlyIcon,
-	textTitle?: string,
-	buttonMaxWidth?: number,
+	description: string,
+	buttonMaxHeight: number,
+	iconClass: string,
+	iconHeight: number,
+	iconWidth: number,
+	iconPath: IUserFriendlyIcon,
+	textTitle: string,
+	buttonMaxWidth: number,
 }
 
 export class InfoButton extends sqlButton {
@@ -30,14 +30,14 @@ export class InfoButton extends sqlButton {
 	private _pTitle: HTMLElement;
 	private _pDesc: HTMLElement;
 
-	private _description?: string;
-	private _buttonMaxHeight?: number;
-	private _iconClass?: string;
-	private _iconHeight?: number;
-	private _iconWidth?: number;
-	private _iconPath?: IUserFriendlyIcon;
-	private _textTitle?: string;
-	private _buttonMaxWidth?: number;
+	private _description: string;
+	private _buttonMaxHeight: number;
+	private _iconClass: string;
+	private _iconHeight: number;
+	private _iconWidth: number;
+	private _iconPath: IUserFriendlyIcon;
+	private _textTitle: string;
+	private _buttonMaxWidth: number;
 
 	constructor(container: HTMLElement, options?: IInfoButtonOptions) {
 		super(container, options);
@@ -102,71 +102,71 @@ export class InfoButton extends sqlButton {
 	public get textTitle(): string {
 		return this._textTitle;
 	}
-	public set textTitle(value: string | undefined) {
+	public set textTitle(value: string) {
 		this._textTitle = value;
 		this._pTitle.innerText = this.textTitle;
 	}
 
-	public get description(): string | undefined {
+	public get description(): string {
 		return this._description;
 	}
-	public set description(value: string | undefined) {
+	public set description(value: string) {
 		this._description = value;
 		this._pDesc.innerText = this.description;
 	}
 
-	public get buttonMaxHeight(): number | undefined {
+	public get buttonMaxHeight(): number {
 		return this._buttonMaxHeight;
 	}
-	public set buttonMaxHeight(value: number | undefined) {
+	public set buttonMaxHeight(value: number) {
 		this._buttonMaxHeight = value;
 		this._main.style.maxHeight = this._buttonMaxHeight.toString() + 'px';
 		this._iconContainer.style.height = (this._buttonMaxHeight - 20).toString() + 'px';
 		this._textContainer.style.height = (this._buttonMaxHeight - 20).toString() + 'px';
 	}
 
-	public get buttonMaxWidth(): number | undefined {
+	public get buttonMaxWidth(): number {
 		return this._buttonMaxWidth;
 	}
-	public set buttonMaxWidth(value: number | undefined) {
+	public set buttonMaxWidth(value: number) {
 		this._buttonMaxWidth = value;
 		this._main.style.width = this._buttonMaxWidth.toString() + 'px';
 		this._textContainer.style.width = (this._buttonMaxWidth - this._iconWidth).toString() + 'px';
 	}
 
-	public get iconHeight(): number | undefined {
+	public get iconHeight(): number {
 		return this._iconHeight;
 	}
-	public set iconHeight(value: number | undefined) {
+	public set iconHeight(value: number) {
 		this._iconHeight = value;
 		this._iconElement.style.height = this._iconHeight.toString() + 'px';
 	}
 
-	public get iconWidth(): number | undefined {
+	public get iconWidth(): number {
 		return this._iconWidth;
 	}
-	public set iconWidth(value: number | undefined) {
+	public set iconWidth(value: number) {
 		this._iconWidth = value;
 		this._iconContainer.style.width = this._iconWidth.toString() + 'px';
 		this._iconElement.style.width = this._iconWidth.toString() + 'px';
 	}
 
-	public get iconClass(): string | undefined {
+	public get iconClass(): string {
 		return this._iconClass;
 	}
-	public set iconClass(value: string | undefined) {
+	public set iconClass(value: string) {
 		this._iconClass = value;
 	}
 
-	public get iconPath(): IUserFriendlyIcon | undefined {
+	public get iconPath(): IUserFriendlyIcon {
 		return this._iconPath;
 	}
-	public set iconPath(value: IUserFriendlyIcon | undefined) {
+	public set iconPath(value: IUserFriendlyIcon) {
 		this._iconPath = value;
 		DOM.addClass(this._iconElement, this._iconPath.toString());
 	}
 
-	public set infoButtonOptions(options: IInfoButtonOptions | undefined) {
+	public set infoButtonOptions(options: IInfoButtonOptions) {
 		if (!options) {
 			return;
 		}
