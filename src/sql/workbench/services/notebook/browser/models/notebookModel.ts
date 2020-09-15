7c3e3a44902e41b3f47dd918f1269ebd740d21e1
chlafreniere <hichise@gmail.com>
2020-09-15 11:08:20 -0700

    Watch for on load event

--- src/sql/workbench/services/notebook/browser/models/notebookModel.ts
+++ src/sql/workbench/services/notebook/browser/models/notebookModel.ts
@@ -322,7 +322,7 @@ export class NotebookModel extends Disposable implements INotebookModel {
 		return !ids ? [] : ids;
 	}
 
-	public async loadContents(isTrusted: boolean = false): Promise<void> {
+	public async loadContents(isTrusted = false, forceLayoutChange = false): Promise<void> {
 		try {
 			this._trustedMode = isTrusted;
 
@@ -368,6 +368,9 @@ export class NotebookModel extends Disposable implements INotebookModel {
 			// Trust notebook by default if there are no code cells
 			if (this._cells.length === 0 || this._cells.every(cell => cell.cellType === CellTypes.Markdown)) {
 				this.trustedMode = true;
+			}
+			if (forceLayoutChange) {
+				this._layoutChanged.fire();
 			}
 		} catch (error) {
 			this._inErrorState = true;
