397d57d3d97fa9215255590836e9fbf81e868742
Brian Bergeron <brberger@microsoft.com>
2020-09-17 11:06:50 -0700

    get overview, conn strings, properties pages working

--- extensions/arc/src/models/postgresModel.ts
+++ extensions/arc/src/models/postgresModel.ts
@@ -23,20 +23,24 @@ export class PostgresModel extends ResourceModel {
 		this._azdataApi = <azdataExt.IExtension>vscode.extensions.getExtension(azdataExt.extension.name)?.exports;
 	}
 
+	/** Returns the configuration of Postgres */
 	public get config(): azdataExt.PostgresServerShowResult | undefined {
 		return this._config;
 	}
 
-	/** Returns the IP address and port of the server */
-	public get endpoint(): { ip: string, port: string } {
-		return this._config
-			? parseIpAndPort(this._config.status.externalEndpoint)
-			: { ip: '', port: '' };
+	/** Returns the major version of Postgres */
+	public get engineVersion(): string | undefined {
+		const kind = this._config?.kind;
+		return kind
+			? kind.substring(kind.lastIndexOf('-') + 1)
+			: undefined;
 	}
 
-	/** Returns the server's configuration e.g. '3 nodes, 1.5 vCores, 1GiB RAM, 2GiB storage per node' */
-	public get configuration(): string {
-		return ''; // TODO
+	/** Returns the IP address and port of the server */
+	public get endpoint(): { ip: string, port: string } | undefined {
+		return this._config?.status.externalEndpoint
+			? parseIpAndPort(this._config.status.externalEndpoint)
+			: undefined;
 	}
 
 	/** Refreshes the model */
