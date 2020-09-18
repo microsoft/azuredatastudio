a2f6cfc273c3183fa27011841422d182e33e28b4
Alan Ren <alanren@microsoft.com>
2020-09-17 17:04:28 -0700

    fix the legacy card style issue

--- src/sql/workbench/browser/modelComponents/card.component.ts
+++ src/sql/workbench/browser/modelComponents/card.component.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import 'vs/css!./media/card';
+import 'vs/css!./media/legacycard';
 
 import {
 	Component, Input, Inject, ChangeDetectorRef, forwardRef, ElementRef, OnDestroy, ViewChild
