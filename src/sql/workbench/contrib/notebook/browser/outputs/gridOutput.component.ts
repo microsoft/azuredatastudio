117ecfebd13a8e7ecd18f27a42bb4806889c5843
Chris LaFreniere <40371649+chlafreniere@users.noreply.github.com>
2020-09-18 18:40:21 -0700

    Notebooks: Fix Grids Not Rendering when Unsaved Notebook Reloaded (#12483)
    * Clear Output and fix output change
    
    * Fix tests after forced clear + append output

--- src/sql/workbench/contrib/notebook/browser/outputs/gridOutput.component.ts
+++ src/sql/workbench/contrib/notebook/browser/outputs/gridOutput.component.ts
@@ -392,6 +392,7 @@ export class DataResourceDataProvider implements IGridDataProvider {
 			let rows = await this._queryRunner.getQueryRows(i, numRows, this._batchId, this._id);
 			this.convertData(rows);
 		}
+		this.cellModel.sendChangeToNotebook(NotebookChangeType.CellOutputUpdated);
 	}
 
 	private convertData(rows: ResultSetSubset): void {
