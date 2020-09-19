ada550d2e817b18bbb9d0c6863e8a6776d5f75fb
Brian Bergeron <brian.e.bergeron@gmail.com>
2020-09-18 19:34:56 -0700

    Arc Postgres - Add Azure params to overview page, update notebook (#12482)
    * add azure params to pg overview page, update troubelshooting notebook, string changes
    
    * no default pg version for notebook
    
    Co-authored-by: Brian Bergeron <brberger@microsoft.com>

--- extensions/arc/src/ui/dashboards/postgres/postgresPropertiesPage.ts
+++ extensions/arc/src/ui/dashboards/postgres/postgresPropertiesPage.ts
@@ -92,11 +92,12 @@ export class PostgresPropertiesPage extends DashboardPage {
 
 	private getProperties(): KeyValue[] {
 		const endpoint = this._postgresModel.endpoint;
+		const status = this._postgresModel.config?.status;
 
 		return [
 			new InputKeyValue(this.modelView.modelBuilder, loc.coordinatorEndpoint, endpoint ? `postgresql://postgres@${endpoint.ip}:${endpoint.port}` : ''),
 			new InputKeyValue(this.modelView.modelBuilder, loc.postgresAdminUsername, 'postgres'),
-			new TextKeyValue(this.modelView.modelBuilder, loc.status, this._postgresModel.config?.status.state ?? loc.unknown),
+			new TextKeyValue(this.modelView.modelBuilder, loc.status, status ? `${status.state} (${status.readyPods} ${loc.podsReady})` : loc.unknown),
 			// TODO: Make this a LinkKeyValue that opens the controller dashboard
 			new TextKeyValue(this.modelView.modelBuilder, loc.dataController, this._controllerModel.controllerConfig?.metadata.namespace ?? ''),
 			new TextKeyValue(this.modelView.modelBuilder, loc.nodeConfiguration, this._postgresModel.scaleConfiguration ?? ''),
