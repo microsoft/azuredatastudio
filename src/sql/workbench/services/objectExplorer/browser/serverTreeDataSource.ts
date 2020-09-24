f90506bf6838fad18021c06d4c07c447cb49b526
chgagnon <chgagnon@microsoft.com>
2020-09-24 13:24:49 -0700

    test2

--- src/sql/workbench/services/objectExplorer/browser/serverTreeDataSource.ts
+++ src/sql/workbench/services/objectExplorer/browser/serverTreeDataSource.ts
@@ -30,7 +30,6 @@ export class ServerTreeDataSource implements IDataSource {
 	 * No more than one element may use a given identifier.
 	 */
 	public getId(tree: ITree, element: any): string {
-		//
 		return element.id;
 	}
 
