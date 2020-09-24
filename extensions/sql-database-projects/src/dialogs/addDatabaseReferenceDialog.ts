e6bcb42ce421753aa6991ce90e9c87b3ba1cdc07
Kim Santiago <kisantia@microsoft.com>
2020-09-23 17:08:01 -0700

    add type

--- extensions/sql-database-projects/src/dialogs/addDatabaseReferenceDialog.ts
+++ extensions/sql-database-projects/src/dialogs/addDatabaseReferenceDialog.ts
@@ -263,7 +263,8 @@ export class AddDatabaseReferenceDialog {
 		});
 
 		// get projects in workspace and filter to only sql projects
-		let projectFiles: vscode.Uri[] = (vscode.extensions.getExtension(dataworkspace.extension.name)?.exports.getProjectsInWorkspace()).filter((p: vscode.Uri) => path.parse(p.fsPath).ext === constants.sqlprojExtension);
+		let projectFiles: vscode.Uri[] = (<dataworkspace.IExtension>vscode.extensions.getExtension(dataworkspace.extension.name)?.exports).getProjectsInWorkspace()
+			.filter((p: vscode.Uri) => path.parse(p.fsPath).ext === constants.sqlprojExtension);
 
 		// filter out current project
 		projectFiles = projectFiles.filter(p => p.fsPath !== this.project.projectFilePath);
