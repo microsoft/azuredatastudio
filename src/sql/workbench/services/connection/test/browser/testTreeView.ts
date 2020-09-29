15c5306399aca8751a3c820872855ddea9c28a3a
Alex Ma <alma1@microsoft.com>
2020-09-29 10:10:37 -0700

    tslint disable added

--- src/sql/workbench/services/connection/test/browser/testTreeView.ts
+++ src/sql/workbench/services/connection/test/browser/testTreeView.ts
@@ -126,6 +126,7 @@ export class TreeView extends Disposable implements ITreeView {
 		this._register(this.themeService.onDidColorThemeChange(() => this.doRefresh([this.root]) /** soft refresh **/));
 		this._register(this.configurationService.onDidChangeConfiguration(e => {
 			if (e.affectsConfiguration('explorer.decorations')) {
+				// tslint:disable-next-line:no-floating-promises
 				this.doRefresh([this.root]); /** soft refresh **/
 			}
 		}));
@@ -189,6 +190,7 @@ export class TreeView extends Disposable implements ITreeView {
 				this._register(this._dataProvider.onDidChangeEmpty(() => this._onDidChangeWelcomeState.fire()));
 			}
 			this.updateMessage();
+			// tslint:disable-next-line:no-floating-promises
 			this.refresh();
 		} else {
 			this._dataProvider = undefined;
@@ -312,6 +314,7 @@ export class TreeView extends Disposable implements ITreeView {
 			}
 
 			if (this.isVisible && this.elementsToRefresh.length) {
+				// tslint:disable-next-line:no-floating-promises
 				this.doRefresh(this.elementsToRefresh);
 				this.elementsToRefresh = [];
 			}
