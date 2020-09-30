ae5c0e26f6a15707b58c8ef896653c4fe54a792e
Alex Ma <alma1@microsoft.com>
2020-09-30 08:39:54 -0700

    connection tests for connectionDialogWidget (#11160)
    * Added providerRegistered test
    
    * test for EditConnectionDialog
    
    * changed wording
    
    * added test for connectionInfo
    
    * utils.ts tests added
    
    * hasRegisteredServers test
    
    * commented out editconnection tests, addl. tests
    
    * added onConnectionChangedNotification test
    
    * added changeGroupId tests
    
    * Added connection  profile changes
    
    * added connectIfNotConnected test
    
    * added delete connection test
    
    * isRecent and disconnect editor tests
    
    * Add CodeQL Analysis workflow (#10195)
    
    * Add CodeQL Analysis workflow
    
    * Fix path
    
    * added registerIconProvider test
    
    * Fix for ensureDefaultLanguageFlavor test
    
    * added a few tests
    
    * utils prefix test updated
    
    * added utils tests
    
    * disconnect tests added
    
    * Added additional get connection info tests
    
    * added some more tests
    
    * minor additions to tests
    
    * again another commit
    
    * another change
    
    * connectionManagementService finalized
    
    * connectionDialogWidget test WIP
    
    * wip connectiondialogwidget test
    
    * added working connectionDialogWidget test
    
    * added more tests
    
    * current connectionDialogWidget tests
    
    * undid space
    
    * hanging promise addressed
    
    * added open test
    
    * finished connectionDialogWidget tests
    
    * Added showDialog test
    
    * mockConnectionDialogService added
    
    * added accessorConnectionDialogService
    
    * removed accessor service
    
    * added openDialogAndWait test
    
    * added fake error to test
    
    * added error tests
    
    * Added comments to test
    
    * more coverage
    
    * async to await change
    
    * registerCapabilities test added
    
    * connectionDialogService tests finished
    
    * undefined added
    
    * Added views for tests
    
    * tslint disable added
    
    * error catchers added
    
    * added empty connectioninfo
    
    Co-authored-by: Justin Hutchings <jhutchings1@users.noreply.github.com>

---
+++ src/sql/workbench/services/connection/test/browser/testConnectionDialogWidget.ts
@@ -0,0 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ConnectionDialogWidget } from 'sql/workbench/services/connection/browser/connectionDialogWidget';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { IConnectionManagementService } from 'sql/platform/connection/common/connectionManagement';
+import { IThemeService } from 'vs/platform/theme/common/themeService';
+import { ILayoutService } from 'vs/platform/layout/browser/layoutService';
+import { IAdsTelemetryService } from 'sql/platform/telemetry/common/telemetry';
+import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
+import { IContextMenuService, IContextViewService } from 'vs/platform/contextview/browser/contextView';
+import { IClipboardService } from 'vs/platform/clipboard/common/clipboardService';
+import { ILogService } from 'vs/platform/log/common/log';
+import { ITextResourcePropertiesService } from 'vs/editor/common/services/textResourceConfigurationService';
+import { IViewDescriptorService } from 'vs/workbench/common/views';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+
+export class TestConnectionDialogWidget extends ConnectionDialogWidget {
+	constructor(
+		providerDisplayNameOptions: string[],
+		selectedProviderType: string,
+		providerNameToDisplayNameMap: { [providerDisplayName: string]: string },
+		@IInstantiationService _instantiationService: IInstantiationService,
+		@IConnectionManagementService _connectionManagementService: IConnectionManagementService,
+		@IContextMenuService _contextMenuService: IContextMenuService,
+		@IContextViewService _contextViewService: IContextViewService,
+		@IViewDescriptorService viewDescriptorService: IViewDescriptorService,
+		@IThemeService themeService: IThemeService,
+		@ILayoutService layoutService: ILayoutService,
+		@IAdsTelemetryService telemetryService: IAdsTelemetryService,
+		@IContextKeyService contextKeyService: IContextKeyService,
+		@IClipboardService clipboardService: IClipboardService,
+		@ILogService logService: ILogService,
+		@ITextResourcePropertiesService textResourcePropertiesService: ITextResourcePropertiesService,
+		@IConfigurationService configurationService: IConfigurationService
+	) {
+		super(providerDisplayNameOptions, selectedProviderType, providerNameToDisplayNameMap, _instantiationService, _connectionManagementService, _contextMenuService, _contextViewService, viewDescriptorService, themeService, layoutService, telemetryService, contextKeyService, clipboardService, logService, textResourcePropertiesService, configurationService);
+	}
+	public renderBody(container: HTMLElement) {
+		super.renderBody(container);
+	}
+}
