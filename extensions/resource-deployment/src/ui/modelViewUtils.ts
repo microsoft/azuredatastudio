5184f893a325e4efcea6b272efd6b8f85a1602c8
Arvind Ranasaria <ranasaria@outlook.com>
2020-10-01 09:08:04 -0700

    use selected subscriptions (#12691)
    * working version
    
    * pr feedback

--- extensions/resource-deployment/src/ui/modelViewUtils.ts
+++ extensions/resource-deployment/src/ui/modelViewUtils.ts
@@ -1007,6 +1007,7 @@ function createAzureSubscriptionDropdown(
 	subscriptionValueToSubscriptionMap: Map<string, azureResource.AzureResourceSubscription>): azdata.DropDownComponent {
 	const label = createLabel(context.view, {
 		text: loc.subscription,
+		description: loc.subscriptionDescription,
 		required: context.fieldInfo.required,
 		width: context.fieldInfo.labelWidth,
 		cssStyles: context.fieldInfo.labelCSSStyles
@@ -1066,7 +1067,7 @@ async function handleSelectedAccountChanged(
 	}
 
 	try {
-		const response = await apiService.azurecoreApi.getSubscriptions(selectedAccount, true);
+		const response = await apiService.azurecoreApi.getSubscriptions(selectedAccount, true, false);
 		if (!response) {
 			return;
 		}
