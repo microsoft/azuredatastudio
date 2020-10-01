21f0eb2ea663f47309ef089bc845680e9812ef19
Karl Burtram <karlb@microsoft.com>
2020-10-01 11:11:24 -0700

    Turn off server publish

--- build/azure-pipelines/win32/createDrop.ps1
+++ build/azure-pipelines/win32/createDrop.ps1
@@ -14,7 +14,7 @@ $ServerZip = "$ServerZipLocation\azuredatastudio-server-win32-$Arch.zip"
 # Create server archive
 New-Item $ServerZipLocation -ItemType Directory # this will throw even when success for we don't want to exec this
 $global:LASTEXITCODE = 0
-exec { Rename-Item -Path $LegacyServer -NewName $ServerName } "Rename Item"
-exec { .\node_modules\7zip\7zip-lite\7z.exe a -tzip $ServerZip $Server -r } "Zip Server"
+# exec { Rename-Item -Path $LegacyServer -NewName $ServerName } "Rename Item"
+# exec { .\node_modules\7zip\7zip-lite\7z.exe a -tzip $ServerZip $Server -r } "Zip Server"
 
 exec { node build/azure-pipelines/common/copyArtifacts.js } "Copy Artifacts"
