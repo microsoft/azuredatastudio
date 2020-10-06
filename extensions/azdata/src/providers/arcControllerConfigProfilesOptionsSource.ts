17b168ecadf965cf5dcdc76dfb2a9ac24d502adf
Arvind Ranasaria <ranasaria@outlook.com>
2020-10-05 19:29:40 -0700

    allow registering options source providers to resource-deployment (#12712)
    * first draft
    
    * compile fixes
    
    * uncomment code
    
    * waitForAzdataToolDisovery added to azdata api
    
    * missed change in last commit
    
    * remove switchReturn
    
    * contributeOptionsSource renamed
    
    * remove switchReturn reference
    
    * create optionSourceService
    
    * azdataTool usage more reliable
    
    * package.json fixes and cleanup
    
    * cleanup
    
    * revert 4831a6e6b8b08684488b2c9e18092fa252e3057f
    
    * pr feedback
    
    * pr feedback
    
    * pr feedback
    
    * cleanup
    
    * cleanup
    
    * fix eulaAccepted check
    
    * fix whitespade in doc comments.

---
+++ extensions/azdata/src/providers/arcControllerConfigProfilesOptionsSource.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as rd from 'resource-deployment';
+import * as azdataExt from 'azdata-ext';
+
+/**
+ * Class that provides options sources for an Arc Data Controller
+ */
+export class ArcControllerConfigProfilesOptionsSource implements rd.IOptionsSourceProvider {
+	readonly optionsSourceId = 'arc.controller.config.profiles';
+	constructor(private _azdataExtApi: azdataExt.IExtension) { }
+	async getOptions(): Promise<string[]> {
+		if (!this._azdataExtApi.isEulaAccepted()) { // if eula has not yet be accepted then give user a chance to accept it
+			await this._azdataExtApi.promptForEula();
+		}
+		return (await this._azdataExtApi.azdata.arc.dc.config.list()).result;
+	}
+}
--- extensions/resource-deployment/src/interfaces.ts
+++ extensions/resource-deployment/src/interfaces.ts
@@ -4,8 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as azdata from 'azdata';
+import { IOptionsSourceProvider } from 'resource-deployment';
 import * as vscode from 'vscode';
-import { OptionsSourceType } from './helpers/optionSources';
 
 export const NoteBookEnvironmentVariablePrefix = 'AZDATA_NB_VAR_';
 
@@ -231,11 +231,9 @@ export type ComponentCSSStyles = {
 };
 
 export interface IOptionsSource {
-	readonly type: OptionsSourceType;
-	readonly variableNames: { [index: string]: string; };
-	getOptions(): Promise<string[] | azdata.CategoryValue[]>;
-	getVariableValue(variableName: string, input: string): Promise<string>;
-	getIsPassword(variableName: string): boolean;
+	provider?: IOptionsSourceProvider
+	readonly variableNames?: { [index: string]: string; };
+	readonly providerId: string;
 }
 
 
