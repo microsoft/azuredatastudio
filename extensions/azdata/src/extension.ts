867faae14f80c761bc01255469859b152d8a0b35
Charles Gagnon <chgagnon@microsoft.com>
2020-10-05 18:52:16 -0700

    [Port] Improved behavior for accepting EULA. (#12453) (#12749)
    * Improved behavior for accepting EULA. (#12453)
    
    * working version of overloading "select" button
    
    * promptForEula to use showErrorMessage
    
    * make parameter optional in promptForEula
    
    * remove test code
    
    * PR feedback
    
    * eula to EULA
    
    * minor fix
    
    * Fix compile error
    
    Co-authored-by: Arvind Ranasaria <ranasaria@outlook.com>

--- extensions/azdata/src/extension.ts
+++ extensions/azdata/src/extension.ts
@@ -59,6 +59,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<azdata
 
 	return {
 		isEulaAccepted: () => !!context.globalState.get<boolean>(constants.eulaAccepted),
+		promptForEula: (onError: boolean = true): Promise<boolean> => promptForEula(context.globalState, true /* userRequested */, onError),
 		azdata: {
 			arc: {
 				dc: {
