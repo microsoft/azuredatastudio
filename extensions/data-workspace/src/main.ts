cc12edd14a990e0418cb4a70bd7b7ac42fb5dd6e
Alan Ren <alanren@microsoft.com>
2020-10-08 11:48:22 -0700

    only show the view when proj provider is available

--- extensions/data-workspace/src/main.ts
+++ extensions/data-workspace/src/main.ts
@@ -10,12 +10,18 @@ import { WorkspaceTreeItem, IExtension } from 'dataworkspace';
 import { DataWorkspaceExtension } from './common/dataWorkspaceExtension';
 import { NewProjectDialog } from './dialogs/newProjectDialog';
 import { OpenProjectDialog } from './dialogs/openProjectDialog';
+import { IWorkspaceService } from './common/interfaces';
+
+let treeDataProviderDisposable: vscode.Disposable | undefined = undefined;
 
 export function activate(context: vscode.ExtensionContext): Promise<IExtension> {
 	const workspaceService = new WorkspaceService();
 	const workspaceTreeDataProvider = new WorkspaceTreeDataProvider(workspaceService);
 	const dataWorkspaceExtension = new DataWorkspaceExtension(workspaceService);
-	context.subscriptions.push(vscode.window.registerTreeDataProvider('dataworkspace.views.main', workspaceTreeDataProvider));
+	context.subscriptions.push(vscode.extensions.onDidChange(() => {
+		setTreeDataProvider(context, workspaceService, workspaceTreeDataProvider);
+	}));
+	setTreeDataProvider(context, workspaceService, workspaceTreeDataProvider);
 	context.subscriptions.push(vscode.commands.registerCommand('projects.newProject', async () => {
 		if (vscode.workspace.workspaceFile) {
 			const dialog = new NewProjectDialog(workspaceService);
@@ -39,6 +45,22 @@ export function activate(context: vscode.ExtensionContext): Promise<IExtension> 
 		await workspaceService.removeProject(vscode.Uri.file(treeItem.element.project.projectFilePath));
 	}));
 	return Promise.resolve(dataWorkspaceExtension);
+}
+
+function setTreeDataProvider(context: vscode.ExtensionContext, workspaceService: IWorkspaceService, treeDataProvider: WorkspaceTreeDataProvider): void {
+	const isProjectProviderAvailable = workspaceService.isProjectProviderAvailable;
+	vscode.commands.executeCommand('setContext', 'isProjectProviderAvailable', isProjectProviderAvailable);
+	if (isProjectProviderAvailable) {
+		if (treeDataProviderDisposable) {
+			treeDataProviderDisposable.dispose();
+			const idx = context.subscriptions.findIndex(x => x === treeDataProviderDisposable);
+			if (idx !== -1) {
+				context.subscriptions.splice(idx, 1);
+			}
+		}
+		treeDataProviderDisposable = vscode.window.registerTreeDataProvider('dataworkspace.views.main', treeDataProvider);
+		context.subscriptions.push(treeDataProviderDisposable);
+	}
 }
 
 export function deactivate(): void {
