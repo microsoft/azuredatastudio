c157f5599c437ab6e69bc5356712b2c569d0a0ac
Arvind Ranasaria <ranasaria@outlook.com>
2020-10-12 17:08:52 -0700

    throw when onOk, onGenerateScript errors (#12838)
    * throw when onOk, onGenerateScript errors
    
    * pr feedback
    
    * remove try catch in onOk && onGenerateScript

--- extensions/resource-deployment/src/ui/modelViewUtils.ts
+++ extensions/resource-deployment/src/ui/modelViewUtils.ts
@@ -446,7 +446,7 @@ async function processOptionsTypeField(context: FieldContext): Promise<void> {
 				description: '',
 				level: azdata.window.MessageLevel.Error
 			};
-			context.fieldInfo.options.values = [];
+			throw e;
 		}
 		context.fieldInfo.subFields = context.fieldInfo.subFields || [];
 	}
@@ -483,7 +483,7 @@ async function configureOptionsSourceSubfields(context: FieldContext, optionsSou
 					description: '',
 					level: azdata.window.MessageLevel.Error
 				};
-				return '';
+				throw e;
 			}
 		},
 		isPassword: await optionsSourceProvider.getIsPassword!(variableKey)
