0fb50214c810045f8a2f076f3aa6b0d15022a200
chgagnon <chgagnon@microsoft.com>
2020-10-29 10:49:56 -0700

    Comments

--- src/sql/base/browser/ui/table/plugins/buttonColumn.plugin.ts
+++ src/sql/base/browser/ui/table/plugins/buttonColumn.plugin.ts
@@ -12,9 +12,21 @@ export interface ButtonColumnDefinition<T extends Slick.SlickData> extends TextW
 }
 
 export interface ButtonColumnOptions {
+	/**
+	 * The CSS class of the icon (either a common icon or one added by the user of this column) to display in the button
+	 */
 	iconCssClass?: string;
+	/**
+	 * The aria-label title of the button
+	 */
 	title?: string;
+	/**
+	 * The unique ID used by SlickGrid
+	 */
 	id?: string;
+	/**
+	 * Whether the column is sortable or not
+	 */
 	sortable?: boolean;
 }
 
