b9297c6e662610cbf929dfe00aa90cab029e5b9d
Arvind Ranasaria <ranasaria@outlook.com>
2020-10-29 10:55:43 -0700

    capitalize parameter names

--- extensions/resource-deployment/src/test/services/platformService.test.ts
+++ extensions/resource-deployment/src/test/services/platformService.test.ts
@@ -29,15 +29,15 @@ class TestChildProcessPromise<T> implements cp.ChildProcessPromise {
 	}
 	resolve!: (value?: T | PromiseLike<T>) => void;
 	reject!: (reason?: any) => void;
-	then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null): Promise<TResult1 | TResult2> {
-		return this._promise.then(onfulfilled, onrejected);
+	then<TResult1 = T, TResult2 = never>(onFulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null, onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null): Promise<TResult1 | TResult2> {
+		return this._promise.then(onFulfilled, onRejected);
 	}
-	catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | null): Promise<T | TResult> {
-		return this._promise.catch(onrejected);
+	catch<TResult = never>(onRejected?: ((reason: any) => TResult | PromiseLike<TResult>) | null): Promise<T | TResult> {
+		return this._promise.catch(onRejected);
 	}
 	[Symbol.toStringTag]: string;
-	finally(onfinally?: (() => void) | null): Promise<T> {
-		return this._promise.finally(onfinally);
+	finally(onFinally?: (() => void) | null): Promise<T> {
+		return this._promise.finally(onFinally);
 	}
 	stdin: any = this._event;
 	stdout: Readable | null = <Readable>this._event;
