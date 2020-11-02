1c2ebf27a45703ff89129d618a961a55557ff00d
barbaravaldez <bavaldez@microsoft.com>
2020-11-02 13:31:01 -0800

    remove unused parameter

--- extensions/notebook/src/book/bookTreeView.ts
+++ extensions/notebook/src/book/bookTreeView.ts
@@ -140,7 +140,7 @@ export class BookTreeViewProvider implements vscode.TreeDataProvider<BookTreeIte
 		await this.bookTocManager.createBook(bookPath, contentPath);
 	}
 
-	async editBook(book: BookTreeItem, section: BookTreeItem, sectionParent: BookTreeItem): Promise<void> {
+	async editBook(book: BookTreeItem, section: BookTreeItem): Promise<void> {
 		await this.bookTocManager.updateBook(section, book);
 	}
 
