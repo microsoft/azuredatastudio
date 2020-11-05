658a21933e68a04b2d0fb121ece1709667c29e7d
Benjin Dubishar <benjin.dubishar@gmail.com>
2020-11-05 10:05:26 -0800

    Adding comment to and genercizing column names in external streaming job script

--- extensions/sql-database-projects/resources/templates/newTsqlExternalStreamingJobTemplate.sql
+++ extensions/sql-database-projects/resources/templates/newTsqlExternalStreamingJobTemplate.sql
@@ -1,7 +1,10 @@
+-- External Streaming Jobs have dependencies on External Streams.
+-- You will need to separately create External Streams in order to
+-- successfully deploy this script.  For more information, see:
+-- https://docs.microsoft.com/en-us/azure/azure-sql-edge/create-stream-analytics-job
+
 EXEC sys.sp_create_streaming_job @NAME = '@@OBJECT_NAME@@', @STATEMENT = 'INSERT INTO SqlOutputStream SELECT
     timeCreated,
-    machine.temperature as machine_temperature,
-    machine.pressure as machine_pressure,
-    ambient.temperature as ambient_temperature,
-    ambient.humidity as ambient_humidity
+    streamColumn1 as column1,
+    streamColumn2 as column2
 FROM EdgeHubInputStream'
