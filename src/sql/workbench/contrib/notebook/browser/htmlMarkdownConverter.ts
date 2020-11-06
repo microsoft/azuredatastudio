70dfca715833567af34120ef5103ee2349a7a493
Chris LaFreniere <40371649+chlafreniere@users.noreply.github.com>
2020-11-05 18:21:33 -0800

    WYSIWYG fix list nesting (#13251)
    * Improvements to nested lists
    
    * OL tests and PR feedback

--- src/sql/workbench/contrib/notebook/browser/htmlMarkdownConverter.ts
+++ src/sql/workbench/contrib/notebook/browser/htmlMarkdownConverter.ts
@@ -111,34 +111,32 @@ export class HTMLMarkdownConverter {
 				return `[${node.innerText}](${node.href})`;
 			}
 		});
-		this.turndownService.addRule('p', {
-			filter: 'p',
-			replacement: function (content, node) {
-				let text = node.textContent;
-				let mapTags = { '<': '\\<', '>': '\\>' };
-
-				let escapedText = text.replace(/<|>/gi, function (matched) {
-					return mapTags[matched];
-				});
-
-				return '\n\n' + escapedText + '\n\n';
+		this.turndownService.addRule('listItem', {
+			filter: 'li',
+			replacement: function (content, node, options) {
+				content = content
+					.replace(/^\n+/, '') // remove leading newlines
+					.replace(/\n+$/, '\n') // replace trailing newlines with just a single one
+					.replace(/\n/gm, '\n    '); // indent
+				let prefix = options.bulletListMarker + ' ';
+				let parent = node.parentNode;
+				let nestedCount = 0;
+				if (parent.nodeName === 'OL') {
+					let start = parent.getAttribute('start');
+					let index = Array.prototype.indexOf.call(parent.children, node);
+					prefix = (start ? Number(start) + index : index + 1) + '. ';
+				} else if (parent.nodeName === 'UL') {
+					while (parent?.nodeName === 'UL') {
+						nestedCount++;
+						parent = parent?.parentNode;
+					}
+					prefix = ('    '.repeat(nestedCount - 1)) + options.bulletListMarker + ' ';
+				}
+				return (
+					prefix + content + (node.nextSibling && !/\n$/.test(content) ? '\n' : '')
+				);
 			}
 		});
-
-		// this.turndownService.addRule('escapeAngleBrackets', {
-		// 	filter: ['span', 'p', 'h1', 'h2', 'h3', 'u', 'mark'],
-		// 	replacement: function (content, node) {
-
-
-		// 		let text = node.textContent;
-		// 		let mapTags = { '<': '\\<', '>': '\\>' };
-
-		// 		let escapedText = text.replace(/<|>/gi, function (matched) {
-		// 			return mapTags[matched];
-		// 		});
-
-		// 	}
-		// });
 	}
 }
 
