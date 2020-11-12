126e780c5a6cb72362fcf8d221cd2e491690ca83
Kim Santiago <kisantia@microsoft.com>
2020-11-12 15:22:38 -0800

    change afterClean to beforeBuild for removing the generated assests.json

--- extensions/sql-database-projects/src/models/project.ts
+++ extensions/sql-database-projects/src/models/project.ts
@@ -190,27 +190,11 @@ export class Project {
 		this.projFileXmlDoc = undefined;
 	}
 
-	public async updateProjectForRoundTrip(): Promise<void> {
-		if (this.importedTargets.includes(constants.NetCoreTargets) && !this.containsSSDTOnlySystemDatabaseReferences()) {
-			return;
-		}
-
-		if (!this.importedTargets.includes(constants.NetCoreTargets)) {
-			const result = await window.showWarningMessage(constants.updateProjectForRoundTrip, constants.yesString, constants.noString);
-			if (result === constants.yesString) {
-				await fs.copyFile(this.projectFilePath, this.projectFilePath + '_backup');
-				await this.updateImportToSupportRoundTrip();
-				await this.updatePackageReferenceInProjFile();
-				await this.updateAfterCleanTargetInProjFile();
-				await this.updateSystemDatabaseReferencesInProjFile();
-			}
-		} else if (this.containsSSDTOnlySystemDatabaseReferences()) {
-			const result = await window.showWarningMessage(constants.updateProjectDatabaseReferencesForRoundTrip, constants.yesString, constants.noString);
-			if (result === constants.yesString) {
-				await fs.copyFile(this.projectFilePath, this.projectFilePath + '_backup');
-				await this.updateSystemDatabaseReferencesInProjFile();
-			}
-		}
+	public async updateProjectForRoundTrip() {
+		await fs.copyFile(this.projectFilePath, this.projectFilePath + '_backup');
+		await this.updateImportToSupportRoundTrip();
+		await this.updatePackageReferenceInProjFile();
+		await this.updateBeforeBuildTargetInProjFile();
 	}
 
 	private async updateImportToSupportRoundTrip(): Promise<void> {
@@ -232,21 +216,21 @@ export class Project {
 		await this.updateImportedTargetsToProjFile(constants.NetCoreCondition, constants.NetCoreTargets, undefined);
 	}
 
-	private async updateAfterCleanTargetInProjFile(): Promise<void> {
+	private async updateBeforeBuildTargetInProjFile(): Promise<void> {
 		// Search if clean target already present, update it
 		for (let i = 0; i < this.projFileXmlDoc.documentElement.getElementsByTagName(constants.Target).length; i++) {
-			const afterCleanNode = this.projFileXmlDoc.documentElement.getElementsByTagName(constants.Target)[i];
-			const name = afterCleanNode.getAttribute(constants.Name);
-			if (name === constants.AfterCleanTarget) {
-				return await this.createCleanFileNode(afterCleanNode);
+			const beforeBuildNode = this.projFileXmlDoc.documentElement.getElementsByTagName(constants.Target)[i];
+			const name = beforeBuildNode.getAttribute(constants.Name);
+			if (name === constants.BeforeBuildTarget) {
+				return await this.createCleanFileNode(beforeBuildNode);
 			}
 		}
 
 		// If clean target not found, create new
-		const afterCleanNode = this.projFileXmlDoc.createElement(constants.Target);
-		afterCleanNode.setAttribute(constants.Name, constants.AfterCleanTarget);
-		this.projFileXmlDoc.documentElement.appendChild(afterCleanNode);
-		await this.createCleanFileNode(afterCleanNode);
+		const beforeBuildNode = this.projFileXmlDoc.createElement(constants.Target);
+		beforeBuildNode.setAttribute(constants.Name, constants.BeforeBuildTarget);
+		this.projFileXmlDoc.documentElement.appendChild(beforeBuildNode);
+		await this.createCleanFileNode(beforeBuildNode);
 	}
 
 	private async createCleanFileNode(parentNode: any): Promise<void> {
