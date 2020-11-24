cd321c00e8dfcf50adf2121d7e5efe9be3f2732b
Naia Scott <nasc@microsoft.com>
2020-11-24 10:13:57 -0800

    Miaa models provides dialog title

--- extensions/arc/src/models/miaaModel.ts
+++ extensions/arc/src/models/miaaModel.ts
@@ -189,7 +189,7 @@ export class MiaaModel extends ResourceModel {
 						if (!result.connected) {
 							vscode.window.showErrorMessage(loc.connectToMSSqlFailed(connectionProfile.serverName, result.errorMessage));
 							const connectToSqlDialog = new ConnectToMiaaSqlDialog(this._controllerModel, this);
-							connectToSqlDialog.showDialog(this.info.name, connectionProfile);
+							connectToSqlDialog.showDialog(loc.connectToMSSql(this.info.name), connectionProfile);
 							connectionProfile = await connectToSqlDialog.waitForClose();
 						}
 					}
@@ -203,7 +203,7 @@ export class MiaaModel extends ResourceModel {
 		if (!connectionProfile?.userName || !connectionProfile?.password) {
 			// Need to prompt user for password since we don't have one stored
 			const connectToSqlDialog = new ConnectToMiaaSqlDialog(this._controllerModel, this);
-			connectToSqlDialog.showDialog(this.info.name, connectionProfile);
+			connectToSqlDialog.showDialog(loc.connectToMSSql(this.info.name), connectionProfile);
 			connectionProfile = await connectToSqlDialog.waitForClose();
 		}
 
--- extensions/arc/src/ui/dialogs/connectSqlDialog.ts
+++ extensions/arc/src/ui/dialogs/connectSqlDialog.ts
@@ -27,8 +27,8 @@ export abstract class ConnectToSqlDialog extends InitializingComponent {
 		super();
 	}
 
-	public showDialog(instanceName: string, connectionProfile?: azdata.IConnectionProfile): azdata.window.Dialog {
-		const dialog = azdata.window.createModelViewDialog(loc.connectToMSSql(instanceName));
+	public showDialog(dialogTitle: string, connectionProfile?: azdata.IConnectionProfile): azdata.window.Dialog {
+		const dialog = azdata.window.createModelViewDialog(dialogTitle);
 		dialog.cancelButton.onClick(() => this.handleCancel());
 		dialog.registerContent(async view => {
 			this.modelBuilder = view.modelBuilder;
