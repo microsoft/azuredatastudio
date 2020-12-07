ff3258c59133af2bdd9150792f63bbd717832503
dagrajed <dhgrajeda@gmail.com>
2020-12-07 11:09:31 -0800

    Notebook view model testing

---
+++ src/sql/workbench/contrib/notebook/test/browser/notebookViewModel.test.ts
@@ -0,0 +1,134 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as TypeMoq from 'typemoq';
+import { nb } from 'azdata';
+import * as assert from 'assert';
+
+import { INotificationService } from 'vs/platform/notification/common/notification';
+import { TestNotificationService } from 'vs/platform/notification/test/common/testNotificationService';
+import { URI } from 'vs/base/common/uri';
+
+import { NotebookManagerStub } from 'sql/workbench/contrib/notebook/test/stubs';
+import { NotebookModel } from 'sql/workbench/services/notebook/browser/models/notebookModel';
+import { ModelFactory } from 'sql/workbench/services/notebook/browser/models/modelFactory';
+import { IClientSession, INotebookModelOptions, IClientSessionOptions } from 'sql/workbench/services/notebook/browser/models/modelInterfaces';
+import { ClientSession } from 'sql/workbench/services/notebook/browser/models/clientSession';
+import { Memento } from 'vs/workbench/common/memento';
+import { TestCapabilitiesService } from 'sql/platform/capabilities/test/common/testCapabilitiesService';
+import { ICapabilitiesService } from 'sql/platform/capabilities/common/capabilitiesService';
+import { TestStorageService } from 'vs/workbench/test/common/workbenchTestServices';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { InstantiationService } from 'vs/platform/instantiation/common/instantiationService';
+import { ServiceCollection } from 'vs/platform/instantiation/common/serviceCollection';
+import { NullLogService } from 'vs/platform/log/common/log';
+import { TestConnectionManagementService } from 'sql/platform/connection/test/common/testConnectionManagementService';
+import { NotebookEditorContentManager } from 'sql/workbench/contrib/notebook/browser/models/notebookInput';
+import { SessionManager } from 'sql/workbench/contrib/notebook/test/emptySessionClasses';
+import { NullAdsTelemetryService } from 'sql/platform/telemetry/common/adsTelemetryService';
+import { NotebookViewsExtension } from 'sql/workbench/services/notebook/browser/models/notebookViewsExtension';
+import { Deferred } from 'sql/base/common/promise';
+import { CellTypes } from 'sql/workbench/services/notebook/common/contracts';
+
+let expectedNotebookContent: nb.INotebookContents = {
+	cells: [{
+		cell_type: CellTypes.Code,
+		source: ['insert into t1 values (c1, c2)'],
+		metadata: { language: 'python' },
+		execution_count: 1
+	}, {
+		cell_type: CellTypes.Markdown,
+		source: ['I am *markdown*'],
+		metadata: { language: 'python' },
+		execution_count: 1
+	}],
+	metadata: {
+		kernelspec: {
+			name: 'mssql',
+			language: 'sql'
+		}
+	},
+	nbformat: 4,
+	nbformat_minor: 5
+};
+
+let defaultUri = URI.file('/some/path.ipynb');
+
+let mockClientSession: TypeMoq.Mock<IClientSession>;
+let clientSessionOptions: IClientSessionOptions;
+let sessionReady: Deferred<void>;
+let mockModelFactory: TypeMoq.Mock<ModelFactory>;
+let notificationService: TypeMoq.Mock<INotificationService>;
+let capabilitiesService: TypeMoq.Mock<ICapabilitiesService>;
+let instantiationService: IInstantiationService;
+
+suite('notebook view model', function (): void {
+	let notebookManagers = [new NotebookManagerStub()];
+	let mockSessionManager: TypeMoq.Mock<nb.SessionManager>;
+	let memento: TypeMoq.Mock<Memento>;
+	let queryConnectionService: TypeMoq.Mock<TestConnectionManagementService>;
+	let defaultModelOptions: INotebookModelOptions;
+	const logService = new NullLogService();
+	setup(() => {
+		mockSessionManager = TypeMoq.Mock.ofType(SessionManager);
+		notebookManagers[0].sessionManager = mockSessionManager.object;
+		sessionReady = new Deferred<void>();
+		notificationService = TypeMoq.Mock.ofType(TestNotificationService, TypeMoq.MockBehavior.Loose);
+		capabilitiesService = TypeMoq.Mock.ofType(TestCapabilitiesService);
+		memento = TypeMoq.Mock.ofType(Memento, TypeMoq.MockBehavior.Loose, '');
+		memento.setup(x => x.getMemento(TypeMoq.It.isAny())).returns(() => void 0);
+		queryConnectionService = TypeMoq.Mock.ofType(TestConnectionManagementService, TypeMoq.MockBehavior.Loose, memento.object, undefined, new TestStorageService());
+		queryConnectionService.callBase = true;
+		let serviceCollection = new ServiceCollection();
+		instantiationService = new InstantiationService(serviceCollection, true);
+		defaultModelOptions = {
+			notebookUri: defaultUri,
+			factory: new ModelFactory(instantiationService),
+			notebookManagers,
+			contentManager: undefined,
+			notificationService: notificationService.object,
+			connectionService: queryConnectionService.object,
+			providerId: 'SQL',
+			cellMagicMapper: undefined,
+			defaultKernel: undefined,
+			layoutChanged: undefined,
+			capabilitiesService: capabilitiesService.object
+		};
+		clientSessionOptions = {
+			notebookManager: defaultModelOptions.notebookManagers[0],
+			notebookUri: defaultModelOptions.notebookUri,
+			notificationService: notificationService.object,
+			kernelSpec: defaultModelOptions.defaultKernel
+		};
+		mockClientSession = TypeMoq.Mock.ofType(ClientSession, undefined, clientSessionOptions);
+		mockClientSession.setup(c => c.initialize()).returns(() => {
+			return Promise.resolve();
+		});
+		mockClientSession.setup(c => c.ready).returns(() => sessionReady.promise);
+		mockModelFactory = TypeMoq.Mock.ofType(ModelFactory);
+		mockModelFactory.callBase = true;
+		mockModelFactory.setup(f => f.createClientSession(TypeMoq.It.isAny())).returns(() => {
+			return mockClientSession.object;
+		});
+	});
+
+	test('Should create view properly', async function (): Promise<void> {
+		// Given a notebook with 2 cells
+		let mockContentManager = TypeMoq.Mock.ofType(NotebookEditorContentManager);
+		mockContentManager.setup(c => c.loadContent()).returns(() => Promise.resolve(expectedNotebookContent));
+
+		// When I initalize the model
+		let model = new NotebookModel(defaultModelOptions, undefined, logService, undefined, new NullAdsTelemetryService());
+		await model.loadContents();
+
+		const extension = new NotebookViewsExtension(model);
+
+		const newView = extension.createNewView();
+
+		assert.notDeepEqual(model.toJSON(), undefined, 'Model should not be undefined');
+		assert.notDeepEqual(newView, undefined, 'Notebook should not be null');
+		//assert.deepEqual(model.cells[0].toJSON().)
+	});
+});
