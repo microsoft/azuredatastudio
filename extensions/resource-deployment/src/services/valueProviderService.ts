3013105c5798013de82225c12c98d6bbcddce54a
Charles Gagnon <chgagnon@microsoft.com>
2020-12-07 10:27:37 -0800

    Have resource deployment providers return disposables (#13690)
    * Add dependent field provider to resource deployment
    
    * Change name to value provider service
    
    * Add error handling
    
    * providerId -> id
    
    * Set dropdown value correctly
    
    * missed id
    
    * back to providerId
    
    * fix updating missed id
    
    * Make resource deployment providers disposable

--- extensions/resource-deployment/src/services/valueProviderService.ts
+++ extensions/resource-deployment/src/services/valueProviderService.ts
@@ -3,16 +3,24 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
 import * as rd from 'resource-deployment';
 import * as loc from '../localizedConstants';
 
 class ValueProviderService {
 	private _valueProviderStore = new Map<string, rd.IValueProvider>();
-	registerValueProvider(provider: rd.IValueProvider): void {
+	registerValueProvider(provider: rd.IValueProvider): vscode.Disposable {
 		if (this._valueProviderStore.has(provider.id)) {
 			throw new Error(loc.valueProviderAlreadyDefined(provider.id));
 		}
 		this._valueProviderStore.set(provider.id, provider);
+		return {
+			dispose: () => this.unregisterValueProvider(provider.id)
+		};
+	}
+
+	private unregisterValueProvider(providerId: string): void {
+		this._valueProviderStore.delete(providerId);
 	}
 
 	getValueProvider(providerId: string): rd.IValueProvider {
