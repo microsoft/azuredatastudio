cbfffc5ac2cc1d05ba47abd472b1db3490d20a93
Rajesh Kamath <rajkashop@hotmail.com>
2020-12-14 13:09:17 -0800

    Moved status to bottom and actionbar to right of table

--- src/sql/workbench/services/notebook/browser/models/cell.ts
+++ src/sql/workbench/services/notebook/browser/models/cell.ts
@@ -703,7 +703,8 @@ export class CellModel extends Disposable implements ICellModel {
 				if (this._outputs.length > 0) {
 					for (let i = 0; i < this._outputs.length; i++) {
 						if (this._outputs[i].output_type === 'execute_result') {
-							this._outputs.splice(i, 0, this.rewriteOutputUrls(output));
+							// this._outputs.splice(i, 0, this.rewriteOutputUrls(output));
+							this._outputs.push(this.rewriteOutputUrls(output));
 							this.fireOutputsChanged();
 							added = true;
 							break;
