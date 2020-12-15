7e16bd1bea85bcb5352f31df0b144af853a6432b
Rajesh Kamath <rajkashop@hotmail.com>
2020-12-15 08:10:53 -0800

    Revert "Addressed CR"
    This reverts commit 6318ca46bf848a3d3cb9484140332ffc94d278f9.

--- src/sql/workbench/services/notebook/browser/models/cell.ts
+++ src/sql/workbench/services/notebook/browser/models/cell.ts
@@ -699,6 +699,17 @@ export class CellModel extends Disposable implements ICellModel {
 			case 'display_data':
 				output = msg.content as nb.ICellOutput;
 				output.output_type = msgType;
+				// Display message outputs before grid outputs
+				if (this._outputs.length > 0) {
+					for (let i = 0; i < this._outputs.length; i++) {
+						if (this._outputs[i].output_type === 'execute_result') {
+							this._outputs.splice(i, 0, this.rewriteOutputUrls(output));
+							this.fireOutputsChanged();
+							added = true;
+							break;
+						}
+					}
+				}
 				break;
 			case 'stream':
 			case 'error':
