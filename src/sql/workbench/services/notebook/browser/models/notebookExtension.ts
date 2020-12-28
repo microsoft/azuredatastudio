f83041b62feb8f82a70b0ef660d48c7385225a91
dagrajed <dhgrajeda@gmail.com>
2020-12-28 11:07:14 -0800

    Rename type arguments

--- src/sql/workbench/services/notebook/browser/models/notebookExtension.ts
+++ src/sql/workbench/services/notebook/browser/models/notebookExtension.ts
@@ -6,29 +6,29 @@
 import { INotebookModel, ICellModel } from 'sql/workbench/services/notebook/browser/models/modelInterfaces';
 import { NotebookChangeType } from 'sql/workbench/services/notebook/common/contracts';
 
-export class NotebookExtension<ViewMetadata, CellMetadata> {
+export class NotebookExtension<TNotebookMeta, TCellMeta> {
 	readonly version = 1;
 	readonly extensionName = 'azuredatastudio';
 	readonly extensionNamespace = 'extensions';
 
-	public getNotebookMetadata(notebook: INotebookModel): ViewMetadata {
+	public getNotebookMetadata(notebook: INotebookModel): TNotebookMeta {
 		const metadata = notebook.getMetaValue(this.extensionNamespace) || {};
-		return metadata[this.extensionName] as ViewMetadata;
+		return metadata[this.extensionName] as TNotebookMeta;
 	}
 
-	public setNotebookMetadata(notebook: INotebookModel, metadata: ViewMetadata) {
+	public setNotebookMetadata(notebook: INotebookModel, metadata: TNotebookMeta) {
 		const meta = {};
 		meta[this.extensionName] = metadata;
 		notebook.setMetaValue(this.extensionNamespace, meta);
 		notebook.serializationStateChanged(NotebookChangeType.MetadataChanged);
 	}
 
-	public getCellMetadata(cell: ICellModel): CellMetadata {
+	public getCellMetadata(cell: ICellModel): TCellMeta {
 		const namespaceMeta = cell.metadata[this.extensionNamespace] || {};
-		return namespaceMeta[this.extensionName] as CellMetadata;
+		return namespaceMeta[this.extensionName] as TCellMeta;
 	}
 
-	public setCellMetadata(cell: ICellModel, metadata: CellMetadata) {
+	public setCellMetadata(cell: ICellModel, metadata: TCellMeta) {
 		const meta = {};
 		meta[this.extensionName] = metadata;
 		cell.metadata[this.extensionNamespace] = meta;
