51510db1e27be9691653b8d1c4e3cc00b66de190
ADS Merger <karlb@microsoft.com>
2021-01-05 22:33:47 +0000

    Update License Headers

--- extensions/configuration-editing/build/inline-allOf.ts
+++ extensions/configuration-editing/build/inline-allOf.ts
@@ -2,7 +2,7 @@
 
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 // Inlines "allOf"s to allow for "additionalProperties": false. (https://github.com/microsoft/vscode-remote-release/issues/2967)
