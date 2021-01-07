1334585829eb5761637c1ac56e67156e4e2c56d5
Aasim Khan <aasimkhan30@gmail.com>
2021-01-07 14:17:02 -0800

    Fixed deprecation message

--- src/sql/azdata.d.ts
+++ src/sql/azdata.d.ts
@@ -3495,7 +3495,7 @@ declare module 'azdata' {
 
 	export interface RadioButtonComponent extends Component, RadioButtonProperties {
 		/**
-		 * @deprecated use onChanged event instead
+		 * @deprecated use onDidChangeCheckedState event instead
 		 * An event called when the radio button is clicked
 		 */
 		onDidClick: vscode.Event<any>;
