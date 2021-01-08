5d1b3288666fce760e4ac2468a6a4cde22f2662b
Aasim Khan <aasimkhan30@gmail.com>
2021-01-07 23:25:21 -0800

    Fixing the checked variable in radio buttons (#13909)
    * Fixing the checked variable in radio buttons
    
    * Emitting the checked state of radio button.
    
    * Adding onChanged event to radioButtons (exposing it)
    Deprecating onClick event for radioButtons
    Fixing radio button stubs
    
    * Made some type fixes
    
    * Firing event in checked event setter
    
    * updating azdata-test to 1.1 in arc extension
    
    * Some logic fixes in checked setter
    
    * added proper typings and updated package version for azdata-data
    
    * Renamed the event to onDidChangeCheckedState
    
    * Fixed deprecation message
    
    * Fixed broken Schema compare stubs

--- extensions/schema-compare/src/test/testContext.ts
+++ extensions/schema-compare/src/test/testContext.ts
@@ -66,6 +66,7 @@ export function createViewContext(): ViewTestContext {
 	let switchDirectionButtonOnClick: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
 	let cancelCompareButtonOnClick: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
 	let generateScriptButtonOnClick: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+	let onChangeCheckedState: vscode.EventEmitter<boolean> = new vscode.EventEmitter<boolean>();
 
 	let componentBase: azdata.Component = {
 		id: '',
@@ -147,8 +148,9 @@ export function createViewContext(): ViewTestContext {
 	let radioButton: azdata.RadioButtonComponent = Object.assign({}, componentBase, {
 		checked: false,
 		onDidClick: onClick.event,
+		onDidChangeCheckedState: onChangeCheckedState.event
 	});
-	let radioButtonBuilder: azdata.ComponentBuilder<azdata.ButtonComponent, azdata.ButtonProperties> = {
+	let radioButtonBuilder: azdata.ComponentBuilder<azdata.RadioButtonComponent, azdata.RadioButtonProperties> = {
 		component: () => radioButton,
 		withProperties: () => radioButtonBuilder,
 		withProps: () => radioButtonBuilder,
