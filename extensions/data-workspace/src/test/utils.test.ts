f4368b400348e6d9420862c9532aee29b4aed40c
Benjin Dubishar <benjin.dubishar@gmail.com>
2021-01-19 19:11:41 -0800

    making test xplat friendly

--- extensions/data-workspace/src/test/utils.test.ts
+++ extensions/data-workspace/src/test/utils.test.ts
@@ -4,15 +4,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import 'mocha';
+import * as os from 'os';
+import * as path from 'path';
 import should = require('should');
 import { CalculateRelativity } from '../common/telemetry';
 
 suite('Utilities Tests', function (): void {
 	test('test CalculateRelativity', async () => {
-		const workspacePath = 'c:\\Source\\Workspace\\qwerty.code-workspace';
+		const root = os.platform() === 'win32' ? 'Z:\\' : '/';
+		const workspacePath = path.join(root, 'Source', 'Workspace', 'qwerty.code-workspace');
 
-		should.equal(CalculateRelativity('c:\\Source\\Workspace\\qwerty.sqlproj', workspacePath), 'sameFolder');
-		should.equal(CalculateRelativity('c:\\Source\\Workspace\\qwerty\\asdfg\\qwerty.sqlproj', workspacePath), 'directAncestor');
-		should.equal(CalculateRelativity('c:\\Users\\BillG\\\\qwerty.sqlproj', workspacePath), 'other');
+		should.equal(CalculateRelativity(path.join(root, 'Source', 'Workspace', 'qwerty.sqlproj'), workspacePath), 'sameFolder');
+		should.equal(CalculateRelativity(path.join(root, 'Source', 'Workspace', 'qwerty', 'asdfg', 'qwerty.sqlproj'), workspacePath), 'directAncestor');
+		should.equal(CalculateRelativity(path.join(root, 'Users', 'BillG', 'qwerty.sqlproj'), workspacePath), 'other');
 	});
 });
