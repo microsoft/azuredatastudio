2cfcb30c71188181c1a4a8cdc38941747f8b0743
chgagnon <chgagnon@microsoft.com>
2021-02-24 09:10:53 -0800

    Fix build

--- extensions/resource-deployment/src/services/telemetryService.ts
+++ extensions/resource-deployment/src/services/telemetryService.ts
@@ -3,9 +3,10 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
 import AdsTelemetryReporter from '@microsoft/ads-extension-telemetry';
 
-const packageJson = require('../../package.json');
+const packageJson = vscode.extensions.getExtension('Microsoft.resource-deployment')!.packageJSON;
 
 export const TelemetryReporter = new AdsTelemetryReporter(packageJson.name, packageJson.version, packageJson.aiKey);
 
