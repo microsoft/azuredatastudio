69a35b38b2f50bad64f260bb23cfabb4f8eb46f9
Chris LaFreniere <40371649+chlafreniere@users.noreply.github.com>
2021-03-04 12:51:13 -0800

    Callout Dialog Fixes + WYSIWYG Improvements for Insert Link (#14494)
    * wip
    
    * Works in all edit modes
    
    * Default value set
    
    * wip
    
    * preventdefault
    
    * cleanup, add tests
    
    * markup -> markdown
    
    * Ensure selection is persisted for WYSIWYG
    
    * Add simple dialog tests and some PR feedback
    
    * floating promise
    
    * PR comments, formatted markdown refactor
    
    * Change escaping logic + PR comments
    
    * PR feedback

---
+++ src/sql/workbench/contrib/notebook/browser/calloutDialog/common/utils.ts
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as strings from 'vs/base/common/strings';
+
+/**
+ * Escape string to be used as label in markdown link
+ * @param unescapedLabel label to escape
+ */
+export function escapeLabel(unescapedLabel: string): string {
+	let firstEscape = strings.escape(unescapedLabel);
+	return firstEscape.replace(/[[]]/g, function (match) {
+		switch (match) {
+			case '[': return '\[';
+			case ']': return '\]';
+			default: return match;
+		}
+	});
+}
+
+/**
+ * Escape string to be used as url in markdown link
+ * @param unescapedUrl url to escapes
+ */
+export function escapeUrl(unescapedUrl: string): string {
+	let firstEscape = strings.escape(unescapedUrl);
+	return firstEscape.replace(/[()]/g, function (match) {
+		switch (match) {
+			case '(': return '%28';
+			case ')': return '%29';
+			default: return match;
+		}
+	});
+}
