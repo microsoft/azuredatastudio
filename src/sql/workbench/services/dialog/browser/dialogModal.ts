3df763fbb0edc6a5fd25cd3666804913c7c1b093
Hale Rankin <harankin@microsoft.com>
2021-03-05 10:33:08 -0800

    Consolidated styler code and added a flexible custom styler to provide values for dialogModal

--- src/sql/workbench/services/dialog/browser/dialogModal.ts
+++ src/sql/workbench/services/dialog/browser/dialogModal.ts
@@ -23,7 +23,7 @@ import { ILogService } from 'vs/platform/log/common/log';
 import { ITextResourcePropertiesService } from 'vs/editor/common/services/textResourceConfigurationService';
 import { IAdsTelemetryService } from 'sql/platform/telemetry/common/telemetry';
 import { onUnexpectedError } from 'vs/base/common/errors';
-import { attachModalDialogStyler, attachCalloutDialogStyler } from 'sql/workbench/common/styler';
+import { attachCustomDialogStyler } from 'sql/workbench/common/styler';
 import { ILayoutService } from 'vs/platform/layout/browser/layoutService';
 
 export class DialogModal extends Modal {
@@ -56,11 +56,7 @@ export class DialogModal extends Modal {
 
 	public render() {
 		super.render();
-		if (this._modalOptions.dialogStyle === 'callout') {
-			attachCalloutDialogStyler(this, this._themeService);
-		} else {
-			attachModalDialogStyler(this, this._themeService);
-		}
+		attachCustomDialogStyler(this, this._themeService, this._modalOptions.dialogStyle);
 
 		if (this._modalOptions.renderFooter !== false) {
 			this._modalOptions.renderFooter = true;
