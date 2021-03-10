cecbaa6d8014463aca526fba3eb9a5ea616f8e29
Kim Santiago <kisantia@microsoft.com>
2021-03-09 18:38:11 -0800

    fix deploy upgrade existing not choosing the correct database

--- extensions/dacpac/src/wizard/pages/deployConfigPage.ts
+++ extensions/dacpac/src/wizard/pages/deployConfigPage.ts
@@ -142,12 +142,12 @@ export class DeployConfigPage extends DacFxConfigPage {
 
 		//Handle database changes
 		this.databaseDropdown.onValueChanged(() => {
-			const databaseDropdownValue = this.databaseDropdown.value as azdata.CategoryValue;
+			const databaseDropdownValue = this.databaseDropdown.value;
 			if (!databaseDropdownValue) {
 				return;
 			}
 
-			this.model.database = databaseDropdownValue.name;
+			this.model.database = databaseDropdownValue as string;
 		});
 
 		this.databaseLoader = this.view.modelBuilder.loadingComponent().withItem(this.databaseDropdown).withProperties({
