0cff4b85bc0bc6bc8c5e167aaa091d1932e1edb3
nasc17 <69922333+nasc17@users.noreply.github.com>
2021-03-16 08:25:16 -0700

    Updated Postgres Spec for where to find engine version, removed calling calling -ev in edit commands (#14735)
    * Added spec.engine.version, took out calling engine version with edit calls
    
    * Added text wrong place
    
    * missed updates
    
    * PR fix

--- extensions/arc/src/test/mocks/fakeAzdataApi.ts
+++ extensions/arc/src/test/mocks/fakeAzdataApi.ts
@@ -49,7 +49,6 @@ export class FakeAzdataApi implements azdataExt.IAzdataApi {
 							replaceEngineSettings?: boolean,
 							workers?: number
 						},
-						_engineVersion?: string,
 						_additionalEnvVars?: azdataExt.AdditionalEnvVars
 					): Promise<azdataExt.AzdataOutput<void>> { throw new Error('Method not implemented.'); }
 				}
