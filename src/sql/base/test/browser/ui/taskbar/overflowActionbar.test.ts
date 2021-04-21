e02f07bceb53891bf9283b56b757ef87676e22b0
chgagnon <chgagnon@microsoft.com>
2021-04-21 10:34:30 -0700

    Fix overflowactionbar tests

--- src/sql/base/test/browser/ui/taskbar/overflowActionbar.test.ts
+++ src/sql/base/test/browser/ui/taskbar/overflowActionbar.test.ts
@@ -17,59 +17,59 @@ suite('Overflow Actionbar tests', () => {
 	});
 
 	test('Verify moving actions from toolbar to overflow', () => {
-		assert(overflowActionbar.actionsList.children.length === 0);
-		assert(overflowActionbar.items.length === 0);
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 0);
+		assert.strictEqual(overflowActionbar.items.length, 0);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 
 		let a1 = new Action('a1');
 		let a2 = new Action('a1');
 		let a3 = new Action('a3');
 		overflowActionbar.pushAction([a1, a2, a3]);
 
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 
 		// more item element '...' is added when actions are moved to the overflow
 		// and a placeholder undefined element is added to the items array for calculating focus
 		overflowActionbar.createMoreItemElement();
-		// assert(overflowActionbar.actionsList.children.length === 4);
-		// assert(overflowActionbar.items.length === 4);
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 4);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 
 		// move a3 to overflow
 		overflowActionbar.collapseItem();
-		// assert(overflowActionbar.actionsList.children.length === 3);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 2);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 3);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 2);
 		verifyOverflowFocusedIndex(overflowActionbar, 3);
 
 		// move a2 to overflow
 		overflowActionbar.collapseItem();
-		// assert(overflowActionbar.actionsList.children.length === 2);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 1);
-		// assert(overflowActionbar.overflow.childElementCount === 2);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 2);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 1);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 2);
 		verifyOverflowFocusedIndex(overflowActionbar, 2);
 
 		// move a2 to back to toolbar
 		overflowActionbar.restoreItem();
-		// assert(overflowActionbar.actionsList.children.length === 3);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 2);
-		// assert(overflowActionbar.overflow.childElementCount === 1);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 3);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 2);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 1);
 		verifyOverflowFocusedIndex(overflowActionbar, 3);
 
 		// move a3 to back to toolbar
 		overflowActionbar.restoreItem();
-		// assert(overflowActionbar.actionsList.children.length === 4);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 3);
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 4);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 3);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 	});
 
 	test('Verify moving actions and separators from toolbar to overflow', () => {
-		assert(overflowActionbar.actionsList.children.length === 0);
-		assert(overflowActionbar.items.length === 0);
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 0);
+		assert.strictEqual(overflowActionbar.items.length, 0);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 
 		let a1 = new Action('a1');
 		let a2 = new Action('a1');
@@ -79,70 +79,70 @@ suite('Overflow Actionbar tests', () => {
 		overflowActionbar.pushElement(separator);
 		overflowActionbar.pushAction([a3]);
 
-		// assert(overflowActionbar.actionsList.children.length === 4);
-		// assert(overflowActionbar.items.length === 3); // items only has focusable elements
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 4);
+		assert.strictEqual(overflowActionbar.items.length, 3); // items only has focusable elements
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 
 		// more item element '...' is added when actions are moved to the overflow
 		// and a placeholder undefined element is added to the items array for calculating focus
 		overflowActionbar.createMoreItemElement();
-		// assert(overflowActionbar.actionsList.children.length === 5);
-		// assert(overflowActionbar.items.length === 4);
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 5);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 
 		// move a3 to overflow
 		overflowActionbar.collapseItem();
-		// assert(overflowActionbar.actionsList.children.length === 4);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 2);
-		// assert(overflowActionbar.overflow.childElementCount === 1);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 4);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 2);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 1);
 		verifyOverflowFocusedIndex(overflowActionbar, 3);
 
 		// move separator to overflow
 		overflowActionbar.collapseItem();
-		// assert(overflowActionbar.actionsList.children.length === 3);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 2);
-		// assert(overflowActionbar.overflow.childElementCount === 2);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 3);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 2);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 2);
 		verifyOverflowFocusedIndex(overflowActionbar, 3);
 
 		// move a2 to overflow
 		overflowActionbar.collapseItem();
-		// assert(overflowActionbar.actionsList.children.length === 2);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 1);
-		// assert(overflowActionbar.overflow.childElementCount === 3);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 2);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 1);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 3);
 		verifyOverflowFocusedIndex(overflowActionbar, 2);
 
 		// move a2 to back to toolbar
 		overflowActionbar.restoreItem();
-		// assert(overflowActionbar.actionsList.children.length === 3);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 2);
-		// assert(overflowActionbar.overflow.childElementCount === 2);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 3);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 2);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 2);
 		verifyOverflowFocusedIndex(overflowActionbar, 3);
 
 		// move separator to back to toolbar
 		overflowActionbar.restoreItem();
-		// assert(overflowActionbar.actionsList.children.length === 4);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 2);
-		// assert(overflowActionbar.overflow.childElementCount === 1);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 4);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 2);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 1);
 		verifyOverflowFocusedIndex(overflowActionbar, 3);
 
 		// move a3 to back to toolbar
 		overflowActionbar.restoreItem();
-		// assert(overflowActionbar.actionsList.children.length === 5);
-		// assert(overflowActionbar.items.length === 4);
-		assert(getMoreItemPlaceholderIndex(overflowActionbar.items) === 3);
-		assert(overflowActionbar.overflow.childElementCount === 0);
+		assert.strictEqual(overflowActionbar.actionsList.children.length, 5);
+		assert.strictEqual(overflowActionbar.items.length, 4);
+		assert.strictEqual(getMoreItemPlaceholderIndex(overflowActionbar.items), 3);
+		assert.strictEqual(overflowActionbar.overflow.childElementCount, 0);
 	});
 });
 
 function verifyOverflowFocusedIndex(overflowToolbar: OverflowActionBar, expectedIndex: number) {
 	// click more item element to show overflow and set focus to first element
 	overflowToolbar.moreElementOnClick(new MouseEvent('click'));
-	assert(overflowToolbar.focusedItem === expectedIndex);
+	assert.strictEqual(overflowToolbar.focusedItem, expectedIndex);
 	// click to hide overflow
 	overflowToolbar.moreElementOnClick(new MouseEvent('click'));
 }
