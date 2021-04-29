d42db92c08dab5b3a9251d052e50fd5db184b3c6
Kim Santiago <kisantia@microsoft.com>
2021-04-29 10:17:17 -0700

    use single quotes

--- extensions/data-workspace/postInstall.js
+++ extensions/data-workspace/postInstall.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // copy git.d.ts from git extension to data-workspace extension
-fs.copyFile('../git/src/api/git.d.ts', "./src/typings/git.d.ts", (err) => {
+fs.copyFile('../git/src/api/git.d.ts', './src/typings/git.d.ts', (err) => {
 	if (err) {
 		throw err;
 	}
