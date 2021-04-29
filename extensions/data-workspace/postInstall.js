2b3e154cfa4a90c0f3f15c41071c5f96c481f5a0
chgagnon <chgagnon@microsoft.com>
2021-04-29 10:43:12 -0700

    Remove copy git typings

--- extensions/data-workspace/postInstall.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-// copy git.d.ts from git extension to data-workspace extension
-fs.copyFile('../git/src/api/git.d.ts', './src/typings/git.d.ts', (err) => {
-	if (err) {
-		throw err;
-	}
-});
