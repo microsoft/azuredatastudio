7459739ef15dc5184e4a1a0904017f0674b1e313
Justin M <63619224+JustinMDotNet@users.noreply.github.com>
2021-05-05 16:20:20 -0700

    Added missing resources to AzureResource in account interfaces.

--- src/sql/platform/accounts/common/interfaces.ts
+++ src/sql/platform/accounts/common/interfaces.ts
@@ -53,6 +53,10 @@ export enum AzureResource {
 	Sql = 1,
 	OssRdbms = 2,
 	AzureKeyVault = 3,
+	Graph = 4,
+	MicrosoftResourceManagement = 5,
+	AzureDevOps = 6,
+	MsGraph = 7,
 	AzureLogAnalytics = 8
 }
 
