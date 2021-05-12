6d69f407463c6bd9a361e4f3a1d9e287c69fa056
Aditya Bist <adbist@microsoft.com>
2021-05-12 15:52:40 -0700

    set connection profile

--- extensions/azurecore/src/azureResource/commands.ts
+++ extensions/azurecore/src/azureResource/commands.ts
@@ -194,7 +194,7 @@ export function registerAzureResourceCommands(appContext: AppContext, azureViewT
 			// Ensure connection is saved to the Connections list, then open connection dialog
 			connectionProfile = Object.assign({}, treeItem.payload, { saveProfile: true });
 		} else if (node.isConnectionNode) {
-			node.connectionProfile = connectionProfile;
+			connectionProfile = node.connectionProfile;
 		}
 
 		const conn = await azdata.connection.openConnectionDialog(undefined, connectionProfile, { saveConnection: true, showDashboard: true });
