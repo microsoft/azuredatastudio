3d8d34ff20350a9b0ccfd8b01eb381b3ce212a80
kburtram <karlb@microsoft.com>
2021-05-14 16:30:39 -0700

    Change file name

--- build/azure-pipelines/docker/createDrop.sh
+++ build/azure-pipelines/docker/createDrop.sh
@@ -2,12 +2,12 @@ set -e
 REPO="$(pwd)"
 ROOT="$REPO/.."
 
-PLATFORM_LINUX="linux-x64-web"
+PLATFORM_LINUX="linux-x64"
 SERVER_BUILD_NAME="azuredatastudio-server-$PLATFORM_LINUX"
 
 # create docker
 mkdir -p $REPO/.build/docker
-docker build -t azuredatastudio-server -f $REPO/build/azure-pipelines/docker/Dockerfile $ROOT/$SERVER_BUILD_NAME
+docker build -t azuredatastudio-server -f $REPO/build/azure-pipelines/docker/Dockerfile $ROOT/$SERVER_BUILD_NAME-web
 docker save azuredatastudio-server | gzip > $REPO/.build/docker/azuredatastudio-server-docker.tar.gz
 
 node build/azure-pipelines/common/copyArtifacts.js
