69dc6726780cff9317c62a40baaffeb1150e384c
Aasim Khan <aasimkhan30@gmail.com>
2021-05-19 15:40:58 -0700

    Removing all the changes to migration mode page for the infobox

--- extensions/sql-migration/src/wizard/migrationModePage.ts
+++ extensions/sql-migration/src/wizard/migrationModePage.ts
@@ -6,14 +6,11 @@
 import * as azdata from 'azdata';
 import * as vscode from 'vscode';
 import { MigrationWizardPage } from '../models/migrationWizardPage';
-import { MigrationMode, MigrationStateModel, MigrationTargetType, StateChangeEvent } from '../models/stateMachine';
+import { MigrationMode, MigrationStateModel, StateChangeEvent } from '../models/stateMachine';
 import * as constants from '../constants/strings';
-import { IconPathHelper } from '../constants/iconPathHelper';
-import { SqlManagedInstance } from '../api/azure';
 
 export class MigrationModePage extends MigrationWizardPage {
 	private _view!: azdata.ModelView;
-	private _businessCriticalFlexContainer!: azdata.FlexContainer;
 
 	constructor(wizard: azdata.window.Wizard, migrationStateModel: MigrationStateModel) {
 		super(wizard, azdata.window.createWizardPage(constants.DATABASE_BACKUP_MIGRATION_MODE_LABEL, 'MigrationModePage'), migrationStateModel);
@@ -25,22 +22,13 @@ export class MigrationModePage extends MigrationWizardPage {
 		const form = view.modelBuilder.formContainer()
 			.withFormItems(
 				[
-					{
-						component: this._businessCriticalFlexContainer,
-						title: ''
-					},
-					this.migrationModeContainer(view),
+					this.migrationModeContainer(),
 				]
 			);
 		await view.initializeModel(form.component());
 	}
 
 	public async onPageEnter(): Promise<void> {
-		if (this.migrationStateModel._targetType === MigrationTargetType.SQLMI && (<SqlManagedInstance>this.migrationStateModel._targetServerInstance).sku.tier === 'BusinessCritical') {
-			this._businessCriticalFlexContainer.display = 'inline';
-		} else {
-			this._businessCriticalFlexContainer.display = 'none';
-		}
 		this.wizard.registerNavigationValidator((e) => {
 			return true;
 		});
@@ -53,10 +41,10 @@ export class MigrationModePage extends MigrationWizardPage {
 	protected async handleStateChange(e: StateChangeEvent): Promise<void> {
 	}
 
-	private migrationModeContainer(view: azdata.ModelView): azdata.FormComponent {
+	private migrationModeContainer(): azdata.FormComponent {
 		const buttonGroup = 'cutoverContainer';
 
-		const onlineButton = view.modelBuilder.radioButton().withProps({
+		const onlineButton = this._view.modelBuilder.radioButton().withProps({
 			label: constants.DATABASE_BACKUP_MIGRATION_MODE_ONLINE_LABEL,
 			name: buttonGroup,
 			CSSStyles: {
@@ -66,7 +54,7 @@ export class MigrationModePage extends MigrationWizardPage {
 			checked: true
 		}).component();
 
-		const onlineDescription = view.modelBuilder.text().withProps({
+		const onlineDescription = this._view.modelBuilder.text().withProps({
 			value: constants.DATABASE_BACKUP_MIGRATION_MODE_ONLINE_DESCRIPTION,
 			CSSStyles: {
 				'font-size': '13px',
@@ -82,7 +70,7 @@ export class MigrationModePage extends MigrationWizardPage {
 			}
 		});
 
-		const offlineButton = view.modelBuilder.radioButton().withProps({
+		const offlineButton = this._view.modelBuilder.radioButton().withProps({
 			label: constants.DATABASE_BACKUP_MIGRATION_MODE_OFFLINE_LABEL,
 			name: buttonGroup,
 			CSSStyles: {
@@ -91,7 +79,7 @@ export class MigrationModePage extends MigrationWizardPage {
 			},
 		}).component();
 
-		const offlineDescription = view.modelBuilder.text().withProps({
+		const offlineDescription = this._view.modelBuilder.text().withProps({
 			value: constants.DATABASE_BACKUP_MIGRATION_MODE_OFFLINE_DESCRIPTION,
 			CSSStyles: {
 				'font-size': '13px',
@@ -108,7 +96,7 @@ export class MigrationModePage extends MigrationWizardPage {
 			}
 		});
 
-		const flexContainer = view.modelBuilder.flexContainer().withItems(
+		const flexContainer = this._view.modelBuilder.flexContainer().withItems(
 			[
 				onlineButton,
 				onlineDescription,
