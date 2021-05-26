0f6f16de70ff98b59b56454c0609fb85529ba2bd
Aasim Khan <aasimkhan30@gmail.com>
2021-05-25 18:00:30 -0700

    Migraiton enhancements  v: 0.1.1 (#15570)
    * adding filters and cards for failed migrations
    
    * Added card and filter for completing cutover
    
    * Fixing blob container support and some ux enhancements
    
    * Enabling eastus2 and canada central regions
    
    * Increasing height of container to accomodate newer cards and cleaning up database backup page
    
    * vbump migration
    
    * Removing unused code

--- extensions/sql-migration/src/dialog/migrationStatus/migrationStatusDialogModel.ts
+++ extensions/sql-migration/src/dialog/migrationStatus/migrationStatusDialogModel.ts
@@ -10,47 +10,34 @@ export class MigrationStatusDialogModel {
 	public statusDropdownValues: azdata.CategoryValue[] = [
 		{
 			displayName: 'Status: All',
-			name: 'All',
+			name: AdsMigrationStatus.ALL,
 		}, {
 			displayName: 'Status: Ongoing',
-			name: 'Ongoing',
+			name: AdsMigrationStatus.ONGOING,
+		}, {
+			displayName: 'Status: Completing',
+			name: AdsMigrationStatus.COMPLETING
 		}, {
 			displayName: 'Status: Succeeded',
-			name: 'Succeeded',
+			name: AdsMigrationStatus.SUCCEEDED,
+		}, {
+			displayName: 'Status: Failed',
+			name: AdsMigrationStatus.FAILED
 		}
 	];
 
 	constructor(public _migrations: MigrationContext[]) {
 	}
 
-	public filterMigration(databaseName: string, category: string): MigrationContext[] {
-		let filteredMigration: MigrationContext[] = [];
-		if (category === 'All') {
-			filteredMigration = this._migrations;
-		} else if (category === 'Ongoing') {
-			filteredMigration = this._migrations.filter((value) => {
-				const status = value.migrationContext.properties.migrationStatus;
-				const provisioning = value.migrationContext.properties.provisioningState;
-				return status === 'InProgress' || status === 'Creating' || status === 'Completing' || provisioning === 'Creating';
-			});
-		} else if (category === 'Succeeded') {
-			filteredMigration = this._migrations.filter((value) => {
-				const status = value.migrationContext.properties.migrationStatus;
-				return status === 'Succeeded';
-			});
-		}
-		if (databaseName) {
-			filteredMigration = filteredMigration.filter((value) => {
-				return value.migrationContext.name.toLowerCase().includes(databaseName.toLowerCase());
-			});
-		}
-
-		return filteredMigration;
-	}
 }
 
-export enum MigrationCategory {
-	ALL,
-	ONGOING,
-	SUCCEEDED
+/**
+ * This enum is used to categorize migrations internally in ADS. A migration has 2 statuses: Provisioning Status and Migration Status. The values from both the statuses are mapped to different values in this enum
+ */
+export enum AdsMigrationStatus {
+	ALL = 'all',
+	ONGOING = 'ongoing',
+	SUCCEEDED = 'succeeded',
+	FAILED = 'failed',
+	COMPLETING = 'completing'
 }
