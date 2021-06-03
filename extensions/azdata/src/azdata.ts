63affd4d9a9845f320440f6867467a2ea6ba89f9
Charles Gagnon <chgagnon@microsoft.com>
2021-06-03 09:58:26 -0700

    Update min azdata version for new release (#15619)

--- extensions/azdata/src/azdata.ts
+++ extensions/azdata/src/azdata.ts
@@ -20,7 +20,7 @@ import * as loc from './localizedConstants';
 /**
  * The minimum required azdata CLI version for this extension to function properly
  */
-export const MIN_AZDATA_VERSION = new SemVer('20.3.3');
+export const MIN_AZDATA_VERSION = new SemVer('20.3.4');
 
 export const enum AzdataDeployOption {
 	dontPrompt = 'dontPrompt',
