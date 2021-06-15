db3a0ef7acefd58bb397d23cb930aba6f42bedfc
Charles Gagnon <chgagnon@microsoft.com>
2021-06-15 09:52:27 -0700

    Trim pathname for link callout file paths (#15716)
    * Don't use pathname for link URLs
    
    * Trim pathname instead

--- src/sql/workbench/contrib/notebook/browser/cellViews/markdownToolbar.component.ts
+++ src/sql/workbench/contrib/notebook/browser/cellViews/markdownToolbar.component.ts
@@ -351,10 +351,12 @@ export class MarkdownToolbarComponent extends AngularDisposable {
 
 	private getCurrentLinkUrl(): string {
 		if (this.cellModel.currentMode === CellEditModes.WYSIWYG) {
-			if (document.getSelection().anchorNode.parentNode['protocol'] === 'file:') {
-				return document.getSelection().anchorNode.parentNode['pathname'] || '';
+			const parentNode = document.getSelection().anchorNode.parentNode as HTMLAnchorElement;
+			if (parentNode.protocol === 'file:') {
+				// Pathname starts with / per https://developer.mozilla.org/en-US/docs/Web/API/HTMLAnchorElement/pathname so trim it off
+				return parentNode.pathname?.slice(1) || '';
 			} else {
-				return document.getSelection().anchorNode.parentNode['href'] || '';
+				return parentNode.href || '';
 			}
 		} else {
 			const editorControl = this.getCellEditorControl();
