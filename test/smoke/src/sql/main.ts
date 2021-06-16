be1ff8e37b76eac4335d3f683b903430e64f414d
Charles Gagnon <chgagnon@microsoft.com>
2021-06-16 10:54:04 -0700

    Add smoke test for Create Book dialog (#15714)
    * Add smoke test for Create Book dialog
    
    * Add comment
    
    * Update distro
    
    * update distro 2
    
    * distro

--- test/smoke/src/sql/main.ts
+++ test/smoke/src/sql/main.ts
@@ -7,6 +7,7 @@ import { setup as setupQueryEditorTests, setupWeb as setupQueryEditorWebTests } 
 import { setup as setupNotebookTests } from './areas/notebook/notebook.test';
 import { setup as setupNotebookViewTests } from './areas/notebook/notebookView.test';
 import { setup as setupImportTests } from './areas/import/import.test';
+import { setup as setupCreateBookDialogTests } from './areas/notebook/createBook.test';
 import { ApplicationOptions } from '../../../automation';
 import * as yazl from 'yauzl';
 import * as fs from 'fs';
@@ -22,6 +23,7 @@ export function main(isWeb: boolean = false): void {
 	}
 	setupNotebookTests();
 	setupNotebookViewTests();
+	setupCreateBookDialogTests();
 	setupImportTests();
 }
 
