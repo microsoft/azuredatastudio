5a5c990dab5a5c8e76644654095195d7e378feff
Kim Santiago <kisantia@microsoft.com>
2021-06-25 14:32:43 -0700

    add notification when there's an error

--- extensions/data-workspace/src/main.ts
+++ extensions/data-workspace/src/main.ts
@@ -21,6 +21,9 @@ export async function activate(context: vscode.ExtensionContext): Promise<IExten
 	// checking for projects not added to the workspace yet
 	workspaceService.loadTempProjects().then(() => {
 		return workspaceService.checkForProjectsNotAddedToWorkspace();
+	}).catch(error => {
+		console.error(error);
+		vscode.window.showErrorMessage(error instanceof Error ? error.message : error);
 	});
 
 	context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(() => {
