79910f06c5664caf7df7f7b9b1eb655c1b3678af
Alex Ma <alma1@microsoft.com>
2021-06-25 14:55:39 -0700

    removed useless handlebars function

--- extensions/server-report/src/utils.ts
+++ extensions/server-report/src/utils.ts
@@ -5,11 +5,7 @@
 
 'use strict';
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs-extra';
-import * as handlebars from 'handlebars';
 import * as Constants from './constants';
-import * as LocalizedConstants from './localizedConstants';
 
 /**
  * Helper to log messages to the developer console if enabled
@@ -23,24 +19,4 @@ export function logDebug(msg: any): void {
 		let outputMsg = '[' + currentTime + ']: ' + msg ? msg.toString() : '';
 		console.log(outputMsg);
 	}
-}
-
-export function renderTemplateHtml(extensionPath: string, templateName: string, templateValues: object): Thenable<string> {
-	let templatePath = path.join(extensionPath, 'resources', templateName);
-
-	// 1) Read the template from the disk
-	// 2) Compile it as a handlebars template and render the HTML
-	// 3) On failure, return a simple string as an error
-	return fs.readFile(templatePath, 'utf-8')
-		.then(templateText => {
-			let template = handlebars.compile(templateText);
-			return template(templateValues);
-		})
-		.then(
-			undefined,
-			error => {
-				logDebug(error);
-				return LocalizedConstants.msgErrorLoadingTab;
-			}
-		);
 }
