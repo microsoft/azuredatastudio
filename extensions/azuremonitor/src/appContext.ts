5e4dbf7095304a5cf586d1ec7d300633b650901f
Justin M <63619224+JustinMDotNet@users.noreply.github.com>
2021-07-06 15:27:19 -0700

    Add Azure Monitor Extension (#15397)
    * Added Azure Log Analytics resource for generating AAD Token.
    
    * Fixed AzureResource
    
    * Removed debug code from connectionManagementService
    
    * Moved AzureLogAnalytics from AzureResource enum in azdata.d.ts to azdata.proposed.d.ts.  Added azureLogAnalyticsResource to all azureSettings in providerSettings.ts
    
    * Updated endpoint for generating AAD Token for LogAnalytics for UsGov, UsNat, and China
    
    * Initial Commit of Azure Monitor Extension
    
    * Added extension name to azuremonitor package strings
    
    * Removed azureMonitor resource from germanyCloud in providerSettings
    
    * Added logic to exclude menuItems in object explorer for LogAnalytics
    
    * Changed exe from AzureMonitor to Kusto
    
    * Added if clause for queryName for new queries
    
    * Changed queryWindow name from KustoQuery to KQLQuery for Kusto and LogAnalytics.
    
    * Added LogAnalytics for setTaskBarContent
    
    * Added serialization and telemetry feature classes to AzureMonitor. Added references for azdata and vscode.
    
    * Added azure monitor light and dark icons
    
    * Added config for Dashboard in package.json
    
    * Added workspace information to dashboard
    
    * Added language support for LogAnalytics
    
    * Added Notebook support
    
    * Added Hide flag to package.json for databaseName
    
    * Changed providerId from LogAnalytics to LOGANALYTICS
    
    * Changed Workspace to Workspace ID in package.nls.json
    
    * Added support for Azure Widget browser
    
    * Changed fullName to use workspaceId when connecting
    
    * Changed providerId from alertsManagement to azureMonitor
    
    * Added .gitignore and *.vsix to vscodeignore.
    
    * Removed unused devDependencies
    
    * Code Review Feedback
    
    * Changed tsconfig.json to match Kusto and Sql
    
    * Changed package.json to match kusto package.
    
    * Changed tsconfig to validate unused params and implictAny. Changed existing code to satisfy build.
    
    * Fixed tsconfig to use the correct base class.
    
    * Added objectExplorerNodeProvider and all related classes.
    
    * Removed unused tmLanguage file
    
    * Added logic to to download extension from toolservice
    
    * Fixed launchArgs. Removed commented code from extension.ts. Changed config.json to use net5.0
    
    * Added displayName to package.nls.json. Removed hide flag from databaseName. Other code review feedback.
    
    * Added readme info to AzureMonitor
    
    * Removed unused client-error-handler and ui-references files. Combined outputChannel in azuremonitorServer. Removed TODO from contextProvider. Renamed function in extension.ts. Removed unneeded 'use strict' from cancelableStream.ts. Removed second outputChannel from objectExplorerNodeProvider.
    
    * Removed unused files

---
+++ extensions/azuremonitor/src/appContext.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import * as vscode from 'vscode';
+
+/**
+ * Global context for the application
+ */
+export class AppContext {
+
+	private serviceMap: Map<string, any> = new Map();
+
+	constructor(public readonly extensionContext: vscode.ExtensionContext) { }
+
+	public getService<T>(serviceName: string): T | undefined {
+		const service = this.serviceMap.get(serviceName) as T;
+		if (!service) {
+			console.warn('Service ', serviceName, ' is not registered');
+		}
+		return service;
+	}
+
+	public registerService<T>(serviceName: string, service: T): void {
+		if (this.serviceMap.has(serviceName)) {
+			console.warn('Multiple services ', serviceName, ' registered!');
+		} else {
+			this.serviceMap.set(serviceName, service);
+		}
+	}
+}
