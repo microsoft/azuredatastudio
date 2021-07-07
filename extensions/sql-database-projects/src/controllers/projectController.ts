aa346c0c56a56c6538c5be97cba3f48c1e52c533
Kim Santiago <kisantia@microsoft.com>
2021-07-07 16:24:09 -0700

    move constant

--- extensions/sql-database-projects/src/controllers/projectController.ts
+++ extensions/sql-database-projects/src/controllers/projectController.ts
@@ -136,8 +136,6 @@ export class ProjectsController {
 			throw new Error(`Specified GUID is invalid: '${creationParams.projectGuid}'`);
 		}
 
-		const defaultDSP = constants.targetPlatformToVersion.get(SqlTargetPlatform.sqlServer2019)!;
-
 		const macroDict: Record<string, string> = {
 			'PROJECT_NAME': creationParams.newProjName,
 			'PROJECT_GUID': creationParams.projectGuid ?? UUID.generateUuid().toUpperCase(),
