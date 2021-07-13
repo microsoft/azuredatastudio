d28c7ba9ebdcbbad7844c32b4a72544335b46c8f
Cory Rivera <corivera@microsoft.com>
2021-07-13 12:11:41 -0700

    Fix some notebook files.

--- src/vs/workbench/contrib/notebook/browser/viewModel/codeCellViewModel.ts
+++ src/vs/workbench/contrib/notebook/browser/viewModel/codeCellViewModel.ts
@@ -19,24 +19,19 @@ import { BaseCellViewModel } from './baseCellViewModel';
 
 export class CodeCellViewModel extends BaseCellViewModel implements ICellViewModel {
 	readonly cellKind = CellKind.Code;
-
-	protected readonly _onDidChangeOutputs = this._register(new Emitter<NotebookCellOutputsSplice[]>());
+	protected readonly _onDidChangeOutputs = new Emitter<NotebookCellOutputsSplice>();
 	readonly onDidChangeOutputs = this._onDidChangeOutputs.event;
-
-	private readonly _onDidRemoveOutputs = this._register(new Emitter<readonly ICellOutputViewModel[]>());
+	private readonly _onDidRemoveOutputs = new Emitter<readonly ICellOutputViewModel[]>();
 	readonly onDidRemoveOutputs = this._onDidRemoveOutputs.event;
-
-	private readonly _onDidHideInput = this._register(new Emitter<void>());
+	private readonly _onDidHideInput = new Emitter<void>();
 	readonly onDidHideInput = this._onDidHideInput.event;
-
-	private readonly _onDidHideOutputs = this._register(new Emitter<readonly ICellOutputViewModel[]>());
+	private readonly _onDidHideOutputs = new Emitter<readonly ICellOutputViewModel[]>();
 	readonly onDidHideOutputs = this._onDidHideOutputs.event;
-
 	private _outputCollection: number[] = [];
 
 	private _outputsTop: PrefixSumComputer | null = null;
 
-	protected readonly _onDidChangeLayout = this._register(new Emitter<CodeCellLayoutChangeEvent>());
+	protected readonly _onDidChangeLayout = new Emitter<CodeCellLayoutChangeEvent>();
 	readonly onDidChangeLayout = this._onDidChangeLayout.event;
 
 	private _editorHeight = 0;
@@ -108,15 +103,13 @@ export class CodeCellViewModel extends BaseCellViewModel implements ICellViewMod
 		super(viewType, model, UUID.generateUuid(), viewContext, configurationService, modelService);
 		this._outputViewModels = this.model.outputs.map(output => new CellOutputViewModel(this, output, this._notebookService));
 
-		this._register(this.model.onDidChangeOutputs((splices) => {
+		this._register(this.model.onDidChangeOutputs((splice) => {
 			const removedOutputs: ICellOutputViewModel[] = [];
-			splices.reverse().forEach(splice => {
-				this._outputCollection.splice(splice[0], splice[1], ...splice[2].map(() => 0));
-				removedOutputs.push(...this._outputViewModels.splice(splice[0], splice[1], ...splice[2].map(output => new CellOutputViewModel(this, output, this._notebookService))));
-			});
+			this._outputCollection.splice(splice.start, splice.deleteCount, ...splice.newOutputs.map(() => 0));
+			removedOutputs.push(...this._outputViewModels.splice(splice.start, splice.deleteCount, ...splice.newOutputs.map(output => new CellOutputViewModel(this, output, this._notebookService))));
 
 			this._outputsTop = null;
-			this._onDidChangeOutputs.fire(splices);
+			this._onDidChangeOutputs.fire(splice);
 			this._onDidRemoveOutputs.fire(removedOutputs);
 			this.layoutChange({ outputHeight: true }, 'CodeCellViewModel#model.onDidChangeOutputs');
 		}));
