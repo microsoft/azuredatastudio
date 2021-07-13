adcd706d06b51dba2fdde73d22d8b8becbc62a56
Vasu Bhog <vabhog@microsoft.com>
2021-07-13 16:55:54 -0700

    fix relative links not correctly formatted

--- src/sql/workbench/contrib/notebook/browser/htmlMarkdownConverter.ts
+++ src/sql/workbench/contrib/notebook/browser/htmlMarkdownConverter.ts
@@ -312,6 +312,8 @@ export function findPathRelativeToContent(notebookFolder: string, contentPath: U
 			relativePath = relativePath.replace(/\s/g, '%20');
 			if (relativePath.startsWith(path.join('..', path.sep) || path.join('.', path.sep))) {
 				return relativePath;
+			} else if (relativePath.match(/^(?!.*\.\\])\.\W/)) {
+				return relativePath.replace(/^(?!.*\.\\])\.\W/, '..\\');
 			} else {
 				// if the relative path does not contain ./ at the beginning, we need to add it so it's recognized as a link
 				return `.${path.join(path.sep, relativePath)}`;
