93143730b7dbbadeb0f1b5ac88e560cf86772f32
Alex Ma <alma1@microsoft.com>
2021-07-14 14:36:52 -0700

    added on end function to end of pipe (#16144)

--- build/lib/locFunc.ts
+++ build/lib/locFunc.ts
@@ -387,9 +387,10 @@ export function renameVscodeLangpacks(): Promise<void> {
 			console.log('vscode pack is not in ADS yet: ' + langId);
 			continue;
 		}
-		gulp.src(`i18n/ads-language-pack-${langId}/*.md`)
-			.pipe(vfs.dest(locVSCODEFolder, {overwrite: true}))
-			.end(function () {
+		gulp.src(path.join(locADSFolder, '*.md'))
+			.pipe(rename(filepath => filepath.dirname = ''))
+			.pipe(gulp.dest(locVSCODEFolder))
+			.on('end', () => {
 				rimraf.sync(locADSFolder);
 				fs.renameSync(locVSCODEFolder, locADSFolder);
 			});
