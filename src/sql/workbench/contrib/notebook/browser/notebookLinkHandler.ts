17339a2f4ad287b3cdb949a42fda6b824c0bccdb
Vasu Bhog <vabhog@microsoft.com>
2021-07-29 17:18:31 -0700

    add header

--- src/sql/workbench/contrib/notebook/browser/notebookLinkHandler.ts
+++ src/sql/workbench/contrib/notebook/browser/notebookLinkHandler.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { URI } from 'vs/base/common/uri';
 import * as path from 'vs/base/common/path';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
