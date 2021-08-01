6069af675772387d966bdb7f2ef30d91117d3b0d
Candice Ye <canye@microsoft.com>
2021-08-01 10:11:53 -0700

    Reverting adding azdata tool back in

--- extensions/resource-deployment/src/typings/ref.d.ts
+++ extensions/resource-deployment/src/typings/ref.d.ts
@@ -8,7 +8,6 @@
 /// <reference path='../../../../src/sql/azdata.proposed.d.ts'/>
 /// <reference path='../../../arc/src/typings/arc.d.ts'/>
 /// <reference path='../../../azcli/src/typings/az-ext.d.ts'/>
-/// <reference path='../../../azdata/src/typings/azdata-ext.d.ts'/>
 /// <reference path='../../../azurecore/src/azurecore.d.ts'/>
 /// <reference path='../../../azurecore/src/azureResource/azure-resource.d.ts'/>
 /// <reference types='@types/node'/>
