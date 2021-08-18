cc75b9988f55a8fc4283ccbfe0a1d8f9d6833d2b
chgagnon <chgagnon@microsoft.com>
2021-08-18 14:36:38 -0700

    Fix typo

--- extensions/azurecore/src/azureResource/tree/accountTreeNode.ts
+++ extensions/azurecore/src/azureResource/tree/accountTreeNode.ts
@@ -77,7 +77,7 @@ export class AzureResourceAccountTreeNode extends AzureResourceContainerTreeNode
 						errMsg = AzureResourceErrorMessageUtil.getErrorMessage(err);
 					}
 					if (!token) {
-						vscode.window.showWarningMessage(localize('azure.unableToAccessSubscription', "Unable to access subscription {0} ({1})}. Please [refresh the account](command:azure.resource.signin) to try again. {2}", s.name, s.id, errMsg));
+						vscode.window.showWarningMessage(localize('azure.unableToAccessSubscription', "Unable to access subscription {0} ({1}). Please [refresh the account](command:azure.resource.signin) to try again. {2}", s.name, s.id, errMsg));
 						return false;
 					}
 					return true;
