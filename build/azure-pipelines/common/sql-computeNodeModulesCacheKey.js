31782052795a820beaf8c49ec99004b5998baf5e
chgagnon <chgagnon@microsoft.com>
2021-08-23 16:35:26 -0700

    Add debugging

--- build/azure-pipelines/common/sql-computeNodeModulesCacheKey.js
+++ build/azure-pipelines/common/sql-computeNodeModulesCacheKey.js
@@ -39,7 +39,11 @@ shasum.update(fs.readFileSync(path.join(ROOT, 'remote/.yarnrc')));
 // Adding all yarn.lock files into sha sum.
 const result = [];
 findFiles('', 'yarn.lock', result);
-result.forEach(f => shasum.update(fs.readFileSync(f)));
+console.log('Result: ' + result.length);
+result.forEach(f => {
+    shasum.update(fs.readFileSync(f));
+    console.log(`Updating SHA with ${f}`);
+});
 // Add any other command line arguments
 for (let i = 2; i < process.argv.length; i++) {
     shasum.update(process.argv[i]);
