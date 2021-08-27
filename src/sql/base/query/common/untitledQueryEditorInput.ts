c8bc6270277f10b1af8332dabd3c7afcde7a6600
Alex Ma <alma1@microsoft.com>
2021-08-27 13:15:58 -0700

    added extends for IUntitledQueryEditorInput

--- src/sql/base/query/common/untitledQueryEditorInput.ts
+++ src/sql/base/query/common/untitledQueryEditorInput.ts
@@ -3,13 +3,15 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { IConnectableInput } from 'sql/platform/connection/common/connectionManagement';
 import { IResolvedTextEditorModel } from 'vs/editor/common/services/resolverService';
 import { EditorInputCapabilities } from 'vs/workbench/common/editor';
-import { EncodingMode, IEncodingSupport } from 'vs/workbench/services/textfile/common/textfiles';
+import { EditorInput } from 'vs/workbench/common/editor/editorInput';
+import { EncodingMode } from 'vs/workbench/services/textfile/common/textfiles';
 import { UntitledTextEditorInput } from 'vs/workbench/services/untitled/common/untitledTextEditorInput';
 import { IUntitledTextEditorModel } from 'vs/workbench/services/untitled/common/untitledTextEditorModel';
 
-export interface IUntitledQueryEditorInput extends IEncodingSupport {
+export interface IUntitledQueryEditorInput extends EditorInput, IConnectableInput {
 
 	resolve(): Promise<IUntitledTextEditorModel & IResolvedTextEditorModel>;
 
