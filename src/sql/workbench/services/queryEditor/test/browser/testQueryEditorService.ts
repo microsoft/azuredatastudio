132feee8b022adc3b6941d5dfd856dce91329c7b
Alex Ma <alma1@microsoft.com>
2021-08-27 13:04:47 -0700

    Revert "Revert "added IUntitledQueryEditorInput""
    This reverts commit 8b2258ab49275a271a39036ea1734feca98ee753.

--- src/sql/workbench/services/queryEditor/test/browser/testQueryEditorService.ts
+++ src/sql/workbench/services/queryEditor/test/browser/testQueryEditorService.ts
@@ -10,6 +10,7 @@ import { UntitledQueryEditorInput } from 'sql/base/query/browser/untitledQueryEd
 import { UntitledTextEditorInput } from 'vs/workbench/services/untitled/common/untitledTextEditorInput';
 import { QueryResultsInput } from 'sql/workbench/common/editor/query/queryResultsInput';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
+import { IUntitledQueryEditorInput } from 'sql/base/query/common/untitledQueryEditorInput';
 
 export class TestQueryEditorService implements IQueryEditorService {
 	_serviceBrand: undefined;
@@ -19,7 +20,7 @@ export class TestQueryEditorService implements IQueryEditorService {
 		@IEditorService private readonly editorService: IEditorService) {
 	}
 
-	newSqlEditor(options?: INewSqlEditorOptions): Promise<UntitledQueryEditorInput> {
+	newSqlEditor(options?: INewSqlEditorOptions): Promise<IUntitledQueryEditorInput> {
 		const base = this.editorService.createEditorInput({ forceUntitled: true }) as UntitledTextEditorInput;
 		return Promise.resolve(this.instantiationService.createInstance(UntitledQueryEditorInput, '', base, new QueryResultsInput(base.resource.toString(true))));
 	}
