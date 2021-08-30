1c720713729219a9a28250ce993dc2f0a871e0ed
Alex Ma <alma1@microsoft.com>
2021-08-30 11:01:28 -0700

    changed uri to public

--- src/sql/workbench/common/editor/query/queryResultsInput.ts
+++ src/sql/workbench/common/editor/query/queryResultsInput.ts
@@ -46,7 +46,7 @@ export class QueryResultsInput extends EditorInput {
 		return this._state;
 	}
 
-	constructor(private _uri: string) {
+	constructor(public uri: string) {
 		super();
 	}
 
@@ -60,7 +60,7 @@ export class QueryResultsInput extends EditorInput {
 
 	override matches(other: any): boolean {
 		if (other instanceof QueryResultsInput) {
-			return (other._uri === this._uri);
+			return (other.uri === this.uri);
 		}
 
 		return false;
@@ -82,14 +82,6 @@ export class QueryResultsInput extends EditorInput {
 
 	static get ID() {
 		return 'workbench.query.queryResultsInput';
-	}
-
-	get uri(): string {
-		return this._uri;
-	}
-
-	set uri(newUri: string) {
-		this._uri = newUri;
 	}
 
 	get resource(): URI | undefined {
