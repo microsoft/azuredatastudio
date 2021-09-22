76a69a0afb389167c0b07deeccd310a0030951f4
brian-harris <61598682+brian-harris@users.noreply.github.com>
2021-09-21 23:31:36 -0700

    add error banner for failed migration cutover and cancel migration (#17106)

--- extensions/sql-migration/src/constants/strings.ts
+++ extensions/sql-migration/src/constants/strings.ts
@@ -337,6 +337,7 @@ export const YES = localize('sql.migration.yes', "Yes");
 export const NO = localize('sql.migration.no', "No");
 export const EMPTY_TABLE_TEXT = localize('sql.migration.empty.table.text', "No backup files");
 export const EMPTY_TABLE_SUBTEXT = localize('sql.migration.empty.table.subtext', "If results were expected, verify the connection to the SQL Server instance.");
+export const MIGRATION_CUTOVER_ERROR = localize('sql.migration.cutover.error', 'An error occurred while initiating cutover.');
 
 //Migration confirm cutover dialog
 export const COMPLETING_CUTOVER_WARNING = localize('sql.migration.completing.cutover.warning', "Completing cutover without restoring all the backups may result in a data loss.");
