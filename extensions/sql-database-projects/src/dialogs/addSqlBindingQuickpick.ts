6693ca65ecdd093294e1d22ba2a2c95bb41d8662
Kim Santiago <kisantia@microsoft.com>
2021-09-29 10:41:46 -0700

    Addressing comments

--- extensions/sql-database-projects/src/dialogs/addSqlBindingQuickpick.ts
+++ extensions/sql-database-projects/src/dialogs/addSqlBindingQuickpick.ts
@@ -22,7 +22,7 @@ export async function launchAddSqlBindingQuickpick(uri: vscode.Uri | undefined, 
 				return;
 			}
 
-			await vscode.window.activeTextEditor?.document.save();
+			await vscode.window.activeTextEditor!.document.save();
 		}
 	}
 
