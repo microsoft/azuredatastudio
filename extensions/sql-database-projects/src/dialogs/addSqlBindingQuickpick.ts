a40a45ae9279fa9d2db59955e48aed94c0d3281f
Kim Santiago <kisantia@microsoft.com>
2021-10-04 16:13:46 -0700

    use placeholder for table name in add sql bindings quickpick

--- extensions/sql-database-projects/src/dialogs/addSqlBindingQuickpick.ts
+++ extensions/sql-database-projects/src/dialogs/addSqlBindingQuickpick.ts
@@ -79,7 +79,7 @@ export async function launchAddSqlBindingQuickpick(uri: vscode.Uri | undefined, 
 	// 3. ask for object name for the binding
 	const objectName = await vscode.window.showInputBox({
 		prompt: selectedBinding.type === BindingType.input ? constants.sqlTableOrViewToQuery : constants.sqlTableToUpsert,
-		value: constants.placeHolderObject,
+		placeHolder: constants.placeHolderObject,
 		ignoreFocusOut: true
 	});
 
