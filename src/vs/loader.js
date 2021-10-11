3503749215d9649142a916be8819495e52aa64b7
chgagnon <chgagnon@microsoft.com>
2021-10-11 15:41:51 -0700

    add debug logging warn

--- src/vs/loader.js
+++ src/vs/loader.js
@@ -1333,6 +1333,7 @@ var AMDLoader;
          */
         ModuleManager.prototype.enqueueDefineAnonymousModule = function (dependencies, callback) {
             if (this._currentAnonymousDefineCall !== null) {
+				console.warn(this._currentAnonymousDefineCall.stack);
                 throw new Error('Can only have one anonymous define call per script file');
             }
             var stack = null;
