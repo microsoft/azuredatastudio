dfc253a8fe69307876e896a92555b3d121277903
Cory Rivera <corivera@microsoft.com>
2021-11-03 17:18:54 -0700

    Fix file extension conversion to match ADS extension behavior.

--- src/vs/workbench/contrib/notebook/browser/extensionPoint.ts
+++ src/vs/workbench/contrib/notebook/browser/extensionPoint.ts
@@ -193,11 +193,11 @@ const adsNotebookRegistry = Registry.as<INotebookProviderRegistry>(NotebookProvi
 notebooksExtensionPoint.setHandler(extensions => {
 	for (let extension of extensions) {
 		for (const notebookContribution of extension.value) {
-			// Remove any leading regex characters from the filename pattern
+			// Remove any leading regex characters and period from the filename pattern
 			let extensions = notebookContribution.selector?.map(s => {
 				let lastDotPosition = s.filenamePattern?.lastIndexOf('.');
-				if (lastDotPosition > 0) {
-					return s.filenamePattern.slice(lastDotPosition);
+				if (lastDotPosition >= 0) {
+					return s.filenamePattern.slice(lastDotPosition + 1);
 				}
 				return s.filenamePattern;
 			}).filter(ext => ext !== undefined);
