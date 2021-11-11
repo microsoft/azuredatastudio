f11df571176632752cb02848255b676b0ca2a868
luczhan <luczhan@microsoft.com>
2021-11-11 09:10:19 -0800

    reorder

--- extensions/sql-database-projects/src/dialogs/addSqlBindingQuickpick.ts
+++ extensions/sql-database-projects/src/dialogs/addSqlBindingQuickpick.ts
@@ -148,7 +148,7 @@ export async function launchAddSqlBindingQuickpick(uri: vscode.Uri | undefined, 
 					{
 						title: constants.enterConnectionString,
 						ignoreFocusOut: true,
-						value: 'Server=localhost;Initial Catalog={db_name};Persist Security Info=False;User ID=sa;Password={your_password}',
+						value: 'Server=localhost;Initial Catalog={db_name};User ID=sa;Password={your_password};Persist Security Info=False',
 						validateInput: input => input ? undefined : constants.valueMustNotBeEmpty
 					}
 				) ?? '';
