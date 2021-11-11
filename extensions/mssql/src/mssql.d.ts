0d7cc9fa7efb78dd68320448ba1838b3166814ec
Benjin Dubishar <benjin.dubishar@gmail.com>
2021-11-11 14:16:23 -0800

    Adding comment for keeping enum values in sync

--- extensions/mssql/src/mssql.d.ts
+++ extensions/mssql/src/mssql.d.ts
@@ -124,6 +124,8 @@ export const enum SchemaCompareEndpointType {
 	Database = 0,
 	Project = 1,
 	Dacpac = 2
+	// must be kept in-sync with SchemaCompareEndpointType in SQL Tools Service
+	// located at \src\Microsoft.SqlTools.ServiceLayer\SchemaCompare\Contracts\SchemaCompareRequest.cs
 }
 
 export interface SchemaCompareEndpointInfo {
