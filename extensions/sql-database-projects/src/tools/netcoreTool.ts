eef07871e225836706530394568bd98b168b8255
Kim Santiago <kisantia@microsoft.com>
2021-11-15 13:10:47 -0800

    fix double warning

--- extensions/sql-database-projects/src/tools/netcoreTool.ts
+++ extensions/sql-database-projects/src/tools/netcoreTool.ts
@@ -208,7 +208,8 @@ export class NetCoreTool extends ShellExecutionHelper {
 			} else if (this.netCoreInstallState === netCoreInstallState.netCoreVersionTooHigh && vscode.workspace.getConfiguration(DBProjectConfigurationKey)[NetCoreDowngradeDoNotShowAgainKey] === true) {
 				// Assume user has used global.json to override SDK version and proceed with build as is
 			} else {
-				throw new DotNetError(NetCoreSupportedVersionInstallationConfirmation(this.netCoreSdkInstalledVersion!));
+				// don't continue building if the user hasn't selected the do not show again on the warnings shown from findOrInstallNetCore()
+				return '';
 			}
 		}
 
