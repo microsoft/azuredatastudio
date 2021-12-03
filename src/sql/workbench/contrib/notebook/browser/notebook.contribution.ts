23dfeae73cd6694d464ec955d5af0aa203ff7037
luczhan <luczhan@microsoft.com>
2021-12-03 15:17:40 -0800

    await editor model resolved

--- src/sql/workbench/contrib/notebook/browser/notebook.contribution.ts
+++ src/sql/workbench/contrib/notebook/browser/notebook.contribution.ts
@@ -225,16 +225,15 @@ CommandsRegistry.registerCommand({
 			if (editor instanceof NotebookInput) {
 				// Wait for NotebookInput.resolve() to be called to ensure the _model field in NotebookInput
 				// is not undefined when trying to get the notebookModel.
-				editor.modelResolved.then(async () => {
-					let model: INotebookModel = (<NotebookInput>editor).notebookModel;
-					if (model?.providerId === JUPYTER_PROVIDER_ID) {
-						let jupyterNotebookManager: IExecuteManager = model.executeManagers.find(x => x.providerId === JUPYTER_PROVIDER_ID);
-						await jupyterNotebookManager.sessionManager.shutdownAll();
-						jupyterNotebookManager.sessionManager.dispose();
-						await jupyterNotebookManager.serverManager.stopServer();
-						return;
-					}
-				});
+				await editor.modelResolved;
+				let model: INotebookModel = (<NotebookInput>editor).notebookModel;
+				if (model?.providerId === JUPYTER_PROVIDER_ID) {
+					let jupyterNotebookManager: IExecuteManager = model.executeManagers.find(x => x.providerId === JUPYTER_PROVIDER_ID);
+					await jupyterNotebookManager.sessionManager.shutdownAll();
+					jupyterNotebookManager.sessionManager.dispose();
+					await jupyterNotebookManager.serverManager.stopServer();
+					return;
+				}
 			}
 		}
 	}
