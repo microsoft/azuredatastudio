dca8d871174b44542330605b1bc1a5a95d0ce566
barbaravaldez <bavaldez@microsoft.com>
2021-12-03 12:20:22 -0800

    address comments in bookUndoRedoService

--- extensions/notebook/src/book/bookUndoRedoService.ts
+++ extensions/notebook/src/book/bookUndoRedoService.ts
@@ -10,37 +10,36 @@ export interface IBookUndoRedoElement {
 }
 
 export class BookUndoRedoService {
-	private _undoBookChange: IBookUndoRedoElement[] = [];
-	private _redoBookChange: IBookUndoRedoElement[] = [];
+	private _undoStack: IBookUndoRedoElement[] = [];
+	private _redoStack: IBookUndoRedoElement[] = [];
 	private _undoMaxSize = 10;
 
 	constructor() {
 	}
 
 	public async undo(): Promise<void> | undefined {
-		if (this._undoBookChange.length > 0) {
-			const change = this._undoBookChange.pop();
-			this._redoBookChange.push(change);
+		if (this._undoStack.length > 0) {
+			const change = this._undoStack.pop();
+			this._redoStack.push(change);
 			return change.undo();
 		}
 		return undefined;
 	}
 
 	public async redo(): Promise<void> | undefined {
-		if (this._redoBookChange.length > 0) {
-			const change = this._redoBookChange.pop();
-			this._undoBookChange.push(change);
+		if (this._redoStack.length > 0) {
+			const change = this._redoStack.pop();
+			this._undoStack.push(change);
 			return change.redo();
 		}
 		return undefined;
 	}
 
 	public pushElement(change: IBookUndoRedoElement): void {
-		if (this._undoBookChange.length < this._undoMaxSize) {
-			this._undoBookChange.push(change);
-			this._redoBookChange = [];
-		} else {
-			this._undoBookChange.shift();
+		if (this._undoStack.length >= this._undoMaxSize) {
+			this._undoStack.shift();
 		}
+		this._undoStack.push(change);
+		this._redoStack = [];
 	}
 }
