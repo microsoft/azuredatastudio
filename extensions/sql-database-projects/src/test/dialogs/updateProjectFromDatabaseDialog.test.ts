72d9511a81cb0f731bea96205537e8edd6b2d6b6
Benjin Dubishar <benjin.dubishar@gmail.com>
2022-01-10 10:40:26 -0800

    Adding missing header

--- extensions/sql-database-projects/src/test/dialogs/updateProjectFromDatabaseDialog.test.ts
+++ extensions/sql-database-projects/src/test/dialogs/updateProjectFromDatabaseDialog.test.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as azdata from 'azdata';
 import * as should from 'should';
 import * as sinon from 'sinon';
