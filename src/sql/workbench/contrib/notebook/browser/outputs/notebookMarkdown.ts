992f5a2786327dfab2e87ef600c82d1acefd18f0
chgagnon <chgagnon@microsoft.com>
2022-01-27 11:52:24 -0800

    fixes

--- src/sql/workbench/contrib/notebook/browser/outputs/notebookMarkdown.ts
+++ src/sql/workbench/contrib/notebook/browser/outputs/notebookMarkdown.ts
@@ -17,7 +17,7 @@ import { IMarkdownStringWithCellAttachments, MarkdownRenderOptionsWithCellAttach
 import { replaceInvalidLinkPath } from 'sql/workbench/contrib/notebook/common/utils';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { useNewMarkdownRendererKey } from 'sql/workbench/contrib/notebook/common/notebookCommon';
-import { FileAccess } from 'vs/base/common/network';
+import { FileAccess, Schemas } from 'vs/base/common/network';
 
 // Based off of HtmlContentRenderer
 export class NotebookMarkdownRenderer {
@@ -103,7 +103,11 @@ export class NotebookMarkdownRenderer {
 				// first. Since the href here can be either a file path, an HTTP/S link or embedded data though we first
 				// check if it's any of the others and if so then don't need to do anything.
 				let uri = URI.parse(href);
-				if (!(uri.scheme === 'https' || uri.scheme === 'http' || uri.scheme === 'data')) {
+				if (!(uri.scheme === Schemas.https ||
+					uri.scheme === Schemas.http ||
+					uri.scheme === Schemas.data ||
+					uri.scheme === Schemas.attachment ||
+					uri.scheme === Schemas.vscodeFileResource)) {
 					uri = FileAccess.asBrowserUri(URI.file(href));
 				}
 				attributes.push(`src="${uri.toString(true)}"`);
