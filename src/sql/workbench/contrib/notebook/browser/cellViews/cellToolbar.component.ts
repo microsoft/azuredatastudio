5ced890ef1facc0dcde6478ef9d5499080b0f83b
maddydev <makoripa@microsoft.com>
2022-03-01 11:40:58 -0800

    update styles on focus_in

--- src/sql/workbench/contrib/notebook/browser/cellViews/cellToolbar.component.ts
+++ src/sql/workbench/contrib/notebook/browser/cellViews/cellToolbar.component.ts
@@ -22,7 +22,7 @@ export const CELL_TOOLBAR_SELECTOR: string = 'cell-toolbar-component';
 
 @Component({
 	selector: CELL_TOOLBAR_SELECTOR,
-	template: `<div #celltoolbar></div>`
+	template: `<div #celltoolbar [style.visibility]="cellModel.active ? 'visible' : 'hidden'"></div>`
 })
 export class CellToolbarComponent {
 	@ViewChild('celltoolbar', { read: ElementRef }) private celltoolbar: ElementRef;
