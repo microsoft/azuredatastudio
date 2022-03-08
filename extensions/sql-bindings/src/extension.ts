3c12b8de28c76ea85de8694d1cb4c5e58a770d88
Vasu Bhog <vabhog@microsoft.com>
2022-03-07 19:01:38 -0800

    add extension.ts

--- extensions/sql-bindings/src/extension.ts
+++ extensions/sql-bindings/src/extension.ts
@@ -2,9 +2,24 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import * as vscode from 'vscode';
+import { getAzdataApi } from './common/utils';
+import MainController from './controllers/mainController';
 
-export function activate(): void {
+let controllers: MainController[] = [];
+
+export function activate(context: vscode.ExtensionContext): Promise<void> {
+	void vscode.commands.executeCommand('setContext', 'azdataAvailable', !!getAzdataApi());
+	// Start the main controller
+	const mainController = new MainController(context);
+	controllers.push(mainController);
+	context.subscriptions.push(mainController);
+
+	return mainController.activate();
 }
 
 export function deactivate(): void {
+	for (let controller of controllers) {
+		controller.deactivate();
+	}
 }
