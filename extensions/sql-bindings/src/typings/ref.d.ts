e1592be7d6cecbc9c3bd3cefebb795ee99cebb7b
chgagnon <chgagnon@microsoft.com>
2022-03-17 16:36:59 -0700

    Consolidate extension typings files

--- extensions/sql-bindings/src/typings/ref.d.ts
+++ extensions/sql-bindings/src/typings/ref.d.ts
@@ -6,4 +6,7 @@
 /// <reference path='../../../../src/vs/vscode.d.ts'/>
 /// <reference path='../../../../src/sql/azdata.d.ts'/>
 /// <reference path='../../../../src/sql/azdata.proposed.d.ts'/>
+/// <reference path='../../../types/vscode-mssql.d.ts'/>
+/// <reference path='../../../types/vscode-azurefunctions.api.d.ts'/>
+/// <reference path='../../../types/vscode-azuretools.api.d.ts'/>
 /// <reference types='@types/node'/>
