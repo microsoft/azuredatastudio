747b0a46c6af0aad8f472449fce19d73ea19a60f
Kim Santiago <kisantia@microsoft.com>
2022-03-18 15:56:29 -0700

    send version for extract

--- extensions/dacpac/src/wizard/dataTierApplicationWizard.ts
+++ extensions/dacpac/src/wizard/dataTierApplicationWizard.ts
@@ -334,6 +334,7 @@ export class DataTierApplicationWizard {
 	}
 
 	private async extract(): Promise<mssql.DacFxResult> {
+		let additionalProps: TelemetryEventProperties = {};
 		let additionalMeasurements: TelemetryEventMeasures = {};
 
 		const service = await this.getService();
@@ -344,8 +345,9 @@ export class DataTierApplicationWizard {
 
 		additionalMeasurements.totalDurationMs = (new Date().getTime() - extractStartTime);
 		additionalMeasurements.extractedDacpacFileSizeBytes = await utils.tryGetFileSize(this.model.filePath);
+		additionalProps.version = this.model.version;
 
-		this.sendDacFxOperationTelemetryEvent(result, TelemetryAction.ExtractDacpac, undefined, additionalMeasurements);
+		this.sendDacFxOperationTelemetryEvent(result, TelemetryAction.ExtractDacpac, additionalProps, additionalMeasurements);
 
 		return result;
 	}
