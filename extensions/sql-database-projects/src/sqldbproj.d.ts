b65a6bd741ca88915c902bbf6b770dd3e83e3cfc
Kim Santiago <31145923+kisantia@users.noreply.github.com>
2022-03-31 15:30:30 -0700

    update DW target platform name in sql projects (#18843)
    * update DW target platform name
    
    * remove Microsoft from target platform names
    
    * fix merge conflict

--- extensions/sql-database-projects/src/sqldbproj.d.ts
+++ extensions/sql-database-projects/src/sqldbproj.d.ts
@@ -178,8 +178,8 @@ declare module 'sqldbproj' {
 		sqlServer2016 = 'SQL Server 2016',
 		sqlServer2017 = 'SQL Server 2017',
 		sqlServer2019 = 'SQL Server 2019',
-		sqlAzure = 'Microsoft Azure SQL Database',
-		sqlDW = 'Microsoft Azure SQL Data Warehouse',
-		sqlEdge = 'Microsoft Azure SQL Edge'
+		sqlAzure = 'Azure SQL Database',
+		sqlDW = 'Azure Synapse Dedicated SQL Pool',
+		sqlEdge = 'Azure SQL Edge'
 	}
 }
