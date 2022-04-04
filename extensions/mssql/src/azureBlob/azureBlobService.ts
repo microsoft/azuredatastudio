6055b7c0720eb115cdccbc29f1545c462a63b37e
chgagnon <chgagnon@microsoft.com>
2022-04-04 09:39:26 -0700

    fixes

--- extensions/mssql/src/azureBlob/azureBlobService.ts
+++ extensions/mssql/src/azureBlob/azureBlobService.ts
@@ -3,7 +3,7 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as mssql from 'mssql';
+import type * as mssql from 'mssql';
 import { SqlOpsDataClient } from 'dataprotocol-client';
 import * as contracts from '../contracts';
 
