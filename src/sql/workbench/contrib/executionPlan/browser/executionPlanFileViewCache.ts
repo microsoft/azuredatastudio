ea01937a21f6f52077ea26f5813768122d2496f1
Aasim Khan <aasimkhan30@gmail.com>
2022-04-03 22:01:10 -0700

    Making ep code modular for easy swithcing in and out

---
+++ src/sql/workbench/contrib/executionPlan/browser/executionPlanFileViewCache.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExecutionPlanFileView } from 'sql/workbench/contrib/executionPlan/browser/executionPlanFileView';
+
+export class ExecutionPlanFileViewCache {
+	private static instance: ExecutionPlanFileViewCache;
+
+	public executionPlanFileViewMap: Map<string, ExecutionPlanFileView> = new Map();
+
+	private constructor() { }
+
+	public static getInstance(): ExecutionPlanFileViewCache {
+		if (!ExecutionPlanFileViewCache.instance) {
+			ExecutionPlanFileViewCache.instance = new ExecutionPlanFileViewCache();
+		}
+
+		return ExecutionPlanFileViewCache.instance;
+	}
+}
