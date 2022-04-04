69c254aeac9378a28b6f2850e3a8bb7fb5b8c264
Vasu Bhog <vabhog@microsoft.com>
2022-04-04 15:13:00 -0700

    Add progress bar for adding sql binding (#18923)
    * add progress bar for adding sql binding

--- extensions/sql-bindings/src/services/azureFunctionsService.ts
+++ extensions/sql-bindings/src/services/azureFunctionsService.ts
@@ -193,7 +193,15 @@ export async function addSqlBinding(
 
 	const vscodeMssqlApi = await utils.getVscodeMssqlApi();
 
-	return vscodeMssqlApi.sendRequest(azureFunctionsContracts.AddSqlBindingRequest.type, params);
+	return await vscode.window.withProgress(
+		{
+			location: vscode.ProgressLocation.Notification,
+			title: constants.addSqlBinding(functionName),
+			cancellable: false
+		}, async (_progress, _token) => {
+			return vscodeMssqlApi.sendRequest(azureFunctionsContracts.AddSqlBindingRequest.type, params);
+		}
+	);
 }
 
 /**
