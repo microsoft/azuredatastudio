679fdda146c2d07d838f66d95e54632e05840efd
Vasu Bhog <vabhog@microsoft.com>
2022-04-06 20:11:56 -0700

    suppress create function prompt

--- extensions/sql-bindings/src/services/azureFunctionsService.ts
+++ extensions/sql-bindings/src/services/azureFunctionsService.ts
@@ -59,7 +59,9 @@ export async function createAzureFunction(connectionString: string, schema: stri
 				// because of an AF extension API issue, we have to get the newly created file by adding a watcher
 				// issue: https://github.com/microsoft/vscode-azurefunctions/issues/3052
 				newHostProjectFile = await azureFunctionsUtils.waitForNewHostFile();
-				await azureFunctionApi.createFunction({ language: 'C#', targetFramework: 'netcoreapp3.1' });
+				await azureFunctionApi.createFunction({
+					language: 'C#', targetFramework: 'netcoreapp3.1', suppressCreateProjectPrompt: true,
+				});
 				const timeoutForHostFile = utils.timeoutPromise(constants.timeoutProjectError);
 				hostFile = await Promise.race([newHostProjectFile.filePromise, timeoutForHostFile]);
 				if (hostFile) {
