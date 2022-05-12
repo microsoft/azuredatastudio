784d8e9e9631a5e0829a5d9e462c9e32ee474581
Aditya Bist <adbist@microsoft.com>
2022-05-12 10:49:45 -0700

    Table Designer: Move columns UI (#19154)
    * format
    
    * added buttons and initial drag plugin
    
    * initial drag and drop working
    
    * add actions and taskbar
    
    * drag and drop bugfix and other changes
    
    * fix few issues
    
    * more changes
    
    * fix all move and insertion issues
    
    * PRcomments
    
    * fit and finish comments
    
    * remove dead code
    
    * bump sts
    
    * add style for object being dragged
    
    * add plugin to copyright filter
    
    * Add to eslintrc
    
    * fix drag contrast ratio
    
    * generalize logic for cell focus
    
    * demo feedback
    
    * feedback
    
    * add action state
    
    * feedback
    
    * remove unncecessary check
    
    * add move actions to context menu
    
    * change to const
    
    * fix bug with tables and fix drop color
    
    Co-authored-by: chgagnon <chgagnon@microsoft.com>

---
+++ src/sql/workbench/browser/designer/tableActions.ts
@@ -0,0 +1,194 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Table } from 'sql/base/browser/ui/table/table';
+import { Designer } from 'sql/workbench/browser/designer/designer';
+import { DesignerEditType, DesignerPropertyPath, DesignerTableProperties, DesignerUIArea } from 'sql/workbench/browser/designer/interfaces';
+import { Action } from 'vs/base/common/actions';
+import { localize } from 'vs/nls';
+
+export interface DesignerTableActionContext {
+	table: Table<Slick.SlickData>;
+	path: DesignerPropertyPath;
+	source: DesignerUIArea;
+	selectedRow?: number;
+}
+
+export class DesignerTableAction extends Action {
+
+	protected _table: Table<Slick.SlickData>;
+
+	constructor(
+		id: string,
+		label: string,
+		icon: string,
+		protected needsRowSelection: boolean
+	) {
+		super(id, label, icon);
+	}
+
+
+	public set table(table: Table<Slick.SlickData>) {
+		this._table = table;
+	}
+
+	public updateState(row?: number) {
+		if (row === undefined) {
+			if (!this.needsRowSelection) {
+				this.enabled = true;
+			} else {
+				this.enabled = false;
+			}
+		}
+	}
+}
+
+export class AddRowAction extends DesignerTableAction {
+	public static ID = 'designer.addRowAction';
+	public static ICON = 'add-row-button new codicon';
+	public static LABEL = localize('designer.addColumnAction', 'Add New');
+
+	constructor(
+		private designer: Designer,
+		tableProperties: DesignerTableProperties,
+	) {
+		super(AddRowAction.ID, tableProperties.labelForAddNewButton || AddRowAction.LABEL, AddRowAction.ICON, false);
+		this.designer = designer;
+		this._tooltip = localize('designer.newRowButtonAriaLabel', "Add new row to '{0}' table", tableProperties.ariaLabel);
+	}
+
+	public override async run(context: DesignerTableActionContext): Promise<void> {
+		const lastIndex = context.table.getData().getItems().length;
+		return new Promise((resolve) => {
+			this.designer.handleEdit({
+				type: DesignerEditType.Add,
+				path: [...context.path, lastIndex],
+				source: context.source,
+			});
+			resolve();
+		});
+	}
+}
+
+export class MoveRowUpAction extends DesignerTableAction {
+	public static ID = 'designer.moveRowUpAction';
+	public static ICON = 'move-row-up-button arrow-up codicon';
+	public static LABEL = localize('designer.moveRowUpAction', 'Move Up');
+
+	constructor(private designer: Designer) {
+		super(MoveRowUpAction.ID, MoveRowUpAction.LABEL, MoveRowUpAction.ICON, true);
+		this.designer = designer;
+		this._tooltip = localize('designer.moveRowUpButtonAriaLabel', "Move selected row up one position");
+		this.enabled = false;
+	}
+
+	public override async run(context: DesignerTableActionContext): Promise<void> {
+		let rowIndex = context.selectedRow ?? context.table.getSelectedRows()[0];
+		if (rowIndex - 1 < 0) {
+			return;
+		}
+		return new Promise((resolve) => {
+			this.designer.handleEdit({
+				type: DesignerEditType.Move,
+				path: [...context.path, rowIndex],
+				source: context.source,
+				value: rowIndex - 1
+			});
+			resolve();
+		});
+	}
+
+	public override updateState(row?: number): void {
+		if (row === 0) {
+			this.enabled = false;
+		} else {
+			this.enabled = true;
+		}
+		super.updateState(row);
+	}
+}
+
+export class MoveRowDownAction extends DesignerTableAction {
+	public static ID = 'designer.moveRowDownAction';
+	public static ICON = 'move-row-down-button arrow-down codicon';
+	public static LABEL = localize('designer.moveRowDownAction', 'Move Down');
+
+	constructor(private designer: Designer) {
+		super(MoveRowDownAction.ID, MoveRowDownAction.LABEL, MoveRowDownAction.ICON, true);
+		this.designer = designer;
+		this._tooltip = localize('designer.moveRowDownButtonAriaLabel', "Move selected row down one position");
+		this.enabled = false;
+	}
+
+	public override async run(context: DesignerTableActionContext): Promise<void> {
+		let rowIndex = context.selectedRow ?? context.table.getSelectedRows()[0];
+		const tableData = context.table.getData().getItems();
+		if (rowIndex + 1 >= tableData.length) {
+			return;
+		}
+		return new Promise((resolve) => {
+			this.designer.handleEdit({
+				type: DesignerEditType.Move,
+				path: [...context.path, rowIndex],
+				source: context.source,
+				value: rowIndex + 1
+			});
+			resolve();
+		});
+	}
+
+	public override updateState(row?: number): void {
+		super.updateState(row);
+		if (row === this._table.getData().getLength() - 1) {
+			this.enabled = false;
+		} else {
+			this.enabled = true;
+		}
+		super.updateState(row);
+	}
+}
+
+export class InsertBeforeSelectedRowAction extends Action {
+	public static ID = 'designer.insertBeforeSelectedRow';
+	public static LABEL = localize('designer.insertBeforeSelectedRow', 'Insert before');
+
+	constructor(private designer: Designer) {
+		super(InsertBeforeSelectedRowAction.ID, InsertBeforeSelectedRowAction.LABEL, 'insertBeforeSelectedRow', true);
+		this.designer = designer;
+	}
+
+	public override async run(context: DesignerTableActionContext): Promise<void> {
+		const rowIndex = context.selectedRow;
+		return new Promise((resolve) => {
+			this.designer.handleEdit({
+				type: DesignerEditType.Add,
+				path: [...context.path, rowIndex],
+				source: context.source
+			});
+			resolve();
+		});
+	}
+}
+
+export class InsertAfterSelectedRowAction extends Action {
+	public static ID = 'designer.insertAfterSelectedColumn';
+	public static LABEL = localize('designer.insertAfterSelectedColumn', 'Insert after');
+
+	constructor(private designer: Designer) {
+		super(InsertAfterSelectedRowAction.ID, InsertAfterSelectedRowAction.LABEL, 'insertAfterSelectedColumn', true);
+	}
+
+	public override async run(context: DesignerTableActionContext): Promise<void> {
+		const rowIndex = context.selectedRow;
+		return new Promise((resolve) => {
+			this.designer.handleEdit({
+				type: DesignerEditType.Add,
+				path: [...context.path, rowIndex + 1],
+				source: context.source
+			});
+			resolve();
+		});
+	}
+}
