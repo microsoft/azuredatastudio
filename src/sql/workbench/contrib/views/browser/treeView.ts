85d9f2cf35e1aacd0f1acfb78479bba1eab02b92
Charles Gagnon <chgagnon@microsoft.com>
2022-06-01 10:31:58 -0700

    Fix null ref in tree

--- src/sql/workbench/contrib/views/browser/treeView.ts
+++ src/sql/workbench/contrib/views/browser/treeView.ts
@@ -192,7 +192,7 @@ export class TreeView extends Disposable implements ITreeView {
 					}
 					if (node instanceof Root) {
 						const oldEmpty = this._isEmpty;
-						this._isEmpty = children.length === 0;
+						this._isEmpty = !children || children.length === 0;
 						if (oldEmpty !== this._isEmpty) {
 							this._onDidChangeEmpty.fire();
 						}
