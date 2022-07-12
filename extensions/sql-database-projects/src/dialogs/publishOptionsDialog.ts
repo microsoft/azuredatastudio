c74e0b32df6d57d8aaba27fdb35183ee4f73ca81
ssreerama <ssreerama@microsoft.com>
2022-07-12 14:52:53 -0700

    Reset functionliaty is revised and added edge case consideration

--- extensions/sql-database-projects/src/dialogs/publishOptionsDialog.ts
+++ extensions/sql-database-projects/src/dialogs/publishOptionsDialog.ts
@@ -49,7 +49,7 @@ export class PublishOptionsDialog {
 		let resetButton = utils.getAzdataApi()!.window.createButton(constants.ResetButton);
 		resetButton.onClick(async () => await this.reset());
 		// If options values already modified then enable the reset button
-		resetButton.enabled = this.publish.publishOptionsModified!;
+		resetButton.enabled = this.publish.publishOptionsModified;
 		this.dialog.customButtons = [resetButton];
 
 		utils.getAzdataApi()!.window.openDialog(this.dialog);
@@ -94,7 +94,7 @@ export class PublishOptionsDialog {
 					const displayName = this.optionsTable?.data[checkboxState.row][1];
 					this.optionsModel.setOptionValue(displayName, checkboxState.checked);
 					this.optionsChanged = true;
-					// customButton[0] is the reset button, enablling it when option checkbox is changed
+					// customButton[0] is the reset button, enabling it when option checkbox is changed
 					this.dialog.customButtons[0].enabled = true;
 				}
 			}));
@@ -150,11 +150,13 @@ export class PublishOptionsDialog {
 		// Set the publish deploymentoptions with the updated table component values
 		this.publish.setDeploymentOptions(this.optionsModel.deploymentOptions);
 		this.disposeListeners();
+
 		if (this.optionsChanged) {
 			TelemetryReporter.sendActionEvent(TelemetryViews.PublishOptionsDialog, TelemetryActions.optionsChanged);
 		}
-		// When options are Reset to default and clicked Ok, seting optionsChanged flag to false, if not set the state of the option change
-		this.publish.publishOptionsModified = this.isResetOptionsClicked ? false : (this.optionsChanged || this.publish.publishOptionsModified);
+		// When options are Reset to default and clicked Ok, seting optionsChanged flag to false
+		// When options are Reset to default and options are changed and then clicked Ok, seting optionsChanged flag to the state of the option change
+		this.publish.publishOptionsModified = this.isResetOptionsClicked && !this.optionsChanged ? false : (this.optionsChanged || this.publish.publishOptionsModified);
 	}
 
 	/*
@@ -178,7 +180,10 @@ export class PublishOptionsDialog {
 		this.optionsFlexBuilder?.removeItem(this.optionsTable!);
 		this.optionsFlexBuilder?.insertItem(this.optionsTable!, 0, { CSSStyles: { 'overflow': 'scroll', 'height': '65vh', 'padding-top': '2px' } });
 		TelemetryReporter.sendActionEvent(TelemetryViews.PublishOptionsDialog, TelemetryActions.resetOptions);
+
+		// setting optionsChanged to false when reset click, if optionsChanged is true during execute, that means there is an option changed after reset
 		this.isResetOptionsClicked = true;
+		this.optionsChanged = false;
 	}
 
 	private disposeListeners(): void {
