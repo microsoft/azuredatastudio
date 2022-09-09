afad7ad8433cba6e40fd9d530b8a347ff940e2c3
Christopher Suh <chris.s.suh@gmail.com>
2022-09-09 15:16:14 -0700

    change clientId to staging env

--- extensions/azurecore/src/account-provider/providerSettings.ts
+++ extensions/azurecore/src/account-provider/providerSettings.ts
@@ -31,7 +31,7 @@ const publicAzureSettings: ProviderSettings = {
 		id: 'azure_publicCloud',
 		settings: {
 			host: 'https://login.microsoftonline.com/',
-			clientId: 'a69788c6-1d43-44ed-9ca3-b83e194da255',
+			clientId: '7e25583c-be94-4b34-a37d-8dc5ef70b504',
 			microsoftResource: {
 				id: SettingIds.marm,
 				endpoint: 'https://management.core.windows.net/',
@@ -111,7 +111,7 @@ const usGovAzureSettings: ProviderSettings = {
 		id: 'azure_usGovtCloud',
 		settings: {
 			host: 'https://login.microsoftonline.us/',
-			clientId: 'a69788c6-1d43-44ed-9ca3-b83e194da255',
+			clientId: '7e25583c-be94-4b34-a37d-8dc5ef70b504',
 			microsoftResource: {
 				id: SettingIds.marm,
 				endpoint: 'https://management.core.usgovcloudapi.net/',
@@ -175,7 +175,7 @@ const usNatAzureSettings: ProviderSettings = {
 		id: 'azure_usNatCloud',
 		settings: {
 			host: 'https://login.microsoftonline.eaglex.ic.gov/',
-			clientId: 'a69788c6-1d43-44ed-9ca3-b83e194da255',
+			clientId: '7e25583c-be94-4b34-a37d-8dc5ef70b504',
 			microsoftResource: {
 				id: SettingIds.marm,
 				endpoint: 'https://management.azure.eaglex.ic.gov/',
@@ -235,7 +235,7 @@ const germanyAzureSettings: ProviderSettings = {
 		id: 'azure_germanyCloud',
 		settings: {
 			host: 'https://login.microsoftazure.de/',
-			clientId: 'a69788c6-1d43-44ed-9ca3-b83e194da255',
+			clientId: '7e25583c-be94-4b34-a37d-8dc5ef70b504',
 			graphResource: {
 				id: SettingIds.graph,
 				endpoint: 'https://graph.cloudapi.de',
@@ -284,7 +284,7 @@ const chinaAzureSettings: ProviderSettings = {
 		id: 'azure_chinaCloud',
 		settings: {
 			host: 'https://login.partner.microsoftonline.cn/',
-			clientId: 'a69788c6-1d43-44ed-9ca3-b83e194da255',
+			clientId: '7e25583c-be94-4b34-a37d-8dc5ef70b504',
 			microsoftResource: {
 				id: SettingIds.marm,
 				endpoint: 'https://management.core.chinacloudapi.cn/',
