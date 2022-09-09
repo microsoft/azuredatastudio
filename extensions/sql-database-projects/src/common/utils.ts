a6920ca6013fef10907fb796b2220563e2534b3a
Sakshi Sharma <57200045+SakshiS-harma@users.noreply.github.com>
2022-09-09 09:12:12 -0700

    Set target platform for database project from the server metadata (#20558)
    * Set target platform for database project from the server metadata
    
    * Addressed comments

--- extensions/sql-database-projects/src/common/utils.ts
+++ extensions/sql-database-projects/src/common/utils.ts
@@ -15,7 +15,7 @@ import * as vscodeMssql from 'vscode-mssql';
 import * as fse from 'fs-extra';
 import * as which from 'which';
 import { promises as fs } from 'fs';
-import { ISqlProject } from 'sqldbproj';
+import { ISqlProject, SqlTargetPlatform } from 'sqldbproj';
 
 export interface ValidationResult {
 	errorMessage: string;
@@ -710,4 +710,25 @@ export async function fileContainsCreateTableStatement(fullPath: string, project
 	}
 
 	return containsCreateTableStatement;
+}
+
+/**
+ * Gets target platform based on the server edition/version
+ * @param connectionId server connection profile id
+ * @returns target platform for the database project
+ */
+export async function getTargetPlatformFromServerVersion(connectionId: string): Promise<SqlTargetPlatform | undefined> {
+	const serverInfo = await getAzdataApi()!.connection.getServerInfo(connectionId);
+	const isCloud = serverInfo.isCloud;
+
+	let targetPlatform;
+	if (isCloud) {
+		const engineEdition = serverInfo.engineEditionId;
+		targetPlatform = engineEdition === getAzdataApi()!.DatabaseEngineEdition.SqlDataWarehouse ? SqlTargetPlatform.sqlDW : SqlTargetPlatform.sqlAzure;
+	} else {
+		const serverMajorVersion = serverInfo.serverMajorVersion;
+		targetPlatform = serverMajorVersion ? constants.onPremServerVersionToTargetPlatform.get(serverMajorVersion) : undefined;
+	}
+
+	return targetPlatform;
 }
