19bf62032a9a3cacbd06c45d731c2d436eece215
barbaravaldez <bavaldez@microsoft.com>
2022-09-12 10:40:21 -0700

    add comment

--- extensions/notebook/src/test/book/book.test.ts
+++ extensions/notebook/src/test/book/book.test.ts
@@ -768,6 +768,7 @@ describe('BooksTreeViewTests', function () {
 					should(executeCommandSpy.calledWith('markdown.showPreview')).be.true('openMarkdown should have called markdown.showPreview');
 				});
 
+				// skip test for failing on CI runs. Tracking the fix here https://github.com/microsoft/azuredatastudio/issues/20589
 				it.skip('should call showPreviewFile on openBook when showPreview flag is set', async () => {
 					await bookTreeViewProvider.closeBook(bookTreeViewProvider.books[0].bookItems[0]);
 					let showPreviewSpy = sinon.spy(bookTreeViewProvider, 'showPreviewFile');
@@ -777,6 +778,7 @@ describe('BooksTreeViewTests', function () {
 					should(showPreviewSpy.calledOnce).be.true('Should have called showPreviewFile.');
 				});
 
+				// skip test for failing on CI runs. Tracking the fix here https://github.com/microsoft/azuredatastudio/issues/20589
 				it.skip('should add book when bookPath contains special characters on openBook', async () => {
 					let rootFolderPath2 = path.join(os.tmpdir(), `BookTestData(1)_${uuid.v4()}`);
 					let dataFolderPath2 = path.join(rootFolderPath2, '_data');
