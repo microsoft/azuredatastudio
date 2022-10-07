59744947d2985467dc2254ae429173df6550bdd1
Alan Ren <alanren@microsoft.com>
2022-10-07 10:52:09 -0700

    universal flavor

--- build/darwin/create-universal-app.js
+++ build/darwin/create-universal-app.js
@@ -17,11 +17,11 @@ async function main() {
         throw new Error('$AGENT_BUILDDIRECTORY not set');
     }
     const appName = product.nameLong + '.app';
-    const x64AppPath = path.join(buildDir, 'VSCode-darwin-x64', appName);
-    const arm64AppPath = path.join(buildDir, 'VSCode-darwin-arm64', appName);
+    const x64AppPath = path.join(buildDir, 'azuredatastudio-darwin-x64', appName); // {{SQL CARBON EDIT}} - CHANGE VSCode to azuredatastudio
+    const arm64AppPath = path.join(buildDir, 'azuredatastudio-darwin-arm64', appName); // {{SQL CARBON EDIT}} - CHANGE VSCode to azuredatastudio
     const x64AsarPath = path.join(x64AppPath, 'Contents', 'Resources', 'app', 'node_modules.asar');
     const arm64AsarPath = path.join(arm64AppPath, 'Contents', 'Resources', 'app', 'node_modules.asar');
-    const outAppPath = path.join(buildDir, `VSCode-darwin-${arch}`, appName);
+    const outAppPath = path.join(buildDir, `azuredatastudio-darwin-${arch}`, appName); // {{SQL CARBON EDIT}} - CHANGE VSCode to azuredatastudio
     const productJsonPath = path.resolve(outAppPath, 'Contents', 'Resources', 'app', 'product.json');
     const infoPlistPath = path.resolve(outAppPath, 'Contents', 'Info.plist');
     await (0, vscode_universal_bundler_1.makeUniversalApp)({
