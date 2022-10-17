6241b1c43119c6f60f3346c98a5f4a4c790deb62
Kim Santiago <kisantia@microsoft.com>
2022-10-17 12:00:29 -0700

    update vscode-mssql.d.ts to match the one in vscode-mssql

--- extensions/types/vscode-mssql.d.ts
+++ extensions/types/vscode-mssql.d.ts
@@ -118,8 +118,64 @@ declare module 'vscode-mssql' {
 		/**
 		 * Get the server info for a connection
 		 * @param connectionInfo connection info of the connection
+		 * @returns server information
 		 */
 		getServerInfo(connectionInfo: IConnectionInfo): ServerInfo
+	}
+
+	/**
+	 * Information about a SQL Server instance.
+	 */
+	export interface ServerInfo {
+		/**
+		 * The major version of the SQL Server instance.
+		 */
+		serverMajorVersion: number;
+
+		/**
+		 * The minor version of the SQL Server instance.
+		 */
+		serverMinorVersion: number;
+
+		/**
+		 * The build of the SQL Server instance.
+		 */
+		serverReleaseVersion: number;
+
+		/**
+		 * The ID of the engine edition of the SQL Server instance.
+		 */
+		engineEditionId: number;
+
+		/**
+		 * String containing the full server version text.
+		 */
+		serverVersion: string;
+
+		/**
+		 * String describing the product level of the server.
+		 */
+		serverLevel: string;
+
+		/**
+		 * The edition of the SQL Server instance.
+		 */
+		serverEdition: string;
+
+		/**
+		 * Whether the SQL Server instance is running in the cloud (Azure) or not.
+		 */
+		isCloud: boolean;
+
+		/**
+		 * The version of Azure that the SQL Server instance is running on, if applicable.
+		 */
+		azureVersion: number;
+
+		/**
+		 * The Operating System version string of the machine running the SQL Server instance.
+		 */
+		osVersion: string;
 	}
 
 		/**
@@ -168,7 +224,6 @@ declare module 'vscode-mssql' {
 		SqlManagedInstance = 8,
 		SqlOnDemand = 11
 	}
-
 
 	/**
 	 * Information about a database connection
@@ -341,61 +396,6 @@ declare module 'vscode-mssql' {
 		 * Gets or sets the connection string to use for this connection.
 		 */
 		connectionString: string | undefined;
-	}
-
-	/**
-	 * Information about a SQL Server instance.
-	 */
-	export interface ServerInfo {
-		/**
-		 * The major version of the SQL Server instance.
-		 */
-		serverMajorVersion: number;
-
-		/**
-		 * The minor version of the SQL Server instance.
-		 */
-		serverMinorVersion: number;
-
-		/**
-		 * The build of the SQL Server instance.
-		 */
-		serverReleaseVersion: number;
-
-		/**
-		 * The ID of the engine edition of the SQL Server instance.
-		 */
-		engineEditionId: number;
-
-		/**
-		 * String containing the full server version text.
-		 */
-		serverVersion: string;
-
-		/**
-		 * String describing the product level of the server.
-		 */
-		serverLevel: string;
-
-		/**
-		 * The edition of the SQL Server instance.
-		 */
-		serverEdition: string;
-
-		/**
-		 * Whether the SQL Server instance is running in the cloud (Azure) or not.
-		 */
-		isCloud: boolean;
-
-		/**
-		 * The version of Azure that the SQL Server instance is running on, if applicable.
-		 */
-		azureVersion: number;
-
-		/**
-		 * The Operating System version string of the machine running the SQL Server instance.
-		 */
-		osVersion: string;
 	}
 
 	export const enum ExtractTarget {
@@ -672,6 +672,7 @@ declare module 'vscode-mssql' {
 		ownerUri: string;
 		extractTarget?: ExtractTarget;
 		taskExecutionMode: TaskExecutionMode;
+		includePermissions?: boolean;
 	}
 
 	export interface DeployParams {
