6c2c0c4cd7b11804bec904d63796c46d7715d25a
Karl Burtram <karlb@microsoft.com>
2022-10-17 23:41:19 -0700

    Disable lint rules

--- extensions/import/src/common/constants.ts
+++ extensions/import/src/common/constants.ts
@@ -1,7 +1,8 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) Microsoft Corporation. All rights reserved.
-*  Licensed under the Source EULA. See License.txt in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as nls from 'vscode-nls';
 const localize = nls.loadMessageBundle();
 
