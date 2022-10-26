9ff8345134b8a82493fe63b2e22c85dd9e74113e
Hai Cao <hacao@microsoft.com>
2022-10-26 10:14:36 -0700

    add SQL CARBON EDIT

--- build/gulpfile.extensions.js
+++ build/gulpfile.extensions.js
@@ -226,7 +226,7 @@ const cleanExtensionsBuildTask = task.define('clean-extensions-build', util.rimr
 const compileExtensionsBuildTask = task.define('compile-extensions-build', task.series(
 	cleanExtensionsBuildTask,
 	task.define('bundle-extensions-build', () => ext.packageLocalExtensionsStream(false).pipe(gulp.dest('.build'))),
-	task.define('bundle-marketplace-extensions-build', () => { ext.packageMarketplaceExtensionsStream(false).pipe(gulp.dest('.build')) }),
+	task.define('bundle-marketplace-extensions-build', () => { ext.packageMarketplaceExtensionsStream(false).pipe(gulp.dest('.build')) }), // {{SQL CARBON EDIT}}
 ));
 
 gulp.task(compileExtensionsBuildTask);
