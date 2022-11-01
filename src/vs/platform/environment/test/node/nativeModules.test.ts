9642e58b4386eb2271b1186fbb7f6be5c676e771
barbaravaldez <bavaldez@microsoft.com>
2022-11-01 06:40:03 -0700

    revert logic

--- src/vs/platform/environment/test/node/nativeModules.test.ts
+++ src/vs/platform/environment/test/node/nativeModules.test.ts
@@ -56,11 +56,6 @@ flakySuite('Native Modules (all platforms)', () => {
 		assert.ok(typeof sqlite3.Database === 'function', testErrorMessage('@vscode/sqlite3'));
 	});
 
-	test('@vscode/ripgrep', async () => {
-		const ripgrep = await import('@vscode/ripgrep');
-		assert.ok(typeof ripgrep.rgPath === 'function', testErrorMessage('@vscode/ripgrep'));
-	});
-
 	test('vscode-encrypt', async () => {
 		try {
 			const vscodeEncrypt: Encryption = require.__$__nodeRequire('vscode-encrypt');
