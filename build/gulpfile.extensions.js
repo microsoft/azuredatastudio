bb701c4fcf3ea789476bcd0c5942708923e26988
Alex Ma <alma1@microsoft.com>
2022-11-02 09:41:30 -0700

    added fix to localization task (#21063)

--- build/gulpfile.extensions.js
+++ build/gulpfile.extensions.js
@@ -274,7 +274,7 @@ gulp.task(packageLocalizationExtensionsTask);
 const compileLocalizationExtensionsBuildTask = task.define('compile-localization-extensions-build', task.series(
 	cleanExtensionsBuildTask,
 	compileExtensionsTask,
-	task.define('bundle-marketplace-extensions-build', () => ext.packageMarketplaceExtensionsStream(false).pipe(gulp.dest('.build'))),
+	task.define('bundle-marketplace-extensions-build', () => { ext.packageMarketplaceExtensionsStream(false, product.extensionsGallery?.serviceUrl).pipe(gulp.dest('.build')) }),
 	packageLocalizationExtensionsTask,
 ));
 
