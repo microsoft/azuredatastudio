cf4933026dbb4a2c9b6887095df8f20b8059f73f
barbaravaldez <bavaldez@microsoft.com>
2022-11-04 14:38:34 -0700

    remove unused file

--- src/sql/workbench/contrib/notebook/common/constants.ts
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-import { localize } from 'vs/nls';
-
-export const promptReloadTextCells = localize('promptReloadNotebooks', "The setting for how tables are displayed in text cells has been changed. Reload will reload all text cells, which will lose all edits in progress.");
-export const reload = localize('reload', "Reload");
