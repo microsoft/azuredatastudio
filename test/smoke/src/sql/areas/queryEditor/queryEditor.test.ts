ed059aecdbb9a795660245918830d4faa20e6318
Lewis Sanchez <lewissanchez@microsoft.com>
2022-11-08 13:50:02 -0800

    Fixes import statement

--- test/smoke/src/sql/areas/queryEditor/queryEditor.test.ts
+++ test/smoke/src/sql/areas/queryEditor/queryEditor.test.ts
@@ -3,7 +3,7 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Application } from '../../../../../automation';
+import { Application } from '../../../../../automation/src';
 import * as minimist from 'minimist';
 import { afterSuite, beforeSuite } from '../../../utils';
 
