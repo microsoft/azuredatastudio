8cb813de8526c8dd6d18f61ab81e18947676c6e7
Alan Ren <alanren@microsoft.com>
2022-11-09 23:22:07 -0800

    publish script

--- build/azure-pipelines/win32/sql-publish.ps1
+++ build/azure-pipelines/win32/sql-publish.ps1
@@ -8,32 +8,40 @@ Param(
 $env:AZURE_STORAGE_ACCESS_KEY_2 = $storageKey
 $env:AZURE_DOCUMENTDB_MASTERKEY = $documentDbKey
 
-$ExeName = "AzureDataStudioSetup.exe"
-$SystemExe = "$artifactsDir\win32-x64\system-setup\$ExeName"
-$UserExe = "$artifactsDir\win32-x64\user-setup\$ExeName"
-$UserExeName = "AzureDataStudioUserSetup.exe"
-$ZipName = "azuredatastudio-win32-x64.zip"
-$Zip = "$artifactsDir\win32-x64\archive\$ZipName"
+$Flavors = "x64","arm64"
+$FlavorSuffixes = "","-arm64"
 
-$VersionJson = Get-Content -Raw -Path "$artifactsDir\version.json" | ConvertFrom-Json
-$Version = $VersionJson.version
-$Quality = $VersionJson.quality
-$CommitId = $VersionJson.commit
+For($i = 0; $i -lt $Flavors.Length; $i++)
+{
+	$Flavor = $Flavors[$i]
+	$FlavorSuffix = $FlavorSuffixes[$i]
+	$ExeName = "AzureDataStudioSetup.exe"
+	$SystemExe = "$artifactsDir\win32-$Flavor\system-setup\$ExeName"
+	$UserExe = "$artifactsDir\win32-$Flavor\user-setup\$ExeName"
+	$UserExeName = "AzureDataStudioUserSetup.exe"
+	$ZipName = "azuredatastudio-win32-$Flavor.zip"
+	$Zip = "$artifactsDir\win32-$Flavor\archive\$ZipName"
 
-$ZipUploadName = "azuredatastudio-windows-$Version"
-$SetupUploadName = "azuredatastudio-windows-setup-$Version"
-$UserUploadName = "azuredatastudio-windows-user-setup-$Version"
+	$VersionJson = Get-Content -Raw -Path "$artifactsDir\version.json" | ConvertFrom-Json
+	$Version = $VersionJson.version
+	$Quality = $VersionJson.quality
+	$CommitId = $VersionJson.commit
 
-$assetPlatform = "win32-x64"
+	$ZipUploadName = "azuredatastudio-windows$FlavorSuffix-$Version"
+	$SetupUploadName = "azuredatastudio-windows$FlavorSuffix-setup-$Version"
+	$UserUploadName = "azuredatastudio-windows$FlavorSuffix-user-setup-$Version"
 
-If (-NOT ($Quality -eq "stable")) {
-	$ZipUploadName = "$ZipUploadName-$Quality"
-	$SetupUploadName = "$SetupUploadName-$Quality"
-	$UserUploadName = "$UserUploadName-$Quality"
+	$assetPlatform = "win32-$Flavor"
+
+	If (-NOT ($Quality -eq "stable")) {
+		$ZipUploadName = "$ZipUploadName-$Quality"
+		$SetupUploadName = "$SetupUploadName-$Quality"
+		$UserUploadName = "$UserUploadName-$Quality"
+	}
+
+	node $sourcesDir/build/azure-pipelines/common/publish.js $Quality "$assetPlatform-archive" archive "$ZipUploadName.zip" $Version true $Zip $CommitId
+
+	node $sourcesDir/build/azure-pipelines/common/publish.js $Quality "$assetPlatform" setup "$SetupUploadName.exe" $Version true $SystemExe $CommitId
+
+	node $sourcesDir/build/azure-pipelines/common/publish.js $Quality "$assetPlatform-user" setup "$UserUploadName.exe" $Version true $UserExe $CommitId
 }
-
-node $sourcesDir/build/azure-pipelines/common/publish.js $Quality "$assetPlatform-archive" archive "$ZipUploadName.zip" $Version true $Zip $CommitId
-
-node $sourcesDir/build/azure-pipelines/common/publish.js $Quality "$assetPlatform" setup "$SetupUploadName.exe" $Version true $SystemExe $CommitId
-
-node $sourcesDir/build/azure-pipelines/common/publish.js $Quality "$assetPlatform-user" setup "$UserUploadName.exe" $Version true $UserExe $CommitId
