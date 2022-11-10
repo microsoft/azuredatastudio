c8a80a5270224d2dbbebfe54ab781087a7fae6dd
Aasim Khan <aasimkhan30@gmail.com>
2022-11-10 15:46:05 -0800

    Adding SQL Carbon edit comment (#21202)

--- src/vs/base/common/idGenerator.ts
+++ src/vs/base/common/idGenerator.ts
@@ -14,6 +14,11 @@ export class IdGenerator {
 	}
 
 	public nextId(): string {
+		/**
+		 * {{SQL CARBON EDIT}}
+		 * Adding suffix at the end of id to avoid the id getting picked up by faulty
+		 * string matching logic that only checks for id prefixes to find the match.
+		 */
 		return this._prefix + (++this._lastId) + '-id';
 	}
 }
