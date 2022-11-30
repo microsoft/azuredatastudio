a2d99cfc8580f3582848e8b04ac1c0b97979ada1
Cheena Malhotra <cmalhotra@microsoft.com>
2022-11-30 11:38:31 -0800

    Implement lockfile

--- extensions/azurecore/src/account-provider/utils/msalCachePlugin.ts
+++ extensions/azurecore/src/account-provider/utils/msalCachePlugin.ts
@@ -5,14 +5,23 @@
 
 import { ICachePlugin, TokenCacheContext } from "@azure/msal-node";
 import Cryptr = require('cryptr');
+import lockFile = require('lockfile');
 import { constants } from "fs";
 import { promises as fsPromises } from 'fs';
 import * as os from "os";
 import { Logger } from '../../utils/Logger';
 
 export class MsalCachePluginProvider {
-	constructor(private _msalCachePath: string) {
+	constructor(private _msalCachePath: string,
+		// the more the better - otherwise we can have incomplete tenant list.
+		// If users complain they don't see all tenants when expanding multiple nodes together,
+		// we can increase this to allow more time to fetch access tokens.
+		private _maxRetryCount: number = 100,
+		// 10 ms is a good balance between performance and not blocking the thread.
+		private _retryWait: number = 10
+	) {
 		Logger.verbose(`MsalCachePluginProvider: Using cache path ${_msalCachePath}`);
+		Logger.verbose(`MsalCachePluginProvider: Using retries ${_maxRetryCount} and retryWait ${_retryWait}`);
 	}
 
 	private getCrypto(): Cryptr {
@@ -23,37 +32,58 @@ export class MsalCachePluginProvider {
 		return this._msalCachePath;
 	}
 
+	private getLockfilePath(): string {
+		return this._msalCachePath + '.lock';
+	}
+
 	public getCachePlugin(): ICachePlugin {
 		const cachePath = this.getMsalCachePath();
+		const lockFilePath = this.getLockfilePath();
+		const lockFileOptions = {
+			retries: this._maxRetryCount,
+			retryWait: this._retryWait
+		}
 		const beforeCacheAccess = async (cacheContext: TokenCacheContext): Promise<void> => {
 			let exists = true;
-			try {
-				await fsPromises.access(cachePath, constants.R_OK | constants.W_OK);
-			} catch {
-				exists = false;
-			}
-			if (exists) {
+			lockFile.lock(lockFilePath, lockFileOptions, async (er) => {
 				try {
-					const cache = await fsPromises.readFile(cachePath, "utf-8"); // works with " " not ' '
-					cacheContext.tokenCache.deserialize(this.getCrypto().decrypt(cache));
-					Logger.verbose(`MsalCachePlugin: Token read from cache successfully.`);
-				} catch (e) {
-					Logger.error(`MsalCachePlugin: Failed to read from cache file.`);
-					throw e;
+					await fsPromises.access(cachePath, constants.R_OK | constants.W_OK);
+				} catch {
+					exists = false;
 				}
-			}
+				if (exists) {
+					try {
+						const cache = await fsPromises.readFile(cachePath, "utf-8"); // works with " " not ' '
+						cacheContext.tokenCache.deserialize(this.getCrypto().decrypt(cache));
+						Logger.verbose(`MsalCachePlugin: Token read from cache successfully.`);
+					} catch (e) {
+						Logger.error(`MsalCachePlugin: Failed to read from cache file. ${e}`);
+						throw e;
+					}
+				}
+				lockFile.unlock(lockFilePath, function (er) {
+					Logger.error(`MsalCachePlugin: Failed to read from cache file: ${er}`);
+					throw er;
+				})
+			});
 		};
 
 		const afterCacheAccess = async (cacheContext: TokenCacheContext): Promise<void> => {
 			if (cacheContext.cacheHasChanged) {
-				try {
-					const data = this.getCrypto().encrypt(cacheContext.tokenCache.serialize());
-					await fsPromises.writeFile(cachePath, data);
-					Logger.verbose(`MsalCachePlugin: Token written to cache successfully.`);
-				} catch (e) {
-					Logger.error(`MsalCachePlugin: Failed to write to cache file.`);
-					throw e;
-				}
+				lockFile.lock(lockFilePath, lockFileOptions, async (er) => {
+					try {
+						const data = this.getCrypto().encrypt(cacheContext.tokenCache.serialize());
+						await fsPromises.writeFile(cachePath, data);
+						Logger.verbose(`MsalCachePlugin: Token written to cache successfully.`);
+					} catch (e) {
+						Logger.error(`MsalCachePlugin: Failed to write to cache file. ${e}`);
+						throw e;
+					}
+				});
+				lockFile.unlock(lockFilePath, function (er) {
+					Logger.error(`MsalCachePlugin: Failed to write to cache file: ${er}`);
+					throw er;
+				});
 			}
 		};
 
