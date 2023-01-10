018d7748843e8c75ab6457eb92f78c569755e59e
Raymond Truong <ratruong@microsoft.com>
2023-01-09 17:13:45 -0800

    Refactor

--- extensions/sql-migration/src/wizard/integrationRuntimePage.ts
+++ extensions/sql-migration/src/wizard/integrationRuntimePage.ts
@@ -6,7 +6,7 @@
 import * as azdata from 'azdata';
 import * as vscode from 'vscode';
 import { MigrationWizardPage } from '../models/migrationWizardPage';
-import { MigrationMode, MigrationStateModel, NetworkContainerType, StateChangeEvent } from '../models/stateMachine';
+import { MigrationMode, MigrationStateModel, MigrationTargetType, NetworkContainerType, StateChangeEvent } from '../models/stateMachine';
 import { CreateSqlMigrationServiceDialog } from '../dialog/createSqlMigrationService/createSqlMigrationServiceDialog';
 import * as constants from '../constants/strings';
 import { WIZARD_INPUT_COMPONENT_WIDTH } from './wizardController';
@@ -38,6 +38,7 @@ export class IntergrationRuntimePage extends MigrationWizardPage {
 	private _radioButtonContainer!: azdata.FlexContainer;
 	private _networkShareButton!: azdata.RadioButtonComponent;
 	private _blobContainerButton!: azdata.RadioButtonComponent;
+	private _sqlVmPageBlobInfoBox!: azdata.TextComponent;
 	private _originalMigrationMode!: MigrationMode;
 	private _disposables: vscode.Disposable[] = [];
 
@@ -178,11 +179,26 @@ export class IntergrationRuntimePage extends MigrationWizardPage {
 				}
 			}));
 
+		this._sqlVmPageBlobInfoBox = this._view.modelBuilder.infoBox()
+			.withProps({
+				text: constants.DATABASE_BACKUP_SQL_VM_PAGE_BLOB_INFO,
+				style: 'information',
+				width: WIZARD_INPUT_COMPONENT_WIDTH,
+				CSSStyles: { ...styles.BODY_CSS, 'display': 'none' },
+				links: [
+					{
+						text: constants.DATABASE_BACKUP_SQL_VM_PAGE_BLOB_URL_LABEL,
+						url: 'https://learn.microsoft.com/azure/dms/known-issues-azure-sql-migration-azure-data-studio#azure-sql-managed-instance-and-sql-server-on-azure-virtual-machine-known-issues-and-limitations'
+					}
+				]
+			}).component();
+
 		const flexContainer = this._view.modelBuilder.flexContainer()
 			.withItems([
 				selectLocationText,
 				this._blobContainerButton,
 				this._networkShareButton,
+				this._sqlVmPageBlobInfoBox
 			])
 			.withLayout({ flexFlow: 'column' })
 			.component();
@@ -244,6 +260,24 @@ export class IntergrationRuntimePage extends MigrationWizardPage {
 		await utils.updateControlDisplay(
 			this._radioButtonContainer,
 			!isSqlDbTarget);
+
+		//
+
+		// block SQL VM file share scenario for SQL Server <= 2014
+		const sqlServerInfo = await azdata.connection.getServerInfo((await azdata.connection.getCurrentConnection()).connectionId);
+		if (this.migrationStateModel._targetType === MigrationTargetType.SQLVM && utils.isSqlServerVersion2014OrBelow(sqlServerInfo)) {
+			this._networkShareButton.enabled = false;
+			this.migrationStateModel._databaseBackup.networkContainerType = NetworkContainerType.BLOB_CONTAINER
+			this._blobContainerButton.checked = true;
+			await this._sqlVmPageBlobInfoBox.updateCssStyles({ 'display': 'block' });
+			await this._blobContainerButton.focus();
+		} else {
+			this._networkShareButton.enabled = true;
+			this._blobContainerButton.checked = false;
+			await this._sqlVmPageBlobInfoBox.updateCssStyles({ 'display': 'none' });
+		}
+
+		//
 
 		this._subscription.value = this.migrationStateModel._targetSubscription.name;
 		this._location.value = await getLocationDisplayName(
