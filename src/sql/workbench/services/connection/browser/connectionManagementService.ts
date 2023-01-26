f58081a5af3276766e2042b4d671455b18add9a7
Alex Ma <alma1@microsoft.com>
2023-01-26 15:21:44 -0800

    added check fixes

--- src/sql/workbench/services/connection/browser/connectionManagementService.ts
+++ src/sql/workbench/services/connection/browser/connectionManagementService.ts
@@ -569,7 +569,7 @@ export class ConnectionManagementService extends Disposable implements IConnecti
 				if (connectionErrorHandled.success) {
 					connectionResult.errorHandled = true;
 					//copy over altered connection options from the result
-					connection.options = options;
+					connection.options = connectionErrorHandled.options;
 					return this.connectWithOptions(connection, uri, options, callbacks);
 				}
 				else {
