8b046973197ad5a160c09e21c525d2d6c768930b
Siyang Yao <siyao@microsoft.com>
2023-01-31 12:48:03 -0800

    adding collation error code

--- extensions/sql-migration/src/constants/strings.ts
+++ extensions/sql-migration/src/constants/strings.ts
@@ -424,13 +424,14 @@ export function SQL_TARGET_CONNECTION_SOURCE_NOT_MAPPED(sourceDatabaseName: stri
 
 //`Database mapping error. Source database ({0}) collation ({1}) does not match target database ({2}) collation ({3}). Please select a target database with the same collation to the source database.`
 export function SQL_TARGET_SOURCE_COLLATION_NOT_SAME(
+	errorCode: string,
 	sourceDatabaseName: string,
 	targetDatabaseName: string,
 	sourceDatabaseCollation: string | undefined,
 	targetDatabaseCollation: string | undefined): string {
 	return localize(
 		'sql.migration.wizard.target.source.collation.error',
-		"A mapping error was found between '{0}' and '{1}' databases. The source database collation '{2}' does not match the target database collation '{3}'. Please select or re-create a target database with the same collation as the source database.",
+		"A mapping error (Error code: {0}) was found between '{1}' and '{2}' databases. The source database collation '{3}' does not match the target database collation '{4}'. Please select or re-create a target database with the same collation as the source database.",
 		sourceDatabaseName,
 		targetDatabaseName,
 		sourceDatabaseCollation,
