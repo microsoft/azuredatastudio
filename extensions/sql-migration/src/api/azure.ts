microsoft/azuredatastudio

commit fcece32cdd53821ede03879bc722874729f9eff9

[SQL-Migration] Enable login migrations to SQL VM (#21776)
This PR adds support for migrating logins to SQL VM. Adding support for 2 scenarios supported here: VMs with private IP and public IP.

extensions/sql-migration/src/api/azure.ts
@@ -10,12 +10,14 @@ import * as constants from '../constants/strings';
 import { getSessionIdHeader } from './utils';
 import { URL } from 'url';
 import { MigrationSourceAuthenticationType, MigrationStateModel, NetworkShare } from '../models/stateMachine';
+import { NetworkInterface } from './dataModels/azure/networkInterfaceModel';
 
 const ARM_MGMT_API_VERSION = '2021-04-01';
 const SQL_VM_API_VERSION = '2021-11-01-preview';
 const SQL_MI_API_VERSION = '2021-11-01-preview';
 const SQL_SQLDB_API_VERSION = '2021-11-01-preview';
 const DMSV2_API_VERSION = '2022-03-30-preview';
+const COMPUTE_VM_API_VERSION = '2022-08-01';
 
 async function getAzureCoreAPI(): Promise<azurecore.IExtension> {
 	const api = (await vscode.extensions.getExtension(azurecore.extension.name)?.activate()) as azurecore.IExtension;
@@ -168,11 +170,6 @@ export interface ServerPrivateEndpointConnection {
 	readonly id?: string;
 	readonly properties?: PrivateEndpointConnectionProperties;
 }
-
-export function isAzureSqlDatabaseServer(instance: any): instance is AzureSqlDatabaseServer {
-	return (instance as AzureSqlDatabaseServer) !== undefined;
-}
-
 export interface AzureSqlDatabaseServer {
 	id: string,
 	name: string,
@@ -197,6 +194,10 @@ export interface AzureSqlDatabaseServer {
 	},
 }
 
+export function isAzureSqlDatabaseServer(instance: any): instance is AzureSqlDatabaseServer {
+	return (instance as AzureSqlDatabaseServer) !== undefined;
+}
+
 export type SqlVMServer = {
 	properties: {
 		virtualMachineResourceId: string,
@@ -210,8 +211,13 @@ export type SqlVMServer = {
 	name: string,
 	type: string,
 	tenantId: string,
-	subscriptionId: string
+	subscriptionId: string,
+	networkInterfaces: Map<string, NetworkInterface>,
 };
+
+export function isSqlVMServer(instance: any): instance is SqlVMServer {
+	return (instance as SqlVMServer) !== undefined;
+}
 
 export type VirtualMachineInstanceView = {
 	computerName: string,
@@ -224,6 +230,7 @@ export type VirtualMachineInstanceView = {
 	hyperVGeneration: string,
 	patchStatus: { [propertyName: string]: string; },
 	statuses: InstanceViewStatus[],
+	networkProfile: any,
 }
 
 export type InstanceViewStatus = {
@@ -282,7 +289,7 @@ export async function getAvailableSqlVMs(account: azdata.Account, subscription: 
 
 export async function getVMInstanceView(sqlVm: SqlVMServer, account: azdata.Account, subscription: Subscription): Promise<VirtualMachineInstanceView> {
 	const api = await getAzureCoreAPI();
-	const path = encodeURI(`/subscriptions/${subscription.id}/resourceGroups/${getResourceGroupFromId(sqlVm.id)}/providers/Microsoft.Compute/virtualMachines/${sqlVm.name}/instanceView?api-version=2022-08-01`);
+	const path = encodeURI(`/subscriptions/${subscription.id}/resourceGroups/${getResourceGroupFromId(sqlVm.id)}/providers/Microsoft.Compute/virtualMachines/${sqlVm.name}/instanceView?api-version=${COMPUTE_VM_API_VERSION}`);
 	const host = api.getProviderMetadataForAccount(account).settings.armResource?.endpoint;
 	const response = await api.makeAzureRestRequest(account, subscription, path, azurecore.HttpRequestMethod.GET, undefined, true, host);
 
@@ -291,6 +298,24 @@ export async function getVMInstanceView(sqlVm: SqlVMServer, account: azdata.Acco
 	}
 
 	return response.response.data;
+}
+
+export async function getAzureResourceGivenId(account: azdata.Account, subscription: Subscription, id: string, apiVersion: string): Promise<any> {
+	const api = await getAzureCoreAPI();
+	const path = encodeURI(`${id}?api-version=${apiVersion}`);
+	const host = api.getProviderMetadataForAccount(account).settings.armResource?.endpoint;
+	const response = await api.makeAzureRestRequest(account, subscription, path, azurecore.HttpRequestMethod.GET, undefined, true, host);
+
+	if (response.errors.length > 0) {
+		throw new Error(response.errors.toString());
+	}
+
+	return response.response.data;
+}
+
+export async function getComputeVM(sqlVm: SqlVMServer, account: azdata.Account, subscription: Subscription): Promise<any> {
+	const path = encodeURI(`/subscriptions/${subscription.id}/resourceGroups/${getResourceGroupFromId(sqlVm.id)}/providers/Microsoft.Compute/virtualMachines/${sqlVm.name}`);
+	return getAzureResourceGivenId(account, subscription, path, COMPUTE_VM_API_VERSION);
 }
 
 export type StorageAccount = AzureProduct;

extensions/sql-migration/src/api/sqlUtils.ts
@@ -5,10 +5,11 @@
 
 import * as azdata from 'azdata';
 import { azureResource } from 'azurecore';
-import { AzureSqlDatabase, AzureSqlDatabaseServer } from './azure';
+import { AzureSqlDatabase, AzureSqlDatabaseServer, isAzureSqlDatabaseServer, isSqlManagedInstance, isSqlVMServer, SqlManagedInstance, SqlVMServer } from './azure';
 import { generateGuid } from './utils';
 import * as utils from '../api/utils';
 import { TelemetryAction, TelemetryViews, logError } from '../telemetry';
+import { NetworkInterfaceModel } from './dataModels/azure/networkInterfaceModel';
 
 const query_database_tables_sql = `
 	SELECT
@@ -166,12 +167,14 @@ function getSqlDbConnectionProfile(
 }
 
 export function getConnectionProfile(
-	serverName: string,
+	server: string | SqlManagedInstance | SqlVMServer | AzureSqlDatabaseServer,
 	azureResourceId: string,
 	userName: string,
-	password: string): azdata.IConnectionProfile {
+	password: string,
+	trustServerCert: boolean = false): azdata.IConnectionProfile {
 
 	const connectId = generateGuid();
+	const serverName = extractNameFromServer(server);
 	return {
 		serverName: serverName,
 		id: connectId,
@@ -194,12 +197,35 @@ export function getConnectionProfile(
 			connectionTimeout: 60,
 			columnEncryptionSetting: 'Enabled',
 			encrypt: true,
-			trustServerCertificate: false,
+			trustServerCertificate: trustServerCert,
 			connectRetryCount: '1',
 			connectRetryInterval: '10',
 			applicationName: 'azdata',
 		},
 	};
+}
+
+function extractNameFromServer(
+	server: string | SqlManagedInstance | SqlVMServer | AzureSqlDatabaseServer): string {
+
+	// No need to extract name if the server is a string
+	if (typeof server === 'string') {
+		return server
+	}
+
+	if (isSqlVMServer(server)) {
+		// For sqlvm, we need to use ip address from the network interface to connect to the server
+		const sqlVm = server as SqlVMServer;
+		const networkInterfaces = Array.from(sqlVm.networkInterfaces.values());
+		return NetworkInterfaceModel.getIpAddress(networkInterfaces);
+	}
+
+	// check if the target server is a managed instance or a VM
+	if (isSqlManagedInstance(server) || isAzureSqlDatabaseServer(server)) {
+		return server.properties.fullyQualifiedDomainName;
+	}
+
+	return "";
 }
 
 export async function collectSourceDatabaseTableInfo(sourceConnectionId: string, sourceDatabase: string): Promise<TableInfo[]> {
@@ -387,16 +413,17 @@ export async function collectSourceLogins(
 }
 
 export async function collectTargetLogins(
-	targetServer: AzureSqlDatabaseServer,
+	targetServer: SqlManagedInstance | SqlVMServer | AzureSqlDatabaseServer,
 	userName: string,
 	password: string,
 	includeWindowsAuth: boolean = true): Promise<string[]> {
 
 	const connectionProfile = getConnectionProfile(
-		targetServer.properties.fullyQualifiedDomainName,
+		targetServer,
 		targetServer.id,
 		userName,
-		password);
+		password,
+		true /* trustServerCertificate */);
 
 	const result = await azdata.connection.connect(connectionProfile, false, false);
 	if (result.connected && result.connectionId) {

extensions/sql-migration/src/models/stateMachine.ts
@@ -7,7 +7,7 @@ import * as azdata from 'azdata';
 import * as azurecore from 'azurecore';
 import * as vscode from 'vscode';
 import * as mssql from 'mssql';
-import { SqlMigrationService, SqlManagedInstance, startDatabaseMigration, StartDatabaseMigrationRequest, StorageAccount, SqlVMServer, getLocationDisplayName, getSqlManagedInstanceDatabases, AzureSqlDatabaseServer, isSqlManagedInstance, isAzureSqlDatabaseServer, VirtualMachineInstanceView } from '../api/azure';
+import { SqlMigrationService, SqlManagedInstance, startDatabaseMigration, StartDatabaseMigrationRequest, StorageAccount, SqlVMServer, getLocationDisplayName, getSqlManagedInstanceDatabases, AzureSqlDatabaseServer, VirtualMachineInstanceView } from '../api/azure';
 import * as constants from '../constants/strings';
 import * as nls from 'vscode-nls';
 import { v4 as uuidv4 } from 'uuid';
@@ -533,24 +533,13 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 		return await azdata.connection.getConnectionString(this._sourceConnectionId, true);
 	}
 
-	public async setTargetServerName(): Promise<void> {
-		// If target server name has already been set, we can skip this part
-		if (this._targetServerName) {
-			return;
-		}
-
-		if (isSqlManagedInstance(this._targetServerInstance) || isAzureSqlDatabaseServer(this._targetServerInstance)) {
-			this._targetServerName = this._targetServerName ?? this._targetServerInstance.properties.fullyQualifiedDomainName;
-		}
-	}
-
 	public async getTargetConnectionString(): Promise<string> {
-		await this.setTargetServerName();
 		const connectionProfile = getConnectionProfile(
-			this._targetServerName,
+			this._targetServerInstance,
 			this._targetServerInstance.id,
 			this._targetUserName,
-			this._targetPassword);
+			this._targetPassword,
+			true /* trustServerCertificate */);
 
 		const result = await azdata.connection.connect(connectionProfile, false, false);
 		if (result.connected && result.connectionId) {

extensions/sql-migration/src/wizard/loginMigrationTargetSelectionPage.ts
@@ -13,8 +13,9 @@ import * as styles from '../constants/styles';
 import { WIZARD_INPUT_COMPONENT_WIDTH } from './wizardController';
 import * as utils from '../api/utils';
 import { azureResource } from 'azurecore';
-import { AzureSqlDatabaseServer, SqlVMServer } from '../api/azure';
+import { AzureSqlDatabaseServer, getVMInstanceView, SqlVMServer } from '../api/azure';
 import { collectSourceLogins, collectTargetLogins, isSysAdmin, LoginTableInfo } from '../api/sqlUtils';
+import { NetworkInterfaceModel } from '../api/dataModels/azure/networkInterfaceModel';
 
 export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 	private _view!: azdata.ModelView;
@@ -604,7 +605,7 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 			this._testConectionButton.onDidClick(async (value) => {
 				this.wizard.message = { text: '' };
 
-				const targetDatabaseServer = this.migrationStateModel._targetServerInstance as AzureSqlDatabaseServer;
+				const targetDatabaseServer = this.migrationStateModel._targetServerInstance;
 				const userName = this.migrationStateModel._targetUserName;
 				const password = this.migrationStateModel._targetPassword;
 				const loginsOnTarget: string[] = [];
@@ -744,6 +745,31 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 							const selectedVm = this.migrationStateModel._targetSqlVirtualMachines.find(vm => vm.name === value);
 							if (selectedVm) {
 								this.migrationStateModel._targetServerInstance = utils.deepClone(selectedVm)! as SqlVMServer;
+								this.migrationStateModel._vmInstanceView = await getVMInstanceView(this.migrationStateModel._targetServerInstance, this.migrationStateModel._azureAccount, this.migrationStateModel._targetSubscription);
+								this.migrationStateModel._targetServerInstance.networkInterfaces = await NetworkInterfaceModel.getVmNetworkInterfaces(
+									this.migrationStateModel._azureAccount,
+									this.migrationStateModel._targetSubscription,
+									this.migrationStateModel._targetServerInstance);
+
+								this.wizard.message = { text: '' };
+
+								// validate power state from VM instance view
+								const runningState = 'PowerState/running'.toLowerCase();
+								if (!this.migrationStateModel._vmInstanceView.statuses.some(status => status.code.toLowerCase() === runningState)) {
+									this.wizard.message = {
+										text: constants.VM_NOT_READY_POWER_STATE_ERROR(this.migrationStateModel._targetServerInstance.name),
+										level: azdata.window.MessageLevel.Warning
+									};
+								}
+
+								// validate IaaS extension mode
+								const fullMode = 'Full'.toLowerCase();
+								if (this.migrationStateModel._targetServerInstance.properties.sqlManagement.toLowerCase() !== fullMode) {
+									this.wizard.message = {
+										text: constants.VM_NOT_READY_IAAS_EXTENSION_ERROR(this.migrationStateModel._targetServerInstance.name, this.migrationStateModel._targetServerInstance.properties.sqlManagement),
+										level: azdata.window.MessageLevel.Warning
+									};
+								}
 							}
 							break;
 						case MigrationTargetType.SQLMI:
@@ -963,6 +989,15 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 						this.migrationStateModel._location,
 						this.migrationStateModel._resourceGroup?.name,
 						constants.NO_VIRTUAL_MACHINE_FOUND);
+
+					let response = await utils.getVirtualMachinesDropdownValues(
+						this.migrationStateModel._targetSqlVirtualMachines,
+						this.migrationStateModel._location,
+						this.migrationStateModel._resourceGroup,
+						this.migrationStateModel._azureAccount,
+						this.migrationStateModel._targetSubscription);
+					console.log(response);
+
 					break;
 				case MigrationTargetType.SQLDB:
 					this._azureResourceDropdown.values = utils.getAzureResourceDropdownValues(

commit 9f5f49e956f57c3ae71531fb2f77075338e873c0

Fix connecting to MI login bug (#21821)
This PR fixes a regression for migration login for MI instances that was introduced in https://github.com/microsoft/azuredatastudio/pull/21776/files#diff-93c1a62583fa32d99f775b71ad27922cf31f660d10717ecc6966784306de1b6f.

After that change, support for MI would fail as MI server types were going into the Sql VM path in sqlutils because the underlying logic for isSqlServerVM() was returning wrong results.

The new approach uses the targetType set in StateMachine to extract the correct serverName for connection string based on the targetType.

Testing:
- Tested SQL VM login migration end to end
- Tested SQL MI login migration end to end


This change also bumps the sql-migration version to 1.3.0

extensions/sql-migration/src/api/azure.ts
@@ -194,10 +194,6 @@ export interface AzureSqlDatabaseServer {
 	},
 }
 
-export function isAzureSqlDatabaseServer(instance: any): instance is AzureSqlDatabaseServer {
-	return (instance as AzureSqlDatabaseServer) !== undefined;
-}
-
 export type SqlVMServer = {
 	properties: {
 		virtualMachineResourceId: string,
@@ -214,10 +210,6 @@ export type SqlVMServer = {
 	subscriptionId: string,
 	networkInterfaces: Map<string, NetworkInterface>,
 };
-
-export function isSqlVMServer(instance: any): instance is SqlVMServer {
-	return (instance as SqlVMServer) !== undefined;
-}
 
 export type VirtualMachineInstanceView = {
 	computerName: string,

extensions/sql-migration/src/api/sqlUtils.ts
@@ -5,11 +5,10 @@
 
 import * as azdata from 'azdata';
 import { azureResource } from 'azurecore';
-import { AzureSqlDatabase, AzureSqlDatabaseServer, isAzureSqlDatabaseServer, isSqlManagedInstance, isSqlVMServer, SqlManagedInstance, SqlVMServer } from './azure';
+import { AzureSqlDatabase, AzureSqlDatabaseServer } from './azure';
 import { generateGuid } from './utils';
 import * as utils from '../api/utils';
 import { TelemetryAction, TelemetryViews, logError } from '../telemetry';
-import { NetworkInterfaceModel } from './dataModels/azure/networkInterfaceModel';
 
 const query_database_tables_sql = `
 	SELECT
@@ -167,14 +166,13 @@ function getSqlDbConnectionProfile(
 }
 
 export function getConnectionProfile(
-	server: string | SqlManagedInstance | SqlVMServer | AzureSqlDatabaseServer,
+	serverName: string,
 	azureResourceId: string,
 	userName: string,
 	password: string,
 	trustServerCert: boolean = false): azdata.IConnectionProfile {
 
 	const connectId = generateGuid();
-	const serverName = extractNameFromServer(server);
 	return {
 		serverName: serverName,
 		id: connectId,
@@ -203,29 +201,6 @@ export function getConnectionProfile(
 			applicationName: 'azdata',
 		},
 	};
-}
-
-function extractNameFromServer(
-	server: string | SqlManagedInstance | SqlVMServer | AzureSqlDatabaseServer): string {
-
-	// No need to extract name if the server is a string
-	if (typeof server === 'string') {
-		return server
-	}
-
-	if (isSqlVMServer(server)) {
-		// For sqlvm, we need to use ip address from the network interface to connect to the server
-		const sqlVm = server as SqlVMServer;
-		const networkInterfaces = Array.from(sqlVm.networkInterfaces.values());
-		return NetworkInterfaceModel.getIpAddress(networkInterfaces);
-	}
-
-	// check if the target server is a managed instance or a VM
-	if (isSqlManagedInstance(server) || isAzureSqlDatabaseServer(server)) {
-		return server.properties.fullyQualifiedDomainName;
-	}
-
-	return "";
 }
 
 export async function collectSourceDatabaseTableInfo(sourceConnectionId: string, sourceDatabase: string): Promise<TableInfo[]> {
@@ -413,14 +388,15 @@ export async function collectSourceLogins(
 }
 
 export async function collectTargetLogins(
-	targetServer: SqlManagedInstance | SqlVMServer | AzureSqlDatabaseServer,
+	serverName: string,
+	azureResourceId: string,
 	userName: string,
 	password: string,
 	includeWindowsAuth: boolean = true): Promise<string[]> {
 
 	const connectionProfile = getConnectionProfile(
-		targetServer,
-		targetServer.id,
+		serverName,
+		azureResourceId,
 		userName,
 		password,
 		true /* trustServerCertificate */);

extensions/sql-migration/src/models/stateMachine.ts
@@ -17,6 +17,7 @@ import { SKURecommendationPage } from '../wizard/skuRecommendationPage';
 import { excludeDatabases, getConnectionProfile, LoginTableInfo, SourceDatabaseInfo, TargetDatabaseInfo } from '../api/sqlUtils';
 import { LoginMigrationModel, LoginMigrationStep } from './loginMigrationModel';
 import { TdeMigrationDbResult, TdeMigrationModel } from './tdeModels';
+import { NetworkInterfaceModel } from '../api/dataModels/azure/networkInterfaceModel';
 const localize = nls.loadMessageBundle();
 
 export enum ValidateIrState {
@@ -528,6 +529,30 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 		return this._skuRecommendationResults;
 	}
 
+	public setTargetServerName(): void {
+		switch (this._targetType) {
+			case MigrationTargetType.SQLMI:
+				const sqlMi = this._targetServerInstance as SqlManagedInstance;
+				this._targetServerName = sqlMi.properties.fullyQualifiedDomainName;
+			case MigrationTargetType.SQLDB:
+				const sqlDb = this._targetServerInstance as AzureSqlDatabaseServer;
+				this._targetServerName = sqlDb.properties.fullyQualifiedDomainName;
+			case MigrationTargetType.SQLVM:
+				// For sqlvm, we need to use ip address from the network interface to connect to the server
+				const sqlVm = this._targetServerInstance as SqlVMServer;
+				const networkInterfaces = Array.from(sqlVm.networkInterfaces.values());
+				this._targetServerName = NetworkInterfaceModel.getIpAddress(networkInterfaces);
+		}
+	}
+
+	public get targetServerName(): string {
+		// If the target server name is not already set, return it
+		if (!this._targetServerName) {
+			this.setTargetServerName();
+		}
+
+		return this._targetServerName;
+	}
 
 	public async getSourceConnectionString(): Promise<string> {
 		return await azdata.connection.getConnectionString(this._sourceConnectionId, true);
@@ -535,7 +560,7 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 
 	public async getTargetConnectionString(): Promise<string> {
 		const connectionProfile = getConnectionProfile(
-			this._targetServerInstance,
+			this.targetServerName,
 			this._targetServerInstance.id,
 			this._targetUserName,
 			this._targetPassword,

extensions/sql-migration/src/wizard/loginMigrationTargetSelectionPage.ts
@@ -152,9 +152,6 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 				break;
 		}
 
-		const isSqlDbTarget = this.migrationStateModel._targetType === MigrationTargetType.SQLDB;
-		console.log(isSqlDbTarget);
-
 		if (this._targetUserNameInputBox) {
 			await this._targetUserNameInputBox.updateProperty('required', true);
 		}
@@ -174,6 +171,7 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 			this._targetPasswordInputBox.value = '';
 			this.migrationStateModel._sqlMigrationServices = undefined!;
 			this.migrationStateModel._targetServerInstance = undefined!;
+			this.migrationStateModel._targetServerName = undefined!;
 			this.migrationStateModel._resourceGroup = undefined!;
 			this.migrationStateModel._location = undefined!;
 			await this.populateLocationDropdown();
@@ -337,7 +335,6 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 				this.migrationStateModel.isWindowsAuthMigrationSupported));
 			this.migrationStateModel._loginMigrationModel.collectedSourceLogins = true;
 			this.migrationStateModel._loginMigrationModel.loginsOnSource = sourceLogins;
-			console.log(this.migrationStateModel._targetType);
 		}));
 
 		const flexContainer = this._view.modelBuilder.flexContainer()
@@ -616,7 +613,8 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 						this.wizard.nextButton.enabled = false;
 						loginsOnTarget.push(
 							...await collectTargetLogins(
-								targetDatabaseServer,
+								this.migrationStateModel.targetServerName,
+								targetDatabaseServer.id,
 								userName,
 								password,
 								this.migrationStateModel.isWindowsAuthMigrationSupported));
@@ -750,6 +748,7 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 									this.migrationStateModel._azureAccount,
 									this.migrationStateModel._targetSubscription,
 									this.migrationStateModel._targetServerInstance);
+								this.migrationStateModel.setTargetServerName();
 
 								this.wizard.message = { text: '' };
 
@@ -779,6 +778,7 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 
 							if (selectedMi) {
 								this.migrationStateModel._targetServerInstance = utils.deepClone(selectedMi)! as azureResource.AzureSqlManagedInstance;
+								this.migrationStateModel.setTargetServerName();
 
 								this.wizard.message = { text: '' };
 								if (this.migrationStateModel._targetServerInstance.properties.state !== 'Ready') {
@@ -797,6 +797,7 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 
 							if (sqlDatabaseServer) {
 								this.migrationStateModel._targetServerInstance = utils.deepClone(sqlDatabaseServer)! as AzureSqlDatabaseServer;
+								this.migrationStateModel.setTargetServerName();
 								this.wizard.message = { text: '' };
 								if (this.migrationStateModel._targetServerInstance.properties.state === 'Ready') {
 									this._targetUserNameInputBox.value = this.migrationStateModel._targetServerInstance.properties.administratorLogin;
@@ -813,6 +814,7 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 					}
 				} else {
 					this.migrationStateModel._targetServerInstance = undefined!;
+					this.migrationStateModel._targetServerName = undefined!;
 					if (isSqlDbTarget) {
 						this._targetUserNameInputBox.value = '';
 					}
@@ -989,14 +991,6 @@ export class LoginMigrationTargetSelectionPage extends MigrationWizardPage {
 						this.migrationStateModel._location,
 						this.migrationStateModel._resourceGroup?.name,
 						constants.NO_VIRTUAL_MACHINE_FOUND);
-
-					let response = await utils.getVirtualMachinesDropdownValues(
-						this.migrationStateModel._targetSqlVirtualMachines,
-						this.migrationStateModel._location,
-						this.migrationStateModel._resourceGroup,
-						this.migrationStateModel._azureAccount,
-						this.migrationStateModel._targetSubscription);
-					console.log(response);
 
 					break;
 				case MigrationTargetType.SQLDB:

extensions/sql-migration/src/wizard/loginSelectorPage.ts
@@ -11,7 +11,6 @@ import * as constants from '../constants/strings';
 import { debounce, getLoginStatusImage, getLoginStatusMessage } from '../api/utils';
 import * as styles from '../constants/styles';
 import { collectSourceLogins, collectTargetLogins, LoginTableInfo } from '../api/sqlUtils';
-import { AzureSqlDatabaseServer } from '../api/azure';
 import { IconPathHelper } from '../constants/iconPathHelper';
 import * as utils from '../api/utils';
 import { LoginType } from '../models/loginMigrationModel';
@@ -377,7 +376,8 @@ export class LoginSelectorPage extends MigrationWizardPage {
 		try {
 			if (this.isTargetInstanceSet()) {
 				targetLogins.push(...await collectTargetLogins(
-					stateMachine._targetServerInstance as AzureSqlDatabaseServer,
+					stateMachine.targetServerName,
+					stateMachine._targetServerInstance.id,
 					stateMachine._targetUserName,
 					stateMachine._targetPassword,
 					stateMachine.isWindowsAuthMigrationSupported));