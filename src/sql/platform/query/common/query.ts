ae60309c8ea494ddfe5329b9866f777b87f1dd80
Alan Ren <alanren@microsoft.com>
2023-02-10 15:55:31 -0800

    handle json

--- src/sql/platform/query/common/query.ts
+++ src/sql/platform/query/common/query.ts
@@ -6,40 +6,52 @@
 export interface IQueryEditorConfiguration {
 	readonly results: {
 		readonly saveAsCsv: {
-			readonly includeHeaders: boolean,
-			readonly delimiter: string,
-			readonly lineSeperator: string,
-			readonly textIdentifier: string,
-			readonly encoding: string
-		},
+			readonly includeHeaders: boolean;
+			readonly delimiter: string;
+			readonly lineSeperator: string;
+			readonly textIdentifier: string;
+			readonly encoding: string;
+		};
 		readonly saveAsExcel: {
-			readonly includeHeaders: boolean,
-		},
+			readonly includeHeaders: boolean;
+		};
 		readonly saveAsMarkdown: {
-			readonly encoding: string,
-			readonly includeHeaders: boolean,
-			readonly lineSeparator: string,
-		}
+			readonly encoding: string;
+			readonly includeHeaders: boolean;
+			readonly lineSeparator: string;
+		};
 		readonly saveAsXml: {
-			readonly formatted: boolean,
-			readonly encoding: string
-		},
-		readonly streaming: boolean,
-		readonly copyIncludeHeaders: boolean,
-		readonly copyRemoveNewLine: boolean,
-		readonly optimizedTable: boolean,
-		readonly inMemoryDataProcessingThreshold: number,
-		readonly openAfterSave: boolean
+			readonly formatted: boolean;
+			readonly encoding: string;
+		};
+		readonly streaming: boolean;
+		readonly copyIncludeHeaders: boolean;
+		readonly copyRemoveNewLine: boolean;
+		readonly optimizedTable: boolean;
+		readonly inMemoryDataProcessingThreshold: number;
+		readonly openAfterSave: boolean;
 		readonly showActionBar: boolean;
 	},
 	readonly messages: {
-		readonly showBatchTime: boolean,
-		readonly wordwrap: boolean
-	},
+		readonly showBatchTime: boolean;
+		readonly wordwrap: boolean;
+	};
 	readonly chart: {
 		readonly defaultChartType: 'bar' | 'doughnut' | 'horizontalBar' | 'line' | 'pie' | 'scatter' | 'timeSeries',
-	},
-	readonly tabColorMode: 'off' | 'border' | 'fill',
+	};
+	readonly tabColorMode: 'off' | 'border' | 'fill';
 	readonly showConnectionInfoInTitle: boolean;
 	readonly promptToSaveGeneratedFiles: boolean;
+}
+
+export interface IResultGridConfiguration {
+	fontFamily: string;
+	fontWeight: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
+	fontSize: number;
+	letterSpacing: number;
+	rowHeight: number;
+	cellPadding: number | number[];
+	autoSizeColumns: boolean;
+	maxColumnWidth: number;
+	showJsonAsLink: boolean;
 }
