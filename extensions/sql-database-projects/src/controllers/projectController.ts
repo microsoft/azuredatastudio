fd44868b7a3bf70f00fa66e65983c41a8fef499f
Kim Santiago <kisantia@microsoft.com>
2023-02-23 11:38:17 -0800

    delete sqlcmd variable working

--- extensions/sql-database-projects/src/controllers/projectController.ts
+++ extensions/sql-database-projects/src/controllers/projectController.ts
@@ -926,6 +926,8 @@ export class ProjectsController {
 
 		// TODO: update after swap
 		await project.addSqlCmdVariable(variableName, defaultValue);
+		// const sqlProjectsService = await utils.getSqlProjectsService();
+		// sqlProjectsService.addSqlCmdVariable(project.projectFilePath, variableName, defaultValue);
 
 		this.refreshProjectsTree(context);
 	}
