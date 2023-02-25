f804bd4c1d30f083a56c2912cd8ae80b013a8b5a
Akshay Mata <akma@microsoft.com>
2023-02-25 10:23:17 -0800

    Fix log lock crash

--- extensions/sql-migration/src/main.ts
+++ extensions/sql-migration/src/main.ts
@@ -11,6 +11,7 @@ import { migrationServiceProvider } from './service/provider';
 import { TelemetryReporter } from './telemetry';
 
 let widget: DashboardWidget;
+let migrationServiceClient: SqlOpsDataClient | undefined;
 export async function activate(context: vscode.ExtensionContext): Promise<DashboardWidget> {
 	if (!migrationServiceProvider) {
 		await vscode.window.showErrorMessage(constants.serviceProviderInitializationError);
@@ -18,8 +19,9 @@ export async function activate(context: vscode.ExtensionContext): Promise<Dashbo
 	// asynchronously starting the service
 	const outputChannel = vscode.window.createOutputChannel(constants.serviceName);
 	const serviceClient = new ServiceClient(outputChannel);
-	serviceClient.startService(context).catch((e) => {
+	migrationServiceClient = await serviceClient.startService(context).catch((e) => {
 		console.error(e);
+		return undefined;
 	});
 
 	widget = new DashboardWidget(context);
@@ -28,5 +30,8 @@ export async function activate(context: vscode.ExtensionContext): Promise<Dashbo
 	return widget;
 }
 
-export function deactivate(): void {
+export async function deactivate(): Promise<void> {
+	if (migrationServiceClient) {
+		await migrationServiceClient.stop();
+	}
 }
