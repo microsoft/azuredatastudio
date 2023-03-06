293b3bae281e2b242bdd81ea855f770c544e46a1
Aasim Khan <aaskhan@microsoft.com>
2023-03-05 22:04:50 -0800

    Adding new server group colors

--- src/sql/workbench/contrib/objectExplorer/common/serverGroup.contribution.ts
+++ src/sql/workbench/contrib/objectExplorer/common/serverGroup.contribution.ts
@@ -23,13 +23,13 @@ const serverGroupConfig: IConfigurationNode = {
 			items: 'string',
 			'description': localize('serverGroup.colors', "Server Group color palette used in the Object Explorer viewlet."),
 			default: [
-				'#A1634D',
-				'#7F0000',
-				'#914576',
-				'#6E9B59',
-				'#5F82A5',
-				'#4452A6',
-				'#6A6599',
+				'#D13438',
+				'#DB7500',
+				'#038387',
+				'#0078d4',
+				'#004e8c',
+				'#8764b8',
+				'#69797e',
 				DefaultServerGroupColor
 			]
 		},
