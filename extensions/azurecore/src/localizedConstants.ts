d9ba75a1a5ffc780b91374763137f5a5a811d4f6
Cheena Malhotra <13396919+cheenamalhotra@users.noreply.github.com>
2023-03-16 18:48:08 -0700

    MSAL cache encryption + log improvements (#22335) (#22354)
    # Conflicts:
    #	extensions/mssql/config.json

--- extensions/azurecore/src/localizedConstants.ts
+++ extensions/azurecore/src/localizedConstants.ts
@@ -10,6 +10,7 @@ export const extensionName = localize('azurecore.extensionName', "Azure Accounts
 
 export const requiresReload = localize('azurecore.requiresReload', "Modifying this setting requires reloading the window for all changes to take effect.");
 export const reload = localize('azurecore.reload', "Reload");
+export const cancel = localize('azurecore.reload', "Cancel");
 
 export const australiaCentral = localize('azurecore.australiacentral', "Australia Central");
 export const australiaCentral2 = localize('azurecore.australiacentral2', "Australia Central 2");
@@ -84,3 +85,6 @@ export const invalidTenant = localize('azurecore.invalidTenant', "Invalid tenant
 export function unableToFetchTokenError(tenant: string): string {
 	return localize('azurecore.unableToFetchToken', "Unable to get token for tenant {0}", tenant);
 }
+
+// Error Messages
+export const azureCredStoreSaveFailedError = localize('azure.credStoreSaveFailedError', `Keys for token cache could not be saved in credential store, this may cause Azure access token persistence issues and connection instabilities. It's likely that SqlTools has reached credential storage limit on Windows, please clear at least 2 credentials that start with "Microsoft.SqlTools|" in Windows Credential Manager and reload.`);
