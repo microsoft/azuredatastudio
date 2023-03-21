fdbf8e5d55cebdc267abcf7970d854148d300818
Aasim Khan <aaskhan@microsoft.com>
2023-03-20 23:57:33 -0700

    Adding constant for OE timeout

--- src/sql/workbench/contrib/objectExplorer/common/serverGroup.contribution.ts
+++ src/sql/workbench/contrib/objectExplorer/common/serverGroup.contribution.ts
@@ -41,6 +41,7 @@ const serverGroupConfig: IConfigurationNode = {
 	}
 };
 
+export const NODE_EXPANSION_CONFIG = 'serverTree.nodeExpansionTimeout';
 const serverTreeConfig: IConfigurationNode = {
 	'id': 'serverTree',
 	'title': 'Server Tree',
@@ -54,7 +55,7 @@ const serverTreeConfig: IConfigurationNode = {
 		'serverTree.nodeExpansionTimeout': {
 			'type': 'number',
 			'default': '45',
-			'description': localize('serverTree.nodeExpansionTimeout', "The timeout in seconds for expanding a node in the Servers view"),
+			'description': localize(NODE_EXPANSION_CONFIG, "The timeout in seconds for expanding a node in the Servers view"),
 			'minimum': 1
 		}
 	}
