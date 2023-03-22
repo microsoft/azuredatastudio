41481804657dc1c7d078fa883b81bec7e85df54e
Raymond Truong <ratruong@microsoft.com>
2023-03-22 12:07:42 -0700

    Undo azurecore changes

--- extensions/azurecore/src/azurecore.d.ts
+++ extensions/azurecore/src/azurecore.d.ts
@@ -445,16 +445,6 @@ declare module 'azurecore' {
 			},
 		}
 
-		// export interface VirtualNetworkRule {
-		// 	id: string,
-		// 	state: string,
-		// 	action: string
-		// }
-
-		// export interface NetworkRuleSet {
-		// 	virtualNetworkRules: VirtualNetworkRule[]
-		// }
-
 		export interface AzureSqlManagedInstance extends AzureGraphResource {
 			sku: {
 				capacity: number;
@@ -480,8 +470,6 @@ declare module 'azurecore' {
 				proxyOverride: string,
 				vCores: number,
 				dnsZone: string,
-				// allowBlobPublicAccess: string,
-				// networkAcls: NetworkRuleSet,
 			}
 
 		}
