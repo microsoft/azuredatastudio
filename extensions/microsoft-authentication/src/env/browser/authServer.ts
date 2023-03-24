1ed88d12afc0080b450aec2e791dd63d51a6e191
Cory Rivera <corivera@microsoft.com>
2023-03-24 11:02:09 -0700

    Restore vscode extension files.

---
+++ extensions/microsoft-authentication/src/env/browser/authServer.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export function startServer(_: any): any {
+	throw new Error('Not implemented');
+}
+
+export function createServer(_: any): any {
+	throw new Error('Not implemented');
+}
