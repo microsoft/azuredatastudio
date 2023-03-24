738987c4d50ac581baf166e0459300f16bbf3b59
Christopher Suh <chsuh@microsoft.com>
2023-03-24 14:42:45 -0700

    Show message indicating that ADAL is being deprecated (#22432)
    * show message indicating that ADAL is being deprecated
    
    * pr comments update
    
    * added settings deprecation message
    
    * fix deprecation message

--- extensions/azurecore/src/localizedConstants.ts
+++ extensions/azurecore/src/localizedConstants.ts
@@ -66,6 +66,8 @@ export const typeIcon = localize('azurecore.typeIcon', "Type Icon");
 export const reloadPrompt = localize('azurecore.reloadPrompt', "Authentication Library has changed, please reload Azure Data Studio.");
 export const reloadChoice = localize('azurecore.reloadChoice', "Reload Azure Data Studio");
 
+export const deprecatedOption = localize('azurecore.deprecated', "Warning: ADAL has been deprecated, and is scheduled to be removed in a future release. Please use MSAL instead.")
+
 // Azure Resource Types
 export const sqlServer = localize('azurecore.sqlServer', "SQL server");
 export const sqlDatabase = localize('azurecore.sqlDatabase', "SQL database");
