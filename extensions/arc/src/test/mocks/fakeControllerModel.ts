acc5346fb043dd10e7c0b73a7028452169598332
Charles Gagnon <chgagnon@microsoft.com>
2023-03-27 11:08:24 -0700

    Bring back arc/azcli test files (#22471)

---
+++ extensions/arc/src/test/mocks/fakeControllerModel.ts
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// import { ControllerInfo } from 'arc';
+// import { v4 as uuid } from 'uuid';
+// import { ControllerModel } from '../../models/controllerModel';
+// import { AzureArcTreeDataProvider } from '../../ui/tree/azureArcTreeDataProvider';
+
+// export class FakeControllerModel extends ControllerModel {
+
+// 	constructor(treeDataProvider?: AzureArcTreeDataProvider, info?: Partial<ControllerInfo>) {
+// 		const _info: ControllerInfo = Object.assign({ id: uuid(), endpoint: '', kubeConfigFilePath: '', kubeClusterContext: '', name: '', namespace: '', username: '', rememberPassword: false, resources: [], resourceGroup: '', connectionMode: '', location: '', customLocation: '' }, info);
+// 		super(treeDataProvider!, _info);
+// 	}
+
+// }
