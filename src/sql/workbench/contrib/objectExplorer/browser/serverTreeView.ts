03c5bf06219214b0547f2abffda61758e4609015
Aasim Khan <aaskhan@microsoft.com>
2023-03-30 11:38:13 -0700

    Adding comment

--- src/sql/workbench/contrib/objectExplorer/browser/serverTreeView.ts
+++ src/sql/workbench/contrib/objectExplorer/browser/serverTreeView.ts
@@ -472,6 +472,8 @@ export class ServerTreeView extends Disposable implements IServerTreeView {
 				await this._connectionManagementService.deleteConnection(profile);
 			}
 			const connectionProfile = this.getConnectionInTreeInput(profile.id);
+
+			// For the connection profile, we need to clear the password from the last session if the user doesn't want to save it
 			if (!connectionProfile.savePassword) {
 				connectionProfile.password = '';
 			}
