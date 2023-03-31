49320a407a13265d1e812df9e0d25bf8f9999726
Alex Ma <alma1@microsoft.com>
2023-03-31 14:13:53 -0700

    fix whitespace indentation

--- extensions/mssql/src/tableDesigner/tableDesigner.ts
+++ extensions/mssql/src/tableDesigner/tableDesigner.ts
@@ -26,13 +26,13 @@ export function registerTableDesignerCommands(appContext: AppContext) {
 			if (!connectionString) {
 				throw new Error(FailedToGetConnectionStringError);
 			}
-      let titleString = `${context.connectionProfile!.serverName} - ${context.connectionProfile!.databaseName} - ${NewTableText}`;
-		  // append non default options to end to let users know exact connection.
-		  let nonDefaultOptions = await azdata.connection.getEditorConnectionProfileTitle(context.connectionProfile, true);
-		  nonDefaultOptions = nonDefaultOptions.replace('(', '[').replace(')', ']');
-      if (nonDefaultOptions !== '') {
-			  titleString += `${nonDefaultOptions}`;
-		  }
+			let titleString = `${context.connectionProfile!.serverName} - ${context.connectionProfile!.databaseName} - ${NewTableText}`;
+			// append non default options to end to let users know exact connection.
+			let nonDefaultOptions = await azdata.connection.getEditorConnectionProfileTitle(context.connectionProfile, true);
+			nonDefaultOptions = nonDefaultOptions.replace('(', '[').replace(')', ']');
+			if (nonDefaultOptions !== '') {
+				titleString += `${nonDefaultOptions}`;
+			}
 			const tableIcon = context.nodeInfo!.nodeSubType as azdata.designers.TableIcon;
 			const telemetryInfo = await getTelemetryInfo(context, tableIcon);
 			await azdata.designers.openTableDesigner(sqlProviderName, {
@@ -63,13 +63,13 @@ export function registerTableDesignerCommands(appContext: AppContext) {
 			if (!connectionString) {
 				throw new Error(FailedToGetConnectionStringError);
 			}
-      let titleString = `${server} - ${database} - ${schema}.${name}`;
-      // append non default options to end to let users know exact connection.
-		  let nonDefaultOptions = await azdata.connection.getEditorConnectionProfileTitle(context.connectionProfile, true);
-		  nonDefaultOptions = nonDefaultOptions.replace('(', '[').replace(')', ']');
-		  if (nonDefaultOptions !== '') {
-			  titleString += `${nonDefaultOptions}`;
-		  }
+			let titleString = `${server} - ${database} - ${schema}.${name}`;
+			// append non default options to end to let users know exact connection.
+			let nonDefaultOptions = await azdata.connection.getEditorConnectionProfileTitle(context.connectionProfile, true);
+			nonDefaultOptions = nonDefaultOptions.replace('(', '[').replace(')', ']');
+			if (nonDefaultOptions !== '') {
+				titleString += `${nonDefaultOptions}`;
+			}
 			const tableIcon = context.nodeInfo!.nodeSubType as azdata.designers.TableIcon;
 			const telemetryInfo = await getTelemetryInfo(context, tableIcon);
 			await azdata.designers.openTableDesigner(sqlProviderName, {
