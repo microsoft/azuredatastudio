c89399ac6b785b735e258dc799bc548c05e145e1
Alan Ren <alanren@microsoft.com>
2023-04-19 08:56:39 -0700

    fix steal focus issue

--- src/sql/workbench/browser/modal/modal.ts
+++ src/sql/workbench/browser/modal/modal.ts
@@ -612,8 +612,11 @@ export abstract class Modal extends Disposable implements IThemable {
 	protected set messagesElementVisible(visible: boolean) {
 		if (visible) {
 			if (this._useDefaultMessageBoxLocation) {
-				DOM.prepend(this._modalContent!, this._messageElement!);
-				this.setInitialFocusedElement();
+				// only do the append when the messageElement doesn't have parent element.
+				if (!this._messageElement!.parentNode) {
+					DOM.prepend(this._modalContent!, this._messageElement!);
+					this.setInitialFocusedElement();
+				}
 			}
 		} else {
 			// only do the removal when the messageElement has parent element.
