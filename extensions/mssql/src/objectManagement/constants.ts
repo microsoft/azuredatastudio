decbe8ddedeba1f770e78c289b7b2ea91c78020e
Alan Ren <alanren@microsoft.com>
2023-04-19 19:26:29 -0700

    simplify object management feature APIs (#22781)

--- extensions/mssql/src/objectManagement/constants.ts
+++ extensions/mssql/src/objectManagement/constants.ts
@@ -4,56 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 
 /**
- * The object types in object explorer's node context.
+ * The folder types in object explorer.
  */
-export enum NodeType {
-	Column = 'Column',
-	Database = 'Database',
-	Login = 'ServerLevelLogin',
-	Table = 'Table',
-	User = 'User',
-	View = 'View'
+export const enum FolderType {
+	ServerLevelLogins = 'ServerLevelLogins',
+	Users = 'Users'
 }
 
 export const PublicServerRoleName = 'public';
-
-/**
- * User types.
- */
-export enum UserType {
-	/**
-	 * User with a server level login.
-	 */
-	WithLogin = 'WithLogin',
-	/**
-	 * User based on a Windows user/group that has no login, but can connect to the Database Engine through membership in a Windows group.
-	 */
-	WithWindowsGroupLogin = 'WithWindowsGroupLogin',
-	/**
-	 * Contained user, authentication is done within the database.
-	 */
-	Contained = 'Contained',
-	/**
-	 * User that cannot authenticate.
-	 */
-	NoConnectAccess = 'NoConnectAccess'
-}
-
-/**
- * The authentication types.
- */
-export enum AuthenticationType {
-	Windows = 'Windows',
-	Sql = 'Sql',
-	AzureActiveDirectory = 'AAD'
-}
 
 export const CreateUserDocUrl = 'https://learn.microsoft.com/en-us/sql/t-sql/statements/create-user-transact-sql';
 export const AlterUserDocUrl = 'https://learn.microsoft.com/en-us/sql/t-sql/statements/alter-user-transact-sql';
 export const CreateLoginDocUrl = 'https://learn.microsoft.com/en-us/sql/t-sql/statements/create-login-transact-sql';
 export const AlterLoginDocUrl = 'https://learn.microsoft.com/en-us/sql/t-sql/statements/alter-login-transact-sql';
 
-export enum TelemetryActions {
+export const enum TelemetryActions {
 	CreateObject = 'CreateObject',
 	DeleteObject = 'DeleteObject',
 	OpenNewObjectDialog = 'OpenNewObjectDialog',
@@ -62,6 +27,4 @@ export enum TelemetryActions {
 	UpdateObject = 'UpdateObject'
 }
 
-export enum TelemetryViews {
-	ObjectManagement = 'ObjectManagement'
-}
+export const ObjectManagementViewName = 'ObjectManagement';
