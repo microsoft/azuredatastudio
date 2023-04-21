4011906fac5a5460adfddf2271b0e959e5a42478
Charles Gagnon <chgagnon@microsoft.com>
2023-04-21 10:55:29 -0700

    Few more no-unsafe-assignment cleanups

--- extensions/azurecore/src/account-provider/auths/httpClient.ts
+++ extensions/azurecore/src/account-provider/auths/httpClient.ts
@@ -366,7 +366,7 @@ const networkRequestViaHttps = <T>(
 };
 
 /**
- * Check if extra parsing is needed on the repsonse from the server
+ * Check if extra parsing is needed on the response from the server
  * @param statusCode {number} the status code of the response from the server
  * @param statusMessage {string | undefined} the status message of the response from the server
  * @param headers {Record<string, string>} the headers of the response from the server
@@ -382,7 +382,7 @@ const parseBody = (statusCode: number, statusMessage: string | undefined, header
 	 * Server error responses (500 - 599)
 	 */
 
-	let parsedBody;
+	let parsedBody: unknown;
 	try {
 		parsedBody = JSON.parse(body);
 	} catch (error) {
