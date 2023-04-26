bf3aae233b8da1f9111a149a96d77cc78d376094
Lewis Sanchez <lewissanchez@microsoft.com>
2023-04-26 15:13:48 -0700

    Remove deb files that were brought in with the latest merge.

--- build/linux/debian/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-Object.defineProperty(exports, "__esModule", { value: true });
--- build/linux/debian/types.ts
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-export type ArchString = 'amd64' | 'armhf' | 'arm64';
