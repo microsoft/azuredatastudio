4f53d76eb5f412266ad0bcfa73acd98924922e18
Alan Ren <alanren@microsoft.com>
2023-04-28 12:05:20 -0700

    User Management - Support new object types: Server Role, Application Role and Database Role (#22889)
    * server role dialogs
    
    * dialogs for other types
    
    * refactor
    
    * find object dialog
    
    * script button
    
    * refactoring
    
    * fix issues
    
    * fix title
    
    * vbump sts
    
    * remove language from links

--- extensions/mssql/src/objectManagement/localizedConstants.ts
+++ extensions/mssql/src/objectManagement/localizedConstants.ts
@@ -15,6 +15,12 @@ export const TableTypeDisplayName: string = localize('objectManagement.TableDisp
 export const ViewTypeDisplayName: string = localize('objectManagement.ViewDisplayName', "view");
 export const ColumnTypeDisplayName: string = localize('objectManagement.ColumnDisplayName', "column");
 export const DatabaseTypeDisplayName: string = localize('objectManagement.DatabaseDisplayName', "database");
+export const ServerRoleTypeDisplayName: string = localize('objectManagement.ServerRoleTypeDisplayName', "server role");
+export const ServerRoleTypeDisplayNameInTitle: string = localize('objectManagement.ServerRoleTypeDisplayNameInTitle', "Server Role");
+export const ApplicationRoleTypeDisplayName: string = localize('objectManagement.ApplicationRoleTypeDisplayName', "application role");
+export const ApplicationRoleTypeDisplayNameInTitle: string = localize('objectManagement.ApplicationRoleTypeDisplayNameInTitle', "Application Role");
+export const DatabaseRoleTypeDisplayName: string = localize('objectManagement.DatabaseRoleTypeDisplayName', "database role");
+export const DatabaseRoleTypeDisplayNameInTitle: string = localize('objectManagement.DatabaseRoleTypeDisplayNameInTitle', "Database Role");
 
 // Shared Strings
 export const HelpText: string = localize('objectManagement.helpText', "Help");
@@ -26,6 +32,13 @@ export const RenameObjectDialogTitle: string = localize('objectManagement.rename
 export const ScriptText: string = localize('objectManagement.scriptText', "Script");
 export const NoActionScriptedMessage: string = localize('objectManagement.noActionScriptedMessage', "There is no action to be scripted.");
 export const ScriptGeneratedText: string = localize('objectManagement.scriptGenerated', "Script has been generated successfully. You can close the dialog to view it in the newly opened editor.")
+export const OwnerText: string = localize('objectManagement.ownerText', "Owner");
+export const BrowseText = localize('objectManagement.browseText', "Browse…");
+export const BrowseOwnerButtonAriaLabel = localize('objectManagement.browseForOwnerText', "Browse for an owner");
+export const AddText = localize('objectManagement.addText', "Add…");
+export const RemoveText = localize('objectManagement.removeText', "Remove");
+export const AddMemberAriaLabel = localize('objectManagement.addMemberText', "Add a member");
+export const RemoveMemberAriaLabel = localize('objectManagement.removeMemberText', "Remove selected member");
 
 
 export function RefreshObjectExplorerError(error: string): string {
@@ -128,6 +141,9 @@ export const PasswordCannotBeEmptyError = localize('objectManagement.passwordCan
 export const PasswordsNotMatchError = localize('objectManagement.passwordsNotMatchError', "Password must match the confirm password.");
 export const InvalidPasswordError = localize('objectManagement.invalidPasswordError', "Password doesn't meet the complexity requirement. For more information: https://docs.microsoft.com/sql/relational-databases/security/password-policy");
 export const LoginNotSelectedError = localize('objectManagement.loginNotSelectedError', "Login is not selected.");
+export const MembershipSectionHeader = localize('objectManagement.membershipLabel', "Membership");
+export const MemberSectionHeader = localize('objectManagement.membersLabel', "Members");
+export const SchemaText = localize('objectManagement.schemaLabel', "Schema");
 
 // Login
 export const BlankPasswordConfirmationText: string = localize('objectManagement.blankPasswordConfirmation', "Creating a login with a blank password is a security risk.  Are you sure you want to continue?");
@@ -158,4 +174,22 @@ export const UserWithNoConnectAccess = localize('objectManagement.user.userWithN
 export const DefaultSchemaText = localize('objectManagement.user.defaultSchemaLabel', "Default schema");
 export const LoginText = localize('objectManagement.user.loginLabel', "Login");
 export const OwnedSchemaSectionHeader = localize('objectManagement.user.ownedSchemasLabel', "Owned Schemas");
-export const MembershipSectionHeader = localize('objectManagement.user.membershipLabel', "Membership");
+
+// Database Role
+export const SelectDatabaseRoleMemberDialogTitle = localize('objectManagement.databaseRole.SelectMemberDialogTitle', "Select Database Role Members");
+export const SelectDatabaseRoleOwnerDialogTitle = localize('objectManagement.databaseRole.SelectOwnerDialogTitle', "Select Database Role Owner");
+
+// Server Role
+export const SelectServerRoleMemberDialogTitle = localize('objectManagement.serverRole.SelectMemberDialogTitle', "Select Server Role Members");
+export const SelectServerRoleOwnerDialogTitle = localize('objectManagement.serverRole.SelectOwnerDialogTitle', "Select Server Role Owner");
+
+// Find Object Dialog
+export const ObjectTypeText = localize('objectManagement.objectTypeLabel', "Object Type");
+export const FilterText = localize('objectManagement.filterText', "Filter");
+export const FindText = localize('objectManagement.findText', "Find");
+export const SelectText = localize('objectManagement.selectText', "Select");
+export const ObjectsText = localize('objectManagement.objectsLabel', "Objects");
+export const LoadingObjectsText = localize('objectManagement.loadingObjectsLabel', "Loading objects…");
+export function LoadingObjectsCompletedText(count: number): string {
+	return localize('objectManagement.loadingObjectsCompletedLabel', "Loading objects completed, {0} objects found", count);
+}
---
+++ extensions/mssql/src/objectManagement/ui/serverRoleDialog.ts
@@ -0,0 +1,128 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import * as azdata from 'azdata';
+import { ObjectManagementDialogBase, ObjectManagementDialogOptions } from './objectManagementDialogBase';
+import { IObjectManagementService, ObjectManagement } from 'mssql';
+import * as localizedConstants from '../localizedConstants';
+import { AlterServerRoleDocUrl, CreateServerRoleDocUrl } from '../constants';
+import { FindObjectDialog } from './findObjectDialog';
+
+export class ServerRoleDialog extends ObjectManagementDialogBase<ObjectManagement.ServerRoleInfo, ObjectManagement.ServerRoleViewInfo> {
+	// Sections
+	private generalSection: azdata.GroupContainer;
+	private membershipSection: azdata.GroupContainer;
+	private memberSection: azdata.GroupContainer;
+
+	// General section content
+	private nameInput: azdata.InputBoxComponent;
+	private ownerInput: azdata.InputBoxComponent;
+
+	// Member section content
+	private memberTable: azdata.TableComponent;
+
+	// Membership section content
+	private membershipTable: azdata.TableComponent;
+
+
+	constructor(objectManagementService: IObjectManagementService, options: ObjectManagementDialogOptions) {
+		super(objectManagementService, options);
+	}
+
+	protected override get docUrl(): string {
+		return this.options.isNewObject ? CreateServerRoleDocUrl : AlterServerRoleDocUrl;
+	}
+
+	protected async initializeUI(): Promise<void> {
+		this.initializeGeneralSection();
+		this.initializeMemberSection();
+		const sections: azdata.Component[] = [this.generalSection, this.memberSection];
+		if (!this.viewInfo.isFixedRole) {
+			this.initializeMembershipSection();
+			sections.push(this.membershipSection);
+		}
+		this.formContainer.addItems(sections);
+	}
+
+	private initializeGeneralSection(): void {
+		this.nameInput = this.createInputBox(localizedConstants.NameText, async (newValue) => {
+			this.objectInfo.name = newValue;
+		}, this.objectInfo.name, this.options.isNewObject);
+		const nameContainer = this.createLabelInputContainer(localizedConstants.NameText, this.nameInput);
+
+		this.ownerInput = this.createInputBox(localizedConstants.OwnerText, async (newValue) => {
+			this.objectInfo.owner = newValue;
+		}, this.objectInfo.owner, !this.viewInfo.isFixedRole, 'text', 210);
+		const browseOwnerButton = this.createButton(localizedConstants.BrowseText, localizedConstants.BrowseOwnerButtonAriaLabel, async () => {
+			const dialog = new FindObjectDialog(this.objectManagementService, {
+				objectTypes: [ObjectManagement.NodeType.ServerLevelLogin, ObjectManagement.NodeType.ServerLevelServerRole],
+				multiSelect: false,
+				contextId: this.contextId,
+				title: localizedConstants.SelectServerRoleOwnerDialogTitle
+			});
+			await dialog.open();
+			const result = await dialog.waitForClose();
+			if (result.selectedObjects.length > 0) {
+				this.ownerInput.value = result.selectedObjects[0].name;
+			}
+		}, !this.viewInfo.isFixedRole);
+		const ownerContainer = this.createLabelInputContainer(localizedConstants.OwnerText, this.ownerInput);
+		ownerContainer.addItems([browseOwnerButton], { flex: '0 0 auto' });
+		this.generalSection = this.createGroup(localizedConstants.GeneralSectionHeader, [
+			nameContainer,
+			ownerContainer
+		], false);
+	}
+
+	private initializeMemberSection(): void {
+		this.memberTable = this.createTable(localizedConstants.MemberSectionHeader, [
+			{
+				type: azdata.ColumnType.text,
+				value: localizedConstants.NameText
+			}
+		], this.objectInfo.members.map(m => [m]));
+		const buttonContainer = this.addButtonsForTable(this.memberTable, localizedConstants.AddMemberAriaLabel, localizedConstants.RemoveMemberAriaLabel,
+			async () => {
+				const dialog = new FindObjectDialog(this.objectManagementService, {
+					objectTypes: [ObjectManagement.NodeType.ServerLevelLogin, ObjectManagement.NodeType.ServerLevelServerRole],
+					multiSelect: true,
+					contextId: this.contextId,
+					title: localizedConstants.SelectServerRoleMemberDialogTitle
+				});
+				await dialog.open();
+				const result = await dialog.waitForClose();
+				this.addMembers(result.selectedObjects.map(r => r.name));
+			},
+			async () => {
+				if (this.memberTable.selectedRows.length === 1) {
+					this.removeMember(this.memberTable.selectedRows[0]);
+				}
+			});
+		this.memberSection = this.createGroup(localizedConstants.MemberSectionHeader, [this.memberTable, buttonContainer]);
+	}
+
+	private addMembers(names: string[]): void {
+		names.forEach(n => {
+			if (this.objectInfo.members.indexOf(n) === -1) {
+				this.objectInfo.members.push(n);
+			}
+		});
+		this.updateMembersTable();
+	}
+
+	private removeMember(idx: number): void {
+		this.objectInfo.members.splice(idx, 1);
+		this.updateMembersTable();
+	}
+
+	private updateMembersTable(): void {
+		this.setTableData(this.memberTable, this.objectInfo.members.map(m => [m]));
+		this.onFormFieldChange();
+	}
+
+	private initializeMembershipSection(): void {
+		this.membershipTable = this.createTableList<string>(localizedConstants.MembershipSectionHeader, [localizedConstants.ServerRoleTypeDisplayNameInTitle], this.viewInfo.serverRoles, this.objectInfo.memberships);
+		this.membershipSection = this.createGroup(localizedConstants.MembershipSectionHeader, [this.membershipTable]);
+	}
+}
