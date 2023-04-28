5be415bbcd91c0804bfde80e77731e0eb0ae65e2
Cheena Malhotra <13396919+cheenamalhotra@users.noreply.github.com>
2023-04-28 16:13:01 -0700

    Update extensions/azurecore/src/utils/TenantIgnoredError.ts
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>

--- extensions/azurecore/src/utils/TenantIgnoredError.ts
+++ extensions/azurecore/src/utils/TenantIgnoredError.ts
@@ -3,13 +3,4 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export class TenantIgnoredError implements Error {
-	name: string = 'TENANT_IGNORED_ERROR';
-	message: string;
-	stack?: string | undefined;
-
-	public constructor(errorMessage: string, stacktrace?: string | undefined) {
-		this.message = errorMessage;
-		this.stack = stacktrace;
-	}
-}
+export class TenantIgnoredError extends Error { }
