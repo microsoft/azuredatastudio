a8a88ccbebdc545441f28b54587a09a231faa782
AkshayMata <akam520@gmail.com>
2023-05-03 10:35:44 -0700

    [SQL-Migration] Improve log migrations telemetry (#22927)
    - Bucketized errors to track top errors
    - Created separate login migration specific error to improve monitoring
    
    ---------
    
    Co-authored-by: Akshay Mata <akma@microsoft.com>

--- extensions/sql-migration/src/telemetry.ts
+++ extensions/sql-migration/src/telemetry.ts
@@ -77,6 +77,7 @@ export enum TelemetryAction {
 	OpenLoginMigrationWizard = 'OpenLoginMigrationWizard',
 	LoginMigrationStarted = 'LoginMigrationStarted',
 	LoginMigrationCompleted = 'LoginMigrationCompleted',
+	LoginMigrationError = 'LoginMigrationError',
 	TdeMigrationSuccess = 'TdeMigrationSuccess',
 	TdeMigrationFailures = 'TdeMigrationFailures',
 	TdeMigrationClientException = 'TdeMigrationClientException',
