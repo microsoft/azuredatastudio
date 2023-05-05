4e4b281307a81264c33d2073a3b46b55a2e04740
Cheena Malhotra <cmalhotra@microsoft.com>
2023-05-05 14:09:49 -0700

    Validate MSAL library is enabled

--- src/sql/workbench/services/connection/browser/utils.ts
+++ src/sql/workbench/services/connection/browser/utils.ts
@@ -4,7 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
-import { enableSqlAuthenticationProviderConfig, mssqlProviderName } from 'sql/platform/connection/common/constants';
+import { azureAuthenticationLibraryConfig, enableSqlAuthenticationProviderConfig, mssqlProviderName } from 'sql/platform/connection/common/constants';
+import { MSAL_AUTH_LIBRARY } from 'sql/workbench/services/accountManagement/utils';
 
 /**
  * Reads setting 'mssql.enableSqlAuthenticationProvider' returns true if it's enabled.
@@ -14,5 +15,22 @@ import { enableSqlAuthenticationProviderConfig, mssqlProviderName } from 'sql/pl
  * @returns True if provider is MSSQL and Sql Auth provider is enabled.
  */
 export function isMssqlAuthProviderEnabled(provider: string, configService: IConfigurationService): boolean {
-	return provider === mssqlProviderName && (configService?.getValue(enableSqlAuthenticationProviderConfig) ?? true);
+	return provider === mssqlProviderName && isMSALAuthLibraryEnabled(configService) && (configService?.getValue(enableSqlAuthenticationProviderConfig) ?? true);
+}
+
+/**
+ * We need Azure core extension configuration for fetching Authentication Library setting in use.
+ * This is required for 'enableSqlAuthenticationProvider' to be enabled (as it applies to MSAL only).
+ * This can be removed in future when ADAL support is dropped.
+ * @param configService Configuration Service to use.
+ * @returns true if MSAL_AUTH_LIBRARY is enabled.
+ */
+export function isMSALAuthLibraryEnabled(configService: IConfigurationService): boolean {
+	const config = configService.getValue(azureAuthenticationLibraryConfig);
+	if (config) {
+		return config === MSAL_AUTH_LIBRARY;
+	}
+	else {
+		return true; // By default MSAL is enabled.
+	}
 }
