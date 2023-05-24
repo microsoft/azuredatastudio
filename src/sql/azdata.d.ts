514b4546436acc838d1ce7af3fc4e829d155c1ca
Charles Gagnon <chgagnon@microsoft.com>
2023-05-24 13:09:07 -0700

    Fixes

--- src/sql/azdata.d.ts
+++ src/sql/azdata.d.ts
@@ -5397,6 +5397,7 @@ declare module 'azdata' {
 		 * specify the *content* of the document.
 		 *
 		 * @param options Options to control how the document will be created.
+		 * @param options.content The initial content of the document
 		 * @param providerId Optional provider ID this editor will be associated with. Defaults to MSSQL.
 		 * @return A promise that resolves to a {@link QueryDocument}.
 		 */
@@ -5666,11 +5667,8 @@ declare module 'azdata' {
 		 * will be derived from the file name.
 		 * For all other schemes the registered notebook providers are consulted.
 		 *
-		 * @param document A document to be shown.
-		 * @param column A view column in which the {@link NotebookEditor} should be shown. The default is the {@link vscode.ViewColumn}, other values
-		 * are adjusted to be `Min(column, columnCount + 1)`, the {@link vscode.ViewColumn.Active}-column is not adjusted. Use {@link vscode.ViewColumn.Beside}
-		 * to open the editor to the side of the currently active one.
-		 * @param preserveFocus When `true` the editor will not take focus.
+		 * @param uri The URI of the document to show
+		 * @param showOptions Options to control how the Notebook is shown
 		 * @return A promise that resolves to a {@link NotebookEditor}.
 		 */
 		export function showNotebookDocument(uri: vscode.Uri, showOptions?: NotebookShowOptions): Thenable<NotebookEditor>;
