3a6f14da940a86ecbed2d22a23d0ddbd7d542bb6
austincraigbryan <austinbryan@microsoft.com>
2023-06-01 13:45:39 -0700

    Fixing bug where if copy duration went over 24h it would reset to 00:00:00

--- extensions/sql-migration/src/constants/helper.ts
+++ extensions/sql-migration/src/constants/helper.ts
@@ -110,6 +110,25 @@ const _numberFormatter = new Intl.NumberFormat(
 	maximumFractionDigits: 0,
 });
 
+export function formatSecondsIntoReadableTime(seconds: number) {
+	//Get hours from milliseconds
+	var hours = seconds / (60 * 60);
+	var absoluteHours = Math.floor(hours);
+	var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+	//Get remainder from hours and convert to minutes
+	var minutes = (hours - absoluteHours) * 60;
+	var absoluteMinutes = Math.floor(minutes);
+	var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
+
+	//Get remainder from minutes and convert to seconds
+	var seconds = (minutes - absoluteMinutes) * 60;
+	var absoluteSeconds = Math.floor(seconds);
+	var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+	return h + ':' + m + ':' + s;
+}
+
 export function formatDateTimeString(dateTime: string): string {
 	return dateTime
 		? _dateFormatter.format(new Date(dateTime))
