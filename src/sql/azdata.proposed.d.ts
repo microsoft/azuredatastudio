d1a1f0723cce10eb85c71b63d48ab9850bd34bf9
Hai Cao <hacao@microsoft.com>
2023-06-09 17:51:59 -0700

    docs

--- src/sql/azdata.proposed.d.ts
+++ src/sql/azdata.proposed.d.ts
@@ -846,6 +846,7 @@ declare module 'azdata' {
 		 * @param providerId The table designer provider Id.
 		 * @param tableInfo The table information. The object will be passed back to the table designer provider as the unique identifier for the table.
 		 * @param telemetryInfo Optional Key-value pair containing any extra information that needs to be sent via telemetry
+		 * @param objectExplorerContext The object explorer context.
 		 */
 		export function openTableDesigner(providerId: string, tableInfo: TableInfo, telemetryInfo?: { [key: string]: string }, objectExplorerContext?: ObjectExplorerContext): Thenable<void>;
 
