5e4482750d194593acf2f7ad942e59ac49c14ee2
Lauren Nathan <laurennathan@vt.edu>
2023-06-26 13:54:01 -0700

    changes from first PR

--- extensions/database-documentation/src/extension.ts
+++ extensions/database-documentation/src/extension.ts
@@ -3,29 +3,11 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-'use strict';
-
-// The module 'azdata' contains the ADS extensibility API
 import * as azdata from 'azdata';
-
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-// The module 'azdata' contains the Azure Data Studio extensibility API
-// This is a complementary set of APIs that add SQL / Data-specific functionality to the app
-// Import the module and reference it with the alias azdata in your code below
 import { convertMarkdownToJSON, generateMarkdown, saveMarkdown, setupGeneration } from './common/utils';
-
-// The module 'openai' contains the OpenAI API
-import { Configuration, OpenAIApi } from "openai";
-
-// Language localization features
 import * as nls from 'vscode-nls';
 
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
 
     const localize = nls.loadMessageBundle();
