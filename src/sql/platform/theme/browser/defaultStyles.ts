882327984a2cb29f5acc8c5de6d9db187172b957
Alan Ren <alanren@microsoft.com>
2023-06-27 16:39:01 -0700

    fix merge issue

--- src/sql/platform/theme/browser/defaultStyles.ts
+++ src/sql/platform/theme/browser/defaultStyles.ts
@@ -3,13 +3,14 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { ICheckboxStyles } from 'sql/base/browser/ui/checkbox/checkbox';
+import { IDropdownStyles } from 'sql/base/browser/ui/dropdownList/dropdownList';
 import { IEditableDropdownStyles } from 'sql/base/browser/ui/editableDropdown/browser/dropdown';
 import { ITableFilterStyles } from 'sql/base/browser/ui/table/plugins/headerFilter.plugin';
 import *  as sqlcr from 'sql/platform/theme/common/colorRegistry';
 import { disabledCheckboxForeground } from 'sql/platform/theme/common/colors';
 import { IButtonStyles } from 'vs/base/browser/ui/button/button';
 import { IStyleOverride, defaultButtonStyles, defaultCountBadgeStyles, defaultInputBoxStyles, defaultListStyles, overrideStyles } from 'vs/platform/theme/browser/defaultStyles';
-import { asCssVariable, editorBackground, inputBorder } from 'vs/platform/theme/common/colorRegistry';
+import { asCssVariable, editorBackground, inputBorder, inputForeground } from 'vs/platform/theme/common/colorRegistry';
 
 
 export const defaultCheckboxStyles: ICheckboxStyles = {
@@ -33,6 +34,12 @@ export const defaultInfoButtonStyles: IButtonStyles = {
 	buttonDisabledBackground: undefined,
 	buttonDisabledForeground: undefined,
 	buttonDisabledBorder: undefined
+}
+
+export const defaultDropdownStyles: IDropdownStyles = {
+	foregroundColor: asCssVariable(inputForeground),
+	borderColor: asCssVariable(inputBorder),
+	backgroundColor: asCssVariable(editorBackground)
 }
 
 export const defaultEditableDropdownStyles: IEditableDropdownStyles = {
