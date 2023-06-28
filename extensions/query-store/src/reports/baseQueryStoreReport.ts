89c11f19f67197ad48f59e8609919a0aeed12007
Kim Santiago <kisantia@microsoft.com>
2023-06-28 15:52:08 -0700

    add more comments

--- extensions/query-store/src/reports/baseQueryStoreReport.ts
+++ extensions/query-store/src/reports/baseQueryStoreReport.ts
@@ -17,6 +17,9 @@ export abstract class BaseQueryStoreReport {
 		this.editor = azdata.workspace.createModelViewEditor(reportName, { retainContextWhenHidden: true, supportsSave: false }, reportName);
 	}
 
+	/**
+	 * Creates and opens the report
+	 */
 	public async open(): Promise<void> {
 		this.editor.registerContent(async (view) => {
 			this.flexModel = <azdata.FlexContainer>view.modelBuilder.flexContainer().component();
@@ -41,23 +44,34 @@ export abstract class BaseQueryStoreReport {
 		await this.editor.openEditor();
 	}
 
-	private async createMainContainer(view: azdata.ModelView, views: azdata.FlexContainer[]): Promise<azdata.FlexContainer | azdata.SplitViewContainer> {
+	/**
+	 * Creates the main container containing the different components of the report
+	 * @param view
+	 * @param containers Array of containers to add to the main container
+	 * @returns FlexContainer or SplitViewContainer containing the containers
+	 */
+	private async createMainContainer(view: azdata.ModelView, containers: azdata.FlexContainer[]): Promise<azdata.FlexContainer | azdata.SplitViewContainer> {
 		let mainContainer;
-		switch (views.length) {
+
+		switch (containers.length) {
 			case 1: {
 				mainContainer = view;
 			}
 			case 2: {
 				// TODO: replace 800 to have the number be based on how big the window is
-				mainContainer = this.resizeable ? utils.createVerticalSplitView(view, views[0], views[1], 800) : await utils.createTwoComponentFlexContainer(view, views[0], views[1], 'column');
+				// one container on top, one on the bottom
+				mainContainer = this.resizeable ? utils.createVerticalSplitView(view, containers[0], containers[1], 800) : await utils.createTwoComponentFlexContainer(view, containers[0], containers[1], 'column');
 				break;
 			} case 3: {
-				mainContainer = this.resizeable ? utils.createVerticalSplitView(view, await utils.createTwoComponentFlexContainer(view, views[0], views[1], 'row'), views[2], 800)
-					: await utils.createTwoComponentFlexContainer(view, await utils.createTwoComponentFlexContainer(view, views[0], views[1], 'row'), views[2], 'column');
+				// 2 containers on top, one on the bottom
+				// TODO: support portrait and landscape view. Right now it's landscape view only
+				mainContainer = this.resizeable ? utils.createVerticalSplitView(view, await utils.createTwoComponentFlexContainer(view, containers[0], containers[1], 'row'), containers[2], 800)
+					: await utils.createTwoComponentFlexContainer(view, await utils.createTwoComponentFlexContainer(view, containers[0], containers[1], 'row'), containers[2], 'column');
 				break;
 			} case 4: {
-				mainContainer = this.resizeable ? utils.createVerticalSplitView(view, await utils.createTwoComponentFlexContainer(view, views[0], views[1], 'row'), await utils.createTwoComponentFlexContainer(view, views[2], views[3], 'row'), 800)
-					: await utils.createTwoComponentFlexContainer(view, await utils.createTwoComponentFlexContainer(view, views[0], views[1], 'row'), await utils.createTwoComponentFlexContainer(view, views[2], views[3], 'row'), 'column');
+				// 2 containers on top, 2 on the bottom
+				mainContainer = this.resizeable ? utils.createVerticalSplitView(view, await utils.createTwoComponentFlexContainer(view, containers[0], containers[1], 'row'), await utils.createTwoComponentFlexContainer(view, containers[2], containers[3], 'row'), 800)
+					: await utils.createTwoComponentFlexContainer(view, await utils.createTwoComponentFlexContainer(view, containers[0], containers[1], 'row'), await utils.createTwoComponentFlexContainer(view, containers[2], containers[3], 'row'), 'column');
 				break;
 			} default: {
 				throw new Error(`{views.length} number of views in a QDS report is not supported`);
