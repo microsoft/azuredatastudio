c217785ecec0f37ac7614f339f676288a07b727a
Aasim Khan <aasimkhan30@gmail.com>
2023-07-07 12:53:19 -0700

    Update src/sql/workbench/contrib/objectExplorer/browser/serverTreeView.ts
    Co-authored-by: Charles Gagnon <chgagnon@microsoft.com>

--- src/sql/workbench/contrib/objectExplorer/browser/serverTreeView.ts
+++ src/sql/workbench/contrib/objectExplorer/browser/serverTreeView.ts
@@ -532,7 +532,7 @@ export class ServerTreeView extends Disposable implements IServerTreeView {
 				return;
 			}
 			// For the connection profile, we need to clear the password from the last session if the user doesn't want to save it
-			if (!connectionProfile?.savePassword) {
+			if (!connectionProfile.savePassword) {
 				connectionProfile.password = '';
 			}
 			// Delete the node from the tree
