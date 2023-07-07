0db15f2d9dbd01360b65205437df3c7d15a1e286
chlafreniere <hichise@gmail.com>
2023-07-07 11:52:42 -0700

    Don't set buttonoption for link/image md nb

--- src/sql/workbench/contrib/notebook/browser/cellViews/markdownToolbar.component.ts
+++ src/sql/workbench/contrib/notebook/browser/cellViews/markdownToolbar.component.ts
@@ -28,7 +28,6 @@ import { NotebookLinkHandler } from 'sql/workbench/contrib/notebook/browser/note
 import { StandardKeyboardEvent } from 'vs/base/browser/keyboardEvent';
 import { KeyCode } from 'vs/base/common/keyCodes';
 import { FileAccess } from 'vs/base/common/network';
-import { defaultButtonStyles } from 'vs/platform/theme/browser/defaultStyles';
 
 export const MARKDOWN_TOOLBAR_SELECTOR: string = 'markdown-toolbar-component';
 const linksRegex = /\[(?<text>.+)\]\((?<url>[^ ]+)(?: "(?<title>.+)")?\)/;
@@ -118,15 +117,9 @@ export class MarkdownToolbarComponent extends AngularDisposable {
 	private initActionBar() {
 		let linkButtonContainer = DOM.$('li.action-item');
 		linkButtonContainer.setAttribute('role', 'presentation');
-		let linkButton = new Button(linkButtonContainer, defaultButtonStyles);
+		let linkButton = new Button(linkButtonContainer, null);
 		linkButton.title = this.insertLink;
 		linkButton.element.setAttribute('class', 'action-label codicon insert-link masked-icon');
-
-		// {{SQL CARBON TODO}} - reenable
-		// let buttonStyle: IButtonStyles = {
-		// 	buttonBackground: null
-		// };
-		// linkButton.style(buttonStyle);
 
 		this._register(DOM.addDisposableListener(linkButtonContainer, DOM.EventType.CLICK, async e => {
 			await this.onInsertButtonClick(e, MarkdownButtonType.LINK_PREVIEW);
@@ -134,12 +127,9 @@ export class MarkdownToolbarComponent extends AngularDisposable {
 
 		let imageButtonContainer = DOM.$('li.action-item');
 		imageButtonContainer.setAttribute('role', 'presentation');
-		let imageButton = new Button(imageButtonContainer, defaultButtonStyles);
+		let imageButton = new Button(imageButtonContainer, null);
 		imageButton.title = this.insertImage;
 		imageButton.element.setAttribute('class', 'action-label codicon insert-image masked-icon');
-
-		// {{SQL CARBON TODO}} - reenable
-		//imageButton.style(buttonStyle);
 
 		this._register(DOM.addDisposableListener(imageButtonContainer, DOM.EventType.CLICK, async e => {
 			await this.onInsertButtonClick(e, MarkdownButtonType.IMAGE_PREVIEW);
