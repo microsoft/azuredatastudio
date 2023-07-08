12651cae55489d03444aabecdd6a31f68fdcf3a3
Cheena Malhotra <cmalhotra@microsoft.com>
2023-07-08 02:29:09 -0700

    Fix connection URI default value comparisons

--- src/sql/platform/connection/common/providerConnectionInfo.ts
+++ src/sql/platform/connection/common/providerConnectionInfo.ts
@@ -231,7 +231,8 @@ export class ProviderConnectionInfo implements azdata.ConnectionInfo {
 				let finalValue = undefined;
 				let options = this.serverCapabilities.connectionOptions.filter(value => value.name === idNames[index]!);
 				if (options.length > 0 && value) {
-					finalValue = value !== options[0].defaultValue ? value : undefined;
+					let defaultValue = options[0].defaultValue ?? '';
+					finalValue = value && value.toString().toLocaleLowerCase() !== defaultValue.toString().toLocaleLowerCase() ? value : undefined;
 					if (options[0].specialValueType === 'appName' && this.providerName === Constants.mssqlProviderName) {
 						finalValue = (value as string).startsWith('azdata') ? undefined : finalValue
 					}
@@ -390,7 +391,7 @@ export class ProviderConnectionInfo implements azdata.ConnectionInfo {
 					element.specialValueType !== ConnectionOptionSpecialType.password) {
 					if (getNonDefault) {
 						let value = this.getOptionValue(element.name);
-						if (value && value !== element.defaultValue) {
+						if (value && value.toString().toLocaleLowerCase() !== element.defaultValue?.toLocaleLowerCase()) {
 							connectionOptions.push(element);
 						}
 					}
