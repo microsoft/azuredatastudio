ac982dca45f11b5c382acea18a21827bd38af2d5
Lewis Sanchez <lewissanchez@microsoft.com>
2023-07-10 21:42:06 -0700

    Revert "Disable failing vscode test suite (#23539)"
    This reverts commit 562a0ce595b86873dfc5217e92885c38d98444f4.

--- src/vs/workbench/services/configuration/test/browser/configurationService.test.ts
+++ src/vs/workbench/services/configuration/test/browser/configurationService.test.ts
@@ -1518,7 +1518,7 @@ suite.skip('WorkspaceConfigurationService - Folder', () => { // {{SQL CARBON EDI
 	}));
 });
 
-suite.skip('WorkspaceConfigurationService - Profiles', () => { // {{SQL CARBON EDIT}} - skip failing suite
+suite('WorkspaceConfigurationService - Profiles', () => {
 
 	let testObject: WorkspaceService, workspaceService: WorkspaceService, fileService: IFileService, environmentService: IBrowserWorkbenchEnvironmentService, userDataProfileService: IUserDataProfileService, instantiationService: TestInstantiationService;
 	const configurationRegistry = Registry.as<IConfigurationRegistry>(ConfigurationExtensions.Configuration);
