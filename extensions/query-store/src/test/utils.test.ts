583541b52f69d7557bd0ddc9a0b57c51caff8f3f
SakshiS-harma <sakshis@microsoft.com>
2023-07-11 13:49:40 -0700

    Update tests for utils.ts

--- extensions/query-store/src/test/utils.test.ts
+++ extensions/query-store/src/test/utils.test.ts
@@ -3,11 +3,140 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as azdata from 'azdata';
 import * as should from 'should';
+import { createOneComponentFlexContainer, createTwoComponentFlexContainer, createVerticalSplitView } from '../common/utils';
 
-describe('Test to verify test infrastructure is working', () => {
-	it('Should run as expected', () => {
-		const testNumber = true;
-		should(testNumber).be.true();
+interface TestContext {
+	view: azdata.ModelView;
+	component: azdata.Component
+}
+
+function createViewContext(): TestContext {
+	let componentBase: azdata.Component = {
+		id: '',
+		updateProperties: () => Promise.resolve(),
+		updateProperty: () => Promise.resolve(),
+		updateCssStyles: undefined!,
+		onValidityChanged: undefined!,
+		valid: true,
+		validate: undefined!,
+		focus: undefined!
+	};
+
+	const components: azdata.Component[] = [];
+	let container = {
+		clearItems: () => { },
+		addItems: () => { },
+		addItem: () => { },
+		removeItem: () => true,
+		insertItem: () => { },
+		items: components,
+		setLayout: () => { },
+		setItemLayout: () => { },
+		updateCssStyles: () => { }
+	};
+
+	let flex: azdata.FlexContainer = Object.assign({}, componentBase, container, {
+	});
+
+	let flexBuilder: azdata.FlexBuilder = Object.assign({}, {
+		component: () => flex,
+		withProperties: () => flexBuilder,
+		withValidation: () => flexBuilder,
+		withItems: () => flexBuilder,
+		withLayout: () => flexBuilder,
+		withProps: () => flexBuilder
+	});
+
+	let split: azdata.SplitViewContainer = Object.assign({}, componentBase, container, {
+	});
+
+	let splitViewBuilder: azdata.SplitViewBuilder = Object.assign({}, {
+		component: () => split,
+		withProperties: () => splitViewBuilder,
+		withValidation: () => splitViewBuilder,
+		withItems: () => splitViewBuilder,
+		withLayout: () => splitViewBuilder,
+		withProps: () => splitViewBuilder
+	});
+
+	let view: azdata.ModelView = {
+		onClosed: undefined!,
+		connection: undefined!,
+		serverInfo: undefined!,
+		valid: true,
+		onValidityChanged: undefined!,
+		validate: undefined!,
+		initializeModel: () => { return Promise.resolve(); },
+		modelBuilder: {
+			listView: undefined!,
+			radioCardGroup: undefined!,
+			navContainer: undefined!,
+			divContainer: undefined!,
+			flexContainer: () => flexBuilder,
+			splitViewContainer: () => splitViewBuilder,
+			card: undefined!,
+			inputBox: () => undefined!,
+			checkBox: undefined!,
+			radioButton: () => undefined!,
+			webView: undefined!,
+			editor: undefined!,
+			diffeditor: undefined!,
+			text: () => undefined!,
+			image: () => undefined!,
+			button: () => undefined!,
+			dropDown: () => undefined!,
+			tree: undefined!,
+			listBox: undefined!,
+			table: undefined!,
+			declarativeTable: () => undefined!,
+			dashboardWidget: undefined!,
+			dashboardWebview: undefined!,
+			formContainer: () => undefined!,
+			groupContainer: undefined!,
+			toolbarContainer: undefined!,
+			loadingComponent: () => undefined!,
+			fileBrowserTree: undefined!,
+			hyperlink: undefined!,
+			tabbedPanel: undefined!,
+			separator: undefined!,
+			propertiesContainer: undefined!,
+			infoBox: undefined!,
+			slider: undefined!,
+			executionPlan: undefined!,
+		}
+	};
+
+	return {
+		view: view,
+		component: componentBase
+	};
+}
+
+let testContext: TestContext;
+
+describe('Test to verify flex container creation util function', () => {
+	beforeEach(() => {
+		testContext = createViewContext();
+	});
+	it('Should create a component as expected with createOneComponentFlexContainer', async () => {
+		let flexContainer: azdata.FlexContainer = await createOneComponentFlexContainer(testContext.view, testContext.component, 'black');
+		should(flexContainer.valid).be.true();
+	});
+
+	it('Should create a component as expected with createTwoComponentFlexContainer with row flow', () => {
+		let flexContainer: azdata.FlexContainer = createTwoComponentFlexContainer(testContext.view, testContext.component, testContext.component, 'row');
+		should(flexContainer.valid).be.true();
+	});
+
+	it('Should create a component as expected with createTwoComponentFlexContainer with column flow', () => {
+		let flexContainer: azdata.FlexContainer = createTwoComponentFlexContainer(testContext.view, testContext.component, testContext.component, 'column');
+		should(flexContainer.valid).be.true();
+	});
+
+	it('Should create a component as expected with createVerticalSplitView', () => {
+		let splitViewContainer: azdata.SplitViewContainer = createVerticalSplitView(testContext.view, testContext.component, testContext.component, 100);
+		should(splitViewContainer.valid).be.true();
 	});
 });
