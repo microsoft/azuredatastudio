285bc510ad02691c17ddda7f3a5941ec5e46cfda
Alexander Perfilyev <aperfilyev@microsoft.com>
2023-07-20 10:22:26 -0700

    addded json stored assessment processing

--- extensions/sql-migration/src/dashboard/sqlServerDashboard.ts
+++ extensions/sql-migration/src/dashboard/sqlServerDashboard.ts
@@ -107,6 +107,7 @@ export class DashboardWidget {
 			};
 
 			const dashboardTab = await new DashboardTab().create(
+				this._context,
 				view,
 				async (filter: AdsMigrationStatus) => await openMigrationFcn(filter),
 				this._onServiceContextChanged,
