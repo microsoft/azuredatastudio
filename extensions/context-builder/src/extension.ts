1f6f2901be0cf2db90a8a30ea1a067d045bc1169
Lewis Sanchez <lewissanchez@microsoft.com>
2023-08-07 16:50:39 -0700

    Fix eslint unused param error

--- extensions/context-builder/src/extension.ts
+++ extensions/context-builder/src/extension.ts
@@ -3,9 +3,9 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from 'vscode';
+//import * as vscode from 'vscode';
 
-export async function activate(context: vscode.ExtensionContext): Promise<void> {
+export async function activate(/*context: vscode.ExtensionContext*/): Promise<void> {
 
 }
 
