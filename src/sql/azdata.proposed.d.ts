17ce309c07337c14b15e0ab127c37bc7d0c689af
Cory Rivera <corivera@microsoft.com>
2023-08-16 13:39:08 -0700

    Add another comment

--- src/sql/azdata.proposed.d.ts
+++ src/sql/azdata.proposed.d.ts
@@ -2028,6 +2028,7 @@ declare module 'azdata' {
 		 * @param connectionUri The URI of the connection to the target server
 		 * @param targetPath The file path on the server machine to open by default in the dialog
 		 * @param fileFilters The filters used to limit which files are displayed in the file browser
+		 * @returns The path of the file chosen from the dialog, and undefined if the dialog is closed without selecting anything.
 		 */
 		export function openFileBrowserDialog(connectionUri: string, targetPath: string, fileFilters: FileFilters[]): Thenable<string>;
 	}
