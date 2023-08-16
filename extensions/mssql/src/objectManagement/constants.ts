443266b605251e9d94b40910bc72f2ae1821b948
Cory Rivera <corivera@microsoft.com>
2023-08-15 17:53:59 -0700

    Replace usage of Delete with Drop in object management code

--- extensions/mssql/src/objectManagement/constants.ts
+++ extensions/mssql/src/objectManagement/constants.ts
@@ -37,13 +37,13 @@ export const DropDatabaseDocUrl = 'https://learn.microsoft.com/sql/t-sql/stateme
 
 export const enum TelemetryActions {
 	CreateObject = 'CreateObject',
-	DeleteObject = 'DeleteObject',
+	DropObject = 'DropObject',
 	OpenNewObjectDialog = 'OpenNewObjectDialog',
 	OpenPropertiesDialog = 'OpenPropertiesDialog',
 	RenameObject = 'RenameObject',
 	UpdateObject = 'UpdateObject',
 	OpenDetachDatabaseDialog = 'OpenDetachDatabaseDialog',
-	OpenDeleteDatabaseDialog = 'OpenDeleteDatabaseDialog'
+	OpenDropDatabaseDialog = 'OpenDropDatabaseDialog'
 }
 
 export const ObjectManagementViewName = 'ObjectManagement';
