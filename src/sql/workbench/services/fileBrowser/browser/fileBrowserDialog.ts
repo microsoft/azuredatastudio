8ccc73cdf67861942ec2220f38f66b87531deb3b
Cory Rivera <corivera@microsoft.com>
2023-09-11 16:01:35 -0700

    Add optional arg

--- src/sql/workbench/services/fileBrowser/browser/fileBrowserDialog.ts
+++ src/sql/workbench/services/fileBrowser/browser/fileBrowserDialog.ts
@@ -112,7 +112,7 @@ export class FileBrowserDialog extends Modal {
 		expandPath: string,
 		fileFilters: [{ label: string, filters: string[] }],
 		fileValidationServiceType: string,
-		showFoldersOnly: boolean
+		showFoldersOnly?: boolean
 	): void {
 		this._viewModel.initialize(ownerUri, expandPath, fileFilters, fileValidationServiceType, showFoldersOnly);
 		this._viewModel.openFileBrowser(0, false).catch(err => onUnexpectedError(err));
