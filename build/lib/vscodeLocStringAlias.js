2a76f9fb2c78701bdbee7be1c18b328d44227fbc
Alex Ma <alma1@microsoft.com>
2023-09-12 12:26:41 -0700

    added working alias replacer, need to add all string entities

---
+++ build/lib/vscodeLocStringAlias.js
@@ -0,0 +1,25 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.aliasStrings = void 0;
+exports.aliasStrings = {
+    "Microsoft.query-store": {
+        '9cb725c039170871de7978892c2fdf0f763f58359bd66bd0b1a06b0bdf3f6b38': 'DOPLabel',
+        '17bc05152f339d6dc2fb2844595760e9009662c4315fa1ac0d61c7e47dfbdbb3': 'UTC'
+    },
+    "Microsoft.data-workspace": {
+        '60dc6b3745804caa2d48a35dbb7306bb3f4f05286e7f9de259423f6abe87ec8f': 'SelectProjectFileAction'
+    },
+    "Microsoft.sql-database-projects": {
+        '99c40ab405926cb5ad1def9cff4d7ce624f8f8abfff4e85f655347fcb949d08e': 'Date',
+        '031a8f0f659df890dfd53c92e45295b0f14c997185bae46e168831e403b273f7': 'Failed',
+        '6ecc3df6bffdd06c58cd4f6e276929e119c8f653c53f5a6a5a5e66dfa1c3fc93': 'Move'
+    },
+    "vscode.git": {
+        'a93447605ef4b2cc8eda934a1e9af7ad57f8d2d2e5dc1defbaa121ed13550e57': '$(info) Remote \"{0}\" has no tags.'
+    }
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidnNjb2RlTG9jU3RyaW5nQWxpYXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ2c2NvZGVMb2NTdHJpbmdBbGlhcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7OztnR0FHZ0c7OztBQUVuRixRQUFBLFlBQVksR0FBRztJQUMzQix1QkFBdUIsRUFBRTtRQUN4QixrRUFBa0UsRUFBRSxVQUFVO1FBQzlFLGtFQUFrRSxFQUFFLEtBQUs7S0FDekU7SUFDRCwwQkFBMEIsRUFBRTtRQUMzQixrRUFBa0UsRUFBRSx5QkFBeUI7S0FDN0Y7SUFDRCxpQ0FBaUMsRUFBRTtRQUNsQyxrRUFBa0UsRUFBRSxNQUFNO1FBQzFFLGtFQUFrRSxFQUFFLFFBQVE7UUFDNUUsa0VBQWtFLEVBQUUsTUFBTTtLQUMxRTtJQUNELFlBQVksRUFBRTtRQUNiLGtFQUFrRSxFQUFFLHFDQUFxQztLQUN6RztDQUNELENBQUEifQ==
---
+++ build/lib/vscodeLocStringAlias.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export const aliasStrings = {
+	"Microsoft.query-store": {
+		'9cb725c039170871de7978892c2fdf0f763f58359bd66bd0b1a06b0bdf3f6b38': 'DOPLabel',
+		'17bc05152f339d6dc2fb2844595760e9009662c4315fa1ac0d61c7e47dfbdbb3': 'UTC'
+	},
+	"Microsoft.data-workspace": {
+		'60dc6b3745804caa2d48a35dbb7306bb3f4f05286e7f9de259423f6abe87ec8f': 'SelectProjectFileAction'
+	},
+	"Microsoft.sql-database-projects": {
+		'99c40ab405926cb5ad1def9cff4d7ce624f8f8abfff4e85f655347fcb949d08e': 'Date',
+		'031a8f0f659df890dfd53c92e45295b0f14c997185bae46e168831e403b273f7': 'Failed',
+		'6ecc3df6bffdd06c58cd4f6e276929e119c8f653c53f5a6a5a5e66dfa1c3fc93': 'Move'
+	},
+	"vscode.git": {
+		'a93447605ef4b2cc8eda934a1e9af7ad57f8d2d2e5dc1defbaa121ed13550e57': '$(info) Remote \"{0}\" has no tags.'
+	}
+}
