6a43ed8eb697d545fcf1918a32026c625fbc65d3
Siyang Yao <siyao@microsoft.com>
2023-10-09 17:37:26 -0700

    fix saving issue in page 0 and page 1

--- extensions/sql-migration/src/models/stateMachine.ts
+++ extensions/sql-migration/src/models/stateMachine.ts
@@ -153,6 +153,7 @@ export interface SavedInfo {
 	serviceSubscription: azurecore.azureResource.AzureResourceSubscription | null;
 	serviceResourceGroup: azurecore.azureResource.AzureResourceResourceGroup | null;
 	serverAssessment: ServerAssessment | null;
+	xEventsFilesFolderPath: string | null;
 	skuRecommendation: SkuRecommendationSavedInfo | null;
 }
 
@@ -1219,6 +1220,7 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 			targetDatabaseNames: [],
 			sqlMigrationService: undefined,
 			serverAssessment: null,
+			xEventsFilesFolderPath: null,
 			skuRecommendation: null,
 			serviceResourceGroup: null,
 			serviceSubscription: null,
@@ -1250,6 +1252,7 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 				saveInfo.migrationTargetType = this._targetType;
 				saveInfo.databaseList = this._databasesForMigration;
 				saveInfo.serverAssessment = this._assessmentResults;
+				saveInfo.xEventsFilesFolderPath = this._xEventsFilesFolderPath;
 
 				if (this._skuRecommendationPerformanceDataSource) {
 					const skuRecommendation: SkuRecommendationSavedInfo = {
@@ -1266,6 +1269,7 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 
 			case Page.DatabaseSelector:
 				saveInfo.databaseAssessment = this._databasesForAssessment;
+				saveInfo.xEventsFilesFolderPath = this._xEventsFilesFolderPath;
 				await this.extensionContext.globalState.update(`${this.mementoString}.${serverName}`, saveInfo);
 		}
 	}
@@ -1312,10 +1316,12 @@ export class MigrationStateModel implements Model, vscode.Disposable {
 			this._sqlMigrationServiceSubscription = this.savedInfo.serviceSubscription || undefined!;
 			this._sqlMigrationServiceResourceGroup = this.savedInfo.serviceResourceGroup || undefined!;
 
+			this._assessedDatabaseList = this.savedInfo.databaseAssessment ?? [];
+			this._databasesForAssessment = this.savedInfo.databaseAssessment ?? [];
+			this._xEventsFilesFolderPath = this.savedInfo.xEventsFilesFolderPath ?? '';
 			const savedAssessmentResults = this.savedInfo.serverAssessment;
 			if (savedAssessmentResults) {
 				this._assessmentResults = savedAssessmentResults;
-				this._assessedDatabaseList = this.savedInfo.databaseAssessment;
 			}
 
 			const savedSkuRecommendation = this.savedInfo.skuRecommendation;
