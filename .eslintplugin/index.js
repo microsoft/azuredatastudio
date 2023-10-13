bae4e1eea2fa00f8eed5e6b72606d152568fd316
Lewis Sanchez <lewissanchez@microsoft.com>
2023-10-13 12:31:41 -0700

    Fix issue with ts-node module resolution

--- .eslintplugin/index.js
+++ .eslintplugin/index.js
@@ -1,7 +1,7 @@
 const glob = require('glob');
 const path = require('path');
 
-require('ts-node').register({ experimentalResolver: true, transpileOnly: true });
+require('ts-node').register({ experimentalResolver: true, transpileOnly: true, compilerOptions: { module: 'commonjs' } });
 
 // Re-export all .ts files as rules
 const rules = {};
