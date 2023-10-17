9d7de3cb2ff718da451c83346bba7ac7da520251
Cory Rivera <corivera@microsoft.com>
2023-10-17 16:08:22 -0700

    Stub out dialog sections

--- extensions/mssql/src/objectManagement/ui/backupDatabaseDialog.ts
+++ extensions/mssql/src/objectManagement/ui/backupDatabaseDialog.ts
@@ -9,6 +9,7 @@ import { IObjectManagementService } from 'mssql';
 import { Database, DatabaseViewInfo } from '../interfaces';
 import { BackupDatabaseDocUrl } from '../constants';
 import * as loc from '../localizedConstants';
+import { DefaultInputWidth } from '../../ui/dialogBase';
 
 export class BackupDatabaseDialog extends ObjectManagementDialogBase<Database, DatabaseViewInfo> {
 
@@ -22,7 +23,105 @@ export class BackupDatabaseDialog extends ObjectManagementDialogBase<Database, D
 	}
 
 	protected override async initializeUI(): Promise<void> {
+		let generalSection = this.initializeGeneralSection();
+		let compressionSection = this.initializeCompressionSection();
+		let encryptionSection = this.initializeEncryptionSection();
+		let mediaSection = this.initializeMediaSection();
+		let transactionSection = this.initializeTransactionSection();
+		let reliabilitySection = this.initializeReliabilitySection();
+		let expirationSection = this.initializeExpirationSection();
+
+		this.formContainer.addItems([
+			generalSection,
+			compressionSection,
+			encryptionSection,
+			mediaSection,
+			transactionSection,
+			reliabilitySection,
+			expirationSection
+		]);
+	}
+
+	private initializeGeneralSection(): azdata.GroupContainer {
 		let components: azdata.Component[] = [];
-		this.formContainer.addItems(components);
+
+		let inputBox = this.createInputBox(newValue => {
+			return Promise.resolve();
+		}, {
+			ariaLabel: '',
+			inputType: 'text',
+			enabled: true,
+			value: '',
+			width: DefaultInputWidth
+		});
+		let backupInput = this.createLabelInputContainer('Backup name', inputBox);
+		components.push(backupInput);
+
+		inputBox = this.createInputBox(newValue => {
+			return Promise.resolve();
+		}, {
+			ariaLabel: '',
+			inputType: 'text',
+			enabled: false,
+			value: '',
+			width: DefaultInputWidth
+		});
+		let recoveryModelInput = this.createLabelInputContainer('Recovery model', inputBox);
+		components.push(recoveryModelInput);
+
+		let backupTypeDropdown = this.createDropdown('Backup type', newValue => {
+			return Promise.resolve();
+		}, [], '', true);
+		components.push(backupTypeDropdown);
+
+		let copyBackupCheckbox = this.createCheckbox('Copy-only backup', checked => {
+			return Promise.resolve();
+		});
+		components.push(copyBackupCheckbox);
+
+		let saveBackupCheckbox = this.createCheckbox('Save backup to URL', checked => {
+			return Promise.resolve();
+		}, false, false);
+		components.push(saveBackupCheckbox);
+
+		// TODO: Add backup files table
+
+		return this.createGroup(loc.GeneralSectionHeader, components);
+	}
+
+	private initializeCompressionSection(): azdata.GroupContainer {
+		let components: azdata.Component[] = [];
+
+		return this.createGroup('Compression', components);
+	}
+
+	private initializeEncryptionSection(): azdata.GroupContainer {
+		let components: azdata.Component[] = [];
+
+		return this.createGroup('Encryption', components);
+	}
+
+	private initializeMediaSection(): azdata.GroupContainer {
+		let components: azdata.Component[] = [];
+
+		return this.createGroup('Media', components);
+	}
+
+	private initializeTransactionSection(): azdata.GroupContainer {
+		let components: azdata.Component[] = [];
+
+		return this.createGroup('Transaction', components);
+	}
+
+	private initializeReliabilitySection(): azdata.GroupContainer {
+		let components: azdata.Component[] = [];
+
+		return this.createGroup('Reliability', components);
+	}
+
+	private initializeExpirationSection(): azdata.GroupContainer {
+		let components: azdata.Component[] = [];
+
+		return this.createGroup('Expiration', components);
 	}
 }
