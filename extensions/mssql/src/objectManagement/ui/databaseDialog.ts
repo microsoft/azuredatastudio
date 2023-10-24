95a026d0cb1e2495fef249ff71abc0ffbc127476
Sai Avishkar Sreerama <74571829+ssreerama@users.noreply.github.com>
2023-10-24 14:02:27 -0700

    Adjusting DSC table height (#24764)

--- extensions/mssql/src/objectManagement/ui/databaseDialog.ts
+++ extensions/mssql/src/objectManagement/ui/databaseDialog.ts
@@ -1360,7 +1360,7 @@ export class DatabaseDialog extends ObjectManagementDialogBase<Database, Databas
 				metaData.valueForPrimary,
 				secondaryUnsupportedConfigsSet.has(metaData.id) ? localizedConstants.NotAvailableText : metaData.valueForSecondary]
 			}),
-			height: getTableHeight(this.objectInfo.databaseScopedConfigurations.length, 1, DscTableRowLength),
+			height: getTableHeight(this.objectInfo.databaseScopedConfigurations.length, 1, DscTableRowLength + 5),
 			width: DefaultTableWidth
 		}).component();
 
