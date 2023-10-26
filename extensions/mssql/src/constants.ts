248ef4a092a8dedc986c32faaf5c338421cf465f
Cheena Malhotra <cmalhotra@microsoft.com>
2023-10-25 19:31:03 -0700

    Send Telemetry Measurement for PMP Limit

--- extensions/mssql/src/constants.ts
+++ extensions/mssql/src/constants.ts
@@ -30,8 +30,8 @@ export const QueryStoreService = 'queryStoreService';
 
 // CONFIGURATION VALUES //////////////////////////////////////////////////////////
 export const configObjectExplorerGroupBySchemaFlagName = 'mssql.objectExplorer.groupBySchema';
-export const configAsyncParallelProcessingName = 'mssql.parallelMessageProcessing';
-export const configAsyncParallelProcessingLimitName = 'mssql.parallelMessageProcessingLimit';
+export const configParallelMessageProcessingName = 'mssql.parallelMessageProcessing';
+export const configParallelMessageProcessingLimitName = 'mssql.parallelMessageProcessingLimit';
 export const configEnableSqlAuthenticationProviderName = 'mssql.enableSqlAuthenticationProvider';
 export const configEnableConnectionPoolingName = 'mssql.enableConnectionPooling';
 export const configHttpProxy = 'http.proxy';
