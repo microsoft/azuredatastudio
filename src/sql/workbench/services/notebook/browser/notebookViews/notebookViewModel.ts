65717fc33fece6f9d73b927dc572ceaf8fc395d9
Kim Santiago <kisantia@microsoft.com>
2023-10-27 11:54:24 -0700

    update 'Source EULA' to 'MIT License' in files in repo

--- src/sql/workbench/services/notebook/browser/notebookViews/notebookViewModel.ts
+++ src/sql/workbench/services/notebook/browser/notebookViews/notebookViewModel.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { NotebookViewsExtension } from 'sql/workbench/services/notebook/browser/notebookViews/notebookViewsExtension';
 import { ICellModel } from 'sql/workbench/services/notebook/browser/models/modelInterfaces';
--- src/vs/workbench/services/userDataSync/browser/userDataSyncWorkbenchService.ts
+++ src/vs/workbench/services/userDataSync/browser/userDataSyncWorkbenchService.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { IUserDataSyncService, IAuthenticationProvider, isAuthenticationProvider, IUserDataAutoSyncService, IUserDataSyncStoreManagementService, SyncStatus, IUserDataSyncEnablementService, IUserDataSyncResource, IResourcePreview, USER_DATA_SYNC_SCHEME, } from 'vs/platform/userDataSync/common/userDataSync';
