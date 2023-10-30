ce4d8788c4a97cfe3e6b80918a5899404d663e87
Erin Stellato <98599126+erinstellato-ms@users.noreply.github.com>
2023-10-30 11:47:31 -0400

    Update license to MIT (#24799)
    * Update license to MIT
    
    * update 'Source EULA' to 'MIT License' in files in repo
    
    * update .js output files
    
    * fix eslint header errors
    
    * update eslint ignore to ignore some files brought in from merge distro
    
    ---------
    
    Co-authored-by: Kim Santiago <kisantia@microsoft.com>

--- src/sql/workbench/contrib/azure/browser/azure.contribution.ts
+++ src/sql/workbench/contrib/azure/browser/azure.contribution.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { MenuRegistry, MenuId } from 'vs/platform/actions/common/actions';
 import { localize } from 'vs/nls';
