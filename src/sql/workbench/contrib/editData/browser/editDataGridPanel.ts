66e09f4887b3f7a1da354a67a11b1c673ba99786
Alex Ma <alma1@microsoft.com>
2023-11-29 15:08:45 -0800

    Added fix to focus after delete row

--- src/sql/workbench/contrib/editData/browser/editDataGridPanel.ts
+++ src/sql/workbench/contrib/editData/browser/editDataGridPanel.ts
@@ -603,8 +603,9 @@ export class EditDataGridPanel extends GridParentComponent {
 			handled = true;
 		}
 
-		if (e.keyCode === KeyCode.Enter && this.isNullRow(this.currentCell.row)) {
-			this.isInNullRow = true;
+		if (e.keyCode === KeyCode.Enter) {
+			if (this.isNullRow(this.currentCell.row))
+				this.isInNullRow = true;
 		}
 
 		return handled;
@@ -848,9 +849,10 @@ export class EditDataGridPanel extends GridParentComponent {
 		this.gridDataProvider = new AsyncDataProvider(this.dataSet.dataRows);
 		// refresh results view
 		return this.refreshGrid().then(() => {
-			// Set focus to the row index column of the removed row if the current selection is in the removed row
+			// Set focus to the replacement row if its deleted in the selection
 			if (this.currentCell.row === row && !this.removingNewRow) {
-				this.focusCell(row, 1);
+				this.setCurrentCell(this.currentCell.row, this.currentCell.column);
+				this.focusCell(this.currentCell.row, this.currentCell.column);
 			}
 			this.removingNewRow = false;
 		});
