4d544420c22edf2a0f303547ea00d865798291eb
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-12-12 13:15:23 +0100

    Fix file header

--- extensions/azurecore/src/azureResource/providers/cosmosdb/nosql/cosmosDbNoSqlService.ts
+++ extensions/azurecore/src/azureResource/providers/cosmosdb/nosql/cosmosDbNoSqlService.ts
@@ -1,8 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 
 import { ResourceServiceBase } from '../../resourceTreeDataProviderBase';
 import { azureResource } from 'azurecore';
