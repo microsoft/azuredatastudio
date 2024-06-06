7fc7157d1c39a67009e4725a49a21fa459482138
Hai Cao (from Dev Box) <hacao@microsoft.com>
2024-06-05 18:15:15 -0700

    test new update srv

--- src/vs/platform/update/electron-main/abstractUpdateService.ts
+++ src/vs/platform/update/electron-main/abstractUpdateService.ts
@@ -15,7 +15,7 @@ import { IRequestService } from 'vs/platform/request/common/request';
 import { AvailableForDownload, DisablementReason, IUpdateService, State, StateType, UpdateType } from 'vs/platform/update/common/update';
 
 export function createUpdateURL(platform: string, quality: string, productService: IProductService): string {
-	return `${productService.updateUrl}/api/update/${platform}/${quality}/${productService.commit}`;
+	return `https://azuredatastudio-update-staging.azurewebsites.net/api/update/${platform}/${quality}/${productService.commit}`;
 }
 
 export type UpdateNotAvailableClassification = {
