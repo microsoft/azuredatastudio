138d508c312a63e80648254451fe4f4dd6ba6ef9
Lewis Sanchez <lewissanchez@microsoft.com>
2025-01-11 00:16:09 -0800

    Fix test start headings

--- scripts/test-extensions-unit.sh
+++ scripts/test-extensions-unit.sh
@@ -85,44 +85,44 @@ echo "VSCODEEXTDIR : '$VSCODEEXTDIR'"
 
 ALL_PLATFORMS_API_TESTS_EXTRA_ARGS="--disable-telemetry --crash-reporter-directory=$VSCODECRASHDIR --no-cached-data --disable-updates --disable-extensions --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR"
 
-echo ***************************************************
-echo *** starting admin tool extension windows tests ***
-echo ***************************************************
+echo "***************************************************"
+echo "*** starting admin tool extension windows tests ***"
+echo "***************************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/admin-tool-ext-win --extensionTestsPath=$ROOT/extensions/admin-tool-ext-win/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ****************************
-echo *** starting agent tests ***
-echo ****************************
+echo "****************************"
+echo "*** starting agent tests ***"
+echo "****************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/agent --extensionTestsPath=$ROOT/extensions/agent/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo **************************
-echo *** starting arc tests ***
-echo **************************
+echo "**************************"
+echo "*** starting arc tests ***"
+echo "**************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/arc --extensionTestsPath=$ROOT/extensions/arc/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo *****************************
-echo *** starting azcli tests ***
-echo *****************************
+echo "*****************************"
+echo "*** starting azcli tests ***"
+echo "*****************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/azcli --extensionTestsPath=$ROOT/extensions/azcli/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ********************************
-echo *** starting azurecore tests ***
-echo ********************************
+echo "********************************"
+echo "*** starting azurecore tests ***"
+echo "********************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/azurecore --extensionTestsPath=$ROOT/extensions/azurecore/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo **************************
-echo *** starting cms tests ***
-echo **************************
+echo "**************************"
+echo "*** starting cms tests ***"
+echo "**************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/cms --extensionTestsPath=$ROOT/extensions/cms/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo *****************************
-echo *** starting dacpac tests ***
-echo *****************************
+echo "*****************************"
+echo "*** starting dacpac tests ***"
+echo "*****************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/dacpac --extensionTestsPath=$ROOT/extensions/dacpac/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo *****************************************
-echo *** starting datavirtualization tests ***
-echo *****************************************
+echo "*****************************************"
+echo "*** starting datavirtualization tests ***"
+echo "*****************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/datavirtualization --extensionTestsPath=$ROOT/extensions/datavirtualization/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
 # {{SQL CARBON TODO}} - disable tests for this extension
@@ -131,14 +131,14 @@ echo *****************************************
 # echo ********************************************
 # "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/data-workspace --extensionTestsPath=$ROOT/extensions/data-workspace/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo *****************************
-echo *** starting import tests ***
-echo *****************************
+echo "*****************************"
+echo "*** starting import tests ***"
+echo "*****************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/import --extensionTestsPath=$ROOT/extensions/import/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ************************************************
-echo *** starting machine-learning tests ***
-echo ************************************************
+echo "************************************************"
+echo "*** starting machine-learning tests ***"
+echo "************************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/machine-learning --extensionTestsPath=$ROOT/extensions/machine-learning/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
 # echo ******************************************
@@ -146,34 +146,34 @@ echo ************************************************
 # echo ******************************************
 # "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/mssql --extensionTestsPath=$ROOT/extensions/mssql/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo *******************************
-echo *** starting notebook tests ***
-echo *******************************
+echo "*******************************"
+echo "*** starting notebook tests ***"
+echo "*******************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/notebook --extensionTestsPath=$ROOT/extensions/notebook/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ******************************************
-echo *** starting query-history tests ***
-echo ******************************************
+echo "******************************************"
+echo "*** starting query-history tests ***"
+echo "******************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/query-history --extensionTestsPath=$ROOT/extensions/query-history/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ******************************************
-echo *** starting resource deployment tests ***
-echo ******************************************
+echo "******************************************"
+echo "*** starting resource deployment tests ***"
+echo "******************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/resource-deployment --extensionTestsPath=$ROOT/extensions/resource-deployment/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo *************************************
-echo *** starting schema compare tests ***
-echo *************************************
+echo "*************************************"
+echo "*** starting schema compare tests ***"
+echo "*************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/schema-compare --extensionTestsPath=$ROOT/extensions/schema-compare/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ********************************************
-echo *** starting sql-bindings tests ***
-echo ********************************************
+echo "********************************************"
+echo "*** starting sql-bindings tests ***"
+echo "********************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/sql-bindings --extensionTestsPath=$ROOT/extensions/sql-bindings/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
-echo ********************************************
-echo *** starting sql-database-projects tests ***
-echo ********************************************
+echo "********************************************"
+echo "*** starting sql-database-projects tests ***"
+echo "********************************************"
 "$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/sql-database-projects --extensionTestsPath=$ROOT/extensions/sql-database-projects/out/test $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
 
 if [[ "$NO_CLEANUP" == "" ]]; then
