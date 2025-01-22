8e6350164cf20f924383b9c1c0024da9a750bd00
Lewis Sanchez <lewissanchez@microsoft.com>
2025-01-22 11:56:50 -0800

    Improve log wording

--- extensions/azurecore/src/account-provider/auths/azureAuth.ts
+++ extensions/azurecore/src/account-provider/auths/azureAuth.ts
@@ -496,7 +496,7 @@ export abstract class AzureAuth implements vscode.Disposable {
 			return response;
 		}
 
-		Logger.verbose("No proxy endpoint found in the HTTP_PROXY, HTTPS_PROXY environment variables or workspace configuration.");
+		Logger.verbose("No proxy endpoint was found in the HTTP_PROXY, HTTPS_PROXY environment variables or in the workspace HTTP configuration.");
 		const response: AxiosResponse = await axios.get<T>(requestUrl, config);
 		Logger.piiSanitized('GET request ', [{ name: 'response', objOrArray: response.data?.value as TenantResponse[] ?? response.data as GetTenantsResponseData }], [], requestUrl,);
 		return response;
