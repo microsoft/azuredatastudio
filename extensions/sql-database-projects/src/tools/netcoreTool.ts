66e53aa4c5c6883857907845696908715532884a
Zi Chen <zijchen@microsoft.com>
2025-03-24 14:37:56 -0700

    Update version to 8.0.0 for proper semver

--- extensions/sql-database-projects/src/tools/netcoreTool.ts
+++ extensions/sql-database-projects/src/tools/netcoreTool.ts
@@ -25,7 +25,7 @@ export const NetCoreLinuxDefaultPath = '/usr/share';
 export const winPlatform: string = 'win32';
 export const macPlatform: string = 'darwin';
 export const linuxPlatform: string = 'linux';
-export const minSupportedNetCoreVersionForBuild: string = '8.0';
+export const minSupportedNetCoreVersionForBuild: string = '8.0.0';
 
 export const enum netCoreInstallState {
 	netCoreNotPresent,
