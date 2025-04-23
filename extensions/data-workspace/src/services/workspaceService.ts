5a789c51dd7a7d509e8242810fb593b476e5b58f
Sai Avishkar Sreerama <ssreerama@microsoft.com>
2025-04-22 23:23:26 -0500

    refactored into best practice for string sorting

--- extensions/data-workspace/src/services/workspaceService.ts
+++ extensions/data-workspace/src/services/workspaceService.ts
@@ -147,12 +147,14 @@ export class WorkspaceService implements IWorkspaceService {
 		// remove excluded projects specified in workspace file
 		this.excludedProjects = this.getWorkspaceConfigurationValue<string[]>(ExcludedProjectsConfigurationName);
 		this.openedProjects = this.openedProjects.filter(project => !this.excludedProjects?.find(excludedProject => excludedProject === vscode.workspace.asRelativePath(project)));
+
 		// Sort the projects based on the project name extracted from the path property
-		this.openedProjects = this.openedProjects.sort(function (a, b) {
-			const aProjectName = a?.path?.split('/').pop() ?? '';
-			const bProjectName = b?.path?.split('/').pop() ?? '';
-			return aProjectName < bProjectName ? -1 : (aProjectName >= bProjectName ? 1 : 0);
+		this.openedProjects = this.openedProjects.sort((a, b) => {
+			const projectA = path.basename(a?.path ?? '');
+			const projectB = path.basename(b?.path ?? '');
+			return projectA.localeCompare(projectB);
 		});
+
 		Logger.log(`Finished looking for projects in workspace. Opened: ${this.openedProjects.length}. Excluded: ${this.excludedProjects.length}. Total time = ${new Date().getTime() - startTime}ms`);
 
 		// filter by specified extension
